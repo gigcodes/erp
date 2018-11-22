de3e4f29b90eeaefeb354f0362ed6b132ad82369
Lukas Markevicius <vysniukass@gmail.com>
2018-11-22 19:20:10 +0200

    compiled js for notifications

--- resources/views/partials/notifications.blade.php
+++ resources/views/partials/notifications.blade.php
@@ -35,29 +35,43 @@
 </li>
 
 <script>
-    let interval = 1000 * 30;  // 1000 = 1 second
-    const notificationShowCount = 5;
 
-    let allUsers = {!! json_encode( \App\Helpers::getUserArray( \App\User::all() ) ) !!};
-    let current_userid = '{{ Auth::id() }}';
-    let current_username = '{{ Auth::user()->name }}';
-    let is_admin = "{{ Auth::user()->hasRole('Admin') }}";
 
-    // Queue class
-    class Queue {
-        // Array is used to implement a Queue
-        constructor() {
+    'use strict';
+// alert('load');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-            this.items = [];
-            this.isFirst = true;
-            this.count = 0;
-        }
-        // Functions to be implemented
-        // enqueue function
-        enqueue(element) {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var interval = 1000 * 30; // 1000 = 1 second
+var notificationShowCount = 5;
+
+var allUsers = {!! json_encode( \App\Helpers::getUserArray( \App\User::all() ) ) !!};
+var current_userid = '{{ Auth::id() }}';
+var current_username = '{{ Auth::user()->name }}';
+var is_admin = "{{ Auth::user()->hasRole('Admin') }}";
+
+// Queue class
+
+var Queue = function () {
+    // Array is used to implement a Queue
+    function Queue() {
+        _classCallCheck(this, Queue);
+
+        this.items = [];
+        this.isFirst = true;
+        this.count = 0;
+    }
+    // Functions to be implemented
+    // enqueue function
+
+
+    _createClass(Queue, [{
+        key: 'enqueue',
+        value: function enqueue(element) {
 
             // adding element to the queue
-            let i;
+            var i = void 0;
             for (i = 0; i < this.items.length; i++) {
 
                 if (this.items[i].id === element.id) {
@@ -69,8 +83,6 @@
 
                 element['isShown'] = false;
                 this.items.push(element);
-
-
             }
 
             /*if(this.isFirst){
@@ -83,29 +95,31 @@
         }
 
         // dequeue function
-        dequeue() {
+
+    }, {
+        key: 'dequeue',
+        value: function dequeue() {
             // removing element from the queue
             // returns underflow when called
             // on empty queue
-            if (this.isEmpty())
-                return false;
-                // return "Underflow";
-            let result = this.items.shift();
+            if (this.isEmpty()) return false;
+            // return "Underflow";
+            var result = this.items.shift();
 
             this.notificationCount();
             return result;
         }
+    }, {
+        key: 'dequeueWithId',
+        value: function dequeueWithId(notificationId) {
 
-        dequeueWithId(notificationId){
+            if (this.isEmpty()) return false;
 
-            if (this.isEmpty())
-                return false;
-
-            for (let i = 0; i < this.items.length; i++) {
+            for (var i = 0; i < this.items.length; i++) {
 
                 if (this.items[i].id === notificationId) {
 
-                    this.items.splice(i,1);
+                    this.items.splice(i, 1);
                     this.count--;
                 }
             }
@@ -114,60 +128,66 @@
         }
 
         // front function
-        front() {
+
+    }, {
+        key: 'front',
+        value: function front() {
             // returns the Front element of
             // the queue without removing it.
-            if (this.isEmpty())
-                return false;
+            if (this.isEmpty()) return false;
             // return "No elements in Queue";
             return this.items[0];
         }
 
         // isEmpty function
-        isEmpty() {
+
+    }, {
+        key: 'isEmpty',
+        value: function isEmpty() {
             // return true if the queue is empty.
             return this.items.length === 0;
         }
-
-        getQueue(){
+    }, {
+        key: 'getQueue',
+        value: function getQueue() {
             return this.items;
         }
+    }, {
+        key: 'notificationCount',
+        value: function notificationCount() {
 
-        notificationCount(){
-
-            if($('#notification_count').length === 0) {
+            if ($('#notification_count').length === 0) {
                 $('#toast-container').prepend('<div id="notification_count"></div>');
             }
 
             $('#notification_count').html(this.items.length);
         }
+    }, {
+        key: 'showNotification',
+        value: function showNotification() {
 
-        showNotification(){
+            for (var i = 0; i < this.items.length; i++) {
 
-            for (let i = 0; i < this.items.length; i++) {
+                if (this.count === notificationShowCount) break;
 
-                if( this.count === notificationShowCount)
-                    break;
-
-                if( !this.items[i]['isShown'] ) {
+                if (!this.items[i]['isShown']) {
 
                     this.items[i]['isShown'] = true;
                     toast(this.items[i]);
                     this.count++;
-
                 }
             }
         }
+    }, {
+        key: 'postPoneNotification',
+        value: function postPoneNotification(notificationId) {
+            if (this.isEmpty()) return false;
 
-        postPoneNotification(notificationId){
-            if (this.isEmpty())
-                return false;
-
-            for (let i = 0; i < this.items.length; i++) {
+            for (var i = 0; i < this.items.length; i++) {
 
                 if (this.items[i].id === notificationId) {
 
-                    let item = this.items.splice(i,1);
+                    var item = this.items.splice(i, 1);
                     this.count--;
                     this.enqueue(item[0]);
                     break;
@@ -176,153 +196,151 @@
             this.notificationCount();
             this.showNotification();
         }
+    }]);
 
-    }
+    return Queue;
+}();
 
-    $(document).on('click','.toast-info',function () {
-       $('.toast-info').toggleClass('toast-stack');
-       $('#toast-container').toggleClass('toast-container-stacked');
+$(document).on('click', '.toast-info', function () {
+    $('.toast-info').toggleClass('toast-stack');
+    $('#toast-container').toggleClass('toast-container-stacked');
+});
+
+var notificationQueue = new Queue();
+
+function getNotificaitons() {
+
+    jQuery.ajax({
+        type: 'GET',
+        url: '{{ Route('pushNotifications') }}',
+        dataType: 'json',
+        success: function success(data) {
+
+            data.forEach(function (notification) {
+                notificationQueue.enqueue(notification);
+            });
+
+            notificationQueue.showNotification();
+            notificationQueue.notificationCount();
+        },
+        complete: function complete(data) {
+            return setTimeout(getNotificaitons, interval);
+        } // Schedule the next
     });
+}
 
-    let notificationQueue = new Queue();
+//Instantly get notifications on page load.
+getNotificaitons();
 
-    function getNotificaitons() {
+function toast(notification) {
 
-        jQuery.ajax({
-            type: 'GET',
-            url: '{{ Route('pushNotifications') }}',
-            dataType: 'json',
-            success: data => {
+    var link = void 0,
+        message = void 0,
+        img_position = void 0,
+        message_without_img = void 0;
 
-                data.forEach(notification => {
-                    notificationQueue.enqueue(notification);
-                });
+    switch (notification.model_type) {
 
-                notificationQueue.showNotification();
-                notificationQueue.notificationCount();
-            },
-            complete: data => setTimeout(getNotificaitons, interval),// Schedule the next
-        });
-    }
+        case 'App\\Sale':
 
-    //Instantly get notifications on page load.
-    getNotificaitons();
+            link = '/sales/' + notification.model_id + '/edit';
+            message = '<h4>ID : ' + notification.model_id + ' New Sale</h4><a href="' + link + '" style="padding-bottom: 10px;">' + (notification.message.length > 33 ? notification.message.substring(0, 33 - 3) + '...' : notification.message) + ' - ' + moment(notification.created_at).format('H:m') + '</a>';
+            break;
 
-    function toast(notification) {
+        case 'App\\Task':
+            link = '/task#task_' + notification.model_id;
+            message = '<h4>ID : ' + notification.model_id + ' Task</h4>\n                            <span>By :- ' + allUsers[notification.user_id] + '</span><br>\n                            <a href="' + link + '">' + (notification.message.length > 33 ? notification.message.substring(0, 33 - 3) + '...' : notification.message) + '</a>' + getStatusButtons(notification);
+            break;
 
-        let link, message, img_position, message_without_img;
+        case 'User':
+            link = '/#task_' + notification.model_id;
+            message = '<h4>ID : ' + notification.model_id + ' Task</h4><a href="' + link + '" style="padding-bottom: 10px; display: block;">' + (notification.message.length > 33 ? notification.message.substring(0, 33 - 3) + '...' : notification.message) + ' - ' + moment(notification.created_at).format('H:m') + '</a>';
+            break;
 
-        switch (notification.model_type) {
+        case 'App\\Leads':
 
-            case 'App\\Sale' :
+            link = '/leads/' + notification.model_id;
+            message = '<h4>ID : ' + notification.model_id + ' New Lead</h4><a href="' + link + '">' + (notification.message.length > 33 ? notification.message.substring(0, 33 - 3) + '...' : notification.message) + ' - ' + moment(notification.created_at).format('H:m') + '</a>' + getStatusButtons(notification);
+            break;
 
-                link = '/sales/' + notification.model_id + '/edit';
-                message = `<h4>ID : ${notification.model_id} New Sale</h4><a href="${link}" style="padding-bottom: 10px;">${notification.message.length > 33 ? (notification.message.substring(0, 33 - 3) + '...') : notification.message} - ${moment(notification.created_at).format('H:m')}</a>`;
-                break;
+        case 'App\\Order':
 
-            case 'App\\Task':
-                link = `/task#task_${notification.model_id}`;
-                message = `<h4>ID : ${notification.model_id} Task</h4>
-                            <span>By :- ${ allUsers[notification.user_id] }</span><br>
-                            <a href="${link}">${notification.message.length > 33 ? (notification.message.substring(0, 33 - 3) + '...') : notification.message}</a>${ getStatusButtons(notification) }`;
-                break;
+            link = '/order/' + notification.model_id + '/edit';
+            message = '<h4>ID : ' + notification.model_id + ' New Order</h4><a href="' + link + '">' + (notification.message.length > 33 ? notification.message.substring(0, 33 - 3) + '...' : notification.message) + ' - ' + moment(notification.created_at).format('H:m') + '</a>' + getStatusButtons(notification);
+            break;
 
-            case  'User':
-                link = `/#task_${notification.model_id}`;
-                message = `<h4>ID : ${notification.model_id} Task</h4><a href="${link}" style="padding-bottom: 10px; display: block;">${notification.message.length > 33 ? (notification.message.substring(0, 33 - 3) + '...') : notification.message} - ${moment(notification.created_at).format('H:m')}</a>`;
-                break;
-
-            case 'App\\Leads':
-
-                link = '/leads/' + notification.model_id;
-                message = `<h4>ID : ${notification.model_id} New Lead</h4><a href="${link}">${notification.message.length > 33 ? (notification.message.substring(0, 33 - 3) + '...') : notification.message} - ${moment(notification.created_at).format('H:m')}</a>${ getStatusButtons(notification) }`;
-                break;
-
-            case 'App\\Order':
-
-                link = '/order/' + notification.model_id + '/edit';
-                message = `<h4>ID : ${notification.model_id} New Order</h4><a href="${link}">${notification.message.length > 33 ? (notification.message.substring(0, 33 - 3) + '...') : notification.message} - ${moment(notification.created_at).format('H:m')}</a>${ getStatusButtons(notification) }`;
-                break;
-
-
-            case 'order':
-                img_position = notification.message.indexOf("<img");
-                message_without_img = img_position != -1 ? notification.message.substring(0, img_position) : notification.message;
-                link = '/order/' + notification.model_id;
-                message = `<h4>New Message on Order from ${notification.user_name}</h4><a href="${link}" style="padding-bottom: 10px; display: block;">${message_without_img.length > 33 ? (message_without_img.substring(0, 33 - 3) + '...') : message_without_img} - ${moment(notification.created_at).format('H:m')}</a>`;
-                break;
-
-            case 'leads':
+        case 'order':
             img_position = notification.message.indexOf("<img");
             message_without_img = img_position != -1 ? notification.message.substring(0, img_position) : notification.message;
-                link = '/leads/' + notification.model_id;
-                message = `
-                            <h4>New Message on Lead from ${notification.user_name}</h4>
-                            <a href="${link}" style="padding-bottom: 10px; display: block;">${notification.message.length > 33 ? (notification.message.substring(0, 33 - 3) + '...') : notification.message} - ${moment(notification.created_at).format('H:m')}</a>`;
-                break;
+            link = '/order/' + notification.model_id;
+            message = '<h4>New Message on Order from ' + notification.user_name + '</h4><a href="' + link + '" style="padding-bottom: 10px; display: block;">' + (message_without_img.length > 33 ? message_without_img.substring(0, 33 - 3) + '...' : message_without_img) + ' - ' + moment(notification.created_at).format('H:m') + '</a>';
+            break;
 
-            default:
-                return;
-        }
+        case 'leads':
+            img_position = notification.message.indexOf("<img");
+            message_without_img = img_position != -1 ? notification.message.substring(0, img_position) : notification.message;
+            link = '/leads/' + notification.model_id;
+            message = '\n                            <h4>New Message on Lead from ' + notification.user_name + '</h4>\n                            <a href="' + link + '" style="padding-bottom: 10px; display: block;">' + (notification.message.length > 33 ? notification.message.substring(0, 33 - 3) + '...' : notification.message) + ' - ' + moment(notification.created_at).format('H:m') + '</a>';
+            break;
 
-        toastr.options = {
-            "closeButton": ((notification.type === 'button') && (is_admin == false)) || (notification.reminder == 1) ? false : true,
-            "debug": false,
-            "newestOnTop": true,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "showDuration": "500",
-            "hideDuration": "400",
-            "timeOut": 0,
-            "extendedTimeOut": 0,
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut",
-            "tapToDismiss" : false,
-        };
-
-        toastr.options.onCloseClick = () => markNotificationRead(notification.id);
-        toastr.options.onHidden = () => nextNotification(notification.id);
-        // toastr.options.onHidden = () => markNotificationRead(notification.id);
-        // toastr.options.onclick = () => { return false; };
-
-        toastr['info'](message);
+        default:
+            return;
     }
 
-    function markNotificationRead(id) {
-        jQuery.ajax({
-            headers: {
-                'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
-            },
-            type: 'POST',
-            url: '/pushNotificationMarkRead/' + id
-        });
-    }
+    toastr.options = {
+        "closeButton": notification.type === 'button' && is_admin == false || notification.reminder == 1 ? false : true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "showDuration": "500",
+        "hideDuration": "400",
+        "timeOut": 0,
+        "extendedTimeOut": 0,
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut",
+        "tapToDismiss": false
+    };
 
-    function nextNotification(id) {
-        notificationQueue.dequeueWithId(parseInt(id));
-        notificationQueue.showNotification();
-    }
+    toastr.options.onCloseClick = function () {
+        return markNotificationRead(notification.id);
+    };
+    toastr.options.onHidden = function () {
+        return nextNotification(notification.id);
+    };
+    // toastr.options.onHidden = () => markNotificationRead(notification.id);
+    // toastr.options.onclick = () => { return false; };
 
-    function getStatusButtons(notificaiton) {
+    toastr['info'](message);
+}
 
-        if(notificaiton.type !== 'button')
-            return '';
-
-        return `<div class="row notification-row">
-                   <div data-id="${notificaiton.id}" class="btn-group btn-group-justified">
-                        <button value="1" class="n-status btn btn-notification text-success">Accept</button>
-                        <button value="2" class="n-status btn btn-notification">Postpone</button>
-                        <button value="3" class="n-status btn btn-notification text-danger">Decline</button>
-                    </div>
-                </div>`;
-
-    }
-
-    $(document).on('click','#notification_count',function () {
-       $('#toast-container .toast').toggle();
+function markNotificationRead(id) {
+    jQuery.ajax({
+        headers: {
+            'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+        },
+        type: 'POST',
+        url: '/pushNotificationMarkRead/' + id
     });
+}
+
+function nextNotification(id) {
+    notificationQueue.dequeueWithId(parseInt(id));
+    notificationQueue.showNotification();
+}
+
+function getStatusButtons(notificaiton) {
+
+    if (notificaiton.type !== 'button') return '';
+
+    return '<div class="row notification-row">\n                   <div data-id="' + notificaiton.id + '" class="btn-group btn-group-justified">\n                        <button value="1" class="n-status btn btn-notification text-success">Accept</button>\n                        <button value="2" class="n-status btn btn-notification">Postpone</button>\n                        <button value="3" class="n-status btn btn-notification text-danger">Decline</button>\n                    </div>\n                </div>';
+}
+
+$(document).on('click', '#notification_count', function () {
+    $('#toast-container .toast').toggle();
+});
 
 </script>
