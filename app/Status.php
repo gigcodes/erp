44501a0c5df8504827b9b65ae0806525ae9b480c
Lukas <vysniukass@gmail.com>
2018-11-25 15:08:17 +0200

    changed lead status list

--- app/Status.php
+++ app/Status.php
@@ -7,17 +7,18 @@ namespace App;
 class Status extends ReadOnlyBase {
 
 	protected $data =[
-		'Not Started' => '1',
-		'In Progress' => '2',
-		'Closed' => '3',
-		'Lost' => '4',		
+		'Cold Lead' => '1',
+		'Cold / Important Lead' => '2',
+		'Hot Lead' => '3',
+		'Very Hot Lead' => '4',
+		'Advance Follow Up' => '5',		
 	];
 
 	protected $messagestatus =[
 		'Reply Pending' => '1',
 		'Approval Pending' => '2',
 		'Approved' => '3',
-		'Send' => '4',		
+		'Send' => '4',
 	];
 
 }
