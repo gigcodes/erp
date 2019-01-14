06df92fcfdebc149de920455456b306d564a3b60
Lukas <vysniukass@gmail.com>
2019-01-14 13:10:45 +0200

    another fix for whatsapp controller

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -121,8 +121,8 @@ class WhatsAppController extends FindByNumberController
             $params['customer_id'] = $customer->id;
             $params = $this->modifyParamsWithMessage($params, $data);
             $message = ChatMessage::create($params);
-            $modal_type = 'leads';
-            $modal_id = $lead->id;
+            $model_type = 'leads';
+            $model_id = $lead->id;
         }
 
         NotificationQueueController::createNewNotification([
