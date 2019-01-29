60d8137d2685822382f3ad8ddb3549e613e5ebfd
bhavenk <bhavenkjwl@gmail.com>
2019-01-29 15:04:33 +0530

    Update name issues and busy ringtone

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,6 +81,7 @@ class UserController extends Controller
 
 
 		$input = $request->all();
+		$input['name'] = str_replace(' ', '_', $input['name']);
 		$input['password'] = Hash::make($input['password']);
 		if(isset($input['agent_role']))
         $input['agent_role'] = implode(',', $input['agent_role']);
@@ -146,6 +147,7 @@ class UserController extends Controller
 
 
 		$input = $request->all();
+		$input['name'] = str_replace(' ', '_', $input['name']);
 		if(isset($input['agent_role'])){
         $input['agent_role'] = implode(',', $input['agent_role']);
     }else{
