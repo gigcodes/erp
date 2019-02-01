7246b0179e8b3eed0eee79ecc47c7a23f3379ea3
Rishabh Aryal <rish.aryal@gmail.com>
2019-02-01 17:24:52 +0545

    fix(ChannegeRequired): Test to fix challenge required

--- app/Services/Instagram/DirectMessage.php
+++ app/Services/Instagram/DirectMessage.php
@@ -18,7 +18,20 @@ class DirectMessage {
         $username = env('IG_USERNAME', 'sololuxury');
         $password = env('IG_PASSWORD', 'U;5%wn~L48E+');
         $this->currentId = env('IG_CURRENT_USER_ID', '6827120791');
-        $this->instagram->login($username, $password);
+
+
+        try {
+            $this->instagram->login($username, $password);
+        } catch (Exception $Exception) {
+            if ($Exception instanceof ChallengeRequiredException)
+            {
+                sleep(5);
+                $customResponse = $this->instagram->request(substr($Exception->getResponse()->getChallenge()->getApiPath(), 1))->setNeedsAuth(false)->addPost('choice', 0)->getDecodedResponse();
+                if (is_array($customResponse)) {
+                    $this->instagram->login($username, $password);
+                }
+            }
+        }
     }
 
     public function getInbox() {
