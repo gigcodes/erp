112d0709e0f8ad9d5ae153af7627a1cff937789a
Lukas <vysniukass@gmail.com>
2019-02-09 21:33:47 +0200

    added customer import functionality

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -26,6 +26,7 @@
 
             <div class="pull-right mt-4">
               @if (Auth::user()->hasRole('Admin'))
+                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#importCustomersModal">Import Customers</button>
                 <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#sendAllModal">Send Message to All</button>
               @endif
               <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#mergeModal">Merge Customers</button>
@@ -221,6 +222,32 @@
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               <button type="submit" class="btn btn-secondary">Send Message</button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+
+    <div id="importCustomersModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Import Customers</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+          <form action="{{ route('customer.import') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="modal-body">
+              <div class="form-group">
+                <input type="file" name="file" required />
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-secondary">Import</button>
             </div>
           </form>
         </div>
