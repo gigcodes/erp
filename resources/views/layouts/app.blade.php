10ddaadf4414829929214a40ced8457c387f649c
Lukas <vysniukass@gmail.com>
2019-02-17 14:08:37 +0200

    fixed modal on mobiles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -185,6 +185,38 @@
 
 <body>
 
+  <div class="modal fade" id="instructionAlertModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">Instruction Reminder</h3>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <a href="" id="instructionAlertUrl" class="btn btn-secondary mx-auto">OK</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="developerAlertModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">Developer Task Reminder</h3>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <a href="" id="developerAlertUrl" class="btn btn-secondary mx-auto">OK</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
 {{-- <div id="fb-root"></div> --}}
 
 
