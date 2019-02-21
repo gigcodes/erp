9f9f34fd593331564840413bbd43033aac9f2392
Rishabh Aryal <rish.aryal@gmail.com>
2019-02-21 22:49:16 +0545

    fix

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -104,8 +104,9 @@ class CustomerController extends Controller
             $customers = $customers->orderBy('last_communicated_at', $orderby);
         }
 
-        $customers = $customers->selectRaw('customers.id, customers.name, orders.order_id, leads.lead_id, orders.order_created as order_created, orders.order_status as order_status, leads.lead_status as lead_status, leads.lead_created as lead_created, leads.rating as rating, CASE WHEN messages.message_created_at > chat_messages.chat_message_created_at THEN messages.message_created_at ELSE chat_messages.chat_message_created_at END AS last_communicated_at, CASE WHEN messages.message_created_at > chat_messages.chat_message_created_at THEN messages.message_body ELSE chat_messages.chat_message_body END AS message, CASE WHEN messages.message_created_at > chat_messages.chat_message_created_at THEN messages.message_status ELSE chat_messages.chat_message_status END AS message_status')->paginate(24);
-
+        $customers = $customers->selectRaw('customers.id, customers.name, orders.order_id, leads.lead_id, orders.order_created as order_created, orders.order_status as order_status, leads.lead_status as lead_status, leads.lead_created as lead_created, leads.rating as rating, CASE WHEN messages.message_created_at > chat_messages.chat_message_created_at THEN messages.message_created_at ELSE chat_messages.chat_message_created_at END AS last_communicated_at, 
+        CASE WHEN messages.message_created_at > chat_messages.chat_message_created_at THEN (SELECT mmm.body FROM messages mmm WHERE mmm.id = message_id) ELSE (SELECT mm2.message FROM chat_messages mm2 WHERE mm2.id = chat_message_id) END AS message, 
+        CASE WHEN messages.message_created_at > chat_messages.chat_message_created_at THEN (SELECT mm3.status FROM messages mm3 WHERE mm3.id = message_id) ELSE (SELECT mm4.status FROM chat_messages mm4 WHERE mm4.id = chat_message_id) END AS message_status')->paginate(24);
         return $customers;
     }
 
