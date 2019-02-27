cca62a2220c28f03809206a6c43534faec13d7e2
Rishabh Aryal <rish.aryal@gmail.com>
2019-02-27 14:38:12 +0545

    quickfix

--- app/Services/Scrap/Scraper.php
+++ app/Services/Scrap/Scraper.php
@@ -21,7 +21,7 @@ abstract class Scraper
                 'headers'=>[
                     'User-Agent' => 'User-Agent\':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
                 ],
-                'proxy' => $proxy
+//                'proxy' => $proxy
             ]);
             $content = $response->getBody()->getContents();
         } catch (\Exception $exception) {
