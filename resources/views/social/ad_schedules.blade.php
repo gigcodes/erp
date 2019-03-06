a06652527cfb0838280631098afe6ac55835f3a0
Rishabh Aryal <rish.aryal@gmail.com>
2019-03-06 15:55:33 +0545

    feat(Adslist)

--- resources/views/social/ad_schedules.blade.php
+++ resources/views/social/ad_schedules.blade.php
@@ -20,34 +20,51 @@
 	
 
 <div class="mt-4">
-	<table class="table">
-		<tr>
-			<th>S.N</th>
-			<th>Ad Set #</th>
-			<th>Name</th>
-			<th>Type Of Ad</th>
-			<th>Target Audience</th>
-			<th>Status</th>
-			<th>Created At</th>
-			<th>Updated At</th>
-		</tr>
-		@foreach($ads as $key=>$ad)
-			<tr data-adId="{{$ad['id']}}">
-				<td>{{ $key+1 }}</td>
-				<td>{{ $ad['adset_id'] }}</td>
-				<td>{{ $ad['name'] }}</td>
-				<td>N/A</td>
-				<td>
-					@foreach($ad['targeting'] as $key=>$value)
-						<span style="display:block"><strong>{{ucfirst($key)}}:</strong> {{title_case($value ?? 'N/A')}}</span>
+	<div id="exTab2">
+		<ul class="nav nav-tabs">
+			<li class="active">
+				<a  href="#1" data-toggle="tab">Ads List</a>
+			</li>
+			<li><a href="#2" data-toggle="tab">Calandar</a>
+			</li>
+		</ul>
+
+		<div class="tab-content ">
+			<div class="tab-pane active" id="1">
+				<table class="table mt-1">
+					<tr>
+						<th>S.N</th>
+						<th>Ad Set #</th>
+						<th>Name</th>
+						<th>Type Of Ad</th>
+						<th>Target Audience</th>
+						<th>Status</th>
+						<th>Created At</th>
+						<th>Updated At</th>
+					</tr>
+					@foreach($ads as $key=>$ad)
+						<tr data-adId="{{$ad['id']}}">
+							<td>{{ $key+1 }}</td>
+							<td>{{ $ad['adset_id'] }}</td>
+							<td>{{ $ad['name'] }}</td>
+							<td>N/A</td>
+							<td>
+								@foreach($ad['targeting'] as $key=>$value)
+									<span style="display:block"><strong>{{ucfirst($key)}}:</strong> {{title_case($value ?? 'N/A')}}</span>
+								@endforeach
+							</td>
+							<td>{{ $ad['status'] }}</td>
+							<td>{{ \Carbon\Carbon::createFromTimeString($ad['created_time'])->diffForHumans() }}</td>
+							<td>{{ \Carbon\Carbon::createFromTimeString($ad['updated_time'])->diffForHumans() }}</td>
+						</tr>
 					@endforeach
-				</td>
-				<td>{{ $ad['status'] }}</td>
-				<td>{{ \Carbon\Carbon::createFromTimeString($ad['created_time'])->diffForHumans() }}</td>
-				<td>{{ \Carbon\Carbon::createFromTimeString($ad['updated_time'])->diffForHumans() }}</td>
-			</tr>
-		@endforeach
-	</table>
+				</table>
+			</div>
+			<div class="tab-pane" id="2">
+				<p>Calandar Will be Displayed <Here class="."></Here></p>
+			</div>
+		</div>
+	</div>
 </div>
 
 @endsection
