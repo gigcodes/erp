6bdde1a96cdce15f1c3d47392625972b58ec97c0
Lukas <vysniukass@gmail.com>
2019-04-17 18:14:19 +0400

    update tory images

---
+++ app/Console/Commands/UpdateToryImages.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Product;
+use App\ScrapedProducts;
+use Plank\Mediable\Media;
+use Plank\Mediable\MediaUploaderFacade as MediaUploader;
+
+class UpdateToryImages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:tory-images';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      $scraped_products = ScrapedProducts::where('website', 'Tory')->get();
+
+      foreach ($scraped_products as $scraped_product) {
+        if ($scraped_product->product) {
+          if ($scraped_product->product->hasMedia(config('constants.media_tags'))) {
+            dump('MEDIA');
+          } else {
+            $images = $scraped_product->images;
+
+            foreach ($images as $image_name) {
+              $path = public_path('uploads') . '/social-media/' . $image_name;
+              $media = MediaUploader::fromSource($path)->upload();
+              $scraped_product->product->attachMedia($media,config('constants.media_tags'));
+            }
+          }
+        }
+      }
+
+      dd('stap');
+    }
+}
