5ef800dac462e968a447ba79bd0ac75352d3b540
Lukas <vysniukass@gmail.com>
2019-04-20 14:32:08 +0400

    added new tab to reviews and link with hashtags

--- app/ReviewSchedule.php
+++ app/ReviewSchedule.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class ReviewSchedule extends Model
 {
   protected $fillable = [
-    'date', 'platform', 'review_count', 'status'
+    'account_id', 'date', 'posted_date', 'platform', 'review_count', 'review_link', 'status'
   ];
 
   public function reviews()
   {
     return $this->hasMany('App\Review', 'review_schedule_id');
+  }
+
+  public function account()
+  {
+    return $this->belongsTo('App\Account');
   }
 }
