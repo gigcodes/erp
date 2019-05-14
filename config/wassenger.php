24bf0b911d69ff3167dd59d1c44036c0d7d14b9b
Lukas <vysniukass@gmail.com>
2019-05-14 19:15:52 +0400

    added new api for whatsapp

---
+++ config/wassenger.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'api_keys' => [
+        [
+            'number' => '919004418502',
+            'key' => '6f7a60eea35c32397f7a9e4de1576e1f73788d383bd33d15e605a7648f38d45aacb74d5c241c817f',
+            'device' => '5cda68cb15d08c001b17dae8',
+        ]
+
+    ],
+    // 'media_path' => realpath(implode(DIRECTORY_SEPARATOR, array(__DIR__, "..", "public", "apiwha", "media")))
+];
