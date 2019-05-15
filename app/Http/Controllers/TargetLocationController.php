e54ed734313166aa7eca8ca95afe4b632f992141
Rishabh Aryal <rish.aryal@gmail.com>
2019-05-15 10:34:02 +0400

    Instagram Location

---
+++ app/Http/Controllers/TargetLocationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TargetLocation;
+use Illuminate\Http\Request;
+
+class TargetLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TargetLocation  $targetLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TargetLocation $targetLocation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TargetLocation  $targetLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TargetLocation $targetLocation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TargetLocation  $targetLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TargetLocation $targetLocation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TargetLocation  $targetLocation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TargetLocation $targetLocation)
+    {
+        //
+    }
+}
