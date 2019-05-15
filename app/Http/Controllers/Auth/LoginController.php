3ae8f2175199225047289e7ebf45b1da0d583492
Lukas <vysniukass@gmail.com>
2019-05-15 10:35:29 +0400

    commented login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -37,7 +38,7 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function login(Request $request) {
-
-    }
+    // public function login(Request $request) {
+    //
+    // }
 }
