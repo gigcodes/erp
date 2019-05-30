2d2db02348ab50c01ba09c81482467fd79132ec5
Rishabh Aryal <rish.aryal@gmail.com>
2019-05-30 14:43:26 +0545

    Quick reply

---
+++ app/Http/Controllers/QuickReplyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\QuickReply;
+use Illuminate\Http\Request;
+
+class QuickReplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\QuickReply  $quickReply
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QuickReply $quickReply)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\QuickReply  $quickReply
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QuickReply $quickReply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\QuickReply  $quickReply
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, QuickReply $quickReply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\QuickReply  $quickReply
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QuickReply $quickReply)
+    {
+        //
+    }
+}
