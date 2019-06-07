24457b8215db83397f11602966e24e1b0ed3297f
Lukas <vysniukass@gmail.com>
2019-06-07 19:23:45 +0400

    delete products command

---
+++ app/Console/Commands/DeleteProducts.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Product;
+use File;
+use Illuminate\Console\Command;
+
+class DeleteProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      $products = Product::where('supplier', 'Carofiglio Junior')->get();
+      // $product = Product::find(127805);
+
+      foreach ($products as $key => $product) {
+        dump("$key - Product");
+
+        if ($product->hasMedia(config('constants.media_tags'))) {
+          dump("$key - Has Images");
+
+          foreach ($product->getMedia(config('constants.media_tags')) as $image) {
+            $image_path = $image->getAbsolutePath();
+
+            if (File::exists($image_path)) {
+              dump("$key - Deleting Image on server");
+              File::delete($image_path);
+              // unlink($image_path);
+            }
+
+            $image->delete();
+          }
+
+          $product->delete();
+        } else {
+          dump("$key - NO IMAGES");
+        }
+      }
+    }
+}
