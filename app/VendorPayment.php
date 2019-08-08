3143d3c9a959901923ea0f4332f39db27849c227
Prabhat Thapa <prabhat.thapachh@gmail.com>
2019-08-08 22:57:33 +0545

    soft delete for vendor payment

--- app/VendorPayment.php
+++ app/VendorPayment.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VendorPayment extends Model
 {
-    protected $fillable = ['vendor_id', 'payment_date', 'paid_date', 'payable_amount', 'paid_amount', 'service_provided', 'module', 'work_hour', 'description', 'other', 'status', 'user_id', 'updated_by','currency'];
+    use SoftDeletes;
+    protected $fillable = ['vendor_id', 'payment_date', 'paid_date', 'payable_amount', 'paid_amount', 'service_provided', 'module', 'work_hour', 'description', 'other', 'status', 'user_id', 'updated_by', 'currency'];
+    protected $dates = ['deleted_at'];
 
     public function vendor()
     {
