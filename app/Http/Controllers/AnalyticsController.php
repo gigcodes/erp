951cfc692c693cf536edc6fc0690f07d933ac67d
ahsan <etwordpress03@gmail.com>
2019-08-11 11:17:57 +0500

    latest changes in analytics

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -11,14 +11,62 @@ use function Opis\Closure\unserialize;
 
 class AnalyticsController extends Controller
 {
+   public function showData(){
+      include(app_path() . '/Functions/Analytics.php');
+      $visitors = Analytics::fetchVisitorsAndPageViews(Period::days(1));
+      DB::table('google_analytics')->where('key', '=', 'visitors')->delete();
+      DB::table('google_analytics')->insert(
+         [
+            'key' => 'visitors',
+            'value' => serialize($visitors)
+         ]
+      );
+      $top_referers = Analytics::fetchTopReferrers(Period::days(4), 20);
+      $user_types = Analytics::fetchUserTypes(Period::days(4));
+      $total_visitors_page_views = Analytics::fetchTotalVisitorsAndPageViews(Period::days(4));
+      DB::table('google_analytics')->where('key', '=', 'top_referers')->delete();
+      DB::table('google_analytics')->insert(
+         [
+            'key' => 'top_referers',
+            'value' => serialize($top_referers)
+         ]
+      );
+      DB::table('google_analytics')->where('key', '=', 'user_types')->delete();
+      DB::table('google_analytics')->insert(
+         [
+            'key' => 'user_types',
+            'value' => serialize($user_types)
+         ]
+      );
+      DB::table('google_analytics')->where('key', '=', 'total_visitors_page_views')->delete();
+      DB::table('google_analytics')->insert(
+         [
+            'key' => 'total_visitors_page_views',
+            'value' => serialize($total_visitors_page_views)
+         ]
+      );
+      $top_referers_ser = DB::table('google_analytics')->select('value')->where('key', 'top_referers')->pluck('value')->toArray();
+      $user_types_ser = DB::table('google_analytics')->select('value')->where('key', 'user_types')->pluck('value')->toArray();
+      $total_visitors_page_views_ser = DB::table('google_analytics')->select('value')->where('key', 'total_visitors_page_views')->pluck('value')->toArray();
+      $visitors_ser = DB::table('google_analytics')->select('value')->where('key', 'visitors')->pluck('value')->toArray();
+      $total_views = unserialize($total_visitors_page_views_ser[0]);
+      
+      if (!empty($request['date'])) {
+            foreach($total_views as $views) {
+               $dates[] = Carbon::parse($views['date'])->toDateString();
+            }
+            if (in_array(Carbon::parse($request['date'])->toDateTimeString(), $dates)) {
+               $total_views = unserialize($total_visitors_page_views_ser[0]);
+            }
+            $total_views = unserialize($total_visitors_page_views_ser[0]);
+      }
+      dd($visitors);
+      return View('analytics.index', compact('top_referers_ser', 'user_types_ser', 'total_views', 'visitors_ser', 'results'));
+   }
+   
 
-   /**
-    * Return google analytics data
-    */
-    public function showData(){
-       
-    }
-   //  /**
+
+   
    //   * Return google analytics Data
    //   */
    //  public function showData(Request $request){
@@ -81,14 +129,14 @@ class AnalyticsController extends Controller
      *
      * @return $items
      */
-    public static function customPaginator($request, $values = array(), $posts_per_page = '5')
-    {
-        $currentPage = LengthAwarePaginator::resolveCurrentPage();
-        $itemCollection = collect($values);
-        $perPage = intval($posts_per_page);
-        $currentPageItems = $itemCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
-        $items = new LengthAwarePaginator($currentPageItems, count($itemCollection), $perPage);
-        $items->setPath($request->url());
-        return $items;
-    }
+   //  public static function customPaginator($request, $values = array(), $posts_per_page = '5')
+   //  {
+   //      $currentPage = LengthAwarePaginator::resolveCurrentPage();
+   //      $itemCollection = collect($values);
+   //      $perPage = intval($posts_per_page);
+   //      $currentPageItems = $itemCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
+   //      $items = new LengthAwarePaginator($currentPageItems, count($itemCollection), $perPage);
+   //      $items->setPath($request->url());
+   //      return $items;
+   //  }
 }
