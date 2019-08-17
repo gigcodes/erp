6f570b7f5a124f9adabdaad83813e86191e0c4c4
B <b@appsbyb.com>
2019-08-17 15:51:09 +0400

    Updated image enhancement API to only take products which are approved

--- app/Http/Controllers/Products/ProductEnhancementController.php
+++ app/Http/Controllers/Products/ProductEnhancementController.php
@@ -16,6 +16,7 @@ class ProductEnhancementController extends Controller
 
         $product = Product::where('is_enhanced', 0)
             ->where('is_crop_ordered', 1)
+            ->where('is_approved', 1)
             ->orderByRaw($orderByPritority)
             ->orderBy('is_approved', 'DESC')
             ->first();
