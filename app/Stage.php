b2d76af914dd75d4f9a676158b30b18ae1406daf
B <b@appsbyb.com>
2019-08-21 18:34:38 +0400

    Readded stage.php

---
+++ app/Stage.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mustafaflexwala
+ * Date: 17/08/18
+ * Time: 1:14 PM
+ */
+
+namespace App;
+
+
+class Stage extends ReadOnlyBase {
+
+	protected $data =[
+		'Selection' => '1',
+		'Searcher' => '1',
+		'Attribute' => '1',
+		'Supervisor' => '1',
+		'ImageCropper' => '1',
+		'Lister' => '1',
+		'Approver' => '1',
+		'Inventory' => '1',
+		'Sale' => '1'
+	];
+
+	
+
+}
