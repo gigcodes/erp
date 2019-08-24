77f1f5b787b450e61f238cfb62481efb43824f0f
B <b@appsbyb.com>
2019-08-24 16:05:31 +0400

    Updated migration

--- database/migrations/2019_08_24_135024_alter_table_products_add_indexes_for_brand_supplier_is_on_sale_listing_approved_at.php
+++ database/migrations/2019_08_24_135024_alter_table_products_add_indexes_for_brand_supplier_is_on_sale_listing_approved_at.php
@@ -20,6 +20,10 @@ class AlterTableProductsAddIndexesForBrandSupplierIsOnSaleListingApprovedAt exte
             $table->index('is_on_sale');
             $table->index('listing_approved_at');
         });
+
+        Schema::table('chat_messages', function (Blueprint $table) {
+            $table->index('customer_id');
+        });
     }
 
     /**
@@ -34,6 +38,10 @@ class AlterTableProductsAddIndexesForBrandSupplierIsOnSaleListingApprovedAt exte
             $table->dropIndex('products_supplier_index');
             $table->dropIndex('products_is_on_sale_index');
             $table->dropIndex('products_listing_approved_at_index');
+        });
+
+        Schema::table('chat_messages', function (Blueprint $table) {
+            $table->dropIndex('chat_messages_customer_id_index');
         });
     }
 }
