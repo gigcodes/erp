96c8304e50479f0a8a2444a8a5191662b85ed05a
B <b@appsbyb.com>
2019-09-03 09:02:30 +0400

    Updated scrap statistics page

--- app/Http/Controllers/ScrapStatisticsController.php
+++ app/Http/Controllers/ScrapStatisticsController.php
@@ -6,11 +6,12 @@ use App\ScrapStatistics;
 use App\Supplier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use \Carbon\Carbon;
 
 class ScrapStatisticsController extends Controller
 {
 
-    //List of supplier with their brand count
+
     private $suppliers = [
         'angelominetti' => 23,
         'Wiseboutique' => 18,
@@ -51,82 +52,38 @@ class ScrapStatisticsController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     *
-     * generate the scrap statistics
      */
     public function index(Request $request)
     {
-        $date = $request->get('date');
+        // Set dates
+        $startDate = date('Y-m-d H:i:s', time() - (2 * 86400));
+        $endDate = date('Y-m-d H:i:s');
 
+        // Get scrape data
+        $sql = '
+            SELECT
+                website,
+                COUNT(id) AS total,
+                SUM(IF(validated=0,1,0)) AS failed,
+                SUM(IF(validated=1,1,0)) AS validated,
+                SUM(IF(validation_result LIKE "%[error]%",1,0)) AS errors,
+                SUM(IF(validation_result LIKE "%[warning]%",1,0)) AS warnings,
+                MAX(created_at) AS last_scrape_date,
+                IF(MAX(created_at) < DATE_SUB(NOW(), INTERVAL 2 DAY),0,1) AS running
+            FROM
+                log_scraper
+            WHERE
+                created_at > "' . $startDate . '" AND
+                created_at < "' . $endDate . '"
+            GROUP BY
+                website
+            ORDER BY
+                website
+        ';
+        $scrapeData = DB::select($sql);
 
-//        $inactiveScrapping = DB::table('scrap_statistics')
-//            ->selectRaw('supplier, MAX(created_at) as created_at, TIMESTAMPDIFF(HOUR, created_at, NOW()) as last_scraped`')
-//            ->groupBy('supplier')
-//            ->get();
-//
-//        dd($inactiveScrapping);
-
-//         get stats for new and existing ones
-        $scrapedExistingProducts = DB::table('scrap_statistics')->selectRaw('COUNT(DISTINCT description) as total, supplier')->where('type', 'EXISTING_SCRAP_PRODUCT');
-        $scrapedNewProducts = DB::table('scrap_statistics')->selectRaw('COUNT(DISTINCT description) as total, supplier')->where('type', 'NEW_SCRAP_PRODUCT');
-
-
-
-        $suppliers = ScrapStatistics::distinct()->get(['supplier']);
-
-        $supplierList = $this->suppliers;
-        $progress = [];
-
-        $totalBrands = 0;
-        $doneBrands = 0;
-
-//        loop through the supplier list and then add count & stat by date with created at and ended at
-        foreach ($supplierList as $key=>$item) {
-            $count = ScrapStatistics::where('supplier', $key);
-            $stat = ScrapStatistics::selectRaw('MIN(created_at) as started_at, MAX(created_at) as ended_at')->where('supplier', $key);
-            if (strlen($date) === 10) {
-                $count = $count->whereRaw('DATE(created_at) = "'. $date . '"');
-                $stat = $stat->whereRaw('DATE(created_at) = "'. $date . '"');
-            } else {
-                $count = $count->whereRaw('DATE(created_at) = "'. date('Y-m-d'.'"'));
-                $stat = $stat->whereRaw('DATE(created_at) = "'. date('Y-m-d'.'"'));
-            }
-
-            $count = $count->distinct()->pluck('brand')->toArray();
-
-            $doneBrands+=count($count);
-            $totalBrands+=$item;
-
-            $blist = implode(', ', $count);
-
-            $progress[$key] = [count($count), round((count($count)/$item)*100),$item, $blist, $stat->first()];
-        }
-
-
-        if (strlen($date) === 10) {
-            $scrapedNewProducts = $scrapedNewProducts->whereRaw('DATE(created_at) = "'. $date.'"');
-            $scrapedExistingProducts = $scrapedExistingProducts->whereRaw('DATE(created_at) = "'. $date.'"');
-        }
-
-        $totalPercent = $doneBrands/$totalBrands;
-        //calculate the percent for particular supplier..
-        $totalProgress = round($totalPercent*100);
-
-        $scrapedNewProducts = $scrapedNewProducts->groupBy(['supplier'])->get();
-        $scrapedExistingProducts = $scrapedExistingProducts->groupBy(['supplier'])->get();
-
-
-        $progressStats = [];
-
-        foreach ($suppliers as $supplier) {
-            $data = DB::table('scrap_statistics')->selectRaw('COUNT(*) as total, brand')->where('supplier', $supplier->supplier)->groupBy('brand');
-            if (strlen($date) === 10) {
-                $data = $data->whereRaw('DATE(created_at) = "'. $date . '"');
-            }
-            $progressStats[$supplier->supplier] = $data->get();
-        }
-
-        return view('scrap.stats', compact('scrapedExistingProducts', 'scrapedNewProducts', 'request', 'progressStats', 'progress', 'totalProgress'));
+        // Return view
+        return view('scrap.stats', compact('scrapeData'));
     }
 
     /**
@@ -142,7 +99,7 @@ class ScrapStatisticsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -168,7 +125,7 @@ class ScrapStatisticsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\ScrapStatistics  $scrapStatistics
+     * @param  \App\ScrapStatistics $scrapStatistics
      * @return \Illuminate\Http\Response
      */
     public function show(ScrapStatistics $scrapStatistics)
@@ -179,7 +136,7 @@ class ScrapStatisticsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\ScrapStatistics  $scrapStatistics
+     * @param  \App\ScrapStatistics $scrapStatistics
      * @return \Illuminate\Http\Response
      */
     public function edit(ScrapStatistics $scrapStatistics)
@@ -190,8 +147,8 @@ class ScrapStatisticsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\ScrapStatistics  $scrapStatistics
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\ScrapStatistics $scrapStatistics
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, ScrapStatistics $scrapStatistics)
@@ -202,11 +159,19 @@ class ScrapStatisticsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\ScrapStatistics  $scrapStatistics
+     * @param  \App\ScrapStatistics $scrapStatistics
      * @return \Illuminate\Http\Response
      */
     public function destroy(ScrapStatistics $scrapStatistics)
     {
         //
+    }
+
+    public function assetManager()
+    {
+        $start = Carbon::now()->format('Y-m-d 00:00:00');
+        $end = Carbon::now()->format('Y-m-d 23:59:00');
+        // dd('hello');
+        return view('scrap.asset-manager');
     }
 }
--- resources/views/scrap/stats.blade.php
+++ resources/views/scrap/stats.blade.php
@@ -1,160 +1,90 @@
 @extends('layouts.app')
 
-@section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h2 class="page-heading">Scrap Stats</h2>
-        </div>
-        <div class="col-md-12 mb-4">
-            <form action="{{ action('ScrapStatisticsController@index') }}" method="get">
-                <div class="row">
-                    <div class="col-md-2">
-                        <input name="date" type="date" class="form-control" value="{{$request->get('date') ?? date('Y-m-d')}}">
-                    </div>
-                    <div class="col-md-1">
-                        <button class="btn btn-default">Filter</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="col-md-12">
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#home">Scrap Counts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#menu1">Scrap Progress</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#menu2">Progress</a>
-                </li>
-            </ul>
-
-            <!-- Tab panes -->
-            <div class="tab-content">
-                <div id="home" class="container tab-pane active">
-                    <br>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <table class="table table-striped table-bordered table-sm">
-                                <tr>
-                                    <th colspan="2">
-                                        Scraped Existing Products
-                                    </th>
-                                </tr>
-                                @foreach($scrapedExistingProducts as $entry)
-                                    <tr>
-                                        <td>
-                                            {{ $entry->supplier }}
-                                        </td>
-                                        <th>
-                                            {{$entry->total}}
-                                        </th>
-                                    </tr>
-                                @endforeach
-                            </table>
-                        </div>
-                        <div class="col-md-6">
-                            <table class="table table-striped table-bordered table-sm">
-                                <tr>
-                                    <th colspan="2">
-                                        Scraped New Products
-                                    </th>
-                                </tr>
-                                @foreach($scrapedNewProducts as $entry)
-                                    <tr>
-                                        <td>
-                                            {{ $entry->supplier }}
-                                        </td>
-                                        <th>
-                                            {{$entry->total}}
-                                        </th>
-                                    </tr>
-                                @endforeach
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <div id="menu1" class="container tab-pane fade">
-                    @foreach($progressStats as $key=>$stat)
-                        <h2 class="text-center">{{ $key }}</h2>
-                        <div class="table-responsive">
-                            <table class="table table-striped table-bordered">
-                                <tr>
-                                    @foreach($stat as $st)
-                                        <td>
-                                            {{ $st->brand }}<br>
-                                            {{ $st->total }}
-                                        </td>
-                                    @endforeach
-                                </tr>
-                            </table>
-                        </div>
-                    @endforeach
-                </div>
-                <div id="menu2" class="container tab-pane fade"><br>
-                    <div class="progress" style="margin-bottom: 10px;">
-                        <div class="progress-bar" role="progressbar" style="width: {{$totalProgress}}%" aria-valuenow="{{$totalProgress}}" aria-valuemin="0" aria-valuemax="100">{{ $totalProgress }}%</div>
-                    </div>
-                    <table class="table table-striped table-bordered">
-                        <tr>
-                            <th>Supplier</th>
-                            <th>Progress</th>
-                            <td>Started At</td>
-                            <td>Last Scraped</td>
-                            <th>Brands Scraped</th>
-                        </tr>
-                        @foreach($progress as $key=>$progressItem)
-                            <tr>
-                                <td>{{$key}}</td>
-                                <td>
-                                    <div class="progress" style="margin-bottom: 5px;">
-                                        <div class="progress-bar" role="progressbar" style="width: {{$progressItem[1]}}%" aria-valuenow="{{$progressItem[1]}}" aria-valuemin="0" aria-valuemax="100">{{ $progressItem[1]}}%</div>
-                                    </div>
-                                    {{ $progressItem[0] . ' of ' . $progressItem[2] }}
-                                </td>
-                                <td>
-                                    {{ $progressItem[4]->started_at ?? 'N/A' }}
-                                </td>
-                                <td>
-                                    {{ $progressItem[4]->ended_at ?? 'N/A' }}
-                                </td>
-                                <td class="expand-row">
-                                    <span class="td-mini-container">
-                                        {!! strlen($progressItem[3]) > 20 ? substr($progressItem[3], 0, 20).'...' : $progressItem[3] !!}
-                                    </span>
-                                    <span class="td-full-container hidden">
-                                        {!! $progressItem[3] !!}
-                                    </span>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
-
-    </div>
-@endsection
+@section('title', 'Supplier Scrapping  Info')
 
 @section('styles')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.5/css/bootstrap-select.min.css">
+@endsection
+
+@section('content')
+
+    <div class="row mb-5">
+        <div class="col-lg-12 margin-tb">
+            <h2 class="page-heading">Supplier Scrapping  Info </h2>
+
+            <div class="pull-left">
+                <form class="form-inline" action="{{ route('mastercontrol.index') }}" method="GET">
+
+                    <div class="form-group ml-3">
+                        <input type='text' class="form-control" name="search" placeholder="Search" required />
+                    </div>
+
+                    <div class="form-group ml-3">
+                        <select name="status" class="form-control">
+                            <option>Select Type</option>
+                            <option>Sucess</option>
+                            <option>Failed</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group ml-3">
+                        <input type="text" value="" name="range_start" hidden/>
+                        <input type="text" value="" name="range_end" hidden/>
+                        <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+                            <i class="fa fa-calendar"></i>&nbsp;
+                            <span></span> <i class="fa fa-caret-down"></i>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-secondary ml-3">Submit</button>
+                </form>
+            </div>
+
+            <div class="pull-right mt-4">
+
+            </div>
+        </div>
+    </div>
+
+    @include('partials.flash_messages')
+
+    <div class="row no-gutters mt-3">
+        <div class="col-xs-12 col-md-12" id="plannerColumn">
+            <div class="table-responsive">
+                <table class="table table-bordered table-sm">
+                    <thead>
+                        <tr>
+                            <th>Supplier</th>
+                            <th>Last Scraped</th>
+                            <th>Inventory</th>
+                            <th>Total</th>
+                            <th>Errors</th>
+                            <th>Warnings</th>
+                            <th>Developer</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach($scrapeData as $data)
+                        <tr<?= $data->running == 0 ? ' style="background-color: red; color: white;"' : '' ?>>
+                            <td class="p-2">{{ $data->website }}</td>
+                            <td class="p-2">{{ date('d-m-Y H:i:s', strtotime($data->last_scrape_date)) }}</td>
+                            <td class="p-2 text-right">{{ $data->total - $data->errors }}</td>
+                            <td class="p-2 text-right">{{ $data->total }}</td>
+                            <td class="p-2 text-right">{{ $data->errors }}</td>
+                            <td class="p-2 text-right">{{ $data->warnings }}</td>
+                            <td class="p-2">Unknown</td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @section('scripts')
-    <script>
-        $(document).on('click', '.expand-row', function() {
-            var selection = window.getSelection();
-            if (selection.toString().length === 0) {
-                // if ($(this).data('switch') == 0) {
-                //   $(this).text($(this).data('details'));
-                //   $(this).data('switch', 1);
-                // } else {
-                //   $(this).text($(this).data('subject'));
-                //   $(this).data('switch', 0);
-                // }
-                $(this).find('.td-mini-container').toggleClass('hidden');
-                $(this).find('.td-full-container').toggleClass('hidden');
-            }
-        });
-    </script>
 @endsection
