139dbc1f65620eb48ef11a9ce8f5656d8ff08add
B <b@appsbyb.com>
2019-09-05 11:21:23 +0400

    Added debug information

--- app/Http/Controllers/ProductApproverController.php
+++ app/Http/Controllers/ProductApproverController.php
@@ -105,6 +105,7 @@ class ProductApproverController extends Controller
 						try {
 							$result = $proxy->catalogProductUpdate($sessionId, $reference_final_sku . '-' . $size , array('status' => 1));
 						} catch (\Exception $e) {
+						    var_dump($e); exit();
 							$errors++;
 						}
 					}
@@ -113,12 +114,14 @@ class ProductApproverController extends Controller
 				try {
 					$result = $proxy->catalogProductUpdate($sessionId, $reference_final_sku, array('status' => 1));
 				} catch (\Exception $e) {
+                    var_dump($e); exit();
 					$errors++;
 				}
 			} else {
 				try {
 					$result = $proxy->catalogProductUpdate($sessionId, $reference_final_sku, array('status' => 1));
 				} catch (\Exception $e) {
+                    var_dump($e); exit();
 					$errors++;
 				}
 			}
