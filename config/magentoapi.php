58cb34d85153937211f4ec8c11405ffc1ed03d3c
B <b@appsbyb.com>
2019-09-05 10:30:29 +0400

    Updated magento to go to new devsite

--- config/magentoapi.php
+++ config/magentoapi.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-	'url' => 'https://www.sololuxury.co.in/api/v2_soap/?wsdl=1',
-//	'url' => 'https://www.sololuxury.co.in/devsite/api/v2_soap/?wsdl=1',
+//	'url' => 'https://www.sololuxury.co.in/api/v2_soap/?wsdl=1',
+	'url' => 'https://devsite.sololuxury.co.in/api/v2_soap/?wsdl=1',
 	'user' => 'apiUser',
 	'password' => 'Sakina88!'
 );
