6be48627f7ee36bbf57c3ff5b3a031872a9b406f
B <b@appsbyb.com>
2019-09-20 09:56:35 +0400

    Added TasksTypes

---
+++ app/TasksTypes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TasksTypes extends Model
+{
+
+    protected $table = 'task_types';
+
+    protected $fillable = [
+        'name'
+    ];
+}
