c7691a0dff31c8ec0786a94ac5a5cdbd21159921
B <b@appsbyb.com>
2019-09-26 17:39:03 +0400

    Updated Log Scraper to store raw data

--- app/Loggers/LogScraper.php
+++ app/Loggers/LogScraper.php
@@ -94,7 +94,7 @@ class LogScraper extends Model
         $logScraper->is_sale = $request->is_sale ?? 0;
         $logScraper->validated = empty($errorLog) ? 1 : 0;
         $logScraper->validation_result = $errorLog . $warningLog;
-        $logScraper->raw_data = isset($_POST) ? serialize($_POST) : null;
+        $logScraper->raw_data = serialize($request->all());
         $logScraper->save();
 
         // Update modified date
