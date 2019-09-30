f10c540f71c4b028a57c3ec3150dce50dc098b3a
B <b@appsbyb.com>
2019-09-30 06:26:52 +0400

    Updated ProductAI job

--- app/Jobs/ProductAi.php
+++ app/Jobs/ProductAi.php
@@ -76,7 +76,7 @@ class ProductAi implements ShouldQueue
 
         // Set json with original data
         $resultScraper = [
-            'category' => isset($product->product_category->title) ? $product->product_category->title : '',
+            'category' => isset($product->product_category->title) ? json_encode($product->product_category->title) : '',
             'color' => $product->color,
             'composite' => $product->composition,
             'gender' => ''
