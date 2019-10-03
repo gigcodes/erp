8b2f46692143e85c050134dc709977949bdf05ad
Pravin Solanki <solanki7492@gmail.com>
2019-10-04 01:45:41 +0530

    mediables issue fix

--- app/Http/Controllers/TmpController.php
+++ app/Http/Controllers/TmpController.php
@@ -15,16 +15,18 @@ class TmpController extends Controller
         foreach ($mediaArr as $media) {
             if ($media->fileExists()) {
                 $mediables = DB::table('mediables')->where('media_id', $media->id)->first();
-                $table = strtolower(str_replace('App\\', '', $mediables->mediable_type));
-                
-                if (!empty($mediables->mediable_id) && $mediables->mediable_id > 1) {
-                    $key   = floor($mediables->mediable_id/10000);
-                } else {
-                    $key   = strtolower(substr($media->basename,0,1).'/'.substr($media->basename,1,1));
-                }
+                if ($mediables) {
+                    $table = strtolower(str_replace('App\\', '', $mediables->mediable_type));
+                    
+                    if (!empty($mediables->mediable_id) && $mediables->mediable_id > 1) {
+                        $key   = floor($mediables->mediable_id/10000);
+                    } else {
+                        $key   = strtolower(substr($media->basename,0,1).'/'.substr($media->basename,1,1));
+                    }
 
-                if ($media->getDiskPath() != $table.'/'.$key.'/'.ltrim($media->basename, '/')) {
-                    $media->move($table.'/'.$key);
+                    if ($media->getDiskPath() != $table.'/'.$key.'/'.ltrim($media->basename, '/')) {
+                        $media->move($table.'/'.$key);
+                    }
                 }
             }
         }
