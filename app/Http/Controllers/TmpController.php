8eb5d3182ce0805d93963d9f3ace90fae63b50a3
Pravin Solanki <solanki7492@gmail.com>
2019-10-03 23:13:51 +0530

    fix without id in media

--- app/Http/Controllers/TmpController.php
+++ app/Http/Controllers/TmpController.php
@@ -15,7 +15,13 @@ class TmpController extends Controller
             if ($media->fileExists()) {
                 $mediables = DB::table('mediables')->where('media_id', $media->id)->first();
                 $table = strtolower(str_replace('App\\', '', $mediables->mediable_type));
-                $key   = floor($mediables->mediable_id/10000);
+                
+                if (!empty($mediables->mediable_id) && $mediables->mediable_id > 1) {
+                    $key   = floor($mediables->mediable_id/10000);
+                } else {
+                    $key   = strtolower(substr($media->basename,0,1).'/'.substr($media->basename,1,1));
+                }
+
                 if ($media->getDiskPath() != $table.'/'.$key.'/'.ltrim($media->basename, '/')) {
                     $media->move($table.'/'.$key);
                 }
