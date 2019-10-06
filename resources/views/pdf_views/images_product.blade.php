078a06631d73952763a6f26badb30874b1a73e92
B <b@appsbyb.com>
2019-10-06 17:45:33 +0400

    Added price and correct dimension format to PDF

--- resources/views/pdf_views/images_product.blade.php
+++ resources/views/pdf_views/images_product.blade.php
@@ -62,14 +62,15 @@
                     }
                     ?>
                     @if($product)
-                        <div style="padding: 10px; text-align: justify">
+                        <div style="padding: 10px; font-size: 36px; text-align: justify">
                             <p><strong>{{ $product->name }}</strong></p>
                             @if($product->brands)
                                 <p style="color: #1a60aa"><strong>{{ $product->brands->name }}</strong></p>
                             @endif
+                            <p><strong>Price: </strong> <span style="text-decoration: line-through">Rs. {{ $product->price_inr }}</span> Rs. {{ $product->price_special }}</p>
                             <p><strong>Code: </strong> {{ $product->sku }}</p>
                             @if($product->lmeasurement && $product->hmeasurement && $product->dmeasurement)
-                                <p><strong>Dimension: </strong> {{ $product->lmeasurement }} x {{ $product->hmeasurement }} x {{ $product->dmeasurement }}</p>
+                                <p><strong>Dimension: </strong> {{ \App\Helpers\ProductHelper::getMeasurements($product) }}</p>
                             @endif
                         </div>
                     @else
