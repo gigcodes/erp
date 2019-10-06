fbede3a1e89244fb0863f60afce7e325574aa039
B <b@appsbyb.com>
2019-10-06 23:28:06 +0400

    Changed API to live

--- config/magentoapi.php
+++ config/magentoapi.php
@@ -2,8 +2,8 @@
 
 return array(
 //	'url' => 'https://www.sololuxury.co.in/api/v2_soap/?wsdl=1',
-	'url' => 'https://devsite.sololuxury.co.in/api/v2_soap/?wsdl=1',
-//	'url' => 'http://jephunertoqato.sololuxury.co.in/api/v2_soap/?wsdl=1',
+//	'url' => 'https://devsite.sololuxury.co.in/api/v2_soap/?wsdl=1',
+	'url' => 'http://jephunertoqato.sololuxury.co.in/api/v2_soap/?wsdl=1',
 	'user' => 'apiUser',
 	'password' => 'Sakina88!'
 );
