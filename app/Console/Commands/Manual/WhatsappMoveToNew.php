7df7f690feb425c9ad0dda4ada026bf40f499a84
B <b@appsbyb.com>
2019-10-11 15:05:42 +0400

    Updated console command to move customers from the old IN numbers to the new Dubai numbers

--- app/Console/Commands/Manual/WhatsappMoveToNew.php
+++ app/Console/Commands/Manual/WhatsappMoveToNew.php
@@ -83,7 +83,7 @@ class WhatsappMoveToNew extends Command
         if ($rs !== null) {
             foreach ($rs as $result) {
                 // Find customer
-                $customer = Customer::find($result->customer_id);
+                $customer = Customer::find($result->id);
 //                $customer = Customer::find(44); // For testing
                 $customer->whatsapp_number = $currentNewNumber;
                 $customer->save();
