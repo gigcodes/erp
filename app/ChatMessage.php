67944f52b660cdb36ec73f740f44a0b8ff1fb6bf
B <b@appsbyb.com>
2019-10-11 16:01:02 +0400

    #DEVTASK-762 Storing unique ID with each message

--- app/ChatMessage.php
+++ app/ChatMessage.php
@@ -9,7 +9,7 @@ class ChatMessage extends Model
 {
     use Mediable;
 
-    protected $fillable = ['lead_id', 'order_id', 'customer_id', 'supplier_id', 'vendor_id', 'user_id', 'task_id', 'erp_user', 'contact_id', 'dubbizle_id', 'assigned_to', 'purchase_id', 'message', 'media_url', 'number', 'approved', 'status', 'error_status', 'resent', 'is_reminder', 'created_at', 'issue_id', 'developer_task_id', 'lawyer_id', 'case_id', 'blogger_id', 'voucher_id', 'document_id'];
+    protected $fillable = ['unique_id', 'lead_id', 'order_id', 'customer_id', 'supplier_id', 'vendor_id', 'user_id', 'task_id', 'erp_user', 'contact_id', 'dubbizle_id', 'assigned_to', 'purchase_id', 'message', 'media_url', 'number', 'approved', 'status', 'error_status', 'resent', 'is_reminder', 'created_at', 'issue_id', 'developer_task_id', 'lawyer_id', 'case_id', 'blogger_id', 'voucher_id', 'document_id'];
     protected $table = "chat_messages";
     protected $dates = ['created_at', 'updated_at'];
     protected $casts = array(
