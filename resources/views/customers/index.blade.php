1922f12c90801782aa7eecb676d7d18418d87e3e
Pravin Solanki <solanki7492@gmail.com>
2019-10-15 18:39:25 +0530

    she size group by

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -178,7 +178,22 @@
                     <div class="form-group ml-3">
                         <input placeholder="Clothing Size" type="text" name="clothing_size" value="{{request()->get('clothing_size')}}" class="form-control-sm form-control">
                     </div>
-
+                    <div class="form-group ml-3">
+                        <select class="form-control" name="shoe_size_group">
+                            <option value="">Select</option>
+                            <?php foreach ($shoe_size_group as $shoe_size => $customerCount) {
+                                echo '<option value="'.$shoe_size.'" '.($shoe_size == request()->get('shoe_size_group') ? 'selected' : '').'>('.$shoe_size.' Size) '.$customerCount.' Customers</option>';
+                            } ?>
+                        </select>
+                    </div>
+                    <div class="form-group ml-3">
+                        <select class="form-control" name="clothing_size_group">
+                            <option value="">Select</option>
+                            <?php foreach ($clothing_size_group as $clothing_size => $customerCount) {
+                                echo '<option value="'.$clothing_size.'" '.($shoe_size == request()->get('shoe_size_group') ? 'selected' : '').'>('.$clothing_size.' Size) '.$customerCount.' Customers</option>';
+                            } ?>
+                        </select>
+                    </div>
                     <button type="submit" class="btn btn-image"><img src="/images/filter.png"/></button>
                 </form>
             </div>
