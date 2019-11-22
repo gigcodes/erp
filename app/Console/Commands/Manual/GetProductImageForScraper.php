9c8bfffd80d9785881ec081493c9d441710c6fd8
B <b@appsbyb.com>
2019-11-22 20:19:10 +0400

    Updated output to show product ID

--- app/Console/Commands/Manual/GetProductImageForScraper.php
+++ app/Console/Commands/Manual/GetProductImageForScraper.php
@@ -46,62 +46,63 @@ class GetProductImageForScraper extends Command
         $scrapedProducts = ScrapedProducts::all();
 
         foreach ($scrapedProducts as $scrapedProduct) {
-            
+
             //get products from scraped products
             $product = $scrapedProduct->product;
-            
+
             //check if scraped product has product
-            if($product != null && $product != ''){
+            if ($product != null && $product != '') {
                 //check if product has media
-                 if($product->hasMedia(\Config('constants.media_tags'))){
+                if ($product->hasMedia(\Config('constants.media_tags'))) {
                     dump('Product has media');
-                }else{
+                } else {
                     //check if scrapedProduct has images
-                    if($scrapedProduct->images == null && $scrapedProduct->images == ''){
+                    if ($scrapedProduct->images == null && $scrapedProduct->images == '') {
                         continue;
                     }
                     //if product does not have media loop over images
-                    $countImageUpdated  = 0; 
+                    $countImageUpdated = 0;
                     foreach ($scrapedProduct->images as $image) {
-                            //check if image has http or https link
-                            if (strpos($image, 'http') === false) {
-                                continue;
-                            }
+                        //check if image has http or https link
+                        if (strpos($image, 'http') === false) {
+                            continue;
+                        }
 
+                        try {
+                            //generating image from image
+                            $jpg = \Image::make($image)->encode('jpg');
+                        } catch (\Exception $e) {
+                            // if images are null
+                            $jpg = null;
+                        }
+                        if ($jpg != null) {
+                            $filename = substr($image, strrpos($image, '/'));
+                            $filename = str_replace("/", "", $filename);
                             try {
-                                //generating image from image
-                                $jpg = \Image::make($image)->encode('jpg');
+                                if (strpos($filename, '.png') !== false) {
+                                    $filename = str_replace(".png", "", $filename);
+                                }
+                                if (strpos($filename, '.jpg') !== false) {
+                                    $filename = str_replace(".jpg", "", $filename);
+                                }
+                                if (strpos($filename, '.JPG') !== false) {
+                                    $filename = str_replace(".JPG", "", $filename);
+                                }
                             } catch (\Exception $e) {
-                                // if images are null
-                                $jpg = null;
                             }
-                            if($jpg != null){
-                                $filename = substr($image, strrpos($image, '/'));
-                                $filename = str_replace("/","",$filename);
-                                try {
-                                    if (strpos($filename, '.png') !== false) {
-                                        $filename = str_replace(".png","",$filename);
-                                    }
-                                    if (strpos($filename, '.jpg') !== false) {
-                                        $filename = str_replace(".jpg","",$filename);
-                                    } 
-                                    if (strpos($filename, '.JPG') !== false) {
-                                        $filename = str_replace(".JPG","",$filename);
-                                    } 
-                                } catch (\Exception $e) {}
-                                //save image to media
-                                $media = MediaUploader::fromString($jpg)->useFilename($filename)->upload();
-                                $product->attachMedia($media, config('constants.media_tags'));
-                                $countImageUpdated++; 
+                            //save image to media
+                            $media = MediaUploader::fromString($jpg)->useFilename($filename)->upload();
+                            $product->attachMedia($media, config('constants.media_tags'));
+                            $countImageUpdated++;
 
                         }
                     }
-                    if($countImageUpdated != 0){
-                         $product->status_id = StatusHelper::$AI;
-                         $product->save();
+                    if ($countImageUpdated != 0) {
+                        $product->status_id = StatusHelper::$AI;
+                        $product->save();
                         // Call status update handler
-                         StatusHelper::updateStatus($product, StatusHelper::$AI);
-                         dump('images saved');  
+                        StatusHelper::updateStatus($product, StatusHelper::$AI);
+                        dump('images saved for product ID ' . $product->id);
                     }
                 }
             }
