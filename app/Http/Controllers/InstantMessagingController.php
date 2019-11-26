5c61cc4c5042ed445c175b882f8230fb4c31b8a5
B <b@appsbyb.com>
2019-11-26 17:51:28 +0100

    Added test data for whatsapp number

--- app/Http/Controllers/InstantMessagingController.php
+++ app/Http/Controllers/InstantMessagingController.php
@@ -66,7 +66,7 @@ class InstantMessagingController extends Controller
         if ($queue == null && $numberFrom != '971504752911') {
             $message = ['error' => 'The queue is empty'];
             return json_encode($message, 400);
-        } elseif ($queue == null && $numberFrom != '971504752911') {
+        } elseif ($queue == null && $numberFrom == '971504752911') {
             $queue = new stdClass();
             $queue->id = rand(1000000, 9999999);
             $queue->number_to = '31629987287';
