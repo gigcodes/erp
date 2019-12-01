4a11d4639696f9dd168d5c4cdebcecc2018bccdd
“Satyam <satyam.luvit@gmail.com>
2019-12-02 00:50:39 +0530

    Filter Customer Enable

--- app/Http/Controllers/Marketing/BroadcastController.php
+++ app/Http/Controllers/Marketing/BroadcastController.php
@@ -382,26 +382,47 @@ class BroadcastController extends Controller
 
     public function saveGlobalValues(Request $request)
     {
+
+        $numbers = WhatsappConfig::where('is_customer_support',0)->get();
+        foreach ($numbers as $number) {
+         if($request->frequency != null){
+           $number->frequency = $request->frequency;
+           $number->update();
+        }
+
+        if($request->send_start != null){
+           $number->send_start = $request->send_start;
+           $number->update();
+        }
+
+        if($request->send_end != null){
+           $number->send_end = $request->send_end;
+           $number->update();
+        }
+        }
+
+        return redirect()->back()->with('message', 'Values Updated Globally');
         
-            $numbers = WhatsappConfig::where('is_customer_support',0)->get();
-            foreach ($numbers as $number) {
-               if($request->frequency != null){
-                 $number->frequency = $request->frequency;
-                 $number->update();
-               }
+    }
 
-               if($request->send_start != null){
-                 $number->send_start = $request->send_start;
-                 $number->update();
-               }
+    public function getCustomerCountEnable($value='')
+    {
+         $query = CustomerMarketingPlatform::query();
 
-               if($request->send_end != null){
-                 $number->send_end = $request->send_end;
-                 $number->update();
-               }
-            }
+         if (request('count_date') != null) {
+            $query->whereDate('created_at', request('count_date'));
+        }
 
-           return redirect()->back()->with('message', 'Values Updated Globally');
-        
+        if (request('custom_date') != null) {
+            $range = explode(' - ', request('custom_date'));
+            $query->whereBetween('created_at', [$range[0], end($range)]);
+        }
+
+        $count = $query->whereNull('remark')->count();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $count,
+        ]);
     }
 }
