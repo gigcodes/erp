105b11b9c9d5300da264fb7160df5777916cb723
Pravin Solanki <solanki7492@gmail.com>
2019-12-05 12:00:04 +0530

    + add calander

--- resources/views/erp-events/index.blade.php
+++ resources/views/erp-events/index.blade.php
@@ -12,64 +12,108 @@
   <div class="col-md-12">
   	<div class="container">
         <div class="cal1"></div>
-        <div class="cal2">
-        <script type="text/template" id="template-calendar">
-            <div class="clndr-controls">
-                <div class="clndr-previous-button">&lsaquo;</div>
-                <div class="month"><%= intervalStart.format('M/DD') + ' &mdash; ' + intervalEnd.format('M/DD') %></div>
-                <div class="clndr-next-button">&rsaquo;</div>
-            </div>
-            <div class="clndr-grid">
-                <div class="days-of-the-week">
-                <% _.each(daysOfTheWeek, function(day) { %>
-                    <div class="header-day"><%= day %></div>
-                <% }); %>
-                    <div class="days">
-                    <% _.each(days, function(day) { %>
-                        <div class="<%= day.classes %>"><%= day.day %></div>
-                    <% }); %>
-                    </div>
-                </div>
-            </div>
-            <div class="clndr-today-button">Today</div>
-        </script>
-        </div>
-        <div class="cal3">
-        <script type="text/template" id="template-calendar-months">
-            <div class="clndr-controls top">
-                <div class="clndr-previous-button">&lsaquo;</div>
-                <div class="clndr-next-button">&rsaquo;</div>
-            </div>
-            <div class="clearfix">
-            <% _.each(months, function(cal) { %>
-                <div class="cal">
-                    <div class="clndr-controls">
-                        <div class="month"><%= cal.month.format('MMMM') %></div>
-                    </div>
-                    <div class="clndr-grid">
-                        <div class="days-of-the-week">
-                        <% _.each(daysOfTheWeek, function(day) { %>
-                            <div class="header-day"><%= day %></div>
-                        <% }); %>
-                            <div class="days">
-                            <% _.each(cal.days, function(day) { %>
-                                <div class="<%= day.classes %>"><%= day.day %></div>
-                            <% }); %>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <% }); %>
-            </div>
-            <div class="clndr-today-button">Today</div>
-        </script>
-        </div>
     </div>
   </div>	
 </div>
 @section('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
-<script src="/js/clndr/clndr.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
+	<script src="/js/clndr/clndr.min.js"></script>
+	<script type="text/javascript">
+		// Call this from the developer console and you can control both instances
+var calendars = {};
+
+$(document).ready( function() {
+    console.info(
+        'Welcome to the CLNDR demo. Click around on the calendars and' +
+        'the console will log different events that fire.');
+
+	    // Assuming you've got the appropriate language files,
+	    // clndr will respect whatever moment's language is set to.
+	    // moment.locale('ru');
+
+	    // Here's some magic to make sure the dates are happening this month.
+	    var thisMonth = moment().format('YYYY-MM');
+	    // Events to load into calendar
+	    var eventArray = [
+	        {
+	            title: 'Multi-Day Event',
+	            endDate: thisMonth + '-14',
+	            startDate: thisMonth + '-10'
+	        }, {
+	            endDate: thisMonth + '-23',
+	            startDate: thisMonth + '-21',
+	            title: 'Another Multi-Day Event'
+	        }, {
+	            date: thisMonth + '-27',
+	            title: 'Single Day Event'
+	        }
+	    ];
+
+	    // The order of the click handlers is predictable. Direct click action
+	    // callbacks come first: click, nextMonth, previousMonth, nextYear,
+	    // previousYear, nextInterval, previousInterval, or today. Then
+	    // onMonthChange (if the month changed), inIntervalChange if the interval
+	    // has changed, and finally onYearChange (if the year changed).
+	    calendars.clndr1 = $('.cal1').clndr({
+	        events: eventArray,
+	        clickEvents: {
+	            click: function (target) {
+	                console.log('Cal-1 clicked: ', target);
+	            },
+	            today: function () {
+	                console.log('Cal-1 today');
+	            },
+	            nextMonth: function () {
+	                console.log('Cal-1 next month');
+	            },
+	            previousMonth: function () {
+	                console.log('Cal-1 previous month');
+	            },
+	            onMonthChange: function () {
+	                console.log('Cal-1 month changed');
+	            },
+	            nextYear: function () {
+	                console.log('Cal-1 next year');
+	            },
+	            previousYear: function () {
+	                console.log('Cal-1 previous year');
+	            },
+	            onYearChange: function () {
+	                console.log('Cal-1 year changed');
+	            },
+	            nextInterval: function () {
+	                console.log('Cal-1 next interval');
+	            },
+	            previousInterval: function () {
+	                console.log('Cal-1 previous interval');
+	            },
+	            onIntervalChange: function () {
+	                console.log('Cal-1 interval changed');
+	            }
+	        },
+	        multiDayEvents: {
+	            singleDay: 'date',
+	            endDate: 'endDate',
+	            startDate: 'startDate'
+	        },
+	        showAdjacentMonths: true,
+	        adjacentDaysChangeMonth: false
+	    });
+
+	    // Bind all clndrs to the left and right arrow keys
+	    $(document).keydown( function(e) {
+	        // Left arrow
+	        if (e.keyCode == 37) {
+	            calendars.clndr1.back();
+	        }
+
+	        // Right arrow
+	        if (e.keyCode == 39) {
+	            calendars.clndr1.forward();
+	        }
+	    });
+	});
+	</script>
 @endsection
 @endsection
