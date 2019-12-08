dd05be2d745d666333eca5762dc8fc4a55ae3e54
B <b@appsbyb.com>
2019-12-08 12:46:40 +0100

    Minor bug fix

--- resources/views/google_search_image/details.blade.php
+++ resources/views/google_search_image/details.blade.php
@@ -98,7 +98,7 @@
                     <div class="col-md-4" style="float:left;">
                     <div class="panel panel-primary">
                         <div class="panel-body"><img src="<?php echo $images; ?>" class="img-responsive" style="width:250px; height:250px;" alt="Image"></div>
-                        <div class="panel-footer" <?php if (stristr($pages, '.gucci.')) echo " style='background-color: lightgreen;";?>>
+                        <div class="panel-footer" <?php if (stristr($images, '.gucci.')) echo " style='background-color: lightgreen;";?>>
                             <button data-href="<?php echo $images; ?>" class="btn btn-secondary btn-img-details">
                                 Get Details
                             </button>
@@ -120,7 +120,7 @@
                 <div class="col-md-4" style="float:left">
                 <div class="panel panel-primary">
                     <div class="panel-body"><img src="<?php echo $images; ?>" class="img-responsive" style="width:250px; height:250px;" alt="Image"></div>
-                    <div class="panel-footer" <?php if (stristr($pages, '.gucci.')) echo " style='background-color: lightgreen;";?>>
+                    <div class="panel-footer" <?php if (stristr($images, '.gucci.')) echo " style='background-color: lightgreen;";?>>
                         <button data-href="<?php echo $images; ?>" class="btn btn-secondary btn-img-details">
                             Get Details
                         </button>
