33f79ea2b008693699ec416e03dae25043084fcd
“Satyam <satyam.luvit@gmail.com>
2019-12-10 15:59:10 +0530

    Issues Fixed

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -4410,20 +4410,19 @@ class WhatsAppController extends FindByNumberController
         
         if ($lastQuickSellProduct) {
             $number = str_ireplace('QUICKSELL', '', $product->sku) + 1;
-            dd($number);
         } else {
             $number = date('yz') . sprintf('%02d', 1);
         }
         
         $brand = Brand::where('name', 'LIKE', '%QUICKSELL%')->first();
-       
+        
         
         $product = new Product;
 
         $product->name = 'QUICKSELL';
         $product->sku = 'QuickSell' . $number;
         $product->size = '';
-        $product->brand = '';
+        $product->brand = $brand->id;
         $product->color = '';
         $product->location = '';
         $product->category = '';
@@ -4432,7 +4431,7 @@ class WhatsAppController extends FindByNumberController
         $product->price_special = 0;
         $product->stock = 1;
         $product->quick_product = 1;
-        $product->is_pending = 1;
+        $product->is_pending = 0;
         $product->save();
         preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $imageUrl, $match);
         $imageUrl = $match[0][0];
