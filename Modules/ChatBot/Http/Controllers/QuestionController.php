c56c2bd999e2158d00da78c02a9a7c4e77444382
Pravin Solanki <solanki7492@gmail.com>
2019-12-24 18:21:05 +0530

    + add phases from customer list

--- Modules/ChatBot/Http/Controllers/QuestionController.php
+++ Modules/ChatBot/Http/Controllers/QuestionController.php
@@ -2,14 +2,13 @@
 
 namespace Modules\ChatBot\Http\Controllers;
 
+use App\Library\Watson\Model as WatsonManager;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
 use \App\ChatbotQuestion;
 use \App\ChatbotQuestionExample;
-
-use App\Library\Watson\Model as WatsonManager;
 
 class QuestionController extends Controller
 {
@@ -23,7 +22,7 @@ class QuestionController extends Controller
         $chatQuestions = ChatbotQuestion::leftJoin("chatbot_question_examples as cqe", "cqe.chatbot_question_id", "chatbot_questions.id")
             ->select("chatbot_questions.*", \DB::raw("group_concat(cqe.question) as `questions`"))
             ->groupBy("chatbot_questions.id")
-            ->orderBy("chatbot_questions.id","desc")
+            ->orderBy("chatbot_questions.id", "desc")
             ->paginate(10);
 
         return view('chatbot::question.index', compact('chatQuestions'));
@@ -51,7 +50,7 @@ class QuestionController extends Controller
 
         WatsonManager::pushQuestion($chatbotQuestion->id);
 
-        return response()->json(["code" => 200, "data" => $chatbotQuestion,"redirect" => route("chatbot.question.edit",[$chatbotQuestion->id])]);
+        return response()->json(["code" => 200, "data" => $chatbotQuestion, "redirect" => route("chatbot.question.edit", [$chatbotQuestion->id])]);
     }
 
     public function destroy(Request $request, $id)
@@ -115,6 +114,55 @@ class QuestionController extends Controller
             WatsonManager::pushQuestion($id);
         }
         return redirect()->back();
+    }
+
+    public function saveAjax(Request $request)
+    {
+        $groupId  = $request->get("group_id" , 0);
+        $name     = $request->get("name", "");
+        $question = $request->get("question");
+
+        if (!empty($groupId) && $groupId > 0) {
+            $q = ChatbotQuestionExample::updateOrCreate(
+                ["chatbot_question_id" => $groupId, "question" => $question],
+                ["chatbot_question_id" => $groupId, "question" => $question]
+            );
+            WatsonManager::pushQuestion($groupId);
+        } else if (!empty($name)) {
+
+            $chQuestion = ChatbotQuestion::create([
+                "value" => str_replace(" ", "_", preg_replace('/\s+/', ' ', $name))
+            ]);
+
+            $groupId = $chQuestion->id;
+
+            ChatbotQuestionExample::create(
+                ["chatbot_question_id" => $chQuestion->id, "question" => preg_replace("/\s+/", " ", $question)]
+            );
+        }
+
+        if ($groupId > 0) {
+            WatsonManager::pushQuestion($groupId);
+        }
+
+        return response()->json(["code" => 200]);
+
+    }
+
+    public function search(Request $request)
+    {
+        $keyword = request("term","");
+        $allquestion = ChatbotQuestion::where("value","like","%".$keyword."%")->limit(10)->get();
+
+        $allquestionList = [];
+        if(!$allquestion->isEmpty()) {
+            foreach($allquestion as $all) {
+                $allquestionList[] = ["id" => $all->id , "text" => $all->value]; 
+            }
+        }
+
+        return response()->json(["incomplete_results" => false, "items"=> $allquestionList, "total_count" => count($allquestionList)]);
+
     }
 
 }
--- Modules/ChatBot/Routes/web.php
+++ Modules/ChatBot/Routes/web.php
@@ -31,6 +31,8 @@ Route::prefix('chatbot')->middleware('auth')->group(function () {
     Route::prefix('question')->group(function () {
         Route::get('/', 'QuestionController@index')->name("chatbot.question.list");
         Route::post('/', 'QuestionController@save')->name("chatbot.question.save");
+        Route::post('/submit', 'QuestionController@saveAjax')->name("chatbot.question.saveAjax");
+        Route::get('/search', 'QuestionController@search')->name("chatbot.question.search");
         Route::prefix('{id}')->group(function () {
             Route::get('/edit', 'QuestionController@edit')->name("chatbot.question.edit");
             Route::post('/edit', 'QuestionController@update')->name("chatbot.question.update");
