bd10a062632d47e30c41f508459d47a5bc05909d
B <b@appsbyb.com>
2019-12-24 23:00:21 +0400

    Updated migration

--- database/migrations/2019_12_24_131719_alter_products_add_column_price_eur_discounted.php
+++ database/migrations/2019_12_24_131719_alter_products_add_column_price_eur_discounted.php
@@ -13,20 +13,17 @@ class AlterProductsAddColumnPriceEurDiscounted extends Migration
      */
     public function up()
     {
-        Schema::table('products', function(Blueprint $table)
-        {
+        Schema::table('products', function (Blueprint $table) {
             $table->double('price_eur_discounted')->after('price_eur_special')->default(0);
             $table->double('price_inr_discounted')->after('price_special')->default(0);
         });
 
-        Schema::table('products', function(Blueprint $table)
-        {
-            $table->double('price_special')->default(0)->change();
+        Schema::table('products', function (Blueprint $table) {
+            $table->double('price_special', 8, 2)->default(0)->change();
 
         });
 
-        Schema::table('products', function(Blueprint $table)
-        {
+        Schema::table('products', function (Blueprint $table) {
             $table->renameColumn('price_special', 'price_inr_special');
         });
     }
