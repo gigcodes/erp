3b341c9ae417426fe434f6eb1b821861ab1293f2
B <b@appsbyb.com>
2019-12-24 23:05:10 +0400

    Updated migration

--- database/migrations/2019_12_24_131719_alter_products_add_column_price_eur_discounted.php
+++ database/migrations/2019_12_24_131719_alter_products_add_column_price_eur_discounted.php
@@ -13,18 +13,14 @@ class AlterProductsAddColumnPriceEurDiscounted extends Migration
      */
     public function up()
     {
-//        Schema::table('products', function (Blueprint $table) {
-//            $table->double('price_eur_discounted')->after('price_eur_special')->default(0);
-//            $table->double('price_inr_discounted')->after('price_special')->default(0);
-//        });
-
         Schema::table('products', function (Blueprint $table) {
-            $table->float('price_special', 8, 2)->default(0.00)->change();
-
+            $table->double('price_eur_discounted')->after('price_eur_special')->default(0);
+            $table->double('price_inr_discounted')->after('price_special')->default(0);
         });
 
         Schema::table('products', function (Blueprint $table) {
-            $table->renameColumn('price_special', 'price_inr_special');
+            $table->float('price_special', 8, 2)->default(0)->change();
+
         });
     }
 
@@ -38,10 +34,6 @@ class AlterProductsAddColumnPriceEurDiscounted extends Migration
         Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('price_eur_discounted');
             $table->dropColumn('price_inr_discounted');
-        });
-
-        Schema::table('products', function (Blueprint $table) {
-            $table->renameColumn('price_inr_special', 'price_special');
         });
     }
 }
