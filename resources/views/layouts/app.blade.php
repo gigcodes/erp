b4989d4e9cd215153226576e3e8766f7760fe30a
“Satyam <satyam.luvit@gmail.com>
2020-01-07 10:55:49 +0530

    Merge comment removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -960,9 +960,6 @@
                                           </li>
                                       </ul>
                                 </li>
-<<<<<<< HEAD
-                                
-=======
                                 <li class="nav-item dropdown dropdown-submenu">
                                     <a id="navbarDropdown" class="" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Chatbot<span class="caret"></span></a>
                                     <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
