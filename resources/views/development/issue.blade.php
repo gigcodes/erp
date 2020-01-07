c5f08eb9c68c40af760f02398372ada4bcd90306
Pravin Solanki <solanki7492@gmail.com>
2020-01-07 18:07:26 +0530

    fix issue page design

--- resources/views/development/issue.blade.php
+++ resources/views/development/issue.blade.php
@@ -149,46 +149,52 @@
         </div>
     </div>
     <div class="row">
-        <div class="collapse" id="plannedFilterCount">
-            <div class="card card-body">
-              <?php if(!empty($countPlanned)) { ?>
-                <div class="row col-md-12">
-                    <?php foreach($countPlanned as $listFilter) { ?>
-                      <div class="card">
-                          <div class="card-header">
-                            <?php echo $listFilter["name"]; ?>
-                          </div>
-                          <div class="card-body">
-                              <?php echo $listFilter["count"]; ?>
-                          </div>
-                      </div>
-                  <?php } ?>
+        <div class="col-md-12">
+            <div class="collapse" id="plannedFilterCount">
+                <div class="card card-body">
+                  <?php if(!empty($countPlanned)) { ?>
+                    <div class="row col-md-12">
+                        <?php foreach($countPlanned as $listFilter) { ?>
+                          <div class="col-md-2">
+                                <div class="card">
+                                  <div class="card-header">
+                                    <?php echo $listFilter["name"]; ?>
+                                  </div>
+                                  <div class="card-body">
+                                      <?php echo $listFilter["count"]; ?>
+                                  </div>
+                              </div>
+                           </div> 
+                      <?php } ?>
+                    </div>
+                  <?php } else  { 
+                    echo "Sorry , No data available";
+                  } ?>
                 </div>
-              <?php } else  { 
-                echo "Sorry , No data available";
-              } ?>
             </div>
-        </div>
-        <div class="collapse" id="inProgressFilterCount">
-            <div class="card card-body">
-              <?php if(!empty($countInProgress)) { ?>
-                <div class="row col-md-12">
-                    <?php foreach($countInProgress as $listFilter) { ?>
-                      <div class="card">
-                          <div class="card-header">
-                            <?php echo $listFilter["name"]; ?>
-                          </div>
-                          <div class="card-body">
-                              <?php echo $listFilter["count"]; ?>
-                          </div>
-                      </div>
-                  <?php } ?>
+            <div class="collapse" id="inProgressFilterCount">
+                <div class="card card-body">
+                  <?php if(!empty($countInProgress)) { ?>
+                    <div class="row col-md-12">
+                        <?php foreach($countInProgress as $listFilter) { ?>
+                            <div class="col-md-2">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <?php echo $listFilter["name"]; ?>
+                                    </div>
+                                    <div class="card-body">
+                                        <?php echo $listFilter["count"]; ?>
+                                    </div>
+                                </div>
+                            </div>
+                      <?php } ?>
+                    </div>
+                  <?php } else  { 
+                    echo "Sorry , No data available";
+                  } ?>
                 </div>
-              <?php } else  { 
-                echo "Sorry , No data available";
-              } ?>
             </div>
-        </div>
+        </div>    
     </div>
     <?php
     $query = http_build_query(Request::except('page'));
