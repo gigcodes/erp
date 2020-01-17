8cc6c09c649a935e38521412b6068c482ab7238b
Pravin Solanki <solanki7492@gmail.com>
2020-01-17 16:54:04 +0530

    - clean code for development task module

---
+++ resources/views/development/partials/developer-row-view.blade.php
@@ -0,0 +1,95 @@
+<tr>
+    <td><a href="{{ url("development/task-detail/$issue->id") }}">{{ $issue->developerModule ? $issue->developerModule->name : 'Not Specified' }}</a></td>
+    <td>
+        {{ $issue->task }}
+        @if ($issue->getMedia(config('constants.media_tags'))->first())
+            <br>
+            @foreach ($issue->getMedia(config('constants.media_tags')) as $image)
+                <a href="{{ $image->getUrl() }}" target="_blank" class="d-inline-block">
+                    <img src="{{ $image->getUrl() }}" class="img-responsive" style="width: 50px" alt="">
+                </a>
+            @endforeach
+        @endif
+        <div>
+            <div class="panel-group">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" href="#collapse_{{$issue->id}}">Messages({{count($issue->messages)}})</a>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </td>
+    <td>{{ $issue->subject }}</td>
+    <td>{!! ['N/A', '<strong class="text-danger">Critical</strong>', 'Urgent', 'Normal'][$issue->priority] ?? 'N/A' !!}</td>
+    <td>{{ $issue->task }}</td>
+    <td>{{ \Carbon\Carbon::parse($issue->created_at)->format('H:i d-m') }}</td>
+    <td>&nbsp;</td>
+    <td>
+        @if($issue->assignedUser)
+            {{ $issue->assignedUser->name }}
+        @else
+            Unassigned
+        @endif
+    </td>
+    <td>
+        @if($issue->responsibleUser)
+            {{ $issue->responsibleUser->name  }}
+        @else
+            N/A
+        @endif
+    </td>
+    <td>
+        @if($issue->is_resolved)
+            <strong>Resolved</strong>
+        @else
+            <select name="task_status" id="task_status" class="form-control change-task-status" data-id="{{$issue->id}}">
+                <option value="">Please Select</option>
+                <option value="Planned" {{ (!empty($issue->status) && $issue->status ==  'Planned' ? 'selected' : '') }}>Planned</option>
+                <option value="In Progress" {{ (!empty($issue->status) && $issue->status  ==  'In Progress' ? 'selected' : '') }}>In Progress</option>
+                <option value="Done" {{ (!empty($issue->status) && $issue->status ==   'Done' ? 'selected' : '') }}>Done</option>
+            </select>
+        @endif
+    </td>
+    <td>
+        @if($issue->masterUser)
+            {{ $issue->masterUser->name  }}
+        @else
+            N/A
+        @endif
+    </td>
+    <td>
+        @if($issue->cost > 0)
+            {{ $issue->cost }}
+        @else
+            <input type="text" name="cost" id="cost_{{$issue->id}}" placeholder="Amount..." class="form-control save-cost" data-id="{{$issue->id}}">
+        @endif
+    </td>
+    <td>
+        <?php echo $issue->language; ?>
+    </td>
+    </tr>
+    <tr>
+    <td>&nbsp;</td>
+    <td colspan="13">
+        <div id="collapse_{{$issue->id}}" class="panel-collapse collapse">
+            <div class="panel-body">
+                <div class="messageList" id="message_list_{{$issue->id}}">
+                    @foreach($issue->messages as $message)
+                        <p>
+                            <strong>{{ date('d-M-Y H:i:s', strtotime($message->created_at)) }}</strong>
+                        </p>
+                        {!! nl2br($message->message) !!}
+                        <hr/>
+                    @endforeach
+                </div>
+            </div>
+            <div class="panel-footer">
+                <textarea class="form-control send-message-textbox" data-id="{{$issue->id}}" id="send_message_{{$issue->id}}" name="send_message_{{$issue->id}}"></textarea>
+                <button type="submit" id="submit_message" class="btn btn-secondary ml-3 send-message" data-id="{{$issue->id}}" style="float: right;margin-top: 2%;">Submit</button>
+            </div>
+        </div>
+    </td>
+    </tr>
---
+++ resources/views/development/partials/task-issue-search.blade.php
@@ -0,0 +1,72 @@
+<form action="{{ url("development/list/$title") }}" method="get">
+    <div class="row">
+        <div class="col-md-1">
+            <select class="form-control" name="assigned_to" id="assigned_to">
+                <option value="">Assigned To</option>
+                @foreach($users as $id=>$user)
+                    <option {{$request->get('assigned_to')==$id ? 'selected' : ''}} value="{{$id}}">{{ $user }}</option>
+                @endforeach
+            </select>
+        </div>
+        {{--
+        <div class="col-md-1">
+            <select class="form-control" name="responsible_user" id="responsible_user">
+                <option value="">Responsible User...</option>
+                @foreach($users as $id=>$user)
+                    <option {{$request->get('responsible_user')==$id ? 'selected' : ''}} value="{{$id}}">{{ $user }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-1">
+            <select class="form-control" name="corrected_by" id="corrected_by">
+                <option value="">Correction by</option>
+                @foreach($users as $id=>$user)
+                    <option {{$request->get('corrected_by')==$id ? 'selected' : ''}} value="{{$id}}">{{ $user }}</option>
+                @endforeach
+            </select>
+        </div>
+        --}}
+        <div class="col-md-1">
+            <select name="module" id="module_id" class="form-control">
+                <option value="">Module</option>
+                @foreach($modules as $module)
+                    <option {{ $request->get('module') == $module->id ? 'selected' : '' }} value="{{ $module->id }}">{{ $module->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-2">
+            <input type="text" name="subject" id="subject_query" placeholder="Issue Id / Subject" class="form-control" value="{{ (!empty(app('request')->input('subject'))  ? app('request')->input('subject') : '') }}">
+        </div>
+        <div class="col-md-2">
+            <input type="text" name="language" id="language_query" placeholder="Language" class="form-control" value="{{ (!empty(app('request')->input('language'))  ? app('request')->input('language') : '') }}">
+        </div>
+        <div class="col-md-2">
+            <?php echo Form::select("task_status[]",$statusList,request()->get('task_status', []),["class" => "form-control multiselect","multiple" => true]); ?>
+        </div>
+        <div class="col-md-1">
+            <select name="order" id="order_query" class="form-control">
+                <option {{$request->get('order')== "" ? 'selected' : ''}} value="create">Order by date descending</option>
+                <option {{$request->get('order')== "priority" ? 'selected' : ''}} value="">Order by priority</option>
+                <option {{$request->get('order')== "create_asc" ? 'selected' : ''}} value="create">Order by date</option>
+                <option {{$request->get('order')== "communication_desc" ? 'selected' : ''}} value="communication_desc">Order by Communication</option>
+            </select>
+        </div>
+        <div class="col-md-1">
+            {{--
+            @if ( isset($_REQUEST['show_resolved']) && $_REQUEST['show_resolved'] == 1 )
+                <input type="checkbox" name="show_resolved" value="1" checked> incl.resolved
+            @else
+                <input type="checkbox" name="show_resolved" value="1"> incl.resolved
+            @endif
+             --}}
+            <button class="btn btn-image">
+                <img src="{{ asset('images/search.png') }}" alt="Search">
+            </button>
+        </div>
+       
+        <div class="col-md-1">
+            <a class="btn btn-secondary d-inline priority_model_btn">Priority</a>
+            <!-- <a class="btn btn-secondary d-inline priority_model_head_dev_btn">Priority Bjorn</a> -->
+        </div>
+    </div>
+</form>
