a24d68e7c3c43612ac3643b272ce2aad58c5066c
B <b@appsbyb.com>
2020-02-29 01:38:32 +0400

    Added confirmaiton

--- public/Aupo3Haxo0Jam2Tiqam0Johus0Fu/Eak4Ue4Zu8Hem8.php
+++ public/Aupo3Haxo0Jam2Tiqam0Johus0Fu/Eak4Ue4Zu8Hem8.php
@@ -1,10 +1,17 @@
 <?php
-$cmd = 'sh ' . __dir__ . '/../../server_scripts/restart_db01.sh 2>&1';
 
-$allOutput = array();
-$allOutput[] = $cmd;
-$result = exec($cmd, $allOutput);
+if ( isset($_GET['restart']) && $_GET['restart'] == 'yes' ) {
+    $cmd = 'sh ' . __dir__ . '/../../server_scripts/restart_db01.sh 2>&1';
 
-echo "<pre>\n";
-var_dump($allOutput);
-echo "</pre>\n";
+    $allOutput = array();
+    $allOutput[] = $cmd;
+    $result = exec($cmd, $allOutput);
+
+    echo "<pre>\n";
+    var_dump($allOutput);
+    echo "</pre>\n";
+}
+
+?>
+
+<a href="./Eak4Ue4Zu8Hem8.php?restart=yes">Restart Database</a>
