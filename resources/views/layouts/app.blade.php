68ea8fbb84b33c7c5b5c41176acfd93c893b3c37
Pravin Solanki <solanki7492@gmail.com>
2020-05-30 12:46:26 +0530

    Fix issue with layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1610,6 +1610,8 @@
             <div class="col-md-11">
                 @yield('large_content')
             </div>
+        @elseif (trim($__env->yieldContent('core_content')))
+            @yield('core_content')
         @else
             <main class="container">
                 <!-- Showing fb like page div to all pages  -->
