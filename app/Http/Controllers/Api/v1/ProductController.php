4897d109f9b2ef894b046af314516ce037d1db91
Pravin Solanki <solanki7492@gmail.com>
2020-06-27 14:43:16 +0530

    Fix return params

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -18,9 +18,20 @@ class ProductController extends Controller
 
         if ($product) {
             $price         = $product->getPrice($request->store_website, $request->country_code);
+            $price["sub_total"] = $price["total"];
             $price["duty"] = $product->getDuty($request->store_website);
+            $extra = ($price["total"] * $price["duty"]) / 100;
+            $price["total"] = $price["total"] + $extra;
 
-            return response()->json(["code" => 200, "message" => "Success", "data" => $price]);
+            $return = [
+                "original_price" => $price["original_price"],
+                "promotion" => $price["promotion"],
+                "sub_total" => $price["sub_total"],
+                "duty" => $price["duty"],
+                "total" => number_format($price["total"],2,".",""),
+            ];
+
+            return response()->json(["code" => 200, "message" => "Success", "data" => $return]);
         }
 
         return response()->json(["code" => 500, "message" => "Product not found in records", "data" => []]);
