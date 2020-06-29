5d69f3f4be56a52fef0508f295790ab1a8f31eb8
Pravin Solanki <solanki7492@gmail.com>
2020-06-29 16:41:04 +0530

    added function for customer service

--- app/User.php
+++ app/User.php
@@ -158,6 +158,19 @@ class User extends Authenticatable
         return false;
     }
 
+    public function isInCustomerService()
+    {
+        $roles = $this->roles->pluck('name')->toArray();
+
+        if (in_array('Customer Service', $roles)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
     /**
      * The attributes helps to check if User has Permission Using Route To Check Page.
      *
