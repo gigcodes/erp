7143ddf41ec99382f7a4a1af7d4d645516d472e5
Pravin Solanki <solanki7492@gmail.com>
2020-06-29 11:28:18 +0530

    Added commit for the erp user management

--- app/ErpPriority.php
+++ app/ErpPriority.php
@@ -17,4 +17,13 @@ class ErpPriority extends Model
         'model_id', 'model_type', 'user_id'
     );
 
+    public function detail()
+    {
+        if($this->model_type == \App\Task::class) {
+            return (new $this->model_type)->where("id",$this->model_id)->select(["*","task_subject as task",\DB::raw("concat('Task','#',id) as task_id")])->first();
+        }else{
+            return (new $this->model_type)->where("id",$this->model_id)->select(["*",\DB::raw("concat('DEVTASK','#',id) as task_id")])->first();
+        }
+    }
+
 }
