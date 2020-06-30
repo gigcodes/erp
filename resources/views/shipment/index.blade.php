21ec266d61f681a3412bbdb204bd84bcb2c11776
Trupti Bhuva <trupti@webcluesinfotech.com>
2020-06-30 19:56:21 +0530

    DEVTASK-2513-DHL TRACKING

---
+++ resources/views/shipment/index.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('title', 'Shipment List')
+
+@section('styles')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+@endsection
+
+@section('content')
+<div class="infinite-scroll">
+	<div class="table-responsive mt-3">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>AWB number</th>
+            <th>Customer name</th>
+            <th>Shipped Date</th>
+            <th>Current Status</th>
+            <th> Weight of Shipment</th>
+            <th>Dimensions</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+            @forelse ($waybills_array as $key => $item)
+                <tr>
+                    <td>{{ $item->awb }}</td>
+                    <td>{{ $item->order->customer->name }}</td>
+                    <td>{{ ($item->created_at) ? date('d-m-Y', strtotime($item->created_at)) : '' }}</td>
+                    <td>{{ $item->order->order_status }}</td>
+                    <td>{{ $item->actual_weight }}</td>
+                    <td>{{ $item->dimension }}</td>
+                    <td>
+                        <button type="button" class="btn btn-image" data-toggle="modal" data-target="#send_email_modal" title="Send Email"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+                        <a class="btn" href="javascript:void(0);" id="view_mail_btn" title="View communication sent" data-order-id="{{ $item->order_id }}">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                        </a>
+                    </td>
+                </tr>
+            @empty
+                <tr></tr>
+            @endforelse
+        </tbody>
+      </table>
+
+	{!! $waybills_array->appends(Request::except('page'))->links() !!}
+	</div>
+</div>
+
+@include('shipment.partial.modal')
+
+@endsection
+
+@section('scripts')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.3.7/jquery.jscroll.min.js"></script>
+<script type="text/javascript">
+$(".to-email, .cc-email, .bcc-email").select2({
+    tags: true,
+    tokenSeparators: [',', ' '],
+    placeholder: {
+        id: '-1', // the value of the option
+        text: 'Type Email'
+    },
+    allowClear: true,
+    "language": {
+       "noResults": function(){
+           return "Type email";
+       }
+    },
+    escapeMarkup: function (markup) {
+        return markup;
+    }
+});
+
+$(document).on('click', '#view_mail_btn', function() {
+    var orderId = $(this).data('order-id');
+    $.ajax({
+        url: "{{ route('shipment/view/sent/email') }}",
+        type: 'GET',
+        data: {'order_id': orderId},
+        success: function(data) {
+            $("#view_email_body").html(data);
+            $('#view_sent_email_modal').modal('show');
+        }
+    });
+});
+</script>
+@endsection
---
+++ resources/views/shipment/partial/modal.blade.php
@@ -0,0 +1,76 @@
+<div id="send_email_modal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+  
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Send an Email</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+  
+        <form action="{{ route('shipment/send/email') }}" method="POST" enctype="multipart/form-data">
+          @csrf
+			<input type="hidden" name="order_id" value="2019" >
+          <div class="modal-body">
+            <div class="form-group">
+				<label>To</label>
+				<select class="form-control to-email" name="to[]" multiple="multiple" required style="width: 100%;">
+				</select>
+            </div>
+
+            <div id="cc-label" class="form-group">
+              <strong class="mr-3">Cc</strong>
+				<select class="form-control cc-email" name="cc[]" multiple style="width: 100%;">
+				</select>
+            </div>
+    
+            <div id="bcc-label" class="form-group">
+              	<strong class="mr-3">Bcc</strong>
+              	<select class="form-control bcc-email" name="bcc[]" multiple style="width: 100%;">
+				</select>
+            </div>
+    
+            <div class="form-group">
+              <strong>Subject</strong>
+              <input type="text" class="form-control" name="subject" value="{{ old('subject') }}" required>
+            </div>
+  
+            <div class="form-group">
+              <strong>Message</strong>
+              <textarea name="message" class="form-control" rows="8" cols="80" required>{{ old('message') }}</textarea>
+            </div>
+  
+            <div class="form-group">
+              <strong>Files</strong>
+              <input type="file" name="file[]" value="" multiple>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-secondary">Send</button>
+          </div>
+        </form>
+      </div>
+  
+    </div>
+</div>
+
+
+
+<div id="view_sent_email_modal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+  
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Communication History</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+    
+            <div class="modal-body" id="view_email_body">
+
+			</div>
+		</div>
+	</div>
+</div>
+  
