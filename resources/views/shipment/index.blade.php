86117851f48c61e3d668a3fad30d6daf4306c404
Trupti Bhuva <trupti@webcluesinfotech.com>
2020-07-02 17:08:30 +0530

    DEVTASK-2513 bug fixing

--- resources/views/shipment/index.blade.php
+++ resources/views/shipment/index.blade.php
@@ -31,11 +31,11 @@
             @forelse ($waybills_array as $key => $item)
                 <tr>
                     <td>{{ $item->awb }}</td>
-                    <td>{{ $item->order->customer->name }}</td>
+                    <td>{{ @$item->order->customer->name ?? ''}}</td>
                     <td>{{ ($item->created_at) ? date('d-m-Y', strtotime($item->created_at)) : '' }}</td>
-                    <td>{{ $item->order->order_status }}</td>
-                    <td>{{ $item->actual_weight }}</td>
-                    <td>{{ $item->dimension }}</td>
+                    <td>{{ @$item->order->order_status }}</td>
+                    <td>{{ @$item->actual_weight }}</td>
+                    <td>{{ @$item->dimension }}</td>
                     <td>
                         <button type="button" class="btn btn-image" id="send_email_btn" data-order-id="{{ $item->order_id }}" title="Send Email"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
                         <a class="btn" href="javascript:void(0);" id="view_mail_btn" title="View communication sent" data-order-id="{{ $item->order_id }}">
