dbec45cf4e8ad40d31866ab0a18e12ac95f01a3c
Refixd <sudu>
2020-07-02 14:32:56 +0530

    All sent and incomming emails

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1400,6 +1400,9 @@
                                         <a class="dropdown-item" href="{{ route('shipment.index') }}">Shipment</a>
                                     </ul>
                                 </li>
+                                <li class="nav-item dropdown">
+                                <a class="dropdown-item" href="{{ route('email.index') }}">Emails</a>
+                                </li>
                             </ul>
                         </li>
                         @endif
