1a1160b87d41fb3c8f98e6ee0fa3b0ffd97b2fb6
Pravin Solanki <solanki7492@gmail.com>
2020-07-03 18:44:26 +0530

    Generate product pricing json

--- app/Console/Commands/GenerateProductPricingJson.php
+++ app/Console/Commands/GenerateProductPricingJson.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Services\Explorer\InstagramExplorer;
+use App\Helpers\StatusHelper;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 
@@ -40,44 +40,42 @@ class GenerateProductPricingJson extends Command
     public function handle()
     {
         //try {
-            $report = \App\CronJobReport::create([
-                'signature'  => $this->signature,
-                'start_time' => Carbon::now(),
-            ]);
+        $report = \App\CronJobReport::create([
+            'signature'  => $this->signature,
+            'start_time' => Carbon::now(),
+        ]);
 
-            $storeWebsite  = \App\StoreWebsite::where("is_published",1)->get();
-            $countryGroups = \App\CountryGroup::all();
-            $dutyCountries = \App\CountryDuty::groupBy('destination')->get()->pluck('destination');
+        $storeWebsite  = \App\StoreWebsite::where("is_published", 1)->get();
+        $countryGroups = \App\CountryGroup::all();
+        $dutyCountries = \App\CountryDuty::groupBy('destination')->get()->pluck('destination');
 
-
-            // start pricing
-            $products = \App\Product::limit(2)->get();
-            $priceReturn = [];
-            if(!$products->isEmpty()) {
-                foreach($products as $product) {
-                    foreach($storeWebsite as $website) {
-                        foreach($countryGroups as $cg) {
-                            $price = $product->getPrice($website->id,$cg->id);
-                            foreach($cg->groupItems as $item) {
-                                $priceReturn[$website->website][$product->sku][$item->country_code]['price'] = $price; 
-                                $dutyPrice = $product->getDuty($item->country_code);
-                                $priceReturn[$website->website][$product->sku][$item->country_code]['price']['duty'] = $dutyPrice;
-                                $priceReturn[$website->website][$product->sku][$item->country_code]['price']['total'] = (float)$price['total'] + $dutyPrice;
-                            }
+        // start pricing
+        $products    = \App\Product::where("status_id", StatusHelper::$finalApproval)->get();
+        $priceReturn = [];
+        if (!$products->isEmpty()) {
+            foreach ($products as $product) {
+                foreach ($storeWebsite as $website) {
+                    foreach ($countryGroups as $cg) {
+                        $price = $product->getPrice($website->id, $cg->id);
+                        foreach ($cg->groupItems as $item) {
+                            $priceReturn[$website->website][$product->sku][$item->country_code]['price']          = $price;
+                            $dutyPrice                                                                            = $product->getDuty($item->country_code);
+                            $priceReturn[$website->website][$product->sku][$item->country_code]['price']['duty']  = $dutyPrice;
+                            $priceReturn[$website->website][$product->sku][$item->country_code]['price']['total'] = (float) $price['total'] + $dutyPrice;
                         }
                     }
                 }
             }
+        }
 
-            if(!\Storage::disk('uploads')->put("pricing-".date("Y-m-d").".json", json_encode($priceReturn))) {
-                return false;
-            }
+        if (!\Storage::disk('uploads')->put("pricing-" . date("Y-m-d") . ".json", json_encode($priceReturn))) {
+            return false;
+        }
 
-
-            $report->update(['end_time' => Carbon::now()]);
+        $report->update(['end_time' => Carbon::now()]);
 
         /*} catch (\Exception $e) {
-            \App\CronJob::insertLastError($this->signature, $e->getMessage());
-        }*/
+    \App\CronJob::insertLastError($this->signature, $e->getMessage());
+    }*/
     }
 }
