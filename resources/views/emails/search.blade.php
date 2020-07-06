a6e154c84954ecb14934e7b4262bb27ea0920e6d
Refixd <sudu>
2020-07-06 14:59:51 +0530

    All sent and incomming emails (Modified)

--- resources/views/emails/search.blade.php
+++ resources/views/emails/search.blade.php
@@ -6,6 +6,7 @@
             <td>{{ $email->to }}</td>
             <td>{{ $email->type }}</td>
             <td>{{$email->subject}}</td>
+            <!-- <td>{{$email->subject}}</td> -->
             <td data-toggle="modal" data-target="#viewMail"  onclick="opnMsg({{$email}})" style="cursor: pointer;">{{ substr($email->message, 0,  50) }} {{strlen($email->message) > 50 ? '...' : '' }}</td>
             <td>
             <a title="Resend" class="btn btn-image resend-email-btn" data-id="{{ $email->id }}" >
