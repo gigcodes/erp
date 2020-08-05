2a727c37bbaeff6350da290cd40ed58e4bbcdfdd
Pravin Solanki <solanki7492@gmail.com>
2020-08-05 14:06:40 +0530

    Fix give image

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -48,7 +48,7 @@ class LandingPageController extends Controller
             }
             $productData['images'] = [];
             if ($landingPageProduct->hasMedia(config('constants.attach_image_tag'))) {
-                foreach ($landingPageProduct->getMedia(config('constants.attach_image_tag')) as $image) {
+                foreach ($landingPageProduct->getMedia() as $image) {
                     array_push($productData['images'], ['url' =>$image->getUrl(),'id'=>$image->id,'product_id'=>$landingPageProduct->id]);
                 }
             }
