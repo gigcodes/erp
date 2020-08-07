26a01c8e985ff65ab729a83e8498beea7f03f25c
Bjorn <bjorn@zwaaneveld.nl>
2020-08-07 13:25:24 +0400

    Added s06 and s07 to deploy script

--- deployment_scripts/scraper_nodejs/deploy_branch.sh
+++ deployment_scripts/scraper_nodejs/deploy_branch.sh
@@ -3,3 +3,5 @@ ssh -i ~/.ssh/id_rsa root@s02.theluxuryunlimited.com "cd ~/scraper_nodejs && git
 ssh -i ~/.ssh/id_rsa root@s03.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
 ssh -i ~/.ssh/id_rsa root@s04.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
 ssh -i ~/.ssh/id_rsa root@s05.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
+ssh -i ~/.ssh/id_rsa root@s06.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
+ssh -i ~/.ssh/id_rsa root@s07.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
