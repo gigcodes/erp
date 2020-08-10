db4987b1e3dffb7a7c49e191e0245b9bfa00060f
Sukhwinder <spaghetti.mycode@gmail.com>
2020-08-11 00:11:25 +0530

    Bug fix for vendors broken page

--- resources/views/partials/modals/remarks.blade.php
+++ resources/views/partials/modals/remarks.blade.php
@@ -21,26 +21,29 @@
                 </tr>
               </thead>
               <tbody id="remark-list">
-                
+
               </tbody>
             </table>
         <?php } else{ ?>
         <div class="list-unstyled" id="remark-list">
 
         </div>
-        <?php } ?>  
+        <?php } ?>
         <form id="add-remark">
           <input type="hidden" name="id" value="">
           <div class="form-group">
             <textarea rows="2" name="remark" class="form-control" placeholder="Start the Remark"></textarea>
           </div>
-          <div class="form-group">
-            <label><input type="checkbox" class="need_to_send" value="1">&nbsp;Need to Send Message ?</label>
-          </div>
-          <div class="form-group">
-            <label><input type="checkbox" class="inlcude_made_by" value="1">&nbsp;Want to include Made By ?</label>
-          </div>
-          <button type="button" class="btn btn-secondary btn-block mt-2" id="{{ (!empty($type) && $type = 'scrap') ? 'scrapAddRemarkbutton' : 'addRemarkButton' }}">Add</button>
+          {{-- We dont need following settings for email page --}}
+          @if (empty($type) || $type != 'email')
+            <div class="form-group">
+              <label><input type="checkbox" class="need_to_send" value="1">&nbsp;Need to Send Message ?</label>
+            </div>
+            <div class="form-group">
+              <label><input type="checkbox" class="inlcude_made_by" value="1">&nbsp;Want to include Made By ?</label>
+            </div>
+          @endif
+          <button type="button" class="btn btn-secondary btn-block mt-2" id="{{ (!empty($type) && $type == 'scrap') ? 'scrapAddRemarkbutton' : 'addRemarkButton' }}">Add</button>
         </form>
       </div>
 
