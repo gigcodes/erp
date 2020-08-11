ccda84a796460a3e3434792864bd1e36d987616f
Refixd <sudu>
2020-08-11 18:25:57 +0530

    Discussion task page design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1157,6 +1157,9 @@
                                     <a class="dropdown-item" href="{{ url('development/list') }}">Tasks</a>
                                 </li>
                                 <li class="nav-item">
+                                    <a class="dropdown-item" href="{{url('task?daily_activity_date=&term=&selected_user=&is_statutory_query=3')}}">Discussion tasks</a>
+                                </li>
+                                <li class="nav-item">
                                     <a class="dropdown-item" href="{{ route('task-types.index') }}">Task Types</a>
                                 </li>
                                 <li class="nav-item">
