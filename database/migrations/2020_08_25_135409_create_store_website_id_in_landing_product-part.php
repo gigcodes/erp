a34d17a3c815fb59e50859194a6c660890747867
Pravin Solanki <solanki7492@gmail.com>
2020-08-28 16:16:11 +0530

    run migration

--- database/migrations/2020_08_25_135409_create_store_website_id_in_landing_product-part.php
+++ database/migrations/2020_08_25_135409_create_store_website_id_in_landing_product-part.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStoreWebsiteIdInLandingProduct extends Migration
+class CreateStoreWebsiteIdInLandingProductPart extends Migration
 {
     /**
      * Run the migrations.
