9810edb3556f6d76f74ec86f72c448f199e477ce
krish@sentientgeeks.com <krish@sentientgeeks.com>
2020-09-11 13:41:27 +0530

    Task 2962 - Seeding

--- database/migrations/2020_09_10_162123_create_return_exchange_statuses_table.php
+++ database/migrations/2020_09_10_162123_create_return_exchange_statuses_table.php
@@ -13,10 +13,24 @@ class CreateReturnExchangeStatusesTable extends Migration
      */
     public function up()
     {
-        Schema::create('return_exchange_statuses', function (Blueprint $table) {
+        Schema::create('return_exchange_statuses', function (Blueprint $table) {		
             $table->increments('id');
+			$table->string('status_name');
             $table->timestamps();
         });
+		
+		$arrStatus = [
+            1 => 'Return request received from customer',
+			2 => 'Return request sent to courier',
+			3 => 'Return pickup',
+			4 => 'Return received in warehouse',
+			5 => 'Return accepted',
+			6 => 'Return rejected',
+        ];
+		       
+        foreach ($arrStatus as $status) {
+            DB::table('return_exchange_statuses')->insert(['status_name' => trim($status)]);
+        }
     }
 
     /**
