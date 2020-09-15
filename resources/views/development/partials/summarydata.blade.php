03122f5fe99496b68dbd0aa9634d90f982448975
vaibhavj1107 <vaibhavj1107@gmail.com>
2020-09-15 16:26:10 +0530

    Summary devtask page design

--- resources/views/development/partials/summarydata.blade.php
+++ resources/views/development/partials/summarydata.blade.php
@@ -1,10 +1,10 @@
 
 
 <tr style="color:grey;">
-    <td>
+    <td style="display:table-cell;">
 
     <a href="{{ url("development/task-detail/$issue->id") }}">{{ $issue->id }}
-            @if($issue->is_resolved==0)	
+            @if($issue->is_resolved==0)	 
                 <input type="checkbox" name="selected_issue[]" value="{{$issue->id}}" {{in_array($issue->id, $priority) ? 'checked' : ''}}>	
             @endif	
         </a>
@@ -18,7 +18,7 @@
         @if($issue->task_type_id == 1) Devtask @elseif($issue->task_type_id == 3) Issue @endif
     </td>
     <td style="vertical-align: middle;">    
-        <select name="module" class="form-control task-module" data-id="{{$issue->id}}">
+        <select name="module" class="form-control task-module" data-id="{{$issue->id}}" style="margin-top:-5%;">
             <option value=''>Select Module..</option>
             @foreach($modules as $module)
 
@@ -31,7 +31,7 @@
         </select>
     </td>
        <td>
-        <label for="" style="font-size: 12px;">Assigned To :</label>
+     <!--   <label for="" style="font-size: 12px;">Assigned To :</label>-->
         <select class="form-control assign-user select2" data-id="{{$issue->id}}" name="assigned_to" id="user_{{$issue->id}}">
             <option value="">Select...</option>
             <?php $assignedId = isset($issue->assignedUser->id) ? $issue->assignedUser->id : 0; ?>
@@ -43,8 +43,10 @@
                 @endif
             @endforeach
         </select>
-        <label for="" style="font-size: 12px;margin-top:10px;">Lead :</label>
-        <select class="form-control assign-master-user select2" data-id="{{$issue->id}}" name="master_user_id" id="user_{{$issue->id}}">
+   <!--     <label for="" style="font-size: 12px;margin-top:10px;">Lead :</label>-->
+    </td>
+    <td>
+    <select class="form-control assign-master-user select2" data-id="{{$issue->id}}" name="master_user_id" id="user_{{$issue->id}}">
             <option value="">Select...</option>
             <?php $masterUser = isset($issue->masterUser->id) ? $issue->masterUser->id : 0; ?>
             @foreach($users as $id=>$name)
@@ -58,24 +60,29 @@
     </td>
     <td class="expand-row">
     <!-- class="expand-row" -->
-    <span style="word-break: break-all;">{{  \Illuminate\Support\Str::limit($issue->message, 150, $end='...') }}</span>
-    <input type="text" class="form-control send-message-textbox" data-id="{{$issue->id}}" id="send_message_{{$issue->id}}" name="send_message_{{$issue->id}}" style="margin-bottom:5px"/>
-    <?php echo Form::select("send_message_".$issue->id,[
-                        "to_developer" => "Send To Developer",
-                        "to_master" => "Send To Master Developer"
-                    ],null,["class" => "form-control send-message-number", "style" => "width:85% !important;display: inline;"]); ?>
-    <button style="display: inline-block;width: 10%" class="btn btn-sm btn-image send-message-open" type="submit" id="submit_message"  data-id="{{$issue->id}}" ><img src="/images/filled-sent.png"/></button>
-
   
-        <button type="button" class="btn btn-xs btn-image load-communication-modal" data-object='developer_task' data-id="{{ $issue->id }}" style="margin-top: 2%;" title="Load messages"><img src="/images/chat.png" alt=""></button>
+   
+    <input type="text" class="form-control send-message-textbox" data-id="{{$issue->id}}" id="send_message_{{$issue->id}}" name="send_message_{{$issue->id}}" style="margin-bottom:5px;width:40%;display:inline;"/>
+   
+    <button style="display: inline-block;width: 10%" class="btn btn-sm btn-image send-message-open" type="submit" id="submit_message"  data-id="{{$issue->id}}" ><img src="/images/filled-sent.png"/></button>
+    <button type="button" class="btn btn-xs btn-image load-communication-modal" data-object='developer_task' data-id="{{ $issue->id }}" style="mmargin-top: -0%;margin-left: -2%;" title="Load messages"><img src="/images/chat.png" alt=""></button>
+    <span class="{{ ($issue->message && $issue->message_status == 0) || $issue->message_is_reminder == 1 || ($issue->sent_to_user_id == Auth::id() && $issue->message_status == 0) ? '' : '' }}" style="word-break: break-all;">{{  \Illuminate\Support\Str::limit($issue->message, 150, $end='...') }}</span>
     <br>
         <div class="td-full-container hidden">
             <button class="btn btn-secondary btn-xs" onclick="sendImage({{ $issue->id }} )">Send Attachment</button>
             <button class="btn btn-secondary btn-xs" onclick="sendUploadImage({{$issue->id}} )">Send Images</button>
             <input id="file-input{{ $issue->id }}" type="file" name="files" style="display: none;" multiple/>
-         </div>
+         </div> 
     </td>
-    
+    <td>
+    <?php echo Form::select("send_message_".$issue->id,[
+                        "to_developer" => "Send To Developer",
+                        "to_master" => "Send To Master Developer",
+                        "to_team_lead" => "Send To Team Lead",
+                        "to_tester" => "Send To Tester"
+                    ],null,["class" => "form-control send-message-number", "style" => "width:100% !important;display: inline;"]); ?>
+
+    </td>
   
     
     <td>
