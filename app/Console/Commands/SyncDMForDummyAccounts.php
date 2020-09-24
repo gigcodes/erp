ed203b1654a6960cc0a51da092779a7da8f2dfff
Pravin Solanki <solanki7492@gmail.com>
2020-09-24 14:33:56 +0530

    Fix dummy account

--- app/Console/Commands/SyncDMForDummyAccounts.php
+++ app/Console/Commands/SyncDMForDummyAccounts.php
@@ -13,8 +13,7 @@ use InstagramAPI\Instagram;
 
 Instagram::$allowDangerousWebUsageAtMyOwnRisk = true;
 
-class
-SyncDMForDummyAccounts extends Command
+class SyncDMForDummyAccounts extends Command
 {
     /**
      * The name and signature of the console command.
