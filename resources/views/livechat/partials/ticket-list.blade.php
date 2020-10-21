d7d1d4a7840cda124f9c77be8b5b9239573fdfd4
Freshcodes Technology <you@example.com>
2020-10-21 16:11:51 +0530

    More details of subject  & message set on popup

--- resources/views/livechat/partials/ticket-list.blade.php
+++ resources/views/livechat/partials/ticket-list.blade.php
@@ -4,6 +4,7 @@
     $base_url = URL::to('/')
 @endphp
 @php($statusList = \App\TicketStatuses::all()->pluck('name','id'))
+
 @foreach ($data as $key => $ticket)
 <tr>
     <td>{{ ++$i }}</td>
@@ -11,13 +12,27 @@
     <td>{{ $ticket->source_of_ticket }}</td>
     <td style="width:150px;">{{ $ticket->name}}</td>
     <td>{{ $ticket->email }}</td>
-    <td>{{ substr($ticket->subject,0,6) }}</td>
-    <td>{{ substr($ticket->message,0,6) }}</td>
+    <td><a href="javascript:void(0)" class="subject1_<?=$ticket->ticket_id?>">{{ substr($ticket->subject,0,6) }}</a></td>
+    <td><a href="javascript:void(0)" class="message1_<?=$ticket->ticket_id?>">{{ substr($ticket->message,0,6) }}</a></td>
     <td>{{ $ticket->assigned_to_name }}</td>
     <td>{{ $ticket->type_of_inquiry }}</td>
     <td>{{ $ticket->country }}</td>
     <td>{{ substr($ticket->order_no,0,5) }}</td>
     <td>{{ $ticket->phone_no }}</td>
+    <script type="text/javascript">
+       $(document).on('click', '.subject1_<?=$ticket->ticket_id?>', function () {
+        $('#subject1 #content').html('<?=$ticket->subject?>');
+        $('#subject1').modal("show");
+
+});
+    </script>
+    <script type="text/javascript">
+       $(document).on('click', '.message1_<?=$ticket->ticket_id?>', function () {
+        $('#message1 #content').html('<?=$ticket->message?>');
+        $('#message1').modal("show");
+
+});
+    </script>
     <td class="table-hover-cell" style="word-break: break-all;padding: 5px;">
         <div class="row">
             <div class="col-md-12 form-inline cls_remove_rightpadding">
@@ -31,14 +46,19 @@
                 </div>
             </div>
        </div>
-    </td>     
+    </td>    
+     
     <td><?php echo Form::select("task_status",$statusList,$ticket->status_id,["class" => "form-control resolve-issue","onchange" => "resolveIssue(this,".$ticket->id.")","style"=>"height: 30px;"]); ?></td>
     <td style="width: 100px;">
         <div class="chat-righbox">
           <button type="button" class="btn btn-xs send-email-to-vender" data-subject="{{ $ticket->subject }}" data-message="{{ $ticket->message }}" data-email="{{ $ticket->email }}" data-id="{{$ticket->id}}"><i class="fa fa-envelope-square"></i></button>
           <button type="button" class="btn btn-xs load-communication-modal" data-is_admin="{{ Auth::user()->hasRole('Admin') }}" data-is_hod_crm="{{ Auth::user()->hasRole('HOD of CRM') }}" data-object="ticket" data-id="{{$ticket->id}}" data-load-type="text" data-all="1" title="Load messages"><i class="fa fa-whatsapp"></i></button>
           <button type="button" class="btn btn-xs btn-assigned-to-ticket" data-id="{{$ticket->id}}"><i class="fa fa-comments-o"></i></button>
+          
         </div>
     </td>
 </tr>          
 @endforeach
+
+   
+   
