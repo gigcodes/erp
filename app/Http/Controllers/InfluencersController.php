6743a313c57630b259db45c57116b26f6c219c62
Satyam Tripathi <satyam.luvit@gmail.com>
2020-10-27 17:20:21 +0530

    Stiop space

--- app/Http/Controllers/InfluencersController.php
+++ app/Http/Controllers/InfluencersController.php
@@ -186,7 +186,8 @@ class InfluencersController extends Controller
        $cURLConnection = curl_init();
 
         $url = env('INFLUENCER_SCRIPT_URL').':'.env('INFLUENCER_SCRIPT_PORT').'/start-script?'.$name;
-
+        $url = str_replace($url);
+        
         echo $url;
         die();
 
