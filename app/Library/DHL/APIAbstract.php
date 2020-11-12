e774794a867e3e8504f2d4fef2b5bde019093947
Solanki Pravin <solanki7492@gmail.com>
2020-11-12 10:58:35 +0530

    Update APIAbstract.php

--- app/Library/DHL/APIAbstract.php
+++ app/Library/DHL/APIAbstract.php
@@ -43,7 +43,6 @@ abstract class APIAbstract
         } else {
             $ch = curl_init($this->_stagingUrl);
         }
-        dump($this->document());
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); //ssl
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); //ssl
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/xml']);
@@ -60,7 +59,6 @@ abstract class APIAbstract
         $result = curl_exec($ch);
 
         curl_close($ch);
-        dump($result);
         $this->resultsRAW = $result;
         try {
             $result = str_ireplace(['xmlSOAP-ENV','ser-root:','SOAP-ENV:', 'SOAP:','rateresp:','shipresp:','trac:','dhl:','ns:'], '', $result);
