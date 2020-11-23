1a0768aece0867ffba4f3e51742493465f5e0dfd
Satyam Tripathi <satyam.luvit@gmail.com>
2020-11-23 17:15:30 +0400

    Brand min and max price

---
+++ app/Console/Commands/BrandUpdateMaxAndMinPrice.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Brand;
+
+class BrandUpdateMaxAndMinPrice extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'brand:maxminprice';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $brands = Brand::all();
+        foreach ($brands as $brand) {
+            $min = $brand->products->where('price','>=',0)->min('price');
+            $max = $brand->products->where('price','>=',0)->max('price');
+            //getting brand price from products 
+            $brand->min_sale_price = $min;
+            $brand->max_sale_price = $max;
+            $brand->update();
+        }
+    }
+}
