79344ac5885645eb74e73e7750ee8fdb224c2eca
Pravin Solanki <solanki7492@gmail.com>
2020-12-10 17:02:51 +0530

    fix issue with chat message

--- Modules/ChatBot/Http/Controllers/MessageController.php
+++ Modules/ChatBot/Http/Controllers/MessageController.php
@@ -39,9 +39,7 @@ class MessageController extends Controller
 
         $pendingApprovalMsg = $pendingApprovalMsg->
         where(function($q) {
-            $q->whereIn("status", [ChatMessage::CHAT_SUGGESTED_IMAGES, ChatMessage::CHAT_AUTO_WATSON_REPLY])->orWhere(function($q){
-                $q->whereIn("status",ChatMessage::AUTO_REPLY_CHAT)->where("group_id",">",0);
-            });
+            $q->whereIn("status", [ChatMessage::CHAT_SUGGESTED_IMAGES, ChatMessage::CHAT_AUTO_WATSON_REPLY]);
         })->where("chat_messages.customer_id", ">", 0)
             ->select(["chat_messages.*", "chat_messages.id as chat_id", "cr.question", "c.name as customer_name", "s.id as suggestion_id", 'cq.chat_message_id'])
             ->latest()
