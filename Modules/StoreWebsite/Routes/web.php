962ba22d356e3737b0885983f21e510d11ba32ff
Pravin Solanki <solanki7492@gmail.com>
2020-12-19 16:02:37 +0530

    Fix storewebsite page

--- Modules/StoreWebsite/Routes/web.php
+++ Modules/StoreWebsite/Routes/web.php
@@ -100,6 +100,17 @@ Route::prefix('store-website')->middleware('auth')->group(function () {
         Route::post('push-to-store', 'ColorController@pushToStore')->name('store-website.color.push-to-store');
     });
 
+    Route::prefix('page')->group(function () {
+        Route::get('/', 'PageController@index')->name("store-website.page.index");
+        Route::get('/records', 'PageController@records')->name("store-website.page.records");
+        Route::post('save', 'PageController@store')->name("store-website.page.save");
+        Route::get('/{id}/edit', 'PageController@edit')->name("store-website.page.edit");
+        Route::get('/{id}/delete', 'PageController@delete')->name("store-website.page.delete");
+        Route::get('/{id}/push', 'PageController@push')->name("store-website.page.push");
+        Route::get('/{id}/get-stores', 'PageController@getStores')->name("store-website.page.getStores");
+        Route::get('/{id}/load-page', 'PageController@loadPage')->name("store-website.page.loadPage");
+    });
+
 });
 
 Route::prefix('site-development')->group(function () {
