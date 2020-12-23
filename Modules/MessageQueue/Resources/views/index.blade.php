1b92c410d346bddf759b7f1c7c07248fee13b8a4
Pravin Solanki <solanki7492@gmail.com>
2020-12-23 18:12:22 +0530

    Add chat bubble button'

--- Modules/MessageQueue/Resources/views/index.blade.php
+++ Modules/MessageQueue/Resources/views/index.blade.php
@@ -231,6 +231,26 @@
 <div id="loading-image" style="position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url('/images/pre-loader.gif') 
           50% 50% no-repeat;display:none;">
 </div>
+<div id="chat-list-history" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Communication</h4>&nbsp;&nbsp;
+                <input type="text" name="search_chat_pop"  class="form-control search_chat_pop" placeholder="Search Message" style="width: 50%;">&nbsp;&nbsp;
+                <!-- <input type="text" name="search_chat_pop_time"  class="form-control search_chat_pop_time" placeholder="Search Time" style="width: 200px;"> -->
+				<input style="min-width: 30px;" placeholder="Search by date" value="" type="text" class="form-control search_chat_pop_time" name="search_chat_pop_time">
+                <input type="hidden" id="chat_obj_type" name="chat_obj_type">
+                <input type="hidden" id="chat_obj_id" name="chat_obj_id">
+                <button type="submit" class="btn btn-default downloadChatMessages ml-2">Download</button>
+            </div>
+            <div class="modal-body" style="background-color: #999999;">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 @include("messagequeue::templates.list-template")
 
--- Modules/MessageQueue/Resources/views/templates/list-template.php
+++ Modules/MessageQueue/Resources/views/templates/list-template.php
@@ -29,7 +29,8 @@
 			        		<img width="75px" heigh="75px" src="{{>prop}}">
 			        	{{/props}}
 			        </td>
-			        <td><button type="button" data-id="{{>prop.id}}" class="btn btn-delete-template"><img width="15px" src="/images/delete.png"></button></td>
+			        <td><button type="button" data-id="{{>prop.id}}" class="btn btn-delete-template"><img width="15px" src="/images/delete.png"></button>
+			        	<button type="button" class="btn btn-xs btn-image load-communication-modal" data-object="customer" data-limit="10" data-id="{{:prop.customer_id}}" data-is_admin="1" data-is_hod_crm="" data-load-type="text_with_incoming_img" data-attached="1" data-all="1" title="Load messages"><img src="/images/chat.png" alt="" style="cursor: nwse-resize; width: 0px;"></button></td>
 			      </tr>
 			    {{/props}}  
 		    </tbody>
