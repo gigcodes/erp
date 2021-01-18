80dd77b2dbbbe46ddbff70fa8d453e83854a045e
Indrasinh Bihola <biholaindrasinh@gmail.com>
2021-01-18 15:47:06 +0530

    Parse and remove sizes issue fix.

--- app/Console/Commands/MoveSizeToTable.php
+++ app/Console/Commands/MoveSizeToTable.php
@@ -61,27 +61,63 @@ class MoveSizeToTable extends Command
                 if (!empty($ex)) {
                     foreach ($ex as $e) {
 
-                        if (in_array(trim($e),['1/2', '+', '/2'])) {
-                            $sizes[] = 0.5;
-                            continue;
-                        }
-
-                        if(in_array(trim($e), ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'])){
-                            $sizes[] = $this->romanToNumber($e);
-                            continue;
-                        }
-
                         if (strlen($e)>=4 || $this->dontNeedThisWords($e) || strpos($e, "cm") !== false || strpos($e, "$") !== false || strpos($e, '"') !== false) {
                             continue;
                         }
 
-                        if(strpos($e, "INCH") !== false || strpos($e, "IT") !== false ) {
-                            $sizes[] = (int) $e;
+                        if (strpos($e, "½") !== false) {
+                            $parts = explode('½', $e);
+                            $sizes[] = (int)trim($parts[0]) + 0.5;
                             continue;
                         }
 
-                        if(strpos($e, "-") !== false) {
-                            $sizes[] = abs($e);
+                        if (strpos($e, "/2") !== false) {
+                            $parts = explode(' ', $e);
+                            $sizes[] = (int)trim($parts[0]) + 0.5;
+                            continue;
+                        }
+
+                        if (strpos($e, "1/2") !== false) {
+                            $parts = explode(' ', $e);
+                            if(isset($parts[0])) {
+                                $sizes[] = (int)$parts[0] + 0.5;
+                            }
+                            continue;
+                        }
+
+                        if (strpos($e, "+") !== false) {
+                            $parts = explode('+', $e);
+                            $sizes[] = (int)trim($parts[0]) + 0.5;
+                            continue;
+                        }
+
+                        if(in_array(trim($e), ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'])) {
+                            $sizes[] = $this->romanToNumber($e);
+                            continue;
+                        }
+
+                        if (strpos($e, "IT") !== false) {
+                            $parts = explode(' ', $e);
+                            if(isset($parts[1])) {
+                                $parts = explode('/', $parts[1]);
+                                $sizes[] = $parts[0] + 0.5;
+                            }
+                            continue;
+                        }
+
+                        if(strpos($e, "UK INCH") !== false) {
+                            $prefix = 'UK INCH ';
+                            if (substr($e, 0, strlen($prefix)) == $prefix) {
+                                $sizes[] = substr($e, strlen($prefix));
+                            }
+                            continue;
+                        }
+
+                        if(strpos($e, "UK-") !== false ) {
+                            $prefix = 'UK-';
+                            if (substr($e, 0, strlen($prefix)) == $prefix) {
+                                $sizes[] = substr($e, strlen($prefix));
+                            }
                             continue;
                         }
 
