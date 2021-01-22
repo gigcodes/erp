753dcc7d9896c3d154a450106efdbbe0611b678f
Satyam Tripathi <satyam.luvit@gmail.com>
2021-01-22 20:08:22 +0530

    Missing server added

--- deployment_scripts/scraper_nodejs/deploy_branch.sh
+++ deployment_scripts/scraper_nodejs/deploy_branch.sh
@@ -1,5 +1,5 @@
-#ssh -i ~/.ssh/id_rsa root@s01.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
-#ssh -i ~/.ssh/id_rsa root@s02.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
+ssh -i ~/.ssh/id_rsa root@s01.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
+ssh -i ~/.ssh/id_rsa root@s02.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
 #ssh -i ~/.ssh/id_rsa root@s03.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
 ssh -i ~/.ssh/id_rsa root@s04.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
 #ssh -i ~/.ssh/id_rsa root@s05.theluxuryunlimited.com "cd ~/scraper_nodejs && git pull && git checkout $1 && git pull"
