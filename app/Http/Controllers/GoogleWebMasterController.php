53e739b33eae593faedadd841b2a57eb6dd3de0e
Ravi Mehta <rvmeht745@gmail.com>
2021-02-08 14:12:37 +0530

    set dynamic credentials and upadate code for crawl api

--- app/Http/Controllers/GoogleWebMasterController.php
+++ app/Http/Controllers/GoogleWebMasterController.php
@@ -20,25 +20,24 @@ class GoogleWebMasterController extends Controller
 
      public function googleLogin(Request $request)  {
 
-            $google_redirect_url = env('APP_URL').'/googlewebmaster/get-access-token';
+            $google_redirect_url = route('googlewebmaster.get-access-token');
+
             $gClient = new \Google_Client();
 
-            $gClient->setApplicationName('SoloLux-adwords-ERP');
+            $gClient->setApplicationName(config('constants.GOOGLE_CLIENT_APPLICATION_NAME'));
 
-            $gClient->setClientId('327812634030-ciu6aeb7ab2tk8tal43gv96tt2cdvkm1.apps.googleusercontent.com');
+            $gClient->setClientId(config('constants.GOOGLE_CLIENT_ID'));
 
-            $gClient->setClientSecret('hAze6ItSjAPydnXq-_ZZhSQL');
+            $gClient->setClientSecret(config('constants.GOOGLE_CLIENT_SECRET'));
+
+            $gClient->setDeveloperKey(config('constants.GOOGLE_CLIENT_KEY'));
 
             $gClient->setRedirectUri($google_redirect_url);
-
-            $gClient->setDeveloperKey('AIzaSyApzBpcN13uiCgGyEFbMpRptmsDHvwUCtw');
 
             $gClient->setScopes(array(
                 'https://www.googleapis.com/auth/webmasters',
                 'https://www.googleapis.com/auth/webmasters.readonly',
-            ));
-
-           
+            ));          
 
             $google_oauthV2 = new \Google_Service_Oauth2($gClient);
             if ($request->get('code')){
@@ -74,32 +73,27 @@ class GoogleWebMasterController extends Controller
 				if ($err) {
 				  echo "cURL Error #:" . $err;
 				} else {
-					/*$curl1 = curl_init();
-					//replace website name with code coming form site list
-					curl_setopt_array($curl1, array(
-					    CURLOPT_URL => "https://www.googleapis.com/webmasters/v3/sites/https://sololuxury.com/sitemaps?sitemapIndex=https://sololuxury.com/sitemapindex.xml",
-					  CURLOPT_RETURNTRANSFER => true,
-					  CURLOPT_ENCODING => "",
-					  CURLOPT_MAXREDIRS => 10,
-					  CURLOPT_TIMEOUT => 30,
-					  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-					  CURLOPT_CUSTOMREQUEST => "GET",
-					  CURLOPT_HTTPHEADER => array(
-					    "authorization: Bearer ".$gClient->getAccessToken()['access_token']
-					  ),
-					));*/
-
-					//$response1 = curl_exec($curl1);
-					//$err = curl_error($curl1);
-					
 					if(is_array( json_decode( $response)->siteEntry ) ){
 						foreach(json_decode( $response)->siteEntry as $key=> $site) {
-							$checkSiteExist = GoogleWebMasters::where('sites',$site->siteUrl)->first(); 
-							if( empty( $checkSiteExist ) ) {
-								GoogleWebMasters::create(['sites'=>$site->siteUrl]);
-							}else{
-								GoogleWebMasters::where('sites',$site->siteUrl)->update(['sites'=>$site->siteUrl]);
-							}	
+							// Create ot update site url
+							GoogleWebMasters::updateOrCreate(['sites'=>$site->siteUrl]);
+							$curl1 = curl_init();
+							//replace website name with code coming form site list
+							curl_setopt_array($curl1, array(
+							    CURLOPT_URL => "https://www.googleapis.com/webmasters/v3/sites/".$site->siteUrl."/sitemaps",
+							  CURLOPT_RETURNTRANSFER => true,
+							  CURLOPT_ENCODING => "",
+							  CURLOPT_MAXREDIRS => 10,
+							  CURLOPT_TIMEOUT => 30,
+							  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+							  CURLOPT_CUSTOMREQUEST => "GET",
+							  CURLOPT_HTTPHEADER => array(
+							    "authorization: Bearer ".$gClient->getAccessToken()['access_token']
+							  ),
+							));
+
+							$response1 = curl_exec($curl1);
+							$err = curl_error($curl1);
 						}
 					}else{
 						 return redirect()->route('googlewebmaster.index');  
