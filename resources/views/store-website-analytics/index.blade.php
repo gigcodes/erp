f908787e83289e3550224af6dda6354210646701
Pravin Solanki <solanki7492@gmail.com>
2021-02-22 18:27:51 +0530

    Change order by

--- resources/views/store-website-analytics/index.blade.php
+++ resources/views/store-website-analytics/index.blade.php
@@ -55,7 +55,7 @@
 </div>
 
 <div class="modal fade bd-report-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-body">
 
