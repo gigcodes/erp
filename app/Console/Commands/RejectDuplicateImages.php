d421a15d0e18e2344493b4bb00f673d1b7c0439e
Pravin Solanki <solanki7492@gmail.com>
2021-03-05 21:37:23 +0530

    Fix reject images

--- app/Console/Commands/RejectDuplicateImages.php
+++ app/Console/Commands/RejectDuplicateImages.php
@@ -63,82 +63,88 @@ class RejectDuplicateImages extends Command
                 }])
                 ->first();
 
-        if( !empty( $data ) ){ 
+        try {        
 
-            // Get product Details
-            $products =  Product::find( $data->product_id );
+            if( !empty( $data ) ){ 
 
-            if( !empty( $products ) ){
+                // Get product Details
+                $products =  Product::find( $data->product_id );
 
-                $checksums = array();
+                if( !empty( $products ) ){
 
-                // Get last image cropper
-                $lastImageCropper = $products->crop_approved_by;
-                $user = \App\User::find($lastImageCropper);
+                    $checksums = array();
 
-                foreach ($data->differentWebsiteImages as $key ) {
-                    if(empty($key)) {
-                       continue; 
-                    }
+                    // Get last image cropper
+                    $lastImageCropper = $products->crop_approved_by;
+                    $user = \App\User::find($lastImageCropper);
 
-                    $image = $key->newMedia->directory.'/'.$key->newMedia->filename.'.'.$key->newMedia->extension;
-
-                    $image = public_path( 'uploads/'.$image );
-
-                    // Check is directory
-                    if( File::isDirectory($_="$image") ) continue;
-                    
-                    // Checking is file exists or not 
-                    if( !File::exists( $image ) ) continue;
-                    
-                    $_="$image";
-
-                    $hash = hash_file('md5', $_);
-
-                    // delete duplicate
-                    if (in_array($hash, $checksums)) {
-
-                        $products->status_id              = StatusHelper::$autoReject;
-                        $products->is_crop_rejected       = 1;
-                        $products->crop_remark            = 'Auto rejected';
-                        $products->crop_rejected_by       = 0;
-                        $products->is_approved            = 0;
-                        $products->is_crop_approved       = 0;
-                        $products->is_crop_ordered        = 0;
-                        $products->is_crop_being_verified = 0;
-                        $products->crop_rejected_at       = Carbon::now()->toDateTimeString();
-                        $products->save();
-
-                        if($user) {
-                            if ((int)$lastImageCropper > 0) {
-                                $e             = new ListingHistory();
-                                $e->user_id    = $lastImageCropper;
-                                $e->product_id = $products->id;
-                                $e->content    = ['action' => 'CROP_APPROVAL_DENIED', 'page' => 'Approved Listing Page'];
-                                $e->action     = 'CROP_APPROVAL_DENIED';
-                                $e->save();
-                            }
+                    foreach ($data->differentWebsiteImages as $key ) {
+                        if(empty($key)) {
+                           continue; 
                         }
 
+                        $image = $key->newMedia->directory.'/'.$key->newMedia->filename.'.'.$key->newMedia->extension;
 
-                        // Log crop rejected
-                        $e             = new ListingHistory();
-                        $e->user_id    = 0;
-                        $e->product_id = $products->id;
-                        $e->content    = ['action' => 'CROP_REJECTED', 'page' => 'Approved Listing Page'];
-                        $e->action     = 'CROP_REJECTED';
-                        $e->save();
-                    }
+                        $image = public_path( 'uploads/'.$image );
 
-                    // add hash to list
-                    else {
-                        $checksums[] = $hash;
+                        // Check is directory
+                        if( File::isDirectory($_="$image") ) continue;
+                        
+                        // Checking is file exists or not 
+                        if( !File::exists( $image ) ) continue;
+                        
+                        $_="$image";
+
+                        $hash = hash_file('md5', $_);
+
+                        // delete duplicate
+                        if (in_array($hash, $checksums)) {
+
+                            $products->status_id              = StatusHelper::$autoReject;
+                            $products->is_crop_rejected       = 1;
+                            $products->crop_remark            = 'Auto rejected';
+                            $products->crop_rejected_by       = 0;
+                            $products->is_approved            = 0;
+                            $products->is_crop_approved       = 0;
+                            $products->is_crop_ordered        = 0;
+                            $products->is_crop_being_verified = 0;
+                            $products->crop_rejected_at       = Carbon::now()->toDateTimeString();
+                            $products->save();
+
+                            if($user) {
+                                if ((int)$lastImageCropper > 0) {
+                                    $e             = new ListingHistory();
+                                    $e->user_id    = $lastImageCropper;
+                                    $e->product_id = $products->id;
+                                    $e->content    = ['action' => 'CROP_APPROVAL_DENIED', 'page' => 'Approved Listing Page'];
+                                    $e->action     = 'CROP_APPROVAL_DENIED';
+                                    $e->save();
+                                }
+                            }
+
+
+                            // Log crop rejected
+                            $e             = new ListingHistory();
+                            $e->user_id    = 0;
+                            $e->product_id = $products->id;
+                            $e->content    = ['action' => 'CROP_REJECTED', 'page' => 'Approved Listing Page'];
+                            $e->action     = 'CROP_REJECTED';
+                            $e->save();
+                        }
+
+                        // add hash to list
+                        else {
+                            $checksums[] = $hash;
+                        }
+
                     }
 
                 }
 
             }
 
+        } catch(\Exception $e) {
+            \Log::info("Fix issue on Rejecte duplicate images => ".$e->getMessage());
         }
 
         $this->output->write('Cron complated', true);
