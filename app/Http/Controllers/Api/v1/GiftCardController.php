6b8dd18798346538fc5014b2767a8af2984d98f4
Qrolic <info@qrolic.com>
2021-03-11 10:31:24 +0530

    giftcards/add api issue resoved

--- app/Http/Controllers/Api/v1/GiftCardController.php
+++ app/Http/Controllers/Api/v1/GiftCardController.php
@@ -54,7 +54,7 @@ class GiftCardController extends Controller
             'website' => 'required|exists:store_websites,website',
         ]);
         if ($validator->fails()) {
-            $message = $this->generate_erp_response("giftcard.failed.validation", $storeweb->id, $default = "Please check validation errors !", request('lang_code'));
+            $message = $this->generate_erp_response("giftcard.failed.validation", 0, $default = "Please check validation errors !", request('lang_code'));
             return response()->json(['status' => 'failed', 'message' => $message, 'errors' => $validator->errors()], 400);
         }
         $storeweb = StoreWebsite::where('website', $request->website)->first();
