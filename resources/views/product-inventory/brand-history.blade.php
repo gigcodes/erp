1050a370481ad78c08927b760cea84f5cb4fd52a
Pravin Solanki <solanki7492@gmail.com>
2021-03-16 20:21:34 +0530

    Show the product count and brand changes

---
+++ resources/views/product-inventory/brand-history.blade.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered table-striped">
+            <tr>
+                <th width="1%">No</th>
+                <th width="1%">Brand ID</th>
+                <th width="5%">Brand</th>
+                <th width="15%">Total</th>
+            </tr>
+            <tbody class="">
+                <?php foreach($inventory as $c => $i) { ?>
+                    <tr style="<?php echo ($i->total <= 10 ) ? 'background: red' : ''; ?>">
+                        <td><?php echo $c+1 ?></td>  
+                        <td><?php echo $i->brand ?></td>  
+                        <td><?php echo $i->brand ?></td>  
+                        <td><?php echo $i->name ?></td>  
+                        <td><?php echo $i->total ?></td>  
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
