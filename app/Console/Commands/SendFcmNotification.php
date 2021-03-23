0c6ce5bcff53c8f9ba73ee61d0a5992ac51b6596
Pravin Solanki <solanki7492@gmail.com>
2021-03-23 20:51:31 +0530

    Fix send notification

--- app/Console/Commands/SendFcmNotification.php
+++ app/Console/Commands/SendFcmNotification.php
@@ -45,6 +45,8 @@ class SendFcmNotification extends Command
         $fromdate      = date('Y-m-d H:i');
         $newtimestamp  = strtotime($fromdate . ' + 4 minute');
         $todate        = date('Y-m-d H:i', $newtimestamp);
+        echo $fromdate."#".$todate;
+        echo PHP_EOL;
         $Notifications = PushFcmNotification::select('sw.push_web_key', 'sw.push_web_id', 'ft.token', 'push_fcm_notifications.*')
             ->leftJoin('fcm_tokens as ft', 'ft.store_website_id', '=', 'push_fcm_notifications.store_website_id')
             ->leftJoin('store_websites as sw', 'sw.id', '=', 'push_fcm_notifications.store_website_id')
