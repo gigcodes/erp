e88ced7678d9ad08a567fa029d6568be50d1bd89
Pravin Solanki <solanki7492@gmail.com>
2021-04-09 14:34:15 +0530

    changes related to scraper start time

--- app/Console/Commands/CheckScraperRunningStatus.php
+++ app/Console/Commands/CheckScraperRunningStatus.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\CronJobReport;
 use Carbon\Carbon;
+use App\CronJobReport;
 use Illuminate\Console\Command;
 
 class CheckScraperRunningStatus extends Command
@@ -41,8 +41,8 @@ class CheckScraperRunningStatus extends Command
     {
 
         $report = CronJobReport::create([
-            'signature'  => $this->signature,
-            'start_time' => Carbon::now(),
+        'signature'  => $this->signature,
+        'start_time' => Carbon::now(),
         ]);
 
         $cmd = 'bash ' . getenv('DEPLOYMENT_SCRIPTS_PATH') .'scrapper-running.sh 2>&1';
@@ -50,8 +50,12 @@ class CheckScraperRunningStatus extends Command
         $allOutput = array();
         $allOutput[] = $cmd;
         $result = exec($cmd, $allOutput);
+         
 
-        
+        /*$allOutput   = [];
+        $allOutput[] = "";
+        $allOutput[] = "#####################   Server -   s01 ################### Server Load - 0.27,###############";
+        $allOutput[] = "Mon Apr  5 20:00:02 2021  /root/scraper_nodejs/commands/completeScraps/conceptstore.js";*/
 
         $serverId       = null;
         $scraperNamestr = null;
@@ -61,11 +65,11 @@ class CheckScraperRunningStatus extends Command
                 if ($k == 0) {
                     continue;
                 }
-                $allO = preg_replace('/\s+/', '', $allO);
-                if (strpos($allO, "#####################Server-") !== false) {
-                    $serverArray = explode("#####################Server-", $allO);
+                $allO = preg_replace('/\s+/', ' ', $allO);
+                if (strpos($allO, "##################### Server - ") !== false) {
+                    $serverArray = explode("##################### Server - ", $allO);
                     if (!empty($serverArray[1])) {
-                        $serverNameArr = explode("###################ServerLoad", $serverArray[1]);
+                        $serverNameArr = explode("################### Server Load ", $serverArray[1]);
                         if (!empty(trim($serverNameArr[0]))) {
                             $serverId = trim($serverNameArr[0]);
                             continue;
@@ -74,11 +78,13 @@ class CheckScraperRunningStatus extends Command
                 }
 
                 // start to store scarper name
-                $scraperNamestr = null;
+                $scraperNamestr  = null;
+                $scraperStarTime = null;
                 if (strpos($allO, "/root/scraper_nodejs/commands/completeScraps") !== false) {
                     $scriptNames = explode("/root/scraper_nodejs/commands/completeScraps", $allO);
                     if (!empty($scriptNames[1])) {
-                        $scraperName = explode("/", $scriptNames[1]);
+                        $scraperStarTime = date("Y-m-d H:i:s", strtotime($scriptNames[0]));
+                        $scraperName     = explode("/", $scriptNames[1]);
                         if (count($scraperName) > 2) {
                             $scraperNamestr = $scraperName[1];
                         } else {
@@ -93,6 +99,7 @@ class CheckScraperRunningStatus extends Command
                         "scraper_name"   => $scraperNamestr,
                         "scraper_string" => $allO,
                         "server_id"      => $serverId,
+                        "start_time"     => $scraperStarTime,
                     ]);
                 }
             }
