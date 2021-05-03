72cfbc0dd8e20f6b1e3e9d4b21292970d1fdd47a
root <sahilkataria.1989@gmail.com>
2021-05-03 05:30:34 +0000

    spell mistakes

--- deployment_scripts/autodb-backup.sh
+++ deployment_scripts/autodb-backup.sh
@@ -10,9 +10,9 @@ find $bkproot -mtime +8 -exec rm -rf {} \;
 
 ############### Magento Servers Database Backup #############
 envfile='/mnt/volume_blr1_01/websites/erp.amourint.com/httpdocs/erp/.env'
-magento_servers='BRANDS AVOIRCHIC OLABESL SOLOLUXURY SUVANDNAT THEFITEDIT THESHADESSHOP UPEAU VERALUSSO'
+magento_servers='BRANDS AVOIRCHIC OLABELS SOLOLUXURY SUVANDNAT THEFITEDIT THESHADESSHOP UPEAU VERALUSSO'
 mageroot=/mnt/volume_blr1_02/magento_backup
-magentobkp=$mageroot/$date
+magentobkp=/opt/$date
 
 user=`grep MAGENTO_DB_USER $envfile|cut -d'=' -f2`
 pass=`grep MAGENTO_DB_PASSWORD $envfile|cut -d"'" -f2`
@@ -22,7 +22,8 @@ do
 	mkdir -p $magentobkp/$server
 	db=`grep "$server"_DB $envfile|cut -d'=' -f2`
 	host=`grep "$server"_HOST $envfile|cut -d'=' -f2`
-	mysqldump -h $host -u $user -p"$pass" $db |gzip -v > $magentobkp/$server/$db.gz
+	mysqldump -h $host -u $user -p"$pass" $db |gzip -v > $magentobkp/$server/$db.sql.gz
 done
+mv $magentobkp $mageroot/
 
 find $magentoroot -mtime +8 -exec rm -rf {} \; 
