a6bcb15af1021b8d40f417279e71a990b6856833
Pravin Solanki <solanki7492@gmail.com>
2021-05-03 11:59:28 +0530

    Add log loder fix

--- resources/views/logging/partials/generate-report.blade.php
+++ resources/views/logging/partials/generate-report.blade.php
@@ -4,12 +4,14 @@
          <th>ID</th>
          <th>URL</th>
          <th>Count</th>
+         <th>Time</th>
       </tr>
          @foreach($logsGroupWise as $i => $lgw)
               <tr>
                  <td>{{ $i + 1 }}</td>
                  <td>{{ $lgw->url }}</td>
                  <td>{{ $lgw->total_request }}</td>
+                 <td>{{ $lgw->time_taken }}</td>
               </tr>
          @endforeach
    </tbody>
