56127234ae5c111ee3b0f04e081d30986c030656
Pravin Solanki <solanki7492@gmail.com>
2021-05-03 13:22:37 +0530

    admin access button

--- Modules/StoreWebsite/Resources/views/index.blade.php
+++ Modules/StoreWebsite/Resources/views/index.blade.php
@@ -22,9 +22,11 @@
 	    			<button style="display: inline-block;width: 10%" class="btn btn-sm btn-image btn-add-action">
 		  				<img src="/images/add.png" style="cursor: default;">
 		  			</button>
-                    <button style="display: inline-block;" class="btn btn-sm btn-secondary generate-pem-file">
-                        Generate PEM file
-                    </button>
+                    @if(auth()->user()->isAdmin())
+                        <button style="display: inline-block;" class="btn btn-sm btn-secondary generate-pem-file">
+                            Generate PEM file
+                        </button>
+                    @endif
 				 </div> 		
 		    </div>
 		    <div class="col">
