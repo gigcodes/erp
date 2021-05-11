3ecb4b05cc12ffd8a22d96b2307aa5f3e160ebad
Mahi1105 <mahipal.rathod1992@gmail.com>
2021-05-11 19:07:29 +0530

    user logins page related fixes.

--- app/Http/Middleware/LogLastUserActivity.php
+++ app/Http/Middleware/LogLastUserActivity.php
@@ -29,40 +29,40 @@ class LogLastUserActivity
           $expiresAt = Carbon::now()->addMinutes(5);
           Cache::put('user-is-online-' . Auth::user()->id, true, $expiresAt);
 
-          // if ($user_login = UserLogin::where('user_id', Auth::id())->latest()->first()) {
-          //   if (Carbon::now()->diffInDays($user_login->login_at) != 0) {
-          //     UserLogin::create([
-          //       'user_id'   => Auth::id(),
-          //       'login_at' => Carbon::now()
-          //     ]);
-          //   }
-          // } else {
-          //   UserLogin::create([
-          //     'user_id'   => Auth::id(),
-          //     'login_at' => Carbon::now()
-          //   ]);
-          // }
+          if ($user_login = UserLogin::where('user_id', Auth::id())->latest()->first()) {
+            if (Carbon::now()->diffInDays($user_login->login_at) != 0) {
+              UserLogin::create([
+                'user_id'   => Auth::id(),
+                'login_at' => Carbon::now()
+              ]);
+            }
+          } else {
+            UserLogin::create([
+              'user_id'   => Auth::id(),
+              'login_at' => Carbon::now()
+            ]);
+          }
       }
 
       if(!$this->session->has('lastActivityTime'))
             $this->session->put('lastActivityTime',time());
         elseif(time() - $this->session->get('lastActivityTime') > $this->getTimeOut()){
             $this->session->forget('lastActivityTime');
-            // if ($user_login = UserLogin::where('user_id', Auth::id())->latest()->first()) {
-            //   if (Carbon::now()->diffInDays($user_login->logout_at) == 0) {
-            //     $user_login->update(['logout_at' => Carbon::now()]);
-            //   } else {
-            //     UserLogin::create([
-            //       'user_id'   => Auth::id(),
-            //       'logout_at' => Carbon::now()
-            //     ]);
-            //   }
-            // } else {
-            //   UserLogin::create([
-            //     'user_id'   => Auth::id(),
-            //     'logout_at' => Carbon::now()
-            //   ]);
-            // }
+            if ($user_login = UserLogin::where('user_id', Auth::id())->latest()->first()) {
+              if (Carbon::now()->diffInDays($user_login->logout_at) == 0) {
+                $user_login->update(['logout_at' => Carbon::now()]);
+              } else {
+                UserLogin::create([
+                  'user_id'   => Auth::id(),
+                  'logout_at' => Carbon::now()
+                ]);
+              }
+            } else {
+              UserLogin::create([
+                'user_id'   => Auth::id(),
+                'logout_at' => Carbon::now()
+              ]);
+            }
             Auth::logout();
             return redirect('/login')->withErrors(['You have been inactive for 30 minutes']);
         }
