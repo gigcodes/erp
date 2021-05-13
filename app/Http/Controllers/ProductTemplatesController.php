4ebb293f7ca139f525bcd73daf303519dab61b76
Qrolic <info@qrolic.com>
2021-05-13 17:38:02 +0530

    attach media file when template create1

--- app/Http/Controllers/ProductTemplatesController.php
+++ app/Http/Controllers/ProductTemplatesController.php
@@ -159,7 +159,7 @@ class ProductTemplatesController extends Controller
      */
     public function apiIndex(Request $request)
     {
-        $record = \App\ProductTemplate::where('template_status','python')->latest()->first();
+        $record = \App\ProductTemplate::where('is_processed','!=',2)->where('template_status','python')->latest()->first();
 
 
         if(!$record) {
@@ -405,7 +405,8 @@ class ProductTemplatesController extends Controller
             if (!empty($request->get('product_media_list')) && is_array($request->get('product_media_list'))) {
                 foreach ($request->get('product_media_list') as $mediaid) {
                     $media = Media::find($mediaid);
-                   // $template->attachMedia($media, ['template-image']);
+                    $template->attachMedia($media, ['template-image']);
+                    $template->save();
                     $imagesArray[]=$media->getUrl();
                 }
             }
@@ -414,7 +415,8 @@ class ProductTemplatesController extends Controller
                 foreach ($request->file('files') as $image) {
                     $media = MediaUploader::fromSource($image)->toDirectory('product-template-images')->upload();
 
-                   // $template->attachMedia($media,['template-image']);
+                    $template->attachMedia($media,['template-image']);
+                    $template->save();
                     $imagesArray[]=$media->getUrl();
                 }
             }
@@ -472,7 +474,7 @@ class ProductTemplatesController extends Controller
             $response = \App\Helpers\GuzzleHelper::post($url,$body,$headers);
             
             if( isset( $response->uid ) ){
-                ProductTemplate::where('id',$template->id)->update([ 'uid' => $response->uid, 'template_status' => $response->status ]);
+                ProductTemplate::where('id',$template->id)->update([ 'uid' => $response->uid, 'is_processed' => 2, 'template_status' => $response->status ]);
             }
             return response()->json(["code" => 1, "message" => "Product Template Created successfully!"]);
 
@@ -525,15 +527,15 @@ class ProductTemplatesController extends Controller
             
             if( isset( $response->uid ) ){
 
-                // $template = ProductTemplate::where('id',$response->metadata)->first();
+                $template = ProductTemplate::where('id',$response->metadata)->first();
                 
-                // $path = $response->image_url_png;
-                // $filename = basename($path);
-                // $media = MediaUploader::fromSource($image)->toDirectory(date('Y/m/d'))->useFilename($filename)->upload();
-                // $template->attachMedia($media,'template-image');
-                // $template->save();
+                $path = $response->image_url_png;
+                $filename = basename($path);
+                $media = MediaUploader::fromSource($image)->toDirectory(date('Y/m/d'))->useFilename($filename)->upload();
+                $template->attachMedia($media,'template-image');
+                $template->save();
 
-                ProductTemplate::where('id',$response->metadata)->where( 'uid', $response->uid )->update([ 'template_status' => $response->status, 'image_url' => $response->image_url_png ]);
+                ProductTemplate::where('id',$response->metadata)->where( 'uid', $response->uid )->update([ 'template_status' => $response->status, 'is_processed' => 1, 'image_url' => $response->image_url_png ]);
             }
             return response()->json(["code" => 1, "message" => "Image fetched successfully!" , "image" => $response->image_url_png ]);
 
