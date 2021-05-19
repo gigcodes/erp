48cc45bfc95f64f2cf2bffe8d796eced16f1dc8f
Pravin Solanki <solanki7492@gmail.com>
2021-05-19 15:14:47 +0530

    Add login controller changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -65,9 +65,9 @@ class LoginController extends Controller
                 $date =  date('Y-m-d', strtotime('-2 days'));
                 $hubstaff_activities = \App\Hubstaff\HubstaffActivity::join('hubstaff_members', 'hubstaff_members.hubstaff_user_id', '=', 'hubstaff_activities.user_id')->whereDate('hubstaff_activities.starts_at',$date)->where('hubstaff_members.user_id',auth()->user()->id)->count();
                 //if(!auth()->user()->isAdmin()) {
-                $user_ip = UserLoginIp::where('ip',$request->getClientIp())->where('user_id',auth()->user()->id)->orderBy('created_at','DESC')->get();
-                if(!$user_ip->isEmpty()){
-                    if($user_ip[0]->is_active){
+                $user_ip = UserLoginIp::where('ip',$request->getClientIp())->where('user_id',auth()->user()->id)->orderBy('created_at','DESC')->first();
+                if($user_ip){
+                    if($user_ip->is_active == false){
                         $this->logout($request);
                         return back()
                             ->withInput()
@@ -77,10 +77,9 @@ class LoginController extends Controller
                     $user_ip_add = New UserLoginIp();
                     $user_ip_add->user_id = auth()->user()->id;
                     $user_ip_add->ip = $request->getClientIp();
-                    $user_ip_add->is_active = 0;
+                    $user_ip_add->is_active = 1;
                     $user_ip_add->save();
                 }
-                if($request->getClientIp()){}
                 if($hubstaff_activities) {
                     $activity = \App\Hubstaff\HubstaffActivitySummary::where('user_id',auth()->user()->id)->where('date',$date)->first();
                     if(!$activity) {
