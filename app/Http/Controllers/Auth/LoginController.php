46df9001926a007e0fd7275ee83a5e1021c7c075
Mahi1105 <mahipal.rathod1992@gmail.com>
2021-05-20 12:39:39 +0530

    Admin ips added in the logs.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,7 +45,6 @@ class LoginController extends Controller
   
     public function login(Request $request)
     {
-        $this->validateLogin($request);
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
         // the IP address of the client making these requests into this application.
@@ -55,6 +54,15 @@ class LoginController extends Controller
         }
         $credentials =['email'=>$request->email, 'password'=>$request->password];
         if($this->guard()->attempt($credentials,$request->has('remember'))){
+            $this->validateLogin($request);
+            $user_ip = UserLoginIp::where('ip',$request->getClientIp())->where('user_id',auth()->user()->id)->orderBy('created_at','DESC')->first();
+            if($user_ip->isEmpty()){
+                $user_ip_add = New UserLoginIp();
+                $user_ip_add->user_id = auth()->user()->id;
+                $user_ip_add->ip = $request->getClientIp();
+                $user_ip_add->is_active = (auth()->user()->isAdmin()) ? 0 : 1;
+                $user_ip_add->save();
+            }
             if(auth()->user()->is_active !=1){ //Account has not being activated!
                 $this->logout($request);
                 return back()
@@ -65,7 +73,7 @@ class LoginController extends Controller
                 $date =  date('Y-m-d', strtotime('-2 days'));
                 $hubstaff_activities = \App\Hubstaff\HubstaffActivity::join('hubstaff_members', 'hubstaff_members.hubstaff_user_id', '=', 'hubstaff_activities.user_id')->whereDate('hubstaff_activities.starts_at',$date)->where('hubstaff_members.user_id',auth()->user()->id)->count();
                 //if(!auth()->user()->isAdmin()) {
-                $user_ip = UserLoginIp::where('ip',$request->getClientIp())->where('user_id',auth()->user()->id)->orderBy('created_at','DESC')->first();
+                
                 if($user_ip){
                     if($user_ip->is_active == false){
                         $this->logout($request);
@@ -73,12 +81,6 @@ class LoginController extends Controller
                             ->withInput()
                             ->withErrors(['message'=>'Please ask admin for login approval.']);
                     }
-                }else{
-                    $user_ip_add = New UserLoginIp();
-                    $user_ip_add->user_id = auth()->user()->id;
-                    $user_ip_add->ip = $request->getClientIp();
-                    $user_ip_add->is_active = 1;
-                    $user_ip_add->save();
                 }
                 if($hubstaff_activities) {
                     $activity = \App\Hubstaff\HubstaffActivitySummary::where('user_id',auth()->user()->id)->where('date',$date)->first();
