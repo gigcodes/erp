30fd0de8a549f20c69c1e4b6c7fd6cfa8077505b
Pravin Solanki <solanki7492@gmail.com>
2021-06-07 11:26:58 +0530

    Fix rename

--- database/migrations/2021_05_29_064218_alter_update_plans_table.php
+++ database/migrations/2021_05_29_064218_alter_update_plans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdatePlansTable extends Migration
+class AlterUpdatePlansTable extends Migration
 {
     /**
      * Run the migrations.
