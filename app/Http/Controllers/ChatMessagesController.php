22fe82860ca92ff3f78c28319a87fdd6e7c4d4c3
Pravin Solanki <solanki7492@gmail.com>
2021-06-21 18:22:55 +0530

    changes related to image

--- app/Http/Controllers/ChatMessagesController.php
+++ app/Http/Controllers/ChatMessagesController.php
@@ -98,7 +98,7 @@ class ChatMessagesController extends Controller
         $currentPage = request("page",1);
         $skip        = ($currentPage - 1) * $limit;
 
-        $loadType       = $request->get('load_type');
+        $loadType       = $request->get('load_type','text');
         $onlyBroadcast  = false;
 
         //  if loadtype is brodcast then get the images only
