1b77131bd22555e15bf23e173eb404b48144c708
asifkhalid03 <asifkhalid03@gmail.com>
2021-06-21 10:28:59 +0500

    update

--- Modules/MessageQueue/Http/Controllers/MessageQueueController.php
+++ Modules/MessageQueue/Http/Controllers/MessageQueueController.php
@@ -80,6 +80,7 @@ class MessageQueueController extends Controller
     public function approved(Request $request)
     {
 
+
         $groupIdApprove = ChatMessage::where('group_id', $request->group_id)->update(["is_queue" => 1]);
         return response()->json(["code" => 200, "message" => "Approved Successfully"]);
     }
