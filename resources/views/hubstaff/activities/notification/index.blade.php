0e0eeb67443547c86d1c08514ae58787b9b91b40
mohit bhesaniya <mohitbhesaniya111@gmail.com>
2021-06-23 23:57:32 +0530

    hubstaff notification different and download report

--- resources/views/hubstaff/activities/notification/index.blade.php
+++ resources/views/hubstaff/activities/notification/index.blade.php
@@ -19,18 +19,19 @@
     <br>
     <div class="col-lg-12 margin-tb">
     	<div class="row">
-	    	<div class="col col-md-9">
+	    	<div class="col col-md-6">
 		    	<div class="row">
 	    			<button style="display: inline-block;width: 10%" class="btn btn-sm btn-image btn-add-action">
 		  				<img src="/images/add.png" style="cursor: default;">
 		  			</button>
 		  		</div>
 		    </div>
-		    <div class="col">
+		    <div class="col col-md-6">
 		    	<div class="h" style="margin-bottom:10px;">
-		    		<form class="form-inline message-search-handler" method="post">
+		    		<form class="form-inline message-search-handler" action="{{route('hubstaff-acitivties.notification.download')}}"  method="post">
+		    		@csrf	
 					  <div class="row">
-			  			<div class="form-group">
+			  			<div class="form-group mr-2">
 						    <label for="keyword">Keyword:</label>
 						    <?php echo Form::text("keyword",request("keyword"),["class"=> "form-control","placeholder" => "Enter keyword"]) ?>
 					  	</div>
@@ -38,6 +39,7 @@
 		                    <strong>Date Range</strong>
 		                    <input type="text" value="<?php echo date("Y-m-d"); ?>" name="start_date" hidden/>
 		                    <input type="text" value="<?php echo date("Y-m-d"); ?>" name="end_date" hidden/>
+
 		                    <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
 		                        <i class="fa fa-calendar"></i>&nbsp;
 		                        <span></span> <i class="fa fa-caret-down"></i>
@@ -49,7 +51,12 @@
 					  			<img src="/images/search.png" style="cursor: default;">
 					  		</button>
 					  	</div>	
-					  </div>	
+					  </div>
+
+					  	<div class="form-group ml-5">
+                            <button type="submit" name="submit" value="report_download" title="Download report" class="btn btn-sm btn-secondary"><i class="fa fa-file-excel-o"></i>Download report</button>
+                        </div>
+
 					</form>	
 		    	</div>
 		    </div>
