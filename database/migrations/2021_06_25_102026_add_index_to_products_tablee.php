296891e38766c4404ef05aea138c84cd95a3ff3c
g60 <g60@gopanear.com>
2021-06-28 12:20:19 +0530

    composition page optimization

--- database/migrations/2021_06_25_102026_add_index_to_products_tablee.php
+++ database/migrations/2021_06_25_102026_add_index_to_products_tablee.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIndexToProductsTable extends Migration
+class AddIndexToProductsTablee extends Migration
 {
     /**
      * Run the migrations.
