d6ebfcb1ad2e28d9ceeb3c67385e39e20432c588
Pravin Solanki <solanki7492@gmail.com>
2021-06-28 19:02:29 +0530

    Changes related to the task 10206

--- app/Console/Commands/SendReminderToDevelopmentIfTheyHaventReplied.php
+++ app/Console/Commands/SendReminderToDevelopmentIfTheyHaventReplied.php
@@ -56,8 +56,8 @@ class SendReminderToDevelopmentIfTheyHaventReplied extends Command
         if (!$tasks->isEmpty()) {
             foreach ($tasks as $task) {
                 $templateMessage = $task->reminder_message;
-                $this->info("started for developer #".$task->id);
-                if ($task->diff_min >= $task->frequency && ($task->reminder_from == "0000-00-00 00:00" || strtotime($task->reminder_from) >= strtotime("now"))) {
+                $this->info("started for task #".$task->id ." found frequency {$task->diff_min} and task frequency {$task->frequency} and reminder from {$task->reminder_from}");
+                if ($task->diff_min >= $task->frequency && ($task->reminder_from == "0000-00-00 00:00" || strtotime($task->reminder_from) <= strtotime("now"))) {
                     $this->info("condition matched for developer #".$task->id);
                     $this->sendMessage($task->id, $templateMessage);
                     $task->last_send_reminder = date("Y-m-d H:i:s");
