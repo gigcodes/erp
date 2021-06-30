9394e47dd991f61c4f39bc7e76a612ea99e5794a
Pravin Solanki <solanki7492@gmail.com>
2021-06-30 18:57:21 +0530

    Added function for move size issue

---
+++ app/Console/Commands/FixErpSizeIssue.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\ProductHelper;
+use App\Helpers\StatusHelper;
+use App\Product;
+use App\ProductSizes;
+use App\ProductSupplier;
+use App\ScrapedProducts;
+use Illuminate\Console\Command;
+
+class FixErpSizeIssue extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fix-erp-size-issue';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fix Erp size issue';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $products = Product::join("scraped_products as sp","sp.product_id","products.id")->where("products.status_id", StatusHelper::$unknownSize)->where("products.supplier_id",">",0)
+        ->where(function($q) {
+            $q->orWhereNotNull("sp.size")->orWhere("sp.size","!=","");
+        })
+        ->select("products.*")
+        ->limit(1)->get();
+        if (!$products->isEmpty()) {
+            foreach ($products as $product) {
+                $scrapedProduct = ScrapedProducts::where("product_id", $product->id)->where(function($q) {
+                    $q->orWhereNotNull("size")->orWhere("size","!=","");
+                })->first();
+                if ($scrapedProduct) {
+                    \Log::info("Product being updated for {$product->sku} with {$scrapedProduct->size_system} and {$scrapedProduct->size}"); 
+                    if (!empty($scrapedProduct->size)) {
+                        $sizes  = explode(",", $scrapedProduct->size);
+                        $euSize = [];
+                        // Loop over sizes and redactText
+                        $allSize = [];
+                        if (is_array($sizes) && $sizes > 0) {
+                            foreach ($sizes as $size) {
+                                $allSize[]  = ProductHelper::getRedactedText($size, 'composition');
+                            }
+                        }
+
+                        $product->size = implode(',', $allSize);
+                        // get size system
+                        $supplierSizeSystem = ProductSupplier::getSizeSystem($product->id, $product->supplier_id);
+                        $euSize             = ProductHelper::getEuSize($product, $allSize, !empty($supplierSizeSystem) ? $supplierSizeSystem : $scrapedProduct->size_system);
+                        $product->size_eu   = implode(',', $euSize);
+                        ProductSizes::where('product_id', $product->id)->where('supplier_id', $product->supplier_id)->delete();
+                        if (empty($euSize)) {
+                            $product->status_id = StatusHelper::$unknownSize;
+                        } else {
+                            foreach ($euSize as $es) {
+                                ProductSizes::updateOrCreate([
+                                    'product_id' => $product->id, 'supplier_id' => $product->supplier_id, 'size' => $es,
+                                ], [
+                                    'product_id' => $product->id, 'quantity' => 1, 'supplier_id' => $product->supplier_id, 'size' => $es,
+                                ]);
+                            }
+                        }
+
+                        $product->save();
+
+                        $product->checkExternalScraperNeed();
+
+                        \Log::channel('productUpdates')->info("Saved product id :" . $product->id);
+
+                        // check for the auto crop
+                        $needToCheckStatus = [
+                            StatusHelper::$requestForExternalScraper,
+                            StatusHelper::$unknownComposition,
+                            StatusHelper::$unknownColor,
+                            StatusHelper::$unknownCategory,
+                            StatusHelper::$unknownMeasurement,
+                            StatusHelper::$unknownSize,
+                        ];
+
+                        if (!in_array($product->status_id, $needToCheckStatus)) {
+                            $product->status_id = StatusHelper::$autoCrop;
+                        }
+
+                        $product->save();
+                    }
+                }
+            }
+        }
+
+    }
+}
