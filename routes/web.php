1f1baab3e578be141ed2058f7a131a25bbe2e34a
mohit bhesaniya <mohitbhesaniya111@gmail.com>
2021-07-06 23:00:52 +0530

    ajax listing

--- routes/web.php
+++ routes/web.php
@@ -756,6 +756,7 @@ Route::group(['middleware' => ['auth', 'optimizeImages']], function () {
     Route::post('task/addRemarkStatutory', 'TaskModuleController@addRemark')->name('task.addRemarkStatutory');
 
     Route::get('task/{id}', 'TaskModuleController@show')->name('task.module.show');
+    
     Route::resource('task', 'TaskModuleController');
 
     //START - Purpose : add Route for Remind, Revise Message - DEVTASK-4354
@@ -3128,3 +3129,13 @@ Route::prefix('select2')->middleware('auth')->group(function () {
 });
 
 Route::get('whatsapp-log', 'Logging\WhatsappLogsController@getWhatsappLog')->name('whatsapp.log');
+
+
+//Magento Product Error
+
+Route::prefix('magento-product-error')->middleware('auth')->group(static function () {
+    Route::get('/', 'MagentoProductPushErrors@index')->name('magento-productt-errors.index'); 
+    Route::get('/records', 'MagentoProductPushErrors@records')->name("magento-productt-errors.records"); 
+
+    Route::post('/loadfiled', 'MagentoProductPushErrors@getLoadDataValue'); 
+});
