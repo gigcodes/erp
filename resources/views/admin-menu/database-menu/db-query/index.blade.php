31b88e77af24bc97bcdbdcaf9f741ac57f36785e
g56 <g56@gopanear.com>
2021-07-07 13:33:05 +0100

    delete in db

--- resources/views/admin-menu/database-menu/db-query/index.blade.php
+++ resources/views/admin-menu/database-menu/db-query/index.blade.php
@@ -19,13 +19,31 @@
         .padding-left-zero {
             padding-left: 0px;
         }
+        .border{
+            border: 1px solid grey;
+        } 
+        .update_column{
+            padding: 0;
+            border-right: 1px solid #dee2e6;
+        }
+        .update_column h3{
+            border-bottom: 1px solid #dee2e6;
+            padding-bottom: 15px;
+        }
+        .where_column{
+            padding: 0;
+            border-right: 1px solid #dee2e6;
+        }
+        .left_bar, .right_bar{
+            padding: 0 25px;
+        }
 
     </style>
 @endsection
 @section('content')
 
 
-    <div class="row">
+    <div class="row ml-3">
         <div class="col-lg-12 margin-tb">
             <h2>Direct Database Query Page</h2>
             <br><br>
@@ -41,12 +59,7 @@
                 @endforeach
             </select>
         </div>
-    </div>
-    <!-- <div class="col-xs-2">
-        <div class="form-group">
-            <input type="text" id="column_name" onkeyup="columnSearch()" placeholder="Search for columns.." class="form-control">
-        </div>
-    </div> -->
+    </div> 
     
     <div class="col-xs-2 text-left save_class d-none">
         <button type="submit" class="btn btn-secondary save_change_btn">Update</button>
@@ -56,13 +69,13 @@
     <div class="container_" style="margin: 0 30px">
     <form class="db_query">
         <input type="hidden" name="table_name" class="table_name" value="">
-        <div class="row">
-            <div class="col-md-6">
+        <div class="row border d-none" >
+            <div class="col-md-6 update_column">
                 <h3 class="text-center d-none">Update Columns</h3><br><br>
                 <div class="row left_bar"> 
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 update_column">
                 <h3 class="text-center d-none">Where Query</h3><br><br>
                 <div class="row right_bar">   
                 </div>
@@ -100,7 +113,7 @@ $('.table_class').change(function(){
                 let html = `
                             <div class="col-xs-6 col-sm-6 col-md-6 text-left">
                                 <input name="columns[${value.Field}]" type="checkbox" value="${value.Field}">
-                                <strong>${value.Field}</strong>
+                                <strong class="ml-3">${value.Field}</strong>
                             </div> 
                             <div class="col-xs-6 col-sm-6 col-md-6">
                                 <div class="form-group">
@@ -137,6 +150,7 @@ $('.table_class').change(function(){
     $('.save_class').removeClass('d-none');
     $('.container_ h3').removeClass('d-none');
     $('.container_ h3').removeClass('d-none');
+    $('.container_ .row').removeClass('d-none');
     $('.table_name').val(table_name);
 });
 
