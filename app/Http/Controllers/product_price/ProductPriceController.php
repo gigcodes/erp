4a788e711dcbfcf8e16ad16bf6d854ed2d32edd1
Pravin Solanki <solanki7492@gmail.com>
2021-07-09 18:39:44 +0530

    Cahnges related to website

--- app/Http/Controllers/product_price/ProductPriceController.php
+++ app/Http/Controllers/product_price/ProductPriceController.php
@@ -8,6 +8,7 @@ use App\StoreWebsite;
 use App\Product;
 use App\Setting;
 use App\SimplyDutyCountry;
+use App\Helpers\StatusHelper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
@@ -21,34 +22,43 @@ class ProductPriceController extends Controller
      */
     public function index(Request $request)
     {
+        ini_set("memory_limit", -1);
+
         $countryGroups = SimplyDutyCountry::getSelectList();
         $product_list = [];
-        if( $request->country_code && $request->product ){
-                $storeWebsites = StoreWebsite::select('title', 'id','website')->get()->toArray();
-                $product       = Product::where( 'id', $request->product )->orWhere( 'sku', $request->product )->first();
-            
-                if(empty( $product )){
+        if( $request->country_code){
+                $storeWebsites = StoreWebsite::select('title', 'id','website')->where("is_published","1")->get()->toArray();
+                if(strtolower($request->random) == "yes" && empty($request->product)) {
+                    $products = Product::where( 'status_id', StatusHelper::$finalApproval)->groupBy('category')->limit(50)->latest()->get();
+                }else{
+                    $products = Product::where( 'id', $request->product )->orWhere( 'sku', $request->product )->get();
+                }
+
+                if($products->isEmpty()){
                     return redirect()->back()->with('error','No product found');
                 }
-                foreach ($storeWebsites as $key => $value) {
-                    $dutyPrice = $product->getDuty( $request->country_code );
-                    $price = $product->getPrice( $value['id'], $request->country_code,null, true,$dutyPrice);
-                    $ivaPercentage = \App\Product::IVA_PERCENTAGE;
 
-                    $product_list[] = [
-                        'getPrice'     => $price,
-                        'id'           => $product->id,
-                        'sku'          => $product->sku,
-                        'brand'        => $product->brands->name,
-                        'segment'      => $product->brands->brand_segment,
-                        'website'      => $value['website'],
-                        'eur_price'    => number_format($price['original_price'],2,'.',''),
-                        'seg_discount' => (float)$price['segment_discount'],
-                        'iva'          => \App\Product::IVA_PERCENTAGE."%",
-                        'add_duty'     => $product->getDuty( $request->country_code)."%",
-                        'add_profit'   => number_format($price['promotion'],2,'.',''),
-                        'final_price'  => number_format($price['total'],2,'.',''),
-                    ];
+                foreach ($storeWebsites as $key => $value) {
+                    foreach($products as $product) {
+                        $dutyPrice = $product->getDuty( $request->country_code );
+                        $price = $product->getPrice( $value['id'], $request->country_code,null, true,$dutyPrice);
+                        $ivaPercentage = \App\Product::IVA_PERCENTAGE;
+
+                        $product_list[] = [
+                            'getPrice'     => $price,
+                            'id'           => $product->id,
+                            'sku'          => $product->sku,
+                            'brand'        => ($product->brands) ? $product->brands->name : "-",
+                            'segment'      => ($product->brands) ? $product->brands->brand_segment : "-",
+                            'website'      => $value['website'],
+                            'eur_price'    => number_format($price['original_price'],2,'.',''),
+                            'seg_discount' => (float)$price['segment_discount'],
+                            'iva'          => \App\Product::IVA_PERCENTAGE."%",
+                            'add_duty'     => $product->getDuty( $request->country_code)."%",
+                            'add_profit'   => number_format($price['promotion'],2,'.',''),
+                            'final_price'  => number_format($price['total'],2,'.',''),
+                        ];
+                    }
                 }
         }
 
