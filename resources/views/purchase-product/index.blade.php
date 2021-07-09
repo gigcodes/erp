0371aca753c0427ae8a0340c45f2190789813726
g56 <g56@gopanear.com>
2021-07-09 09:55:13 +0100

    #DEVTASK-19917/20083-IMAGE SEARCH FOR PURCAHSE

--- resources/views/purchase-product/index.blade.php
+++ resources/views/purchase-product/index.blade.php
@@ -274,8 +274,8 @@
                 @if ($order_product && $order_product->product)
               <i title="Add Supplier for this product" class="fa fa-user-plus add_supplier" aria-hidden="true" data-product_id="{{$order_product->product->id}}" data-product_name="{{$order_product->product->name}}"></i>
               @endif 
-              @if(count($order->orderProducts))
-              <button type="button" class="btn btn-xs image-button" data-object="customer" data-order-id="{{$order->id}}" data-product-id="{{implode(',', $order->orderProducts->pluck('id')->toArray())}}" data-attached="1" data-limit="10" data-load-type="images" data-all="1" data-is_admin="1" data-is_hod_crm="" title="Load Auto Images attacheds"><img src="/images/archive.png" alt="" style="cursor: nwse-resize; width: 16px;"></button>
+              @if(count($order->orderProducts)) 
+              <button type="button" class="btn btn-xs image-button" style="cursor: pointer;" data-customer-id="{{$order->customer_id}}" data-order-id="{{$order->id}}" data-product-id="{{implode(',', $order->orderProducts->pluck('product_id')->toArray())}}" data-attached="1" data-limit="10" data-load-type="images" data-all="1" data-is_admin="1" data-is_hod_crm="" title="Load Auto Images attacheds"><img src="/images/archive.png" alt="" style="cursor: pointer; width: 16px;"></button>
               @endif
               </td>
 
@@ -378,6 +378,25 @@
     </div>
 </div>
 <div id="estdelhistoryresponse"></div>
+<div id="order-product-images" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header">
+              <h4 class="modal-title">Product Images</h4>
+          </div>
+          <div class="modal-body" style="background-color: #999999;">
+            <div style="overflow-x:auto;"><input type="text" id="click-to-clipboard-message" class="link" style="position: absolute; left: -5000px;">
+            <table class="table table-bordered">
+              <tbody style="background-color: #999999"></tbody>
+              </table>
+            </div>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+      </div>
+  </div>
+</div>
 @endsection
 @include('common.commonEmailModal')
 @include("partials.modals.update-delivery-date-modal")
@@ -398,75 +417,73 @@
 
 $(document).on('click', '.image-button', function () {
     var thiss = $(this);
-    // var object_type = $(this).data('object');
-    var product_ids = $(this).attr('data-product-id');
-    // var object_id = $(this).data('id');
-    // var load_attached = $(this).data('attached');
-    // var load_all = $(this).data('all');
-    //     load_type = $(this).data('load-type');
-    // var is_admin = $(this).data('is_admin');
-    // var is_hod_crm = $(this).data('is_hod_crm');
-    // var limit = 1000;
-    // if(typeof $(this).data('limit') != "undefined") {
-    //     limit = $(this).data('limit');
-    // }
-    // thiss.parent().find('.td-full-container').toggleClass('hidden');
-	// currentChatParams.url = "/purchase-product/order-product-images?product_ids="+ product_ids;
-  //   currentChatParams.data = {
-  //       limit: limit,
-  //       load_all: load_all,
-  //       load_attached: load_attached,
-  //       load_type: load_type,
-  //       page : 1,
-  //       hasMore : true,
-  //       object_name:object_type,
-  //       object_val:object_id
-  //   }
-
+    var product_ids = $(this).attr('data-product-id'); 
+    var customer_id = $(this).attr('data-customer-id'); 
 
     $.ajax({
         type: "GET",
-        url: "/purchase-product/order-product/images?product_ids="+ 297557, 
+        url: `/purchase-product/order-product/images?product_ids=`+ product_ids, 
         beforeSend: function () {
             // $(thiss).text('Loading...');
         }
-    }).done(function (response) {
-        // var li = getHtml(response);
-        console.log(response, 888);
-        // if ($('#chat-list-history').length > 0) {
-        //     $("#chat-list-history").find(".modal-dialog").css({"width":"1000px","max-width":"1000px"});
-        //     $("#chat-list-history").find(".modal-body").css({"background-color":"white"});
-        //     $("#chat-list-history").find(".modal-body").html(li);
-        //     $("#chat-list-history").find('#chat_obj_type').val(object_type);
-        //     $("#chat-list-history").find('#chat_obj_id').val(object_id);
-        //     $("#chat-list-history").modal("show");
-        // } else {
-        //     $("#chat-list-history").find(".modal-dialog").css({"width":"1000px","max-width":"1000px"});
-        //     $("#chat-list-history").find(".modal-body").css({"background-color":"white"});
-        //     $("#chat-list-history").find('#chat_obj_type').val(object_type);
-        //     $("#chat-list-history").find('#chat_obj_id').val(object_id);
-        //     $("#chat-history").html(li);
-        // }
-
-        // var searchterm = $('.search_chat_pop').val();
-        // if(searchterm && searchterm != '') {
-        //     var value = searchterm.toLowerCase();
-        //     $(".filter-message").each(function () {
-        //         if ($(this).text().search(new RegExp(value, "i")) < 0) {
-        //             $(this).hide();
-        //         } else {
-        //             $(this).show()
-        //         }
-        //     });
-        // }
+    }).done(function (response) { 
+        $("#order-product-images table tbody").html('');
+        $("#order-product-images").find(".modal-dialog").css({"width":"700","max-width":"700"});
+        $("#order-product-images").find(".modal-body").css({"background-color":"white"});
+        let images = response.images;
+        if(images.length){
+          for(let i=0; i< images.length; i++){
+            $("#order-product-images table tbody").append(`
+              <td style="width:45%">
+                <div class="speech-wrapper full-match-img">
+                  <div id="1786233" class="bubble alt">
+                    <div class="txt">
+                      <p class="name alt"></p>
+                      <p class="message" data-message=""></p>
+                      <div style="margin-bottom:10px;">
+                        <div class="row">
+                          <div class="col-md-4">
+                            <a href="${images[i].image_url}" class="show-product-info" target="_blank">
+                              <img src="${images[i].image_url}" style="max-width: 100%; cursor: default;">
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td style="width:30%">&nbsp; 
+                <button style="padding: 3px 6px !important;" title="Search Product Image" data-media-url="'${images[i].image_url}'" data-product-id="${images[i].product_id}" data-customer-id="${customer_id}" class="btn btn-xs btn-secondary search-product-image"><i class="fa fa-search" aria-hidden="true"></i></button>
+              </td>
+            `); 
+          }
+        }
+        $("#order-product-images").modal("show"); 
 
     }).fail(function (response) {
         console.log(response, 777);
         //$(thiss).text('Load More');
 
-        alert('Could not load messages');
+        alert('Could not load images');
 
     });
+});
+
+$(document).on('click','.search-product-image',function(event){
+  $.ajax({
+      type: "GET", 
+      url: "/erp-customer/search-image/"+$(this).attr('data-product-id')+'/'+$(this).attr('data-media-url').replaceAll('/', '|')+'?customer_id='+$(this).attr('data-customer-id'), 
+      beforeSend : function() {
+          toastr['success']('Image find process is started, you will get whatsapp notification as this process will be completed.');
+      },
+      success: function(response) { 
+
+      },
+      error: function() {
+          toastr['error']('Error occured please try again later!');
+      }
+  });
 });
 
 $(document).on('click', '.view-details', function(e) {
