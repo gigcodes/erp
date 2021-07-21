d731b5211f8de63234701dd6719f85ae43e678a9
g60 <g60@gopanear.com>
2021-07-21 11:41:10 +0530

    added scrapper action in in scrapper-phytho page

--- resources/views/scrapper-phyhon/attached-image-load.blade.php
+++ resources/views/scrapper-phyhon/attached-image-load.blade.php
@@ -104,33 +104,34 @@
     </td>
 </tr>
 
-  <!-- Modal -->
-  <div class="modal fade" id="scrapper-python-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal -->
+<div class="modal fade" id="scrapper-python-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title" id="scrapper-python-title"> Site scrapper action </h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="scrapper-python-title"> Site scrapper action </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
 
 
-            <form action="" method="POST" id="scrapper-python-form">
-                @csrf
-          
-                <div class="d-flex justify-content-between mt-2 mb-4" >
+
+                <form action="" method="POST" id="scrapper-python-form">
+                    @csrf
+
+                    <div class="d-flex justify-content-between mt-2 mb-4">
                         <div class="">
                             <div class="mb-3">
-                                <input  type="radio" name="name" id="start" value="start" checked>
+                                <input type="radio" name="name" id="start" value="start" checked>
                                 <label class="form-check-label" for="start">
-                                Start
+                                    Start
                                 </label>
                             </div>
-                            <div >
-                                <input  type="radio" name="type" id="desktop" value="desktop" checked>
+                            <div>
+                                <input type="radio" name="type" id="desktop" value="desktop" checked>
                                 <label class="form-check-label" for="desktop">
                                     Desktop
                                 </label>
@@ -138,13 +139,13 @@
                         </div>
                         <div class="">
                             <div class="mb-3">
-                                <input  type="radio" name="name" id="stop" value="stop">
+                                <input type="radio" name="name" id="stop" value="stop">
                                 <label class="form-check-label" for="stop">
-                                Stop
+                                    Stop
                                 </label>
                             </div>
-                            <div >
-                                <input  type="radio" name="type" id="mobile" value="mobile">
+                            <div>
+                                <input type="radio" name="type" id="mobile" value="mobile">
                                 <label class="form-check-label" for="mobile">
                                     Mobile
                                 </label>
@@ -154,80 +155,79 @@
 
                         <div class="">
                             <div class="mb-3">
-                                <input  type="radio" name="name" id="get-status" value="get-status">
+                                <input type="radio" name="name" id="get-status" value="get-status">
                                 <label class="form-check-label" for="get-status">
-                                Get status
+                                    Get status
                                 </label>
                             </div>
-                            <div >
-                                <input  type="radio" name="type" id="tablet" value="tablet">
+                            <div>
+                                <input type="radio" name="type" id="tablet" value="tablet">
                                 <label class="form-check-label" for="tablet">
                                     Tablet
                                 </label>
-                            </div>    
-                        </div>             
-                </div>
-                <div class="modal-footer pb-0">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-secondary">Send request</button>
-                </div>
-             
-            </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer pb-0">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-secondary">Send request</button>
+                    </div>
+
+                </form>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 
 
-  <script>
-let websiteName = null;
+<script>
+    let websiteName = null;
 
-    $(document).on('click','.scrapper-python-modal',function(e){
+    $(document).on('click', '.scrapper-python-modal', function(e) {
 
-        websiteName = $(this).data('website').replace('www.','').replace('.com','')
-        
+        websiteName = $(this).data('website').replace('www.', '').replace('.com', '')
+
     })
 
 
-$(document).on('submit','#scrapper-python-form',function(e){
+    $(document).on('submit', '#scrapper-python-form', function(e) {
 
-e.preventDefault()
+        e.preventDefault()
 
-$this = $(this)
-const formData = $this.serialize()
-const typeOfData = $('input[name="type"]:checked').val();
-const nameOfData = $('input[name="name"]:checked').val();
+        $this = $(this)
+        const formData = $this.serialize()
+        const typeOfData = $('input[name="type"]:checked').val();
+        const nameOfData = $('input[name="name"]:checked').val();
 
-console.log(websiteName,'aaaaaaaaa')
+        console.log(websiteName, 'aaaaaaaaa')
 
-                $.ajax({
-                    type: 'POST',
-                    url: "/scrapper-python/call",
-                    beforeSend: function () {
-                        $("#loading-image").show();
-                    },
-                    data: {
-                        _token: "{{ csrf_token() }}",
-                        webName: websiteName,
-                        type:typeOfData,
-                        data_name:nameOfData
-                    },
-                    dataType: "json"
-                }).done(function (response) {
-                    $("#loading-image").hide();
-                        if(response.message){
-                        toastr['success'](response.message, 'success');
-                        }else{
-                        toastr['error'](response.err, 'error');
-                        }
-                        $('#scrapper-python-modal').modal('hide')
-                }).fail(function (response) {
-                    $("#loading-image").hide();
-                    $('#scrapper-python-modal').modal('hide')
+        $.ajax({
+            type: 'POST',
+            url: "/scrapper-python/call",
+            beforeSend: function() {
+                $("#loading-image").show();
+            },
+            data: {
+                _token: "{{ csrf_token() }}",
+                webName: websiteName,
+                type: typeOfData,
+                data_name: nameOfData
+            },
+            dataType: "json"
+        }).done(function(response) {
+            $("#loading-image").hide();
+            if (response.message) {
+                toastr['success'](response.message, 'success');
+            } else {
+                toastr['error'](response.err, 'error');
+            }
+            $('#scrapper-python-modal').modal('hide')
+        }).fail(function(response) {
+            $("#loading-image").hide();
+            $('#scrapper-python-modal').modal('hide')
 
-                    console.log("Sorry, something went wrong");
-                });
+            console.log("Sorry, something went wrong");
+        });
 
-})
-
-  </script>
+    })
+</script>
