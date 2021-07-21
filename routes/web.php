59a6ba87c62e07157737dc63be5647e6a0e0bd9d
Pravin Solanki <solanki7492@gmail.com>
2021-07-21 17:59:52 +0530

    Fix drafted product lead create

--- routes/web.php
+++ routes/web.php
@@ -1751,6 +1751,7 @@ Route::group(['middleware' => ['auth', 'optimizeImages']], function () {
     Route::post('/drafted-products/edit', 'ProductController@editDraftedProducts');
     Route::post('/drafted-products/delete', 'ProductController@deleteDraftedProducts');
     Route::post('/drafted-products/addtoquicksell', 'ProductController@addDraftProductsToQuickSell');
+    Route::post('/drafted-products/send-lead-price', 'ProductController@sendLeadPrice');
 
 });
 
