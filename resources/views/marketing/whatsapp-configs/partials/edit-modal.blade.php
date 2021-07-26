bda2fb29998c1f49aabc9186b608508c7e78804b
Pravin Solanki <solanki7492@gmail.com>
2021-07-26 18:07:55 +0530

    fix issue with multi select

--- resources/views/marketing/whatsapp-configs/partials/edit-modal.blade.php
+++ resources/views/marketing/whatsapp-configs/partials/edit-modal.blade.php
@@ -70,11 +70,11 @@
     					</div>
 						
 						@php
-							$defaultForArr = explode(",",$whatsAppConfig->default_for);
+                            $defaultForArr = explode(",",$whatsAppConfig->default_for);
 						@endphp
 						<div class="form-group">
     						<strong>Default For:</strong>
-    						<select class="form-control select-multiple-default_for" name="default_for[]" multiple>
+    						<select class="form-control select-multiple-default_for" id="select-multiple-default_for_{{$whatsAppConfig->id}}" name="default_for[]" multiple>
 								
     							<option value="1" @if(in_array(1,$defaultForArr)) selected @endif>Customer</option>
     							<option value="2" @if(in_array(2,$defaultForArr)) selected @endif>Vendor</option>
