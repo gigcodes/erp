3f8d128143cdaa2f758e5063b931f242703c82c7
g70 <g70@gopanear.com>
2021-08-04 15:17:31 +0530

    DEVTASK-20183 Uploads folder list of files done

--- resources/views/project_directory_manager/index.blade.php
+++ resources/views/project_directory_manager/index.blade.php
@@ -141,7 +141,7 @@
         <div class="modal-dialog modal-lg" role="document">
           <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">File Name And Size Details</h4>
+                <h4 class="modal-title">Uploads folder list of files</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
@@ -156,7 +156,7 @@
                                 <tr>
                                    
                                     <th style="word-wrap: break-word; width=50%">File Name</th>
-                                    <th>FIle Size</th>
+                                    <th>File Size</th>
                                    
                                 </tr>
                             </thead>
