82d1ef3510e3664b4280c36dca8f9e518781b0f3
g56 <g56@gopanear.com>
2021-08-06 19:26:44 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,17 +6,17 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // $this->call(TwilioCredentialSeeder::class);
-        // $this->call(PermissionTableSeeder::class);
-        // $this->call(UserTableSeeder::class);
-        // $this->call(ProductsTableSeeder::class);
-        // $this->call(CustomerTableSeeder::class);
-        // $this->call(ChatMessageSeeder::class);
-        // $this->call(LandingPageStatusSeeder::class);
-        // $this->call(EmailLeadsSeeder::class);
+        $this->call(TwilioCredentialSeeder::class);
+        $this->call(PermissionTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(CustomerTableSeeder::class);
+        $this->call(ChatMessageSeeder::class);
+        $this->call(LandingPageStatusSeeder::class);
+        $this->call(EmailLeadsSeeder::class);
 
-        // $this->call(PaymentReceiptSeeder::class);
-        // $this->call(TeamSeeder::class);
+        $this->call(PaymentReceiptSeeder::class);
+        $this->call(TeamSeeder::class);
         $this->call(SettingsUpdate::class);
 
 
