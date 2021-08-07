8dca6d9c6327baae38079818bf4c6a6d68022fe4
Bhavesh Chadaniya <hotweb07@gmail.com>
2021-08-07 19:09:26 +0530

    Lead and Order Pricing Filter

--- resources/views/lead-order/index.blade.php
+++ resources/views/lead-order/index.blade.php
@@ -16,7 +16,27 @@
 
                 <div class="col-md-10 col-sm-12">
 
-
+                  <div class="col-10" style="padding-left:0px;">
+                    <div >
+                    <form class="form-inline" action="{{ route('lead-order.index') }}" method="GET">
+                        
+                        <div class="form-group col-md-3 pd-3">
+                          <input style="width:100%;" name="term" type="text" class="form-control"
+                                 value="{{ isset($term) ? $term : '' }}"
+                                 placeholder="Search">
+                        </div>        
+        
+                        <div class="form-group col-md-3 pd-3">
+                          <?php echo Form::select("brand_id",["" => "-- Select Brands --"]+$brandList,request('brand_id',[]),["class" => "form-control select2"]); ?>
+                        </div>
+        
+                           <div class="form-group col-md-1 pd-3">
+                        <button type="submit" class="btn btn-image ml-3"><img src="{{asset('images/filter.png')}}" /></button>
+                          </div>
+                      </form>
+                       
+                    </div>
+                     </div>
                 </div>
 
             </div>
@@ -43,9 +63,15 @@
 
     </div>
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.min.js"></script>
     <script type="text/javascript">
-
+    $(document).ready(function() {
+      $('#order-datetime').datetimepicker({
+        format: 'YYYY-MM-DD'
+      });
+      $(".select2").select2({tags:true});
+    });
       $(document).on('click', '.pagination a, th a', function(e) {
 
         e.preventDefault();
@@ -90,4 +116,7 @@
 
     </script>
 
+@endsection
+@section('scripts')
+  
 @endsection
