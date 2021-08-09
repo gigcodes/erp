9fd7862a295f01d4c62aa91201653b181526dd24
g60 <g60@gopanear.com>
2021-08-09 15:55:52 +0530

    added product updated from csv or not

---
+++ app/ProductPushInformationSummery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPushInformationSummery extends Model
+{
+       protected $fillable = ['brand_id','category_id','store_website_id','product_push_count'];
+
+       
+    public function brand()
+    {
+        return $this->hasOne(Brand::class,'id','brand_id');
+    }
+
+
+    public function category()
+    {
+        return $this->hasOne(Category::class,'id','category_id');
+
+    }
+
+    public function storeWebsite()
+    {
+        return $this->hasOne(StoreWebsite::class,'id','store_website_id');
+    }
+
+
+}
+
+
