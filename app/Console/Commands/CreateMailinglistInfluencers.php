56439e206b5413d8ac058cc55811e49cb210ebfc
Pravin Solanki <solanki7492@gmail.com>
2021-08-11 19:38:51 +0530

    add mailing list chanages

--- app/Console/Commands/CreateMailinglistInfluencers.php
+++ app/Console/Commands/CreateMailinglistInfluencers.php
@@ -2,9 +2,8 @@
 
 namespace App\Console\Commands;
 
-
-use Illuminate\Console\Command;
 use App\Customer;
+use Illuminate\Console\Command;
 
 class CreateMailinglistInfluencers extends Command
 {
@@ -32,6 +31,8 @@ class CreateMailinglistInfluencers extends Command
         parent::__construct();
     }
 
+    public $mailList = [];
+
     /**
      * Execute the console command.
      *
@@ -39,132 +40,112 @@ class CreateMailinglistInfluencers extends Command
      */
     public function handle()
     {
-               
-        $services = \App\Service::first();
-        $service_id=$services->id;
-        $influencers = \App\ScrapInfluencer::where('read_status','!=',1)->where('email','!=',"")->get();
-        $websites = \App\StoreWebsite::select('id','title')->orderBy('id','desc')->get();
-       
-        $email_list=array();
-        $email_list2=array();
-        $listIds=array();
-        foreach($influencers as $influencer)
-        {
-            $email_list[]=['email'=>$influencer->email,'name'=>$influencer->name,'platform'=>$influencer->platform];
+
+        $influencers = \App\ScrapInfluencer::where(function($q) {
+            $q->orWhere("read_status","!=",1)->orWhereNull('read_status');
+        })->where('email', '!=', "")->get();
+
+        $websites = \App\StoreWebsite::select('id', 'title')->where("website_source", "magento")->orderBy('id', 'desc')->get();
+
+        /*foreach ($influencers as $influencer) {
+        $email_list[] = ['email' => $influencer->email, 'name' => $influencer->name, 'platform' => $influencer->platform];
+        }*/
+
+        foreach ($websites as $website) {
+            $name = $website->title;
+            if ($name != '') {
+                $name = $name . "_" . date("d_m_Y");
+            } else {
+                $name = 'WELCOME_LIST_' . date("d_m_Y");
+            }
+
+            $mailingList = \App\Mailinglist::where('name', $name)->where("website_id", $website->id)->where('remote_id', ">", 0)->first();
+            if (!$mailingList) {
+                
+                $mailList = \App\Mailinglist::create([
+                    'name'       => $name,
+                    'website_id' => $website->id,
+                    'service_id' => 1,
+                ]);
+
+                $response = $this->callApi("https://api.sendinblue.com/v3/contacts/lists", "POST", $data = [
+                    "folderId" => 1,
+                    "name"     => $name,
+                ]);
+                
+                if (isset($response->id)) {
+                    $mailList->remote_id = $response->id;
+                    $mailList->save();
+                    $this->mailList[] = $mailList;
+                }
+            }else{
+                $this->mailList[] = $mailingList;
+            }
         }
+        
+        if (!empty($influencers) && !empty($this->mailList)) {
 
-        foreach($websites as $website)
-                {
-                    $name=$website->title;
-                    if ($name!='')
-                    $name=$name."_".date("d_m_Y");
-                    else
-                       $name='WELCOME_LIST_'.date("d_m_Y");
-                    $res='';
-                    
-                    
-                    for($count=0;$count<count($email_list);$count++) 
-                    {
-                        $email=$email_list[$count]['email'];
-                        if (!\App\Mailinglist::where('email',$email)->where('website_id',$website->id)->first())
-                        {
-                            
-                           if (!isset($res->id))
-                            {
-                               
-                             $curl = curl_init();
-                             $data = [
-                                 "folderId" => 1,
-                                 "name" =>$name
-                             ];
-                             curl_setopt_array($curl, array(
-                                 CURLOPT_URL => "https://api.sendinblue.com/v3/contacts/lists",
-                                 CURLOPT_RETURNTRANSFER => true,
-                                 CURLOPT_ENCODING => "",
-                                 CURLOPT_MAXREDIRS => 10,
-                                 CURLOPT_TIMEOUT => 0,
-                                 CURLOPT_FOLLOWLOCATION => true,
-                                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                                 CURLOPT_CUSTOMREQUEST => "POST",
-                                 CURLOPT_POSTFIELDS => json_encode($data),
-                                 CURLOPT_HTTPHEADER => array(
-                                     "api-key: ".getenv('SEND_IN_BLUE_API'),
-                                     "Content-Type: application/json"
-                                 ),
-                             ));
-             
-                             $response = curl_exec($curl);
-             
-                             curl_close($curl);
-                             \Log::info($response);
-                             $res = json_decode($response);
-                                       
-                             \App\Mailinglist::create([
-                                 'name' => $name,
-                                 'website_id' => $website->id,
-                                 'service_id' => $service_id,
-                                 'email' =>$email,
-                                 'remote_id' =>$res->id,
-                             ]);
-                             $listIds[]=$res->id;
-     
-                           } 
-                            if (isset($res->id))
-                           {
-                           $email_list2[]=['email'=>$email_list[$count]['email'],'name'=>$email_list[$count]['name']];
-                           
-                            if (!\App\Customer::where('email',$email)->first())
-                            {
-                                    $customer = new Customer;
-                            
-                                    $customer->email            = $email;
-                                    $customer->name             = $email_list[$count]['name'];
-                                    $customer->store_website_id = $website->id;
-                                    $customer->save();
-                            }   
-                           }
-                           
-                            
-                        }
+            $listIds = [];
+            if (!empty($this->mailList)) {
+                foreach ($this->mailList as $mllist) {
+                    $listIds[] = intval($mllist->remote_id);
+                }
+            }
 
-                    }  
-                   
-                   
-                    
+
+            foreach ($influencers as $list) {
+                
+                $response = $this->callApi("https://api.sendinblue.com/v3/contacts", "POST", [
+                    "email"      => $list->email,
+                    "listIds"    => $listIds,
+                    "attributes" => ['firstname' => $list->name],
+                    "updateEnabled" => true
+                ]);
+
+                $customer = Customer::where('email', $list->email)->first();
+                if (!$customer) {
+                    $customer = Customer::create([
+                        "name"   => $list->name,
+                        "email"  => $list->email,
+                        "source" => "scrap_influencer",
+                    ]);
+                }
+                if (!empty($this->mailList)) {
+                    foreach ($this->mailList as $mllist) {
+                        $mllist->listCustomers()->attach($customer->id);
+                    }
                 }
 
-       
-                for($count=0;$count<count($email_list2);$count++) 
-       {
-        $email=$email_list2[$count]['email'];
+                $list->read_status = 1;
+                $list->save();
+            }
+        }
+    }
+
+    public function callApi($url, $method, $data = [])
+    {
         $curl = curl_init();
-        $data = [
-           "email" =>$email,
-           "listIds" =>$listIds,
-           "attributes"=>['firstname'=>$email_list2[$count]['name']]
-       ];
-       curl_setopt_array($curl, array(
-           CURLOPT_URL => "https://api.sendinblue.com/v3/contacts",
-           CURLOPT_RETURNTRANSFER => true,
-           CURLOPT_ENCODING => "",
-           CURLOPT_MAXREDIRS => 10,
-           CURLOPT_TIMEOUT => 0,
-           CURLOPT_FOLLOWLOCATION => true,
-           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-           CURLOPT_CUSTOMREQUEST => "POST",
-           CURLOPT_POSTFIELDS => json_encode($data),
-           CURLOPT_HTTPHEADER => array(
-               "api-key: ".getenv('SEND_IN_BLUE_API'),
-               "Content-Type: application/json"
-           ),
-       ));
-       $response = curl_exec($curl);
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => $method,
+            CURLOPT_POSTFIELDS     => json_encode($data),
+            CURLOPT_HTTPHEADER     => array(
+                "api-key: " . getenv('SEND_IN_BLUE_API'),
+                "Content-Type: application/json",
+            ),
+        ));
+
+        $response = curl_exec($curl);
 
         curl_close($curl);
-        
-       }       
-       
-       \App\ScrapInfluencer::update(['read_status'=>1]);
-       
+        \Log::info($response);
+        return json_decode($response);
     }
-}   
+}
