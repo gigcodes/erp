22f45daf14a97183ee02c482ec4261d348ee264b
g70 <g70@gopanear.com>
2021-08-13 12:49:00 +0530

    DEVTASK-20148 Migration For Every table which is use in this tasks

---
+++ database/migrations/2021_08_13_111410_alter_table_truncate_three_table_20148.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\product_discount_excel_file;
+use App\SupplierDiscountLogHistory;
+use App\SupplierBrandDiscount;
+
+class AlterTableTruncateThreeTable20148 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        product_discount_excel_file::truncate();
+        SupplierDiscountLogHistory::truncate();
+        SupplierBrandDiscount::truncate();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
