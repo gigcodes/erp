e0710023bb6bb3c3bced9b6b8c81256757eae791
developer <madhukantmaurya1988@gmail.com>
2021-08-17 10:44:07 +0530

    subject

--- app/CampaignEvent.php
+++ app/CampaignEvent.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CampaignEvent extends Model
 {
-    protected $fillable = [ 'id', 'email', 'event', 'date_event', 'created_at', 'updated_at'];
+    protected $fillable = [ 'id', 'email', 'event', 'date_event', 'subject', 'created_at', 'updated_at'];
     protected $table = "campaign_events";
 }
