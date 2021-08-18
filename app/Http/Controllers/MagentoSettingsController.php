6ffd640f0a614b8ab72e867aa27aeb1bf9e63ff2
g56 <g56@gopanear.com>
2021-08-18 12:54:09 +0530

    add-edit magento-setting

--- app/Http/Controllers/MagentoSettingsController.php
+++ app/Http/Controllers/MagentoSettingsController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\MagentoSetting;
 use App\StoreWebsite;
-use App\WebsiteStore;
+use App\Website;
+use App\WebsiteStore; 
 use App\WebsiteStoreView;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 
 
 class MagentoSettingsController extends Controller
@@ -17,52 +18,93 @@ class MagentoSettingsController extends Controller
 
         $magentoSettings = MagentoSetting::with(
             'storeview.websiteStore.website.storeWebsite', 
-            'store.website.storeWebsite')
-            ->orderBy('created_at', 'DESC')
-            ->paginate(100);
+            'store.website.storeWebsite',
+            'website');
 
-      //  dd($magentoSettings);
+        if($request->scope){
+            $magentoSettings = $magentoSettings->where('scope', $request->scope);
+        }
 
+        if($request->website){
+            $magentoSettings = $magentoSettings->whereHas('storeview.websiteStore.website.storeWebsite', function($q) use ($request){
+                $q->where('id', $request->website);
+            })->orWhereHas('store.website.storeWebsite', function($q) use ($request){
+                $q->where('id', $request->website);
+            })->orWhereHas('website', function($q) use ($request){
+                $q->where('id', $request->website);
+            });
+        }
+
+        if($request->store){
+            $magentoSettings = $magentoSettings->whereHas('storeview.websiteStore', function($q) use ($request){
+                $q->where('name', $request->store);
+            })->orWhereHas('store', function($q) use ($request){
+                $q->where('name', $request->store);
+            });
+        }
+
+        if($request->store_view){
+            $magentoSettings = $magentoSettings->whereHas('storeview', function($q) use ($request){
+                $q->where('code', $request->store_view);
+            });
+        }
+
+        $magentoSettings = $magentoSettings->orderBy('created_at', 'DESC')->paginate(25);    
+        $storeWebsites = StoreWebsite::get();
+        $websitesStores = WebsiteStore::get()->pluck('name')->unique()->toArray();
+        $websiteStoreViews = WebsiteStoreView::get()->pluck('code')->unique()->toArray();
+        
         return view('magento.settings.index', [
-            'magentoSettings' => $magentoSettings
+            'magentoSettings' => $magentoSettings,
+            'storeWebsites' => $storeWebsites,
+            'websitesStores' => $websitesStores ,
+            'websiteStoreViews' => $websiteStoreViews,
         ]);
     }
 
     public function create(Request $request)
-    {
-
+    { 
+        $name = $request->name;
+        $path = $request->path;
+        $value = $request->value;
         foreach ($request->scope as $scope) {
 
             if ($scope === 'default') {
 
-                $storeWebsites = StoreWebsite::get();
+                $storeWebsites = StoreWebsite::whereIn('id', $request->website)->get();
 
                 foreach($storeWebsites as $storeWebsite){
 
-                    MagentoSetting::create([
-                        'scope' => $scope,
-                        'scope_id' => $storeWebsite->id,
-                        'name' => $request->name,
-                        'path' => $request->path,
-                        'value' => $request->value
-                    ]);
+                    $m_setting = MagentoSetting::where('scope', $scope)->where('scope_id',$storeWebsite->id)->where('path', $path)->first();
+                    if(!$m_setting){
+                        $m_setting = MagentoSetting::Create([
+                            'scope' => $scope,
+                            'scope_id' => $storeWebsite->id,
+                            'name' => $name,
+                            'path' => $path,
+                            'value' => $value
+                        ]);
+                    }
 
                 }
             }
 
             if($scope === 'websites'){
 
-                $websiteStores = WebsiteStore::get();
+                $websiteStores = WebsiteStore::whereIn('id', $request->website_store)->get();
 
                 foreach($websiteStores as $websiteStore){
 
-                    MagentoSetting::create([
-                        'scope' => $scope,
-                        'scope_id' => $websiteStore->id,
-                        'name' => $request->name,
-                        'path' => $request->path,
-                        'value' => $request->value
-                    ]);
+                    $m_setting = MagentoSetting::where('scope', $scope)->where('scope_id',$websiteStore->id)->where('path', $path)->first();
+                    if(!$m_setting){
+                        $m_setting = MagentoSetting::Create([
+                            'scope' => $scope,
+                            'scope_id' => $websiteStore->id,
+                            'name' => $name,
+                            'path' => $path,
+                            'value' => $value
+                        ]);
+                    }
 
                 }
                 
@@ -70,17 +112,20 @@ class MagentoSettingsController extends Controller
 
             if($scope === 'stores'){
 
-                $websiteStoresViews = WebsiteStoreView::get();
+                $websiteStoresViews = WebsiteStoreView::whereIn('id', $request->website_store_view)->get();
 
                 foreach($websiteStoresViews as $websiteStoresView){
 
-                    MagentoSetting::create([
-                        'scope' => $scope,
-                        'scope_id' => $websiteStoresView->id,
-                        'name' => $request->name,
-                        'path' => $request->path,
-                        'value' => $request->value
-                    ]);
+                    $m_setting = MagentoSetting::where('scope', $scope)->where('scope_id',$websiteStoresView->id)->where('path', $path)->first();
+                    if(!$m_setting){
+                        $m_setting = MagentoSetting::Create([
+                            'scope' => $scope,
+                            'scope_id' => $websiteStoresView->id,
+                            'name' => $name,
+                            'path' => $path,
+                            'value' => $value
+                        ]);
+                    }
 
                 }
                 
@@ -94,5 +139,135 @@ class MagentoSettingsController extends Controller
 
     public function update(Request $request)
     {
+        // dd($request->all());
+        // $postURL = 'https://sololuxury.com/rest/V1/store/storeViews';
+        // $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+        // dd($postURL, $result);
+        $scope = $request->scope;
+        $name = $request->name;
+        $path = $request->path;
+        $value = $request->value;
+        $is_live = isset($request->live);
+        $is_development = isset($request->development);
+        $website_ids = $request->websites;
+        if ($scope === 'default') {
+
+            $storeWebsites = StoreWebsite::whereIn('id', $website_ids)->get();
+
+            foreach($storeWebsites as $storeWebsite){
+
+                $m_setting = MagentoSetting::where('scope', $scope)->where('scope_id',$storeWebsite->id)->where('path', $path)->first();
+                if(!$m_setting){
+                    $m_setting = MagentoSetting::Create([
+                        'scope' => $scope,
+                        'scope_id' => $storeWebsite->id,
+                        'name' => $name,
+                        'path' => $path,
+                        'value' => $value
+                    ]);
+                }
+                $token = $storeWebsite->api_token;
+                \Cache::forever('key', $token);
+                if($is_live){
+                    $postURL = 'https://dev.sololuxury.com/rest/V1/configvalue/set?path=' . $path . '&value=' . $path . '&scope=' . $scope . '&scopeID=0';
+                    $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+                    \Log::info("postURL : " . $postURL . " | magento_setting : " . json_encode($m_setting) . ' | response : ' . json_encode($result) );
+                }
+                if($is_development){
+                    $postURL = 'https://dev.sololuxury.com/rest/V1/configvalue/set?path=' . $path . '&value=' . $path . '&scope=' . $scope . '&scopeID=0';
+                    $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+                    \Log::info("postURL : " . $postURL . " | magento_setting : " . json_encode($m_setting) . ' | response : ' . json_encode($result) );
+                }
+
+            }
+        }else if($scope === 'websites'){
+
+            $store = $request->store;
+            $website = $request->website;
+            $websiteStores = WebsiteStore::with('website.storeWebsite')->whereHas('website', function($q) use ($store, $website_ids){
+                $q->whereIn('store_website_id', $website_ids)->where('name', $store);
+            })->get();
+
+            foreach($websiteStores as $websiteStore){
+
+                $m_setting = MagentoSetting::where('scope', $scope)->where('scope_id',$websiteStore->id)->where('path', $path)->first();
+                if(!$m_setting){
+                    $m_setting = MagentoSetting::Create([
+                        'scope' => $scope,
+                        'scope_id' => $websiteStore->id,
+                        'name' => $request->name,
+                        'path' => $request->path,
+                        'value' => $request->value
+                    ]);
+                } 
+                $token = $websiteStore->website->storeWebsite->api_token;
+                $scopeID = $websiteStore->id;
+                \Cache::forever('key', $token); 
+                if($is_live){
+                    $postURL = 'https://dev.sololuxury.com/rest/V1/configvalue/set?path=' . $path . '&value=' . $path . '&scope=' . $scope . '&scopeID=' . $scopeID;
+                    $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+                    \Log::info("postURL : " . $postURL . " | magento_setting : " . json_encode($m_setting) . ' | response : ' . json_encode($result) );
+                }
+                if($is_development){
+                    $postURL = 'https://dev.sololuxury.com/rest/V1/configvalue/set?path=' . $path . '&value=' . $path . '&scope=' . $scope . '&scopeID=' . $scopeID;
+                    $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+                    \Log::info("postURL : " . $postURL . " | magento_setting : " . json_encode($m_setting) . ' | response : ' . json_encode($result) );
+                }
+
+            }
+            
+        }else if($scope === 'stores'){
+
+            $store = $request->store;
+            $store_view = $request->store_view; 
+
+            $websiteStoresViews = WebsiteStoreView::with('websiteStore.website.storeWebsite')->whereHas('websiteStore.website', function($q) use ($store, $website_ids){
+                $q->where('name', $store)->whereIn('store_website_id', $website_ids);
+            })->where('code', $store_view)->get();
+
+            foreach($websiteStoresViews as $websiteStoresView){
+
+                $m_setting = MagentoSetting::where('scope', $scope)->where('scope_id',$websiteStoresView->id)->where('path', $path)->first();
+                if(!$m_setting){
+                    $m_setting = MagentoSetting::Create([
+                        'scope' => $scope,
+                        'scope_id' => $websiteStoresView->id,
+                        'name' => $request->name,
+                        'path' => $request->path,
+                        'value' => $request->value
+                    ]);
+                } 
+                $token = $websiteStoresView->websiteStore->website->storeWebsite->api_token;
+                $scopeID = $websiteStoresView->id;
+                \Cache::forever('key', $token);
+                if($is_live){
+                    $postURL = 'https://dev.sololuxury.com/rest/V1/configvalue/set?path=' . $path . '&value=' . $path . '&scope=' . $scope . '&scopeID=' . $scopeID;
+                    $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+                    \Log::info("postURL : " . $postURL . " | magento_setting : " . json_encode($m_setting) . ' | response : ' . json_encode($result) );
+                }
+                if($is_development){
+                    $postURL = 'https://dev.sololuxury.com/rest/V1/configvalue/set?path=' . $path . '&value=' . $path . '&scope=' . $scope . '&scopeID=' . $scopeID;
+                    $result = app('App\Http\Controllers\LiveChatController')->curlCall($postURL, [], 'application/json', true, 'POST');
+                    \Log::info("postURL : " . $postURL . " | magento_setting : " . json_encode($m_setting) . ' | response : ' . json_encode($result) );
+                }
+
+            }
+            
+        }
+        
     }
+
+    public function websiteStores(Request $request){ 
+        $website_ids = Website::where('store_website_id', $request->website_id)->get()->pluck('id')->toArray();
+        return response()->json([
+            'data' => WebsiteStore::select('id', 'name')->whereNotNull('name')->whereIn('website_id', $website_ids)->get()
+        ]);
+    }
+    
+    public function websiteStoreViews(Request $request){
+        return response()->json([
+            'data' => WebsiteStoreView::select('id', 'code')->whereNotNull('code')->where('website_store_id', $request->website_id)->get()
+        ]);
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,9 @@ Route::middleware('auth')->group(function () {
     Route::post('magento-admin-settings/create', 'MagentoSettingsController@create')->name('magento.setting.create');
     Route::post('magento-admin-settings/update', 'MagentoSettingsController@update')->name('magento.setting.update');
 
+    Route::post('magento-admin-settings/website/stores', 'MagentoSettingsController@websiteStores')->name('get.website.stores');
+    Route::post('magento-admin-settings/website/store/views', 'MagentoSettingsController@websiteStoreViews')->name('get.website.store.views');
+
 });
 //Google Web Master Routes
 Route::prefix('googlewebmaster')->middleware('auth')->group(static function () {
