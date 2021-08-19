ab525da46d728c138613276b5ad1d4437fa68d2e
Upendra <upender.k.c@gmail.com>
2021-08-19 18:32:30 -0400

    DEVTASK-20261-Discounts

--- app/Http/Controllers/ProductInventoryController.php
+++ app/Http/Controllers/ProductInventoryController.php
@@ -2546,7 +2546,22 @@ class ProductInventoryController extends Controller
 					
 					if($brand)
 					{
-						
+						if ($row[$condition_from_retail_index]!='')
+						{
+								$segments = CategorySegment::where('status', 1)->get();
+								if(!$segments->isEmpty())
+								 { 
+									foreach($segments as $segment) 
+									{ 
+										\App\CategorySegmentDiscount::create([
+											"brand_id" => $brand->id,
+											"category_segment_id" => $segment->id,
+											"amount" => $row[$condition_from_retail_index],
+											"amount_type" => "percentage",
+										]);
+									}
+								 }
+					   }
 						$discount = new SupplierBrandDiscount();
 						
 						$exist_row = SupplierBrandDiscount::where('brand_id', $brand->id)->where('supplier_id', $request->supplier)->where('gender', $row[$gender_index])->where('category', $row[$category_index])->first();
