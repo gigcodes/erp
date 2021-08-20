68d843c1082d863d8356b76c131d6f581cb15bfc
developer <madhukantmaurya1988@gmail.com>
2021-08-20 16:01:59 +0530

    DEVTASK-20230

--- app/Observers/CallBusyMessageObserver.php
+++ app/Observers/CallBusyMessageObserver.php
@@ -16,45 +16,10 @@ class CallBusyMessageObserver
     public function created(CallBusyMessage $callBusyMessage)
     {   
 		if($callBusyMessage->recording_url != '') {
-			$ch1 = curl_init();
-			curl_setopt ( $ch1, CURLOPT_URL, $callBusyMessage->recording_url );
-			curl_setopt ( $ch1, CURLOPT_RETURNTRANSFER, 1 );
-			$http_respond = curl_exec($ch1);
-			$http_respond = trim( strip_tags( $http_respond ) );
-			$http_code = curl_getinfo( $ch1, CURLINFO_HTTP_CODE );
-			curl_close( $ch1 );
-			if ( ( $http_code == "200" ) || ( $http_code == "302" ) ) {			
-				$apiKey = "LhLBdFbWlwujC38ym7PcILaalqTBQN7Jb-50H0ij4nkG";
-				$url = "https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/9e2e85a8-4bea-4070-b3d3-cef36b5697f0";
-				$ch = curl_init();
-				$file = file_get_contents($callBusyMessage->recording_url);
-				curl_setopt($ch, CURLOPT_URL, $url.'/v1/recognize');
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_POST, 1);
-				 
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
-				curl_setopt($ch, CURLOPT_POST, 1);
-				curl_setopt($ch, CURLOPT_USERPWD, 'apikey' . ':' . $apiKey);
-
-				$headers = array();
-				$headers[] = "Content-Type: application/octet-stream";
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-				$result = curl_exec($ch);
-				if (curl_errno($ch)) {
-					echo 'Error:' . curl_error($ch);
-				}
-				curl_close($ch); 
-				$result = json_decode($result);
-				$recordedText = $result->results[0]->alternatives[0]->transcript;
-				CallBusyMessage::where('id', $callBusyMessage->id)->update(['audio_text'=>$recordedText]);
-			} else {
-				// you can return $http_code here if necessary or wanted
-				
-			}
-		   
-			
-			
+			 $recordedText = (new CallBusyMessage)->convertSpeechToText($callBusyMessage->recording_url);
+			 if($recordedText != '') {
+				 CallBusyMessage::where('id', $callBusyMessage->id)->update(['audio_text'=>$recordedText]);
+			 }
 		}
     }
 
@@ -65,8 +30,13 @@ class CallBusyMessageObserver
      * @return void
      */
     public function updated(CallBusyMessage $callBusyMessage)
-    {
-        //
+    { 
+		if($callBusyMessage->recording_url != '' and $callBusyMessage->isDirty('recording_url')) { 
+			$recordedText = (new CallBusyMessage)->convertSpeechToText($callBusyMessage->recording_url);
+			if($recordedText != '') {
+				CallBusyMessage::where('id', $callBusyMessage->id)->update(['audio_text'=>$recordedText]);
+			}
+		}
     }
 
     /**
