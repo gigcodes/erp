78f788cabaa19780f913fa1a1c67eeee68822ef8
g56 <g56@gopanear.com>
2021-08-23 12:25:25 +0530

    incomplete task

---
+++ app/CustomerCharityWebsiteStore.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * @SWG\Definition(type="object", @SWG\Xml(name="User"))
+ */
+class CustomerCharityWebsiteStore extends Model
+{
+    /**
+     * @var string
+     * @SWG\Property(property="name",type="string")
+     * @SWG\Property(property="contact_no",type="integer")
+     * @SWG\Property(property="email",type="string")
+     * @SWG\Property(property="whatsapp_number",type="integer")
+     * @SWG\Property(property="assign_to",type="string")
+     */
+	protected $guarded = [];
+
+    public $timestamps = false;   
+
+    public function customerCharity()
+    { 
+        return $this->hasOne('App\CustomerCharity', 'id', 'customer_charity_id');
+    }
+
+    public function websiteStore()
+    { 
+        return $this->hasOne('App\WebsiteStore', 'id', 'website_store_id');
+    }
+}
