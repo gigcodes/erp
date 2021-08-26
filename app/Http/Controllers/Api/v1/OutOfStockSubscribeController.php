c3e08b0db4dd76d41a3405e3570db868ab5af409
developer <madhukantmaurya1988@gmail.com>
2021-08-26 10:51:31 +0530

    DEVTASK-20534 - website id added

--- app/Http/Controllers/Api/v1/OutOfStockSubscribeController.php
+++ app/Http/Controllers/Api/v1/OutOfStockSubscribeController.php
@@ -19,7 +19,7 @@ class OutOfStockSubscribeController extends Controller
 		// validate incoming request
         $validator = Validator::make($params, [
            'email' => 'required',
-           'product_id' => 'required',
+           'sku' => 'required',
 		   'website' => 'required'
        ]);
 
@@ -30,18 +30,28 @@ class OutOfStockSubscribeController extends Controller
 		$storeWebsite = StoreWebsite::where("website","like",$request->website)->select('id')->first();      
 
 		if($storeWebsite) {
-			 $website_id = $storeWebsite->id;
+			$website_id = $storeWebsite->id;
 			$data=$params;
+			$sku = explode("-",$request->get("sku"));
+			$product =  Product::where("sku",$sku[0])->first();
+			if($product) {
+				$customer = Customer::where('email', $data['email'])->first();
+				if($customer == null) {
+					$customer = Customer::create(['name'=>$data['email'], 'email'=>$data['email'], 'store_website_id'=> $website_id]);
+				}
+				$status=0;
+				\App\ErpLeads::create(['customer_id' => $customer->id,'lead_status_id' => 1,'product_id' => $product->id]);
+				$arrayToStore = ['customer_id'=>$customer['id'], 'product_id'=>$product->id, 'status'=>$status, 'website_id'=>$website_id];
+				OutOfStockSubscribe::updateOrCreate( ['customer_id'=>$customer['id'], 'product_id'=>$product->id, 'website_id'=>$website_id], $arrayToStore);
+				
+				 return response()->json(["code" => 'success', "message" => "Subscribed successfully."]);
+			} else{
+                return response()->json(["code" => 500 , "data" => [] , "message" => "Product not found in records"]);
+            }
+		}else{
+            return response()->json(["code" => 500 , "data" => [] , "message" => "Website not found in records"]);
+        }
 
-			$customer = Customer::where('email', $data['email'])->first();
-			if($customer == null) {
-				$customer = Customer::create(['name'=>$data['email'], 'email'=>$data['email'], 'store_website_id'=> $website_id]);
-			}
-			$status=0;
-			\App\ErpLeads::create(['customer_id' => $customer->id,'lead_status_id' => 1,'product_id' => $data['product_id']]);
-			$arrayToStore = ['customer_id'=>$customer['id'], 'product_id'=>$data['product_id'], 'status'=>$status];
-			OutOfStockSubscribe::updateOrCreate( ['customer_id'=>$customer['id'], 'product_id'=>$data['product_id']], $arrayToStore);
-		}
-        return response()->json(["code" => 'success', "message" => "Subscribed successfully."]);
+       
    }
 }
