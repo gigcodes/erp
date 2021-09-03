e32c80649765ab0a0bd4a6acafd71695330fc50a
Hotweb07 <hotweb07@gmail.com>
2021-09-03 15:26:18 +0530

    Add lead OR order, customer name, product id, brand and lead-order id filters for Lead and Order.

--- resources/views/lead-order/index.blade.php
+++ resources/views/lead-order/index.blade.php
@@ -7,42 +7,36 @@
 @section('content')
 
     <div class="row">
-
         <div class="col-lg-12 margin-tb">
-
             <h2 class="page-heading">Lead and Order Pricing</h2>
-
             <div class="row">
-
-                <div class="col-md-10 col-sm-12">
-
+                <div class="col-md-12 col-sm-12">
                   <div class="col-10" style="padding-left:0px;">
-                    <div >
-                    <form class="form-inline" action="{{ route('lead-order.index') }}" method="GET">
-                        
-                        <div class="form-group col-md-3 pd-3">
-                          <input style="width:100%;" name="term" type="text" class="form-control"
-                                 value="{{ isset($term) ? $term : '' }}"
-                                 placeholder="Search">
-                        </div>        
-        
-                        <div class="form-group col-md-3 pd-3">
-                          <?php echo Form::select("brand_id",["" => "-- Select Brands --"]+$brandList,request('brand_id',[]),["class" => "form-control select2"]); ?>
+                        <div>
+                            <form class="form-inline" action="{{ route('lead-order.index') }}" method="GET">
+                                <div class="form-group col-md-4 pd-3">
+                                  <input style="width:100%;" name="term" type="text" class="form-control" value="{{ isset($term) ? $term : '' }}" 
+                                         placeholder="Customer Name, Lead or Order Id, Product Id">
+                                </div>
+                                <div class="form-group col-md-3 pd-3">
+                                    <?php echo Form::select("brand_id",["" => "-- Select Brands --"]+$brandList,request('brand_id',[]),["class" => "form-control select2"]); ?>
+                                </div>                                
+                                <div class="form-group col-md-3 pd-3">
+                                    <select class="form-control select2" name="order_or_lead" tabindex="-1" aria-hidden="true">
+                                        <option value="">-- Lead Or Order --</option>
+                                        <option value="lead" {{ (isset($orderOrLead) && $orderOrLead == 'lead') ? 'selected' : '' }} >Lead</option>
+                                        <option value="order" {{ (isset($orderOrLead) && $orderOrLead == 'order') ? 'selected' : '' }} >Order</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-1 pd-3">
+                                    <button type="submit" class="btn btn-image ml-3"><img src="{{asset('images/filter.png')}}" /></button>
+                                </div>
+                            </form>
                         </div>
-        
-                           <div class="form-group col-md-1 pd-3">
-                        <button type="submit" class="btn btn-image ml-3"><img src="{{asset('images/filter.png')}}" /></button>
-                          </div>
-                      </form>
-                       
                     </div>
-                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
     @if ($message = Session::get('success'))
