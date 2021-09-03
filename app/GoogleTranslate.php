1abea698c2f5aba4d47e3f9231fcce5831317c1a
Pravin Solanki <solanki7492@gmail.com>
2021-09-03 19:06:20 +0530

    Fix google translate

--- app/GoogleTranslate.php
+++ app/GoogleTranslate.php
@@ -31,7 +31,7 @@ class GoogleTranslate
             $file = googleTraslationSettings::select('id','account_json')
             ->where('status','1')
             ->orderBy('id')
-            ->first(); ;
+            ->first();
 
             // on production site it will return the original text
             // if(env("IS_SITE","local") != "production") {
