552715483134095ad041942d3f28a0cc9c8a8fdf
Pravin Solanki <solanki7492@gmail.com>
2021-09-03 15:23:14 +0530

    fix class name

--- database/migrations/2021_09_03_270318_alter_table_store_website_pages_add_is_pushed.php
+++ database/migrations/2021_09_03_270318_alter_table_store_website_pages_add_is_pushed.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableStoreWebsitesPagesAddIsPushed extends Migration
+class AlterTableStoreWebsitePagesAddIsPushed extends Migration
 {
     /**
      * Run the migrations.
