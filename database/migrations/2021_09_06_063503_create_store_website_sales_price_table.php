457bda84375765b5accbb8420bfbd1c44423074b
Pravin Solanki <solanki7492@gmail.com>
2021-09-06 15:37:51 +0530

    Fix migration table

--- database/migrations/2021_09_06_063503_create_store_website_sales_price_table.php
+++ database/migrations/2021_09_06_063503_create_store_website_sales_price_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSiteIssuesTable extends Migration
+class CreateStoreWebsiteSalesPriceTable extends Migration
 {
     /**
      * Run the migrations.
