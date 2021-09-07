970556c040a165c355ae2a2135aebd9c6de6aa1b
developer <technodeviser05@gmail.com>
2021-09-07 17:22:44 +0530

    DEVTASK-20592

--- app/Http/Controllers/Api/v1/CustomerController.php
+++ app/Http/Controllers/Api/v1/CustomerController.php
@@ -55,8 +55,9 @@ class CustomerController extends Controller
 
             $customer_id = $checkCustomer->id;
             foreach($request->item_info as $item){
-               $product = Product::where('sku',$item['sku'])->first();
-               if($product){
+                $skuarr = explode('-', $item['sku'])
+                $product = Product::where('sku',$skuarr[0])->first();
+                if($product){
                     $erp_lead = new ErpLeads;
                     $erp_lead->lead_status_id = 1;
                     $erp_lead->customer_id = $customer_id;
