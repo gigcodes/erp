9d4288390e9f960cba52d42a8dbf7e1d95d18a31
Pravin Solanki <solanki7492@gmail.com>
2021-09-22 18:12:43 +0530

    Fix conflict

--- resources/views/livechat/store_credit_ajax.blade.php
+++ resources/views/livechat/store_credit_ajax.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @foreach ($customers_all as $c)
 @php
  $used_credit = \App\CreditHistory::where('customer_id',$c->id)->where('type','MINUS')->sum('used_credit');
@@ -15,27 +14,4 @@
      <td>{{ $used_credit }}</td>
      <td>{{ ($c->credit + $credit_in ) - $used_credit }}</td>
    </tr>
- @endforeach
-=======
-@foreach ($customers_all as $c) 
-@php 
-         $used_credit = \App\CreditHistory::where('customer_id',$c->id)->where('type','MINUS')->sum('used_credit');
-         $credit_in = \App\CreditHistory::where('customer_id',$c->id)->where('type','ADD')->sum('used_in');
-        
-         @endphp
-           <tr>
-             <td>{{ $c->name }}</td>
-             <td>{{ $c->email }}</td>
-             <td>{{ $c->phone }}</td>
-             <td>{{ $c->title }}</td>
-             <td>{{ date("d-m-Y",strtotime($c->created_at)) }}</td>
-             <td>{{ $c->credit  + $credit_in }}</td>
-             <td>{{ $used_credit }}</td>
-             <td>{{ ($c->credit + $credit_in ) - $used_credit }}</td>
-             <td><a href="#" onclick="getLogs('{{ $c->id}}')"><i class="fa fa-eye"></i></a></td>
-             
-           
-           </tr>
-         @endforeach
->>>>>>> master
-      
+ @endforeach      
