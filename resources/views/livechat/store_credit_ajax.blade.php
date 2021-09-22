e00f4aefd94482b12fcf987af7b3a64aed801ba7
Pravin Solanki <solanki7492@gmail.com>
2021-09-22 18:09:52 +0530

    Fix conflict

--- resources/views/livechat/store_credit_ajax.blade.php
+++ resources/views/livechat/store_credit_ajax.blade.php
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
-=======
 @foreach ($customers_all as $c)
->>>>>>> 90f33185744acea8d0d164ae4b05ccabca476f38
 @php
-         $used_credit = \App\CreditHistory::where('customer_id',$c->id)->where('type','MINUS')->sum('used_credit');
-         $credit_in = \App\CreditHistory::where('customer_id',$c->id)->where('type','ADD')->sum('used_in');
-        
-         @endphp
-           <tr>
-             <td>{{ $c->name }}</td>
-             <td>{{ $c->email }}</td>
-             <td>{{ $c->phone }}</td>
-             <td>{{ $c->title }}</td>
-<<<<<<< HEAD
-=======
-             <td>{{ date("d-m-Y",strtotime($c->created_at)) }}</td>
->>>>>>> 90f33185744acea8d0d164ae4b05ccabca476f38
-             <td>{{ $c->credit  + $credit_in }}</td>
-             <td>{{ $used_credit }}</td>
-             <td>{{ ($c->credit + $credit_in ) - $used_credit }}</td>
-            
-             
-           
-           </tr>
-         @endforeach
+ $used_credit = \App\CreditHistory::where('customer_id',$c->id)->where('type','MINUS')->sum('used_credit');
+ $credit_in = \App\CreditHistory::where('customer_id',$c->id)->where('type','ADD')->sum('used_in');
+
+ @endphp
+   <tr>
+     <td>{{ $c->name }}</td>
+     <td>{{ $c->email }}</td>
+     <td>{{ $c->phone }}</td>
+     <td>{{ $c->title }}</td>
+     <td>{{ date("d-m-Y",strtotime($c->created_at)) }}</td>
+     <td>{{ $c->credit  + $credit_in }}</td>
+     <td>{{ $used_credit }}</td>
+     <td>{{ ($c->credit + $credit_in ) - $used_credit }}</td>
+   </tr>
+ @endforeach
       
