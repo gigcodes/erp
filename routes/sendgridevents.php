3d5013db76cff2ffe67c5309a4780dd7f9277d98
sunil <SUNILTHAKUR@GMAIL.COM>
2021-09-24 18:52:07 +0530

    DEVTASK-20645

--- routes/sendgridevents.php
+++ routes/sendgridevents.php
@@ -6,11 +6,15 @@ Route::group([
     'namespace' => 'App\Http\Controllers',
     'middleware' => SendgridEventMiddleware::class
 ], function () {
-    Route::post(
-        config('sendgridevents.webhook_url'),
-        [
-            'as' => 'sendgrid.webhook',
-            'uses' => 'WebhookController@post'
-        ]
-    );
+	
+	Route::any(config('sendgridevents.webhook_url'),'WebhookController@post')->name('sendgrid.webhook');
+    
+    // Route::post(
+    //     config('sendgridevents.webhook_url'),
+    //     [
+    //         'as' => 'sendgrid.webhook',
+    //         'uses' => 'WebhookController@post'
+    //     ]
+    // );
+
 });
