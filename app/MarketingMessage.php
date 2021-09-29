44d3ff9069276744477303fc6355efc76be9276a
sunil <SUNILTHAKUR@GMAIL.COM>
2021-09-29 16:27:27 +0530

    DEVTASK-20662

---
+++ app/MarketingMessage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MarketingMessage extends Model
+{
+    protected $fillable =
+    [
+        "title",
+        "message_group_id",
+        "scheduled_at",
+        "is_sent"
+    ];
+}
