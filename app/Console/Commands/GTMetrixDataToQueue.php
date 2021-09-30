5f7306fa4ac85a740b5fa3ded4d770d7f83226ad
sunil <SUNILTHAKUR@GMAIL.COM>
2021-09-30 12:09:29 +0530

    DEVTASK-20644

--- app/Console/Commands/GTMetrixDataToQueue.php
+++ app/Console/Commands/GTMetrixDataToQueue.php
@@ -51,9 +51,8 @@ class GTMetrixDataToQueue extends Command{
 		$lists = $query->from(\DB::raw('(SELECT MAX( id) as id, status, store_view_id, website_url, html_load_time FROM store_views_gt_metrix  GROUP BY store_views_gt_metrix.website_url ) as t'))
             ->leftJoin('store_views_gt_metrix', 't.id', '=', 'store_views_gt_metrix.id')->get();
         if($lists){
-
             foreach ($lists as $key => $list) {
-                if($list->status == '' || $list->status == 'not_queued'){
+                if($list->status == 'not_queued'){
                     $this->inQueue($list->id);
                 }
             }
