152345bd9b3ffa39b0f102eb22583b62570e44e4
Pravin Solanki <solanki7492@gmail.com>
2021-10-05 17:07:36 +0530

    Add lead issue

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -950,6 +950,9 @@ class LeadsController extends Controller
 
         $source = $source->get();
 
+        echo "This page is in progress";
+        die;
+
         foreach ($source as $key => $value) {
             
             $curr_cat_title = $value->cat_title;
