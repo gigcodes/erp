e603e66172277918b15027b01ba7e5baf10ab155
Suraj Sharma <sharmasuraj41@gmail.com>
2021-10-05 21:54:59 +0530

    new updates

--- Modules/StoreWebsite/Resources/views/site-development/index.blade.php
+++ Modules/StoreWebsite/Resources/views/site-development/index.blade.php
@@ -382,20 +382,7 @@
 	</div>
 </div>
 
-<div id="previewDoc" class="modal fade" role="dialog">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-        	<div class="modal-body">
-    			<div class="col-md-12">
-	        		<iframe src="" id="previewDocSource" width='700' height='550' allowfullscreen webkitallowfullscreen></iframe>
-				</div>
-			</div>
-           <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 <div id="dev_task_statistics" class="modal fade" role="dialog">
@@ -596,6 +583,21 @@
         </div>
     </div>
   </div>
+</div>
+
+<div id="previewDoc" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+        	<div class="modal-body">
+    			<div class="col-md-12">
+	        		<iframe src="" id="previewDocSource" width='700' height='550' allowfullscreen webkitallowfullscreen></iframe>
+				</div>
+			</div>
+           <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
