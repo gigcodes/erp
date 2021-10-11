23f8f1da27b3282aa2f171cff908cc1d62d14aff
Suraj Sharma <sharmasuraj41@gmail.com>
2021-10-11 13:59:20 +0530

    fixes

--- resources/views/brand-size-chart/index.blade.php
+++ resources/views/brand-size-chart/index.blade.php
@@ -40,7 +40,7 @@
 
     <div class="row">
         <div class="col-12">
-            <div id="exTab1" class="container">
+            <div id="exTab1" class="container">	
                 <ul  class="nav nav-pills">
                     @forelse ($storeWebsite as $key => $item)
                         <li class="@if($loop->first) active @endif">
@@ -49,7 +49,7 @@
                     @empty
                     @endforelse
                 </ul>
-
+        
                 <div class="tab-content clearfix">
                     @forelse ($storeWebsite as $key => $item)
                         <div class="tab-pane @if($loop->first) active @endif" id="tab_div_{{ $key + 1 }}">
@@ -63,6 +63,7 @@
                                             <th></th>
                                         @endforelse
                                     </tr>
+                                    @if($item->brand_id)
                                     @forelse ($item->sizeBrand->unique() as $brandkey => $branditem)
                                         <tr>
                                             <th>{{ $branditem->name }}</th>
@@ -86,12 +87,17 @@
                                             @endforelse
                                         </tr>
                                     @empty
+                                        <tr>
+                                            <th></th>
+                                        </tr>
+                                    @endforelse
+                                    @else
                                     <tr>
                                         <th>No Brand</th>
                                         @forelse ($item->sizeCategory->unique() as $catkey => $catitem)
                                             <td>
                                             @forelse ($sizeChart as $chartitem)
-                                                @if($chartitem->category_id == $catitem->id && $chartitem->brand_id == $branditem->id)
+                                                @if($chartitem->category_id == $catitem->id)
                                                     @if ($chartitem->hasMedia(config('constants.size_chart_media_tag')))
                                                         <a href="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" data-fancybox>
                                                             <span class="td-mini-container">
@@ -104,21 +110,10 @@
                                             @endforelse
                                             </td>
                                         @empty
-                                        <td>
-                                            @forelse ($sizeChart as $chartitem)
-                                                @if ($chartitem->hasMedia(config('constants.size_chart_media_tag')))
-                                                    <a href="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" data-fancybox>
-                                                        <span class="td-mini-container">
-                                                            <img src="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" class="img-responsive thumbnail-200 mb-1">
-                                                        </span>
-                                                    </a>
-                                                @endif
-                                            @empty
-                                            @endforelse
-                                            </td>
+                                            <td></td>
                                         @endforelse
                                         </tr>
-                                    @endforelse
+                                    @endif
                                 </table>
                             </div>
                         </div>
