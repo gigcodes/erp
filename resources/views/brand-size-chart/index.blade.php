30bcd631f786a76c44157fc9114ea0eb9b25be71
Suraj Sharma <sharmasuraj41@gmail.com>
2021-10-11 14:25:48 +0530

    fixes

--- resources/views/brand-size-chart/index.blade.php
+++ resources/views/brand-size-chart/index.blade.php
@@ -63,34 +63,33 @@
                                             <th></th>
                                         @endforelse
                                     </tr>
-                                    @forelse ($item->sizeCategory->unique() as $catkey => $catitem)
+                                    @forelse ($item->sizeBrand->unique() as $brandkey => $branditem)
                                         <tr>
-                                            @forelse ($item->sizeBrand->unique() as $brandkey => $branditem)
-                                              <th>{{ $branditem->name }}</th>
-                                            @empty
-                                            @endforelse
-                                            <td>
-                                            @forelse ($sizeChart as $chartitem)
-                                                @if($chartitem->category_id == $catitem->id && $chartitem->brand_id == $branditem->id)
-                                                    @if ($chartitem->hasMedia(config('constants.size_chart_media_tag')))
-                                                        <a href="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" data-fancybox>
-                                                            <span class="td-mini-container">
-                                                                <img src="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" class="img-responsive thumbnail-200 mb-1">
-                                                            </span>
-                                                        </a>
+                                            <th>{{ $branditem->name }}</th>
+                                            @forelse ($item->sizeCategory->unique() as $catkey => $catitem)
+                                                <td>
+                                                @forelse ($sizeChart as $chartitem)
+                                                    @if($chartitem->category_id == $catitem->id && $chartitem->brand_id == $branditem->id)
+                                                        @if ($chartitem->hasMedia(config('constants.size_chart_media_tag')))
+                                                            <a href="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" data-fancybox>
+                                                                <span class="td-mini-container">
+                                                                    <img src="{{ $chartitem->getMedia(config('constants.size_chart_media_tag'))->first()->getUrl() }}" class="img-responsive thumbnail-200 mb-1">
+                                                                </span>
+                                                            </a>
+                                                        @endif
                                                     @endif
-                                                @endif
+                                                @empty
+                                                @endforelse
+                                                </td>
                                             @empty
+                                                <td></td>
                                             @endforelse
-                                            </td>
-                                           
                                         </tr>
                                     @empty
                                         <tr>
                                             <th></th>
                                         </tr>
                                     @endforelse
-                                   
                                 </table>
                             </div>
                         </div>
