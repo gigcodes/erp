931eb7ad3f05963386e3b19da6b0c4f43e72008d
Pravin Solanki <solanki7492@gmail.com>
2021-10-12 12:14:55 +0530

    added extra changes and pushed product issue

--- app/Console/Commands/UpdatePricesWithDecimals.php
+++ app/Console/Commands/UpdatePricesWithDecimals.php
@@ -45,7 +45,7 @@ class UpdatePricesWithDecimals extends Command
     {
         try {
             $report = CronJobReport::create([
-                'signature' => $this->signature,
+                'signature'  => $this->signature,
                 'start_time' => Carbon::now(),
             ]);
 
@@ -56,30 +56,69 @@ class UpdatePricesWithDecimals extends Command
 
             foreach ($products as $key => $product) {
 
-                dump("$key - Scraped Product - $product->sku");
+                $pmodel = \App\Product::find($product->product_id);
+                if($pmodel) {
 
-                $scPrice = str_replace('euro', '', $product->price);
+                    $scPrice = str_replace('euro', '', $product->price);
 
-                $scPrice = preg_replace('/[^A-Za-z0-9\-]/', '', $scPrice);
+                    $scPrice = preg_replace('/[^A-Za-z0-9\-]/', '', $scPrice);
 
-                if (strlen($scPrice) > 4 && strlen($scPrice) < 6) {
-                    $scPrice = substr($scPrice, 0, 3);
-                    $scPrice = $scPrice . ".00";
-                } elseif (strlen($scPrice) > 5 && strlen($scPrice) < 7) {
-                    $scPrice = substr($scPrice, 0, 4);
-                    $scPrice = $scPrice . ".00";
+                    $needToupdate = false;
+                    if (strlen($scPrice) > 4 && strlen($scPrice) < 6) {
+                        $scPrice      = substr($scPrice, 0, 3);
+                        $scPrice      = $scPrice . ".00";
+                        $needToupdate = true;
+                    } elseif (strlen($scPrice) > 5 && strlen($scPrice) < 7) {
+                        $scPrice      = substr($scPrice, 0, 4);
+                        $scPrice      = $scPrice . ".00";
+                        $needToupdate = true;
+                    }
+
+                    dump("$key - Scraped Product - $product->sku and needToupdate : ".$needToupdate);
+
+                    if ($needToupdate) {
+                        if (is_numeric($scPrice)) {
+                            $scPrice = ceil($scPrice / 10) * 10;
+                        }
+
+                        $priceEurSpecial = 0;
+                        $priceInrSpecial = 0;
+
+                        $brand = \App\Brand::find($pmodel->brand_id);
+
+                        // Check for EUR to INR
+                        $priceInr = 0;
+                        if (!empty($brand->euro_to_inr) && $brand) {
+                            $priceInr = (float) $brand->euro_to_inr * (float) trim($scPrice);
+                        } else {
+                            $priceInr = (float) \App\Setting::get('euro_to_inr') * (float) trim($scPrice);
+                        }
+
+                        if (!empty($scPrice) && !empty($priceInr) && $brand) {
+                            $priceEurSpecial = $scPrice - ($scPrice * $brand->deduction_percentage) / 100;
+                            $priceInrSpecial = $priceInr - ($priceInr * $brand->deduction_percentage) / 100;
+                        }
+                        $oldPrice = $pmodel->price;
+
+                        $pmodel->price             = $scPrice;
+                        $pmodel->price_inr         = $priceInr;
+                        $pmodel->price_eur_special = $priceEurSpecial;
+                        $pmodel->price_inr_special = $priceInrSpecial;
+
+                        $pmodel->save();
+                        $message = "$product->id has been updated with old price $oldPrice to new price $pmodel->price and inr price $pmodel->price_inr and eur special price is $pmodel->price_eur_special";
+
+                        \Log::info($message);
+                        $this->output->write($message);
+                    }
                 }
 
-                if (is_numeric($scPrice)) {
-                    $scPrice = ceil($scPrice / 10) * 10;
-                }
 
-                $product->price = $scPrice;
-                $product->save();
 
             }
             $report->update(['end_time' => Carbon::now()]);
         } catch (\Exception $e) {
+            \Log::error($e);
             \App\CronJob::insertLastError($this->signature, $e->getMessage());
         }
     }
