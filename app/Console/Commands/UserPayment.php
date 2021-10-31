ad94645223ebddb3bf396fb070653e7556dee0af
Your Name <you@example.com>
2021-10-31 17:36:17 +0530

    20885

--- app/Console/Commands/UserPayment.php
+++ app/Console/Commands/UserPayment.php
@@ -10,6 +10,7 @@ use App\UserRate;
 use DB;
 class UserPayment extends Command
 {
+
     /**
      * The name and signature of the console command.
      *
@@ -81,6 +82,8 @@ class UserPayment extends Command
                 }
             }
 
+            echo $total;
+            die();
             /*$billingStartDate = ($lastPayment && !empty($startsAt)) ? $startsAt : date("Y-m-d",strtotime("-1 day"));
             if($user->payment_frequency == 'fornightly') {
                 $billingEndDate   = date('Y-m-d',strtotime($billingStartDate . "+1 days"));  
