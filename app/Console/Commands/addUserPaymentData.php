42f2d94ac25f7c3766be129b58e615e507fe9404
Your Name <you@example.com>
2021-10-31 17:58:33 +0530

    20885

--- app/Console/Commands/addUserPaymentData.php
+++ app/Console/Commands/addUserPaymentData.php
@@ -46,9 +46,7 @@ class addUserPaymentData extends Command
         foreach ($dev_tasks as $dev_task) {
             
             $dev_task_user = User::find($dev_task->team_lead_id !== null ? $dev_task->team_lead_id : $dev_task->assigned_to);
-            echo "<pre/>";
-            print_r($dev_task_user->count());
-            die();
+           
             if(empty($dev_task_user)){
                 dump('dev_task-id - ' . $dev_task->id . ' user not exist');
                 continue;
@@ -62,6 +60,9 @@ class addUserPaymentData extends Command
                 'by_command'        => 1,
                 'developer_task_id' => $dev_task->id,
             ]);
+            echo "<pre/>";
+            print_r($dev_task_payment);
+            die();
             if($dev_task_payment){
                 dump('dev_task-id - ' . $dev_task->id . ' payment-id - ' . $dev_task_payment->id . ' is done');
             }
