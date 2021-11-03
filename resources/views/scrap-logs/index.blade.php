9b1ff35bf5b95eccb1515820537ae6aec2d2d191
Your Name <you@example.com>
2021-11-04 00:56:39 +0530

    changes

--- resources/views/scrap-logs/index.blade.php
+++ resources/views/scrap-logs/index.blade.php
@@ -214,6 +214,22 @@
 	  </div>
 	 </div>
 </div>
+<div id="history" class="modal fade" role="dialog">
+	  <div class="modal-dialog ">
+	  	<!-- Modal content-->
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h4 class="modal-title">Last 7 days history</h4>
+	        <button type="button" class="close" data-dismiss="modal">&times;</button>
+	      </div>
+	  	<div class="modal-body">
+	  		<tbody id="history"></tbody>
+	  	</div>
+	  	<div class="modal-footer">
+	  	</div>
+	  </div>
+	 </div>
+</div>
 @endsection
 
 @section('scripts')
