606c54a33e20ee7e2e7b5b3f1bb5f363f3a7d3a1
Pravin Solanki <solanki7492@gmail.com>
2021-11-08 12:04:43 +0530

    Fix wrong merge issue

--- app/Http/Controllers/ScrapLogsController.php
+++ app/Http/Controllers/ScrapLogsController.php
@@ -46,7 +46,7 @@ class ScrapLogsController extends Controller
 		// $files = File::allFiles(env('SCRAP_LOGS_FOLDER'));
 		$files = File::allFiles(config('env.SCRAP_LOGS_FOLDER'));
 
-	    $date = $dateVal;
+	    $date = strlen($dateVal) == 1  ? "0$dateVal"  : $dateVal;
 
         $lines = [];
         $log_status= '';
@@ -57,8 +57,8 @@ class ScrapLogsController extends Controller
             $day_of_file = explode('-', $val->getFilename());
 			$day_of_file = str_replace('.log', '', $day_of_file);
 
-            if( ( (end($day_of_file) == $date) || (isset($day_of_file[1]) and  $day_of_file[1] == $date.$month) ) && (str_contains($val->getFilename(), $searchVal) || empty($searchVal))) {
-				
+
+            if( ( (end($day_of_file) == $date) || (isset($day_of_file[1]) and  strtolower($day_of_file[1]) == strtolower($date.$month)) ) && (str_contains($val->getFilename(), $searchVal) || empty($searchVal))) {
 				if (!in_array($val->getRelativepath(), $serverArray)) {
 					continue;
 				}
@@ -204,6 +204,23 @@ class ScrapLogsController extends Controller
 		
 		//return  response()->json(["file_list" => $file_list]);
     }
+
+    public function logdata(){
+    	return \App\ScrapLog::select('folder_name','scrap_type','log_messages',\DB::raw('count(*) as log_count'))
+    	->whereNotNull('scrap_type')
+    	->groupBy('scrap_type','log_messages')
+    	->get();	
+    }
+
+    public function loghistory($filename){
+    	$log = \App\ScrapLog::where('file_name',$filename)->first(); 
+
+    	$toDate = date('Y-m-d',strtotime('+1 day'));    	
+		$fromDate = date('Y-m-d', strtotime('-7 days'));		
+		$fileLogs = \App\ScrapLog::where('scrap_type',$log->scrap_type)->whereBetween('created_at',[$fromDate, $toDate])->get();
+		return $fileLogs;
+    }
+
     public function fetchlog()
     {
     	$file_list = [];
