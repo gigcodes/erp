f3c50f85feef4c4b8d918279a172bd51a5bd1bdf
Pravin Solanki <solanki7492@gmail.com>
2021-11-17 14:35:37 +0530

    Added config

--- config/database.php
+++ config/database.php
@@ -118,6 +118,14 @@ return [
             'password' => env('MAGENTO_DB_PASSWORD', ''),
             'strict' => false,
         ],
+        'suvandnat' => [
+            'driver'   => 'mysql',
+            'host'     => env('SUVANDNAT_HOST', 'erp'),
+            'database' => env('SUVANDNAT_DB', 'erp'),
+            'username' => env('MAGENTO_DB_USER', 'root'),
+            'password' => env('MAGENTO_DB_PASSWORD', ''),
+            'strict' => false,
+        ],
         'thefitedit' => [
             'driver'   => 'mysql',
             'host'     => env('THEFITEDIT_HOST', 'erp'),
