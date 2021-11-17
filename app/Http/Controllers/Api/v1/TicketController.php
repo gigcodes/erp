eb6cdc1ea818a4d74cf3a5ab1bc468eba5940bf0
Pravin Solanki <solanki7492@gmail.com>
2021-11-17 15:34:02 +0530

    Fix language code issue

--- app/Http/Controllers/Api/v1/TicketController.php
+++ app/Http/Controllers/Api/v1/TicketController.php
@@ -87,7 +87,7 @@ class TicketController extends Controller
         $data['status_id'] = 1;
         $data['resolution_date'] = Carbon::now()->addDays(2)->format('Y-m-d H:i:s');
         if (isset($request->lang_code) && $request->lang_code != '') {
-            $lang = explode("_", $request->lang_code);
+            $lang = explode("_", str_replace("-","_",$request->lang_code));
             $data['lang_code'] = $lang[1];
         }
 
