367df1bdf4fa2f76e97d488aaf922cf8ffa9a205
Mitali <mitali.sixs@gmail.com>
2021-12-04 23:40:26 +0530

    Query for Scrapper Task

--- resources/views/scrap/quick-stats.blade.php
+++ resources/views/scrap/quick-stats.blade.php
@@ -1462,7 +1462,7 @@
             e.preventDefault();
             var id = $(this).data("id");
             $.ajax({
-                url: '/scrap/task-list',
+                url: '{{ route("scrap.task-list")}}',
                 type: 'GET',
                 data: {id: id},
                 beforeSend: function () {
