412fa410fb3f7755f322f5cb8c3dd27a941a5459
Priyanka Gadhiya <priyanka.g@spurtreetech.in>
2021-12-14 12:25:50 +0530

    task

--- resources/views/task-module/show.blade.php
+++ resources/views/task-module/show.blade.php
@@ -1034,7 +1034,7 @@
     </div>
 
     <div id="priority_model" class="modal fade" role="dialog">
-        <div class="modal-dialog modal-lg">
+        <div class="modal-dialog" style="max-width: 100%; width:95%;">
 
             <!-- Modal content-->
             <div class="modal-content">
@@ -1046,11 +1046,11 @@
                     @csrf
                     <div class="modal-body">
                         <div class="row">
-                            <div class="col-md-12">
-                                <div class="col-md-2">
+                            <div class="col-md-6">
+                                
                                     <strong>User:</strong>
-                                </div>
-                                <div class="col-md-8">
+                                
+                                
                                     <div class="form-group">
                                         @if(auth()->user()->isAdmin())
                                             <select class="form-control"  id="priority_user_id">
@@ -1064,32 +1064,29 @@
                                             {{auth()->user()->name}}
                                         @endif
                                     </div>
-                                </div>
+                                
                             </div>
-                            <div class="col-md-12">
-                                <div class="col-md-2">
-                                    <strong>Remarks:</strong>
-                                </div>
-                                <div class="col-md-8">
+                            <div class="col-md-6">
+                               <strong>Remarks:</strong>
                                     @if(auth()->user()->isAdmin())
                                         <div class="form-group">
-                                            <textarea cols="45" class="form-control" name="global_remarkes"></textarea>
+                                            <textarea cols="45" class="form-control" name="global_remarkes"style="height:32px !important;"></textarea>
                                         </div>
                                     @endif
-                                </div>
+                            
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-md-12">
-                                <table class="table table-bordered table-striped">
+                                <table class="table table-bordered table-striped"style="table-layout: fixed;">
                                     <tr>
-                                        <th width="1%">ID</th>
-                                        <th width="15%">Subject</th>
-                                        <th width="40%">Task</th>
-                                        <th width="20%">Communication</th>
-                                        <th width="9%">Date</th>
-                                        <th width="5%">Submitted By</th>
-                                        <th width="2%">Action</th>
+                                        <th width="4%">ID</th>
+                                        <th width="16%">Subject</th>
+                                        <th width="35%">Task</th>
+                                        <th width="10%">Communication</th>
+                                        <th width="13%">Date</th>
+                                        <th width="8%">Submitted By</th>
+                                        <th width="5%">Action</th>
                                     </tr>
                                     <tbody class="show_task_priority">
 
