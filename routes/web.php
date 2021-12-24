b1830adc81d81f3a673c410bc58e1cd659ba2113
Dev <madhukantmaurya1988@gmail.com>
2021-12-24 16:20:36 +0530

    DEVTASK-21316

--- routes/web.php
+++ routes/web.php
@@ -2066,6 +2066,7 @@ Route::middleware('auth')->group(function () {
     Route::post('instagram/post/sendRequest', 'InstagramPostsController@sendRequest');
 });
 
+Route::get('instagram/logs', 'InstagramPostsController@instagramUserLogs')->name('instagram.logs');
 Route::post('instagram/history', 'InstagramPostsController@history')->name('instagram.accounts.histroy');
 Route::get('instagram/addmailinglist', 'HashtagController@addmailinglist');
 
