5e5e9331a330a70b801adc5d0825dbdd38156204
Mitali <mitali.sixs@gmail.com>
2021-12-30 16:20:37 +0530

    changes

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -967,7 +967,6 @@ class DevelopmentController extends Controller
         $issues =  $issues->with('communications');
 
         // return $issues = $issues->limit(20)->get();
-    //    $issues = $issues->paginate(Setting::get('pagination'));
         $issues = $issues->paginate(15);
         $priority = \App\ErpPriority::where('model_type', '=', DeveloperTask::class)->pluck('model_id')->toArray();
 
