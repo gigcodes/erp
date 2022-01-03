126aa347f8a5809bd067636c446b3615beaaea9a
Mitali <mitali.sixs@gmail.com>
2022-01-03 19:44:56 +0530

    Make Required

--- resources/views/plan-page/index.blade.php
+++ resources/views/plan-page/index.blade.php
@@ -411,7 +411,7 @@ div#plan-action textarea {height: 200px;}
                       <div class="col-md-6">
                         <div class="form-group">
                           <label class="col-form-label">Type</label>
-                          <input type="text" class="form-control" name="type" list="type" id="plan_type" />
+                          <input type="text" class="form-control" name="type" list="type" id="plan_type" required="required"/>
                             <datalist id="type">
                               @foreach($typeList as $value )
                                     <option value="{{$value->type}}">{{$value->type}}</option>
@@ -422,7 +422,7 @@ div#plan-action textarea {height: 200px;}
                       <div class="col-md-6">
                         <div class="form-group">
                           <label class="col-form-label">Category</label>
-                          <input type="text" class="form-control" name="category" list="category" id="plan_cat" />
+                          <input type="text" class="form-control" name="category" list="category" id="plan_cat" required="required" />
                           <datalist id="category">
                             @foreach($categoryList as $value )
                                   <option value="{{$value->category}}">{{$value->category}}</option>
