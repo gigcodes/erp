b80b3effe3c43e2b2d7feb4e0294f7f0f2a9e0ee
Priyanka Gadhiya <priyanka.g@spurtreetech.in>
2022-02-14 16:43:01 +0530

    order Page u i and pop ups

--- resources/views/partials/modals/estimated-delivery-date-histories.blade.php
+++ resources/views/partials/modals/estimated-delivery-date-histories.blade.php
@@ -2,23 +2,22 @@
     <div class="modal-dialog modal-lg">
         <!-- Modal content-->
         <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Estimated Delivery Date History</h5>
+            <div class="pt-4 pl-4 pr-4">
+            <h4 class="modal-title">Estimated Delivery Date History</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
-                <div class="modal-body">
+            <div class="col-md-12">
                 <div class="row">
                 <input type="hidden" name="lead_developer_task_id" id="lead_developer_task_id">
-
                     <div class="col-md-12" id="lead_time_history_div">
-                        <table class="table">
+                        <table class="table" style="table-layout: fixed;">
                             <thead>
                                 <tr>
-                                    <th>Date</th>
-                                    <th>Old Value</th>
-                                    <th>New Value</th>
-                                    <th>Updated by</th>
-                                    <th></th>
+                                    <th width="20%">Date</th>
+                                    <th width="20%">Old Value</th>
+                                    <th width="20%">2022-02-14</th>
+                                    <th width="20%">Updated by</th>
+                                    <th width="20%"></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -26,9 +25,9 @@
                                 @foreach($estimated_delivery_histories as $delDateHistory)
                                 <tr>
                                 <td>{{$delDateHistory->created_at}}</td>
-                                <td>{{$delDateHistory->old_value}}</td>
-                                <td>{{$delDateHistory->new_value}}</td>
-                                <td>{{$delDateHistory->name}}</td>
+                                <td class="Website-task" title="{{$delDateHistory->old_value}}">{{$delDateHistory->old_value}}</td>
+                                <td class="Website-task" title="{{$delDateHistory->new_value}}">{{$delDateHistory->new_value}}</td>
+                                <td class="Website-task" title="{{$delDateHistory->name}}">{{$delDateHistory->name}}</td>
                                 </tr>
                                 @endforeach
                                 @endif
@@ -36,8 +35,8 @@
                         </table>
                     </div>
                 </div>
-                </div>
-                <div class="modal-footer">
+            </div>
+                <div class=" mt-4 mb-5 pl-4"style="margin-left: 707px;">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </div>
             </form>
