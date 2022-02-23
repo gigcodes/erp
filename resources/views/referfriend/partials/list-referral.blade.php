2854f8c1cf22ae1bc0edb622e5c8ccce693f33e9
atul-khatri <atul.k@sphinxworldbiz.com>
2022-02-23 10:56:22 +0530

    Fix refer friend api issue

--- resources/views/referfriend/partials/list-referral.blade.php
+++ resources/views/referfriend/partials/list-referral.blade.php
@@ -8,10 +8,11 @@
                     <td>{{ $refferal->referee_email }}</td>
                     <td>{{ $refferal->referee_phone }}</td>
                     <td>{{ $refferal->website }}</td>
+                    <td>{{ wordWrap($refferal->status, 30) }}</td>
                     <td>{{ Carbon\Carbon::parse($refferal->created_at)->format('d-m-y H:i') }}</td>
                     <td>
                         {!! Form::open(['method' => 'DELETE','route' => ['referfriend.destroy', $refferal->id],'style'=>'display:inline']) !!}
-                        <button type="submit" class="btn btn-image"><img src="/images/delete.png"/></button>
+                        <button type="submit" class="btn btn-image p-0"><i class="fa fa-trash"></i></button>
                         {!! Form::close() !!}
                         <button class="btn btn-image view_error p-0" data-toggle="modal" data-id="{{ $refferal->id }}"> <i class="fa fa-eye"></i> </button>
                     </td>
