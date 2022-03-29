30b51bdab5ecee322dd6e2b72a48b8eff69e001d
dhavaltecmantras <dhaval.tecmantras@gmail.com>
2022-03-29 15:26:05 +0530

    Pending for 2 two column issue discusss with satyam

--- resources/views/logging/partials/magento_prices_data.blade.php
+++ resources/views/logging/partials/magento_prices_data.blade.php
@@ -1,4 +1,13 @@
 @foreach($prices as $log)
+<?php 
+
+$pData = $p::find($log->product_id);
+
+$dutyPrice = $p->getDuty( $pData->product_country_code );
+$category_segment = $pData->category_segment != null ? $pData->category_segment : $pData->brand_segment;
+//dd($p->product_country_code);
+//$price = $p->getPrice( $log->store_websites_id, 'IN',null, true,$dutyPrice, null, null, null, isset($p->suppliers_info) ?  $p->suppliers_info[0]->price : 0, $category_segment);
+?>
 <tr>
     <td>
     @if($log->product_id)
@@ -9,10 +18,19 @@
         {{$log->default_price}}
     </td>
     <td>
+        {{$log->discounted_percentage}}
+    </td>
+    <td>
         {{$log->segment_discount}}
     </td>
     <td>
+        {{-- {{(float)$price['segment_discount_per']}} --}}
+    </td>
+    <td>
         {{$log->duty_price}}
+    </td>
+    <td>
+        {{-- {{$product->getDuty( $price->product_country_code)."%"}} --}}
     </td>
     <td>
         {{$log->override_price}}
