4e6d68dbde905fd90f5f71884cdf10dc3cdfcb3f
Yunoos Deorwine <yunoos.parvezansari@deorwine.com>
2022-04-11 09:42:27 +0530

    ui update

--- resources/views/magento_module/index.blade.php
+++ resources/views/magento_module/index.blade.php
@@ -89,24 +89,32 @@
         </div>
     </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+
+
+
 
     <div class="table-responsive mt-3 mr-5 ml-5">
+        @if ($message = Session::get('success'))
+            <div class="col-lg-12">
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            </div>
+        @endif
+
+        @if ($errors->any())
+            <div class="col-lg-12">
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        @endif
         <table class="table table-bordered" id="erp_table">
             <thead>
                 <tr>
