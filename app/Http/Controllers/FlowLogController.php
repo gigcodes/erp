ab7e75fbf2cedfe968098abd20dd31769181e905
Yunoos Deorwine <yunoos.parvezansari@deorwine.com>
2022-04-11 17:44:00 +0530

    add customer details modal type issue fix

--- app/Http/Controllers/FlowLogController.php
+++ app/Http/Controllers/FlowLogController.php
@@ -22,11 +22,23 @@ class FlowLogController extends Controller
 
         $pagination_count = Setting::get('pagination') ? Setting::get('pagination') : 1;
 
-        $logs = FlowLog::select(['flow_logs.*','flows.flow_name', 'flows.flow_description', 'flows.store_website_id', 'store_websites.website', 'flow_log_messages.modalType'])
-        ->join('flows','flows.id','flow_logs.flow_id')
+        $logs = FlowLog::orderby('updated_at', 'desc')->select(
+            [
+                'flow_logs.*',
+                'flows.flow_name', 
+                'flows.flow_description', 
+                'flows.store_website_id', 
+                'store_websites.website', 
+                'flow_log_messages.modalType',
+                'customers.name as lead_name'
+            ])
+        ->join('flows','flows.id', 'flow_logs.flow_id')
         ->join('flow_log_messages', 'flow_log_messages.flow_log_id', 'flow_logs.id')
+        ->leftJoin('customers', 'customers.id', '=', 'flow_log_messages.leads')
         ->join('store_websites', 'flows.store_website_id', 'store_websites.id');
-        
+
+        // dd($logs->limit(10)->get());
+
         if ($request->flow_name || $request->messages || $request->created_at) {
 
             $logs = $logs->orderby('updated_at', 'desc');
@@ -48,9 +60,9 @@ class FlowLogController extends Controller
         } else {
 
             $paginate = ($pagination_count * 10);
-            $logs = $logs->orderby('created_at', 'desc')
-            ->paginate($paginate);
-            
+            $logs = $logs->orderby('created_at', 'desc');
+            // dd($logs->limit(20)->get());
+            $logs = $logs->paginate($paginate);
         }
 
         if ($request->ajax()) {
@@ -64,23 +76,26 @@ class FlowLogController extends Controller
         return view('logging.flowlog', compact('logs'));
     }
     public function details(Request $request){
+        
 		$messageLogs =  [];
 		if(isset($request->id) || isset($request->scraper_id)) {
 			if(isset($request->id) and $request->id != 0){
 				$messageLogs = FlowLogMessages::where('flow_log_id', $request->id);
 				$messageLogs = $messageLogs->leftJoin('store_websites as sw', 'sw.id', '=', 'flow_log_messages.store_website_id')
-                ->leftJoin('users', 'users.id', '=', 'flow_log_messages.leads')
+                ->leftJoin('customers', 'customers.id', '=', 'flow_log_messages.leads')
                 ->leftJoin('flow_logs', 'flow_logs.id', '=', 'flow_log_messages.flow_log_id' )
                 ->leftJoin('flows', 'flows.id', '=', 'flow_logs.flow_id' )
                 ->leftJoin('store_websites', 'store_websites.id', '=', 'flows.store_website_id' )
-				->select('flow_log_messages.*', 'users.name as lead_name', 'store_websites.website')->get();
+				->select('flow_log_messages.*', 'customers.name as lead_name', 'store_websites.website')->get();
+
 			} else if (isset($request->scraper_id) and $request->scraper_id != 0){
+
 				$messageLogs = FlowLogMessages::where('scraper_id', $request->scraper_id);
                 if($request->assigned){
                     $messageLogs = $messageLogs->where('flow_log_messages.leads',$request->assigned);
                 }
 				$messageLogs = $messageLogs->leftJoin('store_websites as sw', 'sw.id', '=', 'flow_log_messages.store_website_id')->leftJoin('users', 'users.id', '=', 'flow_log_messages.leads')
-				->select('flow_log_messages.*','sw.website as website', 'users.name as lead_name')->get();
+				->select('flow_log_messages.*','sw.website as website', 'customers.name as lead_name')->get();
         	}
         }
         return view('logging.partials.flow_detail_data', compact('messageLogs'));
--- resources/views/logging/partials/flowlogdata.blade.php
+++ resources/views/logging/partials/flowlogdata.blade.php
@@ -3,12 +3,14 @@
         <td>{{ $log->id }}</td>
         <td>{{ $log->flow_name }}</td>
         <td>{{ $log->modalType }}</td>
+        <td>{{ $log->lead_name }}</td>
         <td>{{ $log->website }}</td>
         <td>{{ $log->flow_description }}</td>
-        <td>{{  $log->messages  }}</td>
-        <td>{{ \Carbon\Carbon::parse($log->log_created)->format('d-m-y H:i:s')  }}</td>
+        <td>{{ $log->messages }}</td>
+        <td>{{ \Carbon\Carbon::parse($log->log_created)->format('d-m-y H:i:s') }}</td>
         <td>
-            <button class="btn btn-xs btn-none-border show_error_logs" data-id="{{ $log->id}}"><i class="fa fa-eye"></i></button>
+            <button class="btn btn-xs btn-none-border show_error_logs" data-id="{{ $log->id }}"><i
+                    class="fa fa-eye"></i></button>
         </td>
     </tr>
 @endforeach
