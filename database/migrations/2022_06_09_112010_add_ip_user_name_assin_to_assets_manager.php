12c04488621f6c5a49cffca06ed6b61abd5e730a
dhavaltecmantras <dhaval.tecmantras@gmail.com>
2022-06-09 16:42:44 +0530

    Asset managment Log table

---
+++ database/migrations/2022_06_09_112010_add_ip_user_name_assin_to_assets_manager.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIpUserNameAssinToAssetsManager extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assets_manager', function (Blueprint $table) {
+            $table->text('user_name')->nullable()->after('capcity');
+            $table->text('assigned_to')->nullable();
+            $table->text('ip')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assets_manager', function (Blueprint $table) {
+            //
+        });
+    }
+}
