0c555e382bcef1d8c9ba0828a05e8cd750881839
dhavaltecmantras <dhaval.tecmantras@gmail.com>
2022-06-10 15:50:33 +0530

    Created Postman Response migration

---
+++ database/migrations/2022_06_10_141846_add_request_url_to_postman_request_creates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRequestUrlToPostmanRequestCreatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('postman_request_creates', function (Blueprint $table) {
+            $table->longText('request_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('postman_request_creates', function (Blueprint $table) {
+            $table->dropColumn('request_url');
+        });
+    }
+}
