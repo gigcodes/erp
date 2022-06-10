831d3c6329f892db81989162649c6f1d2b1a1666
Satyam <satyamtripathi@Satyams-MacBook-Air.local>
2022-06-10 17:20:58 +0530

    Magento logs

--- app/LogRequest.php
+++ app/LogRequest.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class LogRequest extends Model
 {
-    //
+    public static function log($startTime,$magentoURL,$method,$request,$response,$httpcode,$api_name,$api_method){
+    	$endTime    = date("Y-m-d H:i:s");
+        $timeTaken  = strtotime($endTime) - strtotime($startTime);
+        $r              = new LogRequest;
+        $r->url         = $magentoURL;
+        $r->status_code = $httpcode;
+        $r->method      = $method;
+        $r->method_name = $api_method;
+        $r->api_name    = $api_name;
+        $r->request     = $request;
+        $r->response    = !empty($response) ? json_encode($response) : json_encode([]);
+        $r->message     = isset($response['message']) ? $response['message'] : '';
+        $r->start_time  = $startTime;
+        $r->end_time    = $endTime;
+        $r->time_taken  = $timeTaken;
+        $r->is_send     = 1;
+        $r->save();
+    }
 }
