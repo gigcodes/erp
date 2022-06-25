a620f1a25fd4535fffaa96e536ee4405f98e7565
Dipen Modi <moddipen@gmail.com>
2022-06-25 12:48:03 +0530

    Fixed Comments on PR

--- app/Console/Commands/OrderCouponMerger.php
+++ app/Console/Commands/OrderCouponMerger.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\CronJobReport;
 use App\Coupon;
 use App\Order;
+use App\StoreWebsite;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
@@ -53,47 +54,49 @@ class OrderCouponMerger extends Command
             $cookieJar = CookieJar::fromArray([
                 '__cfduid' => 'd866a348dc8d8be698f25655b77ada8921560006391',
             ], '.giglio.com');
-
             try {
-                $guzzle = new Client(['headers' => [ 'Content-Type' => 'application/json']]);
-                $params = [];
-                $params['username'] = "admin1";
-                $params['password'] = "Admin@123";
-                $response = $guzzle->request('POST', 'https://dev6.sololuxury.com/rest/V1/integration/admin/token',['headers' => ['Accept' => 'application/json'],'body' => json_encode($params)]);  
-                try {
-                    $guzzle = new Client();
-                    $response = $guzzle->request('GET', 'https://dev6.sololuxury.com/rest/V1/orders?searchCriteria[filter_groups][0][filters][0][field]=coupon_code&searchCriteria[filter_groups][0][filters][0][value]=&searchCriteria[filter_groups][0][filters][0][condition_type]=notnull',['headers' => ['Authorization'=>'Bearer '.trim($response->getBody()->getContents(), '"')]]);  
-                    $response_object = json_decode($response->getBody()->getContents());                    
-                    if(!empty($response_object)){
-                        if(!empty($response_object->items)){
-                            foreach($response_object->items as $obj){
-                                if($obj->coupon_code != ""){
-                                    //Check if coupon is valid
-                                    $get_coupon_id = Coupon::where("code",$obj->coupon_code)->first(); 
-                                    if(!empty($get_coupon_id)){
-                                        foreach($obj->items as $item){
-                                            //Check if order id exists in record
-                                            $get_order_id = Order::where("id",$item->order_id)->first(); 
-                                            if(!empty($get_order_id)){
-                                                //Merge coupon with order is all goes well
-                                                $update_order_table_with_coupon = Order::where("id",$get_order_id->id)->update(["coupon_id"=>$get_coupon_id->id]);
+                $guzzle = new Client();
+                $get_api_endpoint_details = StoreWebsite::get();
+                if(!empty($get_api_endpoint_details)){
+                    foreach($get_api_endpoint_details as $store_detail){
+                        if($store_detail->magento_url != null && $store_detail->api_token != null){
+                            $response = $guzzle->request('GET', $store_detail->magento_url.'/rest/V1/orders?searchCriteria[filter_groups][0][filters][0][field]=coupon_code&searchCriteria[filter_groups][0][filters][0][value]=&searchCriteria[filter_groups][0][filters][0][condition_type]=notnull',['headers' => ['Authorization'=>'Bearer '.$store_detail->api_token]]);  
+                            $response_object = json_decode($response->getBody()->getContents());  
+                            if (json_last_error() == JSON_ERROR_NONE) {
+                                if(!empty($response_object)){
+                                    if(!empty($response_object->items)){
+                                        foreach($response_object->items as $obj){
+                                            if($obj->coupon_code != ""){
+                                                //Check if coupon is valid
+                                                $get_coupon_id = Coupon::where("code",$obj->coupon_code)->first(); 
+                                                if(!empty($get_coupon_id)){
+                                                    foreach($obj->items as $item){
+                                                        //Check if order id exists in record
+                                                        $get_order_id = Order::where("id",$item->order_id)->first(); 
+                                                        if(!empty($get_order_id)){
+                                                            //Merge coupon with order is all goes well
+                                                            $update_order_table_with_coupon = Order::where("id",$get_order_id->order_id)->update(["coupon_id"=>$get_coupon_id->id]);
+                                                        }
+                                                    }       
+                                                }
                                             }
-                                        }       
+                                        }                            
+                                    }else{
+                                        \App\CronJob::insertLastError($this->signature, "No record found");    
                                     }
-                                }
-                            }                            
-                        }else{
-                            \App\CronJob::insertLastError($this->signature, "No record found");    
-                        }
-                    }else{
-                        \App\CronJob::insertLastError($this->signature, "No record found");
-                    }                    
-                }catch (Guzzle\Http\Exception\BadResponseException $e) {
-                    \App\CronJob::insertLastError($this->signature, $response->getBody()->getContents());   
-                }          
-            } catch (Guzzle\Http\Exception\BadResponseException $e) {
+                                }else{
+                                    \App\CronJob::insertLastError($this->signature, "No record found");
+                                }   
+                            }                
+                        }                          
+                    }  
+                    exit;                                              
+                }else{
+                    \App\CronJob::insertLastError($this->signature, "Could not find magento url or token");
+                }                           
+            }catch (Guzzle\Http\Exception\BadResponseException $e) {
                 \App\CronJob::insertLastError($this->signature, $response->getBody()->getContents());   
-            }           
+            }        
             $report->update(['end_time' => Carbon::now()]);
         } catch (\Exception $e) {
             \App\CronJob::insertLastError($this->signature, $e->getMessage());
