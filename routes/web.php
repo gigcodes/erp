031173a66ba77a70b8788ed14a2795d65967d3d2
dhavaltecmantras <dhaval.tecmantras@gmail.com>
2022-06-30 17:49:54 +0530

    Slow log enable disabled  and create history with success message using ajax

--- routes/web.php
+++ routes/web.php
@@ -3671,6 +3671,9 @@ Route::group(['middleware' => 'auth', 'admin'], function () {
 
 Route::group(['middleware' => 'auth', 'admin'], function () {
     Route::any('/database-log', 'ScrapLogsController@databaseLog');
+    Route::get('/database-log/enable', 'ScrapLogsController@enableMysqlAccess');
+    Route::get('/database-log/disable', 'ScrapLogsController@disableMysqlAccess');
+    Route::get('/database-log/history', 'ScrapLogsController@disableEnableHistory');
 });
 
 Route::get('gtmetrix', 'gtmetrix\WebsiteStoreViewGTMetrixController@index')->name('gt-metrix');
