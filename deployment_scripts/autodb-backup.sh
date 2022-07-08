b13bec37cf56205997930ad106189d38944793a0
sahil kataria <sahilkataria.1989@gmail.com>
2022-07-08 11:31:02 +0200

    bkp mysql script

--- deployment_scripts/autodb-backup.sh
+++ deployment_scripts/autodb-backup.sh
@@ -1,6 +1,6 @@
 date=`date +%d-%m-%y`
 bkproot=/mnt/backup/erp_backup
-bkpdir=/mnt/backup/erp_backup/$date
+bkpdir=$bkproot/$date
 
 mkdir -p $bkpdir
 mysqldump erp_live |gzip -v > $bkpdir/erp_live.sql.gz
@@ -11,16 +11,17 @@ find $bkproot -mtime +8 -exec rm -rf {} \;
 ############### Magento Servers Database Backup #############
 envfile='/var/www/erp.theluxuryunlimited.com/.env'
 magento_servers='BRANDS AVOIRCHIC OLABELS SOLOLUXURY SUVANDNAT THEFITEDIT THESHADESSHOP UPEAU VERALUSSO'
-mageroot=/mnt/backup/magento_backup/$date
+mageroot=/mnt/backup/magento_backup
+magebkp=$bkproot/$date
 
 user=`grep MAGENTO_DB_USER $envfile|cut -d'=' -f2`
 pass=`grep MAGENTO_DB_PASSWORD $envfile|cut -d"'" -f2`
 
 for server in $magento_servers
 do
-	mkdir -p $mageroot/$server
+	mkdir -p $magebkp/$server
 	db=`grep "$server"_DB $envfile|cut -d'=' -f2`
 	host=`grep "$server"_HOST $envfile|cut -d'=' -f2`
-	mysqldump -h $host -u $user -p"$pass" $db |gzip -v > $mageroot/$server/$db.sql.gz
+	mysqldump -h $host -u $user -p"$pass" $db |gzip -v > $magebkp/$server/$db.sql.gz
 done
 find $mageroot -mtime +8 -exec rm -rf {} \; 
