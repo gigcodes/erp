81319a9ebb7cb07a1f60207cf8c84e22b6501dca
Modi Dipen <moddipen@gmail.com>
2022-07-11 12:19:18 +0530

    create migration

---
+++ database/migrations/2022_07_11_104041_create_host_items1_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHostItems1Table extends Migration
+{
+    public function up()
+    {
+        Schema::create('host_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('host_id');
+            $table->foreign('host_id')->references('hostid')->on('hosts')->onDelete('cascade');
+            $table->integer('item_id','55');
+            $table->integer('hostid','55');
+            $table->double('free_inode_in', 6, 2);
+            $table->double('space_utilization', 6, 2);
+            $table->bigIncrements('total_space', 55);
+            $table->bigIncrements('used_space ', 55);
+            $table->bigIncrements('available_memory',55);
+            $table->double('available_memory_in', 6, 2);
+            $table->double('cpu_idle_time', 6, 2);
+            $table->double('cpu_utilization', 6, 2);
+            $table->double('interrupts_per_second', 8, 4);
+            $table->timestamps();
+        });
+    }
+
+/**
+ * Reverse the migrations.
+ *
+ * @return void
+ */
+    public function down()
+    {
+        Schema::dropIfExists('host_items');
+    }
+}
