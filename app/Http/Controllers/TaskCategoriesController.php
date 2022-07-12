72ac4cef0e2cbdbbb976577c26d29ac8d9256fb3
Modi Dipen <moddipen@gmail.com>
2022-07-12 19:17:41 +0530

    task_documemt

--- app/Http/Controllers/TaskCategoriesController.php
+++ app/Http/Controllers/TaskCategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\TaskCategories;
+use App\TaskCategory;
 use App\TaskSubCategory;
 use App\TaskSubject;
 use App\TaskHistories;
@@ -16,7 +17,11 @@ class TaskCategoriesController extends Controller
     //
     public function index(Request $request){
         if ($request->ajax()) {
+            $items = TaskCategories::with(["subcategory.task_subject.history"])->get();
             
+            echo "<pre>";
+            print_r($items->toArray());
+            exit;
             //return datatables()->eloquent($items)->toJson();
         }else{
             $title = 'Project Documentation Module';
@@ -27,27 +32,47 @@ class TaskCategoriesController extends Controller
     public function store(Request $request){
         
         if ($request->ajax()) {
-            dd($request->all());
+            
            $name = $request['category_name'];
            $user_id = Auth::user()->id;
            $category_array = array(
                "name" =>$name,
            );
            $task_category = TaskCategories::create($category_array);
-           
-        
-                $sub_category_array = array(
-                    'task_category_id'=>$task_category->id,
-                    'name'=>$request['sub_category'],
-                );
+           $sub_category_array = array(
+                'task_category_id'=>$task_category->id,
+                'name'=>$request['sub_category_name'],
+            );
             
             $task_subcategory = TaskSubCategory::create($sub_category_array);
+           $subject1 = array();
             foreach($request['subject'] as $key=>$subject){
+              
                 $subject_array = array(
                     'task_category_id'=>$task_category->id,
-                    'name'=>$sub_category[$key],
+                    'task_subcategory_id'=>$task_subcategory->id,
+                    'name'=>$request['subjectname'][$key],
+                    'description'=>$subject,
                 );
+                $subject1[$key] = TaskSubject::create($subject_array);
+                
             }
+            
+           
+               foreach($subject1 as $key => $sub){
+               
+                $task_history = array(
+                    'user_id' => $user_id,
+                    'task_subject_id'=>$sub->id,
+                    'name_before'=>$sub->name,
+                    'name_after'=>NULL,
+                    'description_before'=>$sub->description,
+                    'description_after'=>NULL,
+                );
+                $history = TaskHistories::create($task_history);
+               }
+               
+            return back();
             //return datatables()->eloquent($items)->toJson();
         }
        
--- app/TaskSubject.php
+++ app/TaskSubject.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class TaskSubject extends Model
 {
     //
+    protected $table = "task_subjects";
+    protected $fillable =[
+        'task_category_id','task_subcategory_id','name','description'
+    ];
     public function category(){
         return $this->belongsTo(TaskSubCategory::class);
     }
