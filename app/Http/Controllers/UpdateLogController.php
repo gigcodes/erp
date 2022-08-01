ee80249c9406357fcbc95d84910f12e533f06d54
Deven R <deven.ahir13@gmail.com>
2022-08-01 08:16:57 +0530

    DEVTASK-22179 : As per given doc

--- app/Http/Controllers/UpdateLogController.php
+++ app/Http/Controllers/UpdateLogController.php
@@ -7,61 +7,64 @@ use Illuminate\Http\JsonResponse;
 use App\UpdateLog;
 use App\Setting;
 
-class UpdateLogController extends Controller
-{
+class UpdateLogController extends Controller {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        try{
-            $updateLog = UpdateLog::paginate(15);
-            return view("update-log.index", compact('updateLog'));
-        } catch(\Exception $e) {
+    public function index() {
+        try {
+            $q = UpdateLog::query();
+            if ($s = request('api_url')) {
+                $q->where("api_url", "like", "%" . $s . "%");
+            }
+            if ($s = request('device')) {
+                $q->where("device", "like", "%" . $s . "%");
+            }
+            if ($s = request('api_type')) {
+                $q->where("api_type", "like", "%" . $s . "%");
+            }
+            $updateLog = $q->orderBy('id', 'DESC')->paginate(Setting::get('pagination'));
+
+            $listApiUrls = UpdateLog::orderBy('api_url')->select('api_url')->distinct()->pluck('api_url', 'api_url');
+            $listApiMethods = UpdateLog::orderBy('api_type')->select('api_type')->distinct()->pluck('api_type', 'api_type');
+            return view("update-log.index", compact(
+                'updateLog',
+                'listApiUrls',
+                'listApiMethods'
+            ));
+        } catch (\Exception $e) {
             return redirect()->back()->withErrors($e->getMessage());
         }
-        
     }
 
-    public function search(Request $request)
-    {
-        $updateLog = new UpdateLog();
-        
-        if (!empty($request->api_url)) {
-            $updateLog = $updateLog->where("api_url", "like", "%".$request->api_url."%");
-        }
-        if (!empty($request->device)) {
-            $updateLog = $updateLog->where("device", "like", "%".$request->device."%");
-        }
-        if (!empty($request->api_type)) {
-            $updateLog = $updateLog->where("api_type", "like", "%".$request->api_type."%");
-        }
-        $updateLog = $updateLog->paginate(Setting::get('pagination'));
-        return view("update-log.index", compact('updateLog'));
+    public function search(Request $request) {
+        return $this->index();
     }
 
-    
+
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         try {
             $updateLog = new UpdateLog();
             $updateLog->api_url = $request->api_url;
             $updateLog->device = $request->device;
             $updateLog->api_type = $request->api_type;
             $updateLog->user_id = $request->user_id;
-            $updateLog->request_header = is_array($request->header)? json_encode($request->header) : $request->header;
+            $updateLog->request_header = is_array($request->header) ? json_encode($request->header) : $request->header;
+            $updateLog->email = $request->email ?: NULL;
+            $updateLog->request_body = is_array($request->request_body) ? json_encode($request->request_body) : $request->request_body;
+            $updateLog->response_code = $request->response_code ?: NULL;
+            $updateLog->response_body = is_array($request->response_body) ? json_encode($request->response_body) : $request->response_body;
             $updateLog->app_version = $request->app_version;
             $updateLog->other = $request->other;
-            $updateLog->save();
-            if (!empty($updateLog)) {
+            if ($updateLog->save()) {
                 return response()->json(['status' => true, 'response_code' => 200, 'data' => $updateLog], JsonResponse::HTTP_OK);
             }
             return response()->json(['status' => false, 'response_code' => 404, 'message' => "Data not found"], JsonResponse::HTTP_NOT_FOUND);
@@ -70,15 +73,14 @@ class UpdateLogController extends Controller
         }
     }
 
-    
+
     /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         try {
             $updateLog = UpdateLog::all();
 
@@ -97,8 +99,7 @@ class UpdateLogController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         //
     }
 
@@ -109,8 +110,7 @@ class UpdateLogController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 
@@ -120,11 +120,10 @@ class UpdateLogController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request)
-    {
-        try{
+    public function destroy(Request $request) {
+        try {
             $updateLog = UpdateLog::where('id', '=', $request->id)->delete();
-            return response()->json(['code' => 200, 'data' => $updateLog,'message' => 'Deleted successfully!!!']);
+            return response()->json(['code' => 200, 'data' => $updateLog, 'message' => 'Deleted successfully!!!']);
         } catch (\Exception $e) {
             $msg = $e->getMessage();
             return response()->json(['code' => 500, 'message' => $msg]);
--- resources/views/update-log/index.blade.php
+++ resources/views/update-log/index.blade.php
@@ -3,219 +3,227 @@
 @section('title', 'Update Log')
 
 @section('styles')
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css">
-    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css">
-    <style>
-      .multiselect {
-            width: 100%;
-        }
-        .multiselect-container li a {
-          line-height: 3;
-        }
-    </style>
-   
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css">
+<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css">
+<style>
+  .multiselect {
+    width: 100%;
+  }
+
+  .multiselect-container li a {
+    line-height: 3;
+  }
+</style>
+
 @endsection
 
 @section('content')
 
-    <div class="row">
-        <div class="col-12">
-            <h2 class="page-heading">Update Log</h2>
-          </div>
-
-          <div class="col-12 mb-3">
-            <div class="pull-left">
-            </div>
-            <div class="pull-right">
-                <!-- <a title="add new domain" class="btn btn-secondary add-new-btn">+</a> -->
-            </div>
-        </div>
+<div class="row">
+  <div class="col-12">
+    <h2 class="page-heading">Update Log</h2>
+  </div>
+  <div class="col-12 mb-3">
+    <div class="pull-left">
     </div>
-    <div class=" row ">
-      <form class="form-inline" action="/updateLog/search" method="GET">
-        <div class="col">
-          <div class="form-group">
-            <div class="input-group">
-              <?php $requestNamrArr = []; ?>
-              @foreach ($updateLog as $key => $logD)
-                  <?php array_push($requestNamrArr,$logD->api_url);?>
-              @endforeach
-              <?php $requestNamrArr = array_unique($requestNamrArr); ?>
-              <select name="api_url"  class="form-control" id="api_url" >
-                <option value="">--Select Request Name--</option>
-                @foreach ($requestNamrArr as $key => $reqName)
-                <?php $selected  = '';
-                if($reqName == request('api_url')) {
-                  $selected  = 'selected = "selected"';
-                }
-                  ?>
-                    <option {{$selected}} value="{{$reqName}}">{{$reqName}}</option>
-                @endforeach
-              </select>
-              {{-- <input type="text" placeholder="Request Name" class="form-control" name="request_name" value="{{request('request_name')}}"> --}}
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="form-group">
-            <div class="input-group">
-              {{-- <input type="text" placeholder="Request Type" class="form-control" name="api_type" value="{{request('api_type')}}"> --}}
-                <select name="api_type" value="" class="form-control" id="api_types" >
-                 
-                  <option value="">--select Method</option>
-                  <option value="GET" <?php if(request('api_type') == 'GET') { echo 'selected'; } ?>>GET</option>
-                  <option value="POST" <?php if(request('api_type') == 'POST') { echo 'selected'; } ?>>POST</option>
-                  <option value="PUT" <?php if(request('api_type') == 'PUT') { echo 'selected'; } ?>>PUT</option>
-                  <option value="PATCH" <?php if(request('api_type') == 'PATCH') { echo 'selected'; } ?>>PATCH</option>
-                  <option value="DELETE" <?php if(request('api_type') == 'DELETE') { echo 'selected'; } ?>>DELETE</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <button type="submit" class="btn btn-image"><img src="/images/filter.png"></button>
-          <a href="/updateLog" class="btn btn-image" id=""><img src="/images/resend2.png" style="cursor: nwse-resize;"></a>
-        </div>
-      </form>
-
-    
+    <div class="pull-right">
+      <!-- <a title="add new domain" class="btn btn-secondary add-new-btn">+</a> -->
     </div>
-    
-	</br> 
-  <div class="row m-0" >
-    <div class="col-12" style="border: 1px solid;border-color: #dddddd;">
-	<div class="table-responsive mt-2" style="overflow-x: auto !important;">
-      <table class="table table-bordered text-nowrap">
-        <thead>
-          <tr>
-            <th style="width: 3%;">ID</th>
-            <th style="width: 40%;overflow-wrap: anywhere;">API Url</th>
-            <th style="width: 5%;overflow-wrap: anywhere;">Device Name</th>
-            <th style="width: 5%;overflow-wrap: anywhere;">Api Type</th>
-            <th style="width: 25%;overflow-wrap: anywhere;">User Name</th>
-            <th style="width: 10%;overflow-wrap: anywhere;">Action</th>
-          </tr>
-        </thead>
+  </div>
+</div>
 
-        <tbody>
-			  @foreach ($updateLog as $key => $logData)
-              <tr>
-                <td>{{$logData->id}}</td>
-                <td class="expand-row-msg" data-name="url" data-id="{{$logData->id}}">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="card d-normal">
+        <div class="card-header">
+          <h4>
+            <a class="collapsed card-link" data-toggle="collapse" href="#collapseSearch" aria-expanded="true">
+              <i class="fa fa-arrow-up"></i>
+              <i class="fa fa-arrow-down"></i>
+              Filter
+            </a>
+          </h4>
+        </div>
+        <div id="collapseSearch" class="collapse show">
+          <div class="card-body">
+            <form action="/updateLog/search" method="GET">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <div class="form-group">
+                        <label for="form-label">API Urls</label>
+                        <select class="form-control select2" name="api_url">
+                          <option value=""></option>
+                          {!! makeDropdown($listApiUrls ?? [], request('api_url')) !!}
+                        </select>
+                      </div>
+                    </div>
+                    <div class="col-md-2">
+                      <div class="form-group">
+                        <label for="form-label">API Methods</label>
+                        <select class="form-control select2" name="api_type">
+                          <option value=""></option>
+                          {!! makeDropdown($listApiMethods ?? [], request('api_type')) !!}
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <button type="submit" class="btn btn-primary">Search</button>
+                  <a href="/updateLog" class="btn btn-outline-secondary">Clear</a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-12">
+
+      <div class="table-responsive mt-2" style="overflow-x: auto !important;">
+        <table class="table table-bordered" style="border: 1px solid #ddd !important;">
+          <thead>
+            <tr>
+              <th style="width: 4%;">ID</th>
+              <th style="width: 10%;">API Url</th>
+              <th style="width: 8%;">Device</th>
+              <th style="width: 6%;">Api Type</th>
+              <th style="width: 20%;word-break: break-word;">Request Body</th>
+              <th style="width: 8%;">Response Code</th>
+              <th style="width: 20%;">Response Body</th>
+              <th style="width: 6%;">App Version</th>
+              <th style="width: 8%;">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php if ($updateLog->count()) { ?>
+              <?php foreach ($updateLog as $key => $logData) { ?>
+                <tr>
+                  <td>{{$logData->id}}</td>
+                  <td class="expand-row-msg" data-name="url" data-id="{{$logData->id}}">
                     <span class="show-short-url-{{$logData->id}}">{{ str_limit($logData->api_url, 20, '..')}}</span>
                     <span style="word-break:break-all;" class="show-full-url-{{$logData->id}} hidden">{{$logData->api_url}}</span>
-                </td>
-                <td>{{$logData->device}}</td>
-                <td>{{$logData->api_type}}</td>
-                <td>{{$logData->user_id}}</td>
-                <td>
-                  <a class="btn delete-updateLog-btn"  data-id="{{ $logData->id }}" href="#"><img  data-id="{{ $logData->id }}" src="/images/delete.png" style="cursor: nwse-resize; width: 16px;"></a>
-                </td>
+                  </td>
+                  <td>{{$logData->device}}</td>
+                  <td>{{$logData->api_type}}</td>
+                  <td style="word-break: break-word;" >{!! $logData->request_body ?: '-' !!}</td>
+                  <td>{{$logData->response_code}}</td>
+                  <td style="word-break: break-word;" >{!! $logData->response_body ?: '-' !!}</td>
+                  <td>{{$logData->app_version}}</td>
+                  <td>
+                    <a class="btn delete-updateLog-btn" data-id="{{ $logData->id }}" href="#"><img data-id="{{ $logData->id }}" src="/images/delete.png" style="cursor: nwse-resize; width: 16px;"></a>
+                  </td>
+                </tr>
+              <?php } ?>
+            <?php } else { ?>
+              <tr>
+                <td colspan="10">No records found.</td>
               </tr>
-           
-            
-            @endforeach
-        </tbody>
-      </table>
-      <div class="text-center">
-        {!! $updateLog->appends(Request::except('page'))->links() !!}
+            <?php } ?>
+          </tbody>
+        </table>
+        <div class="text-center">
+          {!! $updateLog->appends(Request::except('page'))->links() !!}
+        </div>
+      </div>
     </div>
-	</div>
-    </div>
-    <div id="loading-image" style="position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url('/images/pre-loader.gif') 50% 50% no-repeat;display:none;">
-   </div>
   </div>
+</div>
+
 @endsection
 
 <link rel="stylesheet" type="text/css" href="{{asset('css/jquery.dropdown.min.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('css/jquery.dropdown.css')}}">
 @section('scripts')
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.3.7/jquery.jscroll.min.js"></script>
-    <script src="/js/bootstrap-multiselect.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.3.7/jquery.jscroll.min.js"></script>
+<script src="/js/bootstrap-multiselect.min.js"></script>
 
-    <script src="{{asset('js/mock.js')}}"></script>
-    <script src="{{asset('js/jquery.dropdown.min.js')}}"></script>
-    <script src="{{asset('js/jquery.dropdown.js')}}"></script>
-    
+<script src="{{asset('js/mock.js')}}"></script>
+<script src="{{asset('js/jquery.dropdown.min.js')}}"></script>
+<script src="{{asset('js/jquery.dropdown.js')}}"></script>
 
-    <script>
-      var Random = Mock.Random;
-      var json1 = Mock.mock({
-      "data|10-50": [{
-        name: function () {
-          return Random.name(true)
+
+<script>
+  var Random = Mock.Random;
+  var json1 = Mock.mock({
+    "data|10-50": [{
+      name: function() {
+        return Random.name(true)
+      },
+      "id|+1": 1,
+      "disabled|1-2": true,
+      groupName: 'Group Name',
+      "groupId|1-4": 1,
+      "selected": true
+    }]
+  });
+  $('.dropdown-mul-1').dropdown({
+    data: json1.data,
+    limitCount: 40,
+    multipleMode: 'label',
+    choice: function() {
+      // console.log(arguments,this);
+    }
+  });
+
+  $('.dropdown-sin-1').dropdown({
+    readOnly: true,
+    input: '<input type="text" maxLength="20" placeholder="Search">'
+  });
+</script>
+</div>
+
+<script type="text/javascript">
+  $('.multiselect').multiselect({
+    enableClickableOptGroups: true
+  });
+  $(document).on("click", ".openmodeladdpostman", function(e) {
+    $('#titleUpdate').html("Add");
+    $('#postmanform').find("input[type=text], textarea").val("");
+  });
+
+
+  $(document).on("click", ".delete-updateLog-btn", function(e) {
+    e.preventDefault();
+    if (confirm("Are you sure?")) {
+      var $this = $(this);
+      var id = $this.data('id');
+      $.ajax({
+        url: "/updateLog/delete",
+        type: "delete",
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         },
-        "id|+1": 1,
-        "disabled|1-2": true,
-        groupName: 'Group Name',
-        "groupId|1-4": 1,
-        "selected": true
-      }]
-    });
-      $('.dropdown-mul-1').dropdown({
-      data: json1.data,
-      limitCount: 40,
-      multipleMode: 'label',
-      choice: function () {
-        // console.log(arguments,this);
-      }
-    });
-
-    $('.dropdown-sin-1').dropdown({
-      readOnly: true,
-      input: '<input type="text" maxLength="20" placeholder="Search">'
-    });
-
-    </script>
-  </div>
- 
-  <script type="text/javascript">
-
-    $('.multiselect').multiselect({
-        enableClickableOptGroups: true
-    });
-    $(document).on("click",".openmodeladdpostman",function(e){
-      $('#titleUpdate').html("Add");
-        $('#postmanform').find("input[type=text], textarea").val("");
-    });
-
-    
-    $(document).on("click",".delete-updateLog-btn",function(e){
-        e.preventDefault();
-        if (confirm("Are you sure?")) {
-          var $this = $(this);
-          var id = $this.data('id');
-          $.ajax({
-            url: "/updateLog/delete",
-            type: "delete",
-            headers: {
-                  'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-              },
-            data:{
-              id:id
-            }
-          }).done(function(response) {
-            if(response.code = '200') {
-              toastr['success']('Postman deleted successfully!!!', 'success'); 
-              location.reload();
-            } else {
-              toastr['error'](response.message, 'error'); 
-            }
-          }).fail(function(errObj) {
-            $('#loading-image').hide();
-            $("#addPostman").hide();
-            toastr['error'](errObj.message, 'error');
-          });
-          }
+        data: {
+          id: id
+        }
+      }).done(function(response) {
+        if (response.code = '200') {
+          toastr['success']('Postman deleted successfully!!!', 'success');
+          location.reload();
+        } else {
+          toastr['error'](response.message, 'error');
+        }
+      }).fail(function(errObj) {
+        $('#loading-image').hide();
+        $("#addPostman").hide();
+        toastr['error'](errObj.message, 'error');
       });
-    
-    $(document).ready(function(){
-		  $('#per_user_name').select2();
-    });
-	
-  </script>
+    }
+  });
+
+  $(document).ready(function() {
+    $('#per_user_name').select2();
+  });
+
+  jQuery(document).ready(function() {
+    applySelect2(jQuery('.select2'));
+  });
+</script>
 @endsection
