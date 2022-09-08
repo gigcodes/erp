71b4fef20ae6c710cae0430ea9a25b2d6503d34b
dhavaltecmantras <dhaval.tecmantras@gmail.com>
2022-09-08 19:44:06 +0530

    added fiter on page

--- Modules/StoreWebsite/Http/Controllers/SiteDevelopmentControlller.php
+++ Modules/StoreWebsite/Http/Controllers/SiteDevelopmentControlller.php
@@ -187,12 +187,12 @@ class SiteDevelopmentController extends Controller {
             $site_developement_id = $category->site_development_id;
             $taskStatistics['Devtask'] = DeveloperTask::where('site_developement_id', $site_developement_id)->where('status', '!=', 'Done')->select();
 
-            $query = DeveloperTask::join('users', 'users.id', 'developer_tasks.assigned_to')->where('site_developement_id', $site_developement_id)->where('status', '!=', 'Done')->select('developer_tasks.id', 'developer_tasks.task as subject', 'developer_tasks.status', 'users.name as assigned_to_name');
+            $query = DeveloperTask::join('users', 'users.id', 'developer_tasks.assigned_to')->where('site_developement_id', $site_developement_id)->where('status', '!=', 'Done')->select('developer_tasks.id', 'developer_tasks.task as subject', 'developer_tasks.status', 'users.name as assigned_to_name', 'users.id AS userID');
             $query = $query->addSelect(DB::raw("'Devtask' as task_type,'developer_task' as message_type"));
             $taskStatistics = $query->orderBy("developer_tasks.id", "DESC")->get();
             //print_r($taskStatistics);
             $othertask = Task::where('site_developement_id', $site_developement_id)->whereNull('is_completed')->select();
-            $query1 = Task::join('users', 'users.id', 'tasks.assign_to')->where('site_developement_id', $site_developement_id)->whereNull('is_completed')->select('tasks.id', 'tasks.task_subject as subject', 'tasks.assign_status', 'users.name as assigned_to_name');
+            $query1 = Task::join('users', 'users.id', 'tasks.assign_to')->where('site_developement_id', $site_developement_id)->whereNull('is_completed')->select('tasks.id', 'tasks.task_subject as subject', 'tasks.assign_status', 'users.name as assigned_to_name', 'users.id AS userID');
             $query1 = $query1->addSelect(DB::raw("'Othertask' as task_type,'task' as message_type"));
             $othertaskStatistics = $query1->orderBy("tasks.id", "DESC")->get();
             $merged = $othertaskStatistics->merge($taskStatistics);
