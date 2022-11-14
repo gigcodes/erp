1f141365397a5be69db7a38bae143198b308d4a5
pankaj-godhani <godhanipnj131@gmail.com>
2022-11-14 17:28:28 +0530

    Database fields changes in databaselog

---
+++ database/migrations/153540_2022-11-14_add_url_fields_database_logs.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUrlFieldsDatabaseLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('database_logs', function (Blueprint $table) {
+            $table->bigInteger('time_taken')->nullable()->after('logmessage');
+            $table->string('url')->nullable()->after('time_taken');
+            $table->longText('sql_data')->nullable()->after('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todo_lists', function (Blueprint $table) {
+            $table->dropColumn('time_taken');
+            $table->dropColumn('url');
+            $table->dropColumn('sql_data');
+        });
+    }
+}
