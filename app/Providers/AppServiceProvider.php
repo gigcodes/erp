890b705e9aa1becd5e4cdea06b3c08754c1e5543
shyam <shyam@ghanshyamdigital.com>
2022-11-28 21:00:35 +0530

    #DEVTASK-22301-1 fix Required "app_id" key not supplied in config and could not find fallback environment variable "FACEBOOK_APP_ID"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -68,9 +68,11 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->singleton(Facebook::class, function ($app) {
-            return new Facebook(config('facebook.config'));
-        });
+        if(!env('CI')) {
+            $this->app->singleton(Facebook::class, function ($app) {
+                return new Facebook(config('facebook.config'));
+            });
+        }
 
         $this->app->singleton(ScrapedProducts::class);
 
