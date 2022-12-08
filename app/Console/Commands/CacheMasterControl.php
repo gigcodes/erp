d8f38be8cc780aa6879833ee6cbdb44c2f12c106
Shift <shift@laravelshift.com>
2022-12-08 18:51:56 +0000

    Convert Cache TTL from minutes to seconds
    To conform with PSR-16 Laravel 5.8 changed the cache expiration time from minutes to seconds. If you were passing an integer, Shift converted this to its integer equivalent in seconds. If you were passing a "minutes" variable, Shift converted this to the relative date time equivalent for readability.

--- app/Console/Commands/CacheMasterControl.php
+++ app/Console/Commands/CacheMasterControl.php
@@ -56,42 +56,42 @@ class CacheMasterControl extends Command
             ]);
 
             // update cache for the cropped image references -- using in master control
-            Cache::remember('cropped_image_references', 30, function () {
+            Cache::remember('cropped_image_references', 1800, function () {
                 return CroppedImageReference::count();
             });
 
             // update pending crop references -- using in master control
-            Cache::remember('pending_crop_reference', 30, function () {
+            Cache::remember('pending_crop_reference', 1800, function () {
                 return Product::where('status_id', StatusHelper::$autoCrop)->where('stock', '>=', 1)->count();
             });
 
             // update pending crop references -- using in master control
-            Cache::remember('crop_reference_week_count', 30, function () {
+            Cache::remember('crop_reference_week_count', 1800, function () {
                 return CroppedImageReference::where('created_at', '>=', \Carbon\Carbon::now()->subDays(7)->startOfDay())->count();
             });
 
-            Cache::remember('cronLastErrors', 30, function () {
+            Cache::remember('cronLastErrors', 1800, function () {
                 return \App\CronJob::join('cron_job_reports as cjr', 'cron_jobs.signature', 'cjr.signature')
                     ->where('cjr.start_time', '>', \DB::raw('NOW() - INTERVAL 24 HOUR'))->where('cron_jobs.last_status', 'error')->groupBy('cron_jobs.signature')->get();
             });
 
             // update crop reference daily count -- using in master control
-            Cache::remember('crop_reference_daily_count', 30, function () {
+            Cache::remember('crop_reference_daily_count', 1800, function () {
                 return CroppedImageReference::whereDate('created_at', Carbon::today())->count();
             });
 
             // pending crop category -- using in master control
-            Cache::remember('pending_crop_category', 30, function () {
+            Cache::remember('pending_crop_category', 1800, function () {
                 return Product::where('status_id', StatusHelper::$attributeRejectCategory)->where('stock', '>=', 1)->count();
             });
 
             // status count -- using in master control
-            Cache::remember('status_count', 30, function () {
+            Cache::remember('status_count', 1800, function () {
                 return StatusHelper::getStatusCount();
             });
 
             // scraped product in stock -- using in master control
-            Cache::remember('result_scraped_product_in_stock', 30, function () {
+            Cache::remember('result_scraped_product_in_stock', 1800, function () {
                 $sqlScrapedProductsInStock = "
                     SELECT
                         COUNT(DISTINCT(sp.sku)) as ttl
@@ -183,19 +183,19 @@ class CacheMasterControl extends Command
 
             });*/
 
-            Cache::remember('reply_categories', 30, function () {
+            Cache::remember('reply_categories', 1800, function () {
                 return $reply_categories = ReplyCategory::all();
             });
 
-            Cache::remember('vendorReplier', 30, function () {
+            Cache::remember('vendorReplier', 1800, function () {
                 return $vendorReplier = \App\Reply::where('model', 'Vendor')->whereNull('deleted_at')->pluck('reply', 'id')->toArray();
             });
 
-            Cache::remember('supplierReplier', 30, function () {
+            Cache::remember('supplierReplier', 1800, function () {
                 return $supplierReplier = \App\Reply::where('model', 'Supplier')->whereNull('deleted_at')->pluck('reply', 'id')->toArray();
             });
 
-            Cache::remember('latestScrapRemarks', 30, function () {
+            Cache::remember('latestScrapRemarks', 1800, function () {
                 return \DB::select(
                     '
                     select * 
@@ -204,7 +204,7 @@ class CacheMasterControl extends Command
                 );
             });
 
-            Cache::remember('todaytaskhistory', 30, function () {
+            Cache::remember('todaytaskhistory', 1800, function () {
                 $date = "'%".date('Y-m-d')."%'";
 
                 return DB::select('SELECT users.name, developer_tasks.subject, developer_tasks.id as devtaskId,tasks.id as task_id,tasks.task_subject as task_subject,  hubstaff_activities.starts_at, SUM(tracked) as day_tracked 
@@ -218,7 +218,7 @@ class CacheMasterControl extends Command
                     order by day_tracked desc ');
             });
 
-            Cache::remember('hubstafftrackingnotiification', 30, function () {
+            Cache::remember('hubstafftrackingnotiification', 1800, function () {
                 $yesterdayDate = Carbon::now()->subDays(1)->format('Y-m-d');
 
                 $hubstaff_notifications = \App\Hubstaff\HubstaffActivityNotification::join('users as u', 'hubstaff_activity_notifications.user_id', 'u.id')->leftJoin('user_avaibilities as av', 'hubstaff_activity_notifications.user_id', 'av.user_id')
@@ -234,7 +234,7 @@ class CacheMasterControl extends Command
                 return $hubstaff_notifications;
             });
 
-            Cache::remember('hubstafftrackingnotiification', 30, function () {
+            Cache::remember('hubstafftrackingnotiification', 1800, function () {
                 $yesterdayDate = Carbon::now()->subDays(1)->format('Y-m-d');
 
                 $hubstaff_notifications = \App\Hubstaff\HubstaffActivityNotification::join('users as u', 'hubstaff_activity_notifications.user_id', 'u.id')->leftJoin('user_avaibilities as av', 'hubstaff_activity_notifications.user_id', 'av.user_id')
