0ec2bfcaa2e570d55077133fb82a833ed9935920
Satyam <satyam@Ronaks-MacBook-Pro.local>
2022-12-17 04:41:04 +0530

    Commit zipper

--- config/app.php
+++ config/app.php
@@ -192,6 +192,7 @@ return [
         App\Providers\WhatsappConfigProvider::class,
         Webklex\IMAP\Providers\LaravelServiceProvider::class,
         Brotzka\DotenvEditor\DotenvEditorServiceProvider::class,
+        Madnest\Madzipper\MadzipperServiceProvider::class,
         // seo2websites\ErpCustomer\ErpCustomerServiceProvider::class,
         //LaravelFCM\FCMServiceProvider::class,
 
