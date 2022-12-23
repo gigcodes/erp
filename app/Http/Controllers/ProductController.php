5214a47ced193981cd4d3bdbd263bc2bf2f6e728
Manoj11dev <manoj.manektech@gmail.com>
2022-12-23 14:25:46 +0530

    completed task 22437

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1470,29 +1470,131 @@ class ProductController extends Controller
 
     public function magentoPushStatus(Request $request)
     {
+        
+        if($request->ajax()){
+            $value = $request->get('value');
+            $fieldName = $request->get('fieldname');
+            if (auth()->user()->isReviwerLikeAdmin('final_listing')) {
+                $products = Product::query();
+            } else {
+                $products = Product::query()->where('assigned_to', auth()->user()->id);
+            }
+            // $products = $products->where(function ($query) {
+            //     $query->where('status_id', StatusHelper::$pushToMagento);
+            //     $query->orWhere('status_id', StatusHelper::$inMagento);
+            // });
+            // $products = $products->where('is_push_attempted', 1);
+            // $products = $products->leftJoin('product_verifying_users as pvu', function ($join) {
+            //     $join->on('pvu.product_id', 'products.id');
+            //     $join->where('pvu.user_id', '!=', auth()->user()->id);
+            // });
+    
+            $products = $products->leftJoin('status as s', function ($join) {
+                $join->on('products.status_id', 's.id');
+            });
+    
+            // $products = $products->where('isUploaded', 0);
+            // $products = $products->leftJoin('categories as c', 'c.id', '=', 'products.category');
+
+
+            if($request->get('id') != ""){
+                $products =  $products->where("products.id",$request->get('id'));
+            }
+            if($request->get('name') != ""){
+                $products =  $products->where("products.name",$request->get('name'));
+            }
+            if($request->get('title') != ""){
+                $products =  $products->where("products.name",$request->get('title'));
+            }
+            if($request->get('color') != ""){
+                $products =  $products->where("products.color",$request->get('color'));
+            }
+            if($request->get('composition') != ""){
+                $composition = $request->get('compositon');
+                $products =  $products->where("products.composition","LIKE","%$composition%");
+            }
+            if($request->get('status') != ""){
+                $products =  $products->where("products.status",$request->get('status'));
+            }
+            if($request->get('price') != ""){
+                $products =  $products->where("products.price_usd",$request->get('price'));
+                $products =  $products->orWhere("products.price_usd_special",$request->get('price'));
+                $products =  $products->orWhere("products.price",$request->get('price'));
+            }
+
+
+            if(isset($fieldName)){
+                if($fieldName === 'title' || $fieldName === 'name'){
+                    $products =  $products->where("products.name","LIKE","%$value%");
+                }
+                if($fieldName === 'category'){
+                    $products =  $products->where("categories.title","LIKE","%$value%");
+                }
+                else{
+                    $products =  $products->where("products.$fieldName","LIKE","%$value%");
+                }
+            }
+
+            $products = $products->select(['products.*', 's.name as product_status'])->paginate(10);
+
+            $productsCount = $products->total();
+            $imageCropperRole = auth()->user()->hasRole('ImageCropers');
+            $categoryArray = Category::renderAsArray();
+            $colors = (new Colors)->all();
+            if (! Setting::has('auto_push_product')) {
+                $auto_push_product = Setting::add('auto_push_product', 0, 'int');
+            } else {
+                $auto_push_product = Setting::get('auto_push_product');
+            }
+            
+            $users = User::all();
+            return view('products.magento_push_status.list', compact('products', 'imageCropperRole', 'categoryArray', 'colors', 'auto_push_product', 'users', 'productsCount'));
+        }else{
+            return view('products.magento_push_status.index');
+        }
+       
+    }
+    public function autocompleteSearchPushStatus(Request $request){
+        
         if (auth()->user()->isReviwerLikeAdmin('final_listing')) {
             $products = Product::query();
         } else {
             $products = Product::query()->where('assigned_to', auth()->user()->id);
         }
-        $products = $products->where(function ($query) {
-            $query->where('status_id', StatusHelper::$pushToMagento);
-            $query->orWhere('status_id', StatusHelper::$inMagento);
-        });
-        $products = $products->where('is_push_attempted', 1);
-        $products = $products->leftJoin('product_verifying_users as pvu', function ($join) {
-            $join->on('pvu.product_id', 'products.id');
-            $join->where('pvu.user_id', '!=', auth()->user()->id);
-        });
+        $fieldName = $request->get('filedname');
+        // $products = $products->where(function ($query) {
+        //     $query->where('status_id', StatusHelper::$pushToMagento);
+        //     $query->orWhere('status_id', StatusHelper::$inMagento);
+        // });
+        // $products = $products->where('is_push_attempted', 1);
+        // $products = $products->leftJoin('product_verifying_users as pvu', function ($join) {
+        //     $join->on('pvu.product_id', 'products.id');
+        //     $join->where('pvu.user_id', '!=', auth()->user()->id);
+        // });
 
         $products = $products->leftJoin('status as s', function ($join) {
             $join->on('products.status_id', 's.id');
         });
 
-        $products = $products->where('isUploaded', 0);
-        $products = $products->select(['products.*', 's.name as product_status'])->paginate(10);
+        // $products = $products->where('isUploaded', 0);
+        $products = $products->leftJoin('categories as c', 'c.id', '=', 'products.category');
 
-        $productsCount = $products->total();
+
+        $value = $request->get('value');
+        if(isset($fieldName)){
+            if($fieldName === 'title' || $fieldName === 'name'){
+                $products =  $products->where("products.name","LIKE","%$value%");
+            }
+            if($fieldName === 'category'){
+                $products =  $products->where("c.title","LIKE","%$value%");
+            }
+            else{
+                $products =  $products->where("products.$fieldName","LIKE","%$value%");
+            }
+        }
+
+        $products = $products->select(['products.*', 's.name as product_status']);
+        $products = $products->get()->toArray();
         $imageCropperRole = auth()->user()->hasRole('ImageCropers');
         $categoryArray = Category::renderAsArray();
         $colors = (new Colors)->all();
@@ -1501,10 +1603,9 @@ class ProductController extends Controller
         } else {
             $auto_push_product = Setting::get('auto_push_product');
         }
-        $users = User::all();
-
-        return view('products.magento_push_status.index', compact('products', 'imageCropperRole', 'categoryArray', 'colors', 'auto_push_product', 'users', 'productsCount'));
-    }
+ 
+        return response()->json(['status'=>200,'data'=> array_unique(array_column($products,$fieldName))]);
+  }
 
     public function magentoConditionsCheckLogs($pId, $swId)
     {
@@ -1797,7 +1898,7 @@ class ProductController extends Controller
             }
         }
 
-        \Zipper::make(public_path("$product->sku.zip"))->add($products_array)->close();
+        // \Zipper::make(public_path("$product->sku.zip"))->add($products_array)->close();
 
         return response()->download(public_path("$product->sku.zip"))->deleteFileAfterSend();
     }
--- resources/views/products/magento_push_status/index.blade.php
+++ resources/views/products/magento_push_status/index.blade.php
@@ -4,12 +4,153 @@
 @section('large_content')
     @include('partials.flash_messages')
     <div class="row">
+
         <div class="col-md-12 margin-tb">
-            <h2 class="page-heading">Magento push status ({{ $productsCount }})</h2>
-            <div class="infinite-scroll table-responsive mt-5 infinite-scroll-data">
-                @include("products.magento_conditions_check.list")
+            <form method="get" id="magentoPushStatusForm">
+                <div class="row">
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="id" class="form-control" id="id" placeholder="Product Id" />
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="name" class="form-control" id="name"
+                            placeholder="Product Name" />
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="brand" class="form-control" id="brand" placeholder="Brand" />
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="category" class="form-control" id="category" placeholder="Category" />
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="composition" class="form-control" id="composition"
+                            placeholder="Composition" />
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="color" class="form-control" id="color" placeholder="Color" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                  
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="price" class="form-control" id="price" placeholder="Price" />
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <input type="text" name="status" class="form-control" id="status" placeholder="status" />
+                    </div>
+                    <div class="col-md-4 mt-3 mb-3">
+                        <a class="filterdata" href="#"><i class="fa fa-search"></i></a>
+                    </div>
+                    <div class="col-md-2 mt-3">
+                        <button class="btn btn-secondary float-right push-to-magento">Push to magento</button>
+                    </div>
+                </div>
+            </form>
+            <h2 class="page-heading">Magento push status </h2>
+            <div class="infinite-scroll table-responsive mt-5 infinite-scroll-data appendDataPushMagento">
+
             </div>
-            <img class="infinite-scroll-products-loader center-block" src="/images/loading.gif" alt="Loading..." style="display: none" />
+            <img class="infinite-scroll-products-loader center-block" src="/images/loading.gif" alt="Loading..."
+                style="display: none" />
         </div>
     </div>
 @endsection
+@push('scripts')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            var formData = new FormData($("#magentoPushStatusForm")[0]);
+            $.ajax({
+                url: "{{ route('products.magentoPushStatus') }}",
+                type: 'GET',
+                success: function(response) {
+                    $(".appendDataPushMagento").html(response);
+                }
+            })
+        })
+
+        function commonAjax(field, val) {
+            var fieldData = field;
+            $.ajax({
+                url: "{{ route('products.autocompleteSearchPushStatus') }}",
+                type: 'POST',
+                data: {
+                    "filedname": field,
+                    "value": val,
+                    "_token": "{{ csrf_token() }}"
+                },
+                success: function(response) {
+                    $("#" + field).autocomplete({
+                        source: response.data,
+                        select: function(event, ui) {
+                            var url = '/products/listing/magento-push-status?' + field + '=' + ui.item
+                                .value
+                            $.ajax({
+                                url: url,
+                                type: 'GET',
+                                success: function(response) {
+                                    $(".appendDataPushMagento").html(response);
+                                }
+                            })
+                        }
+                    });
+
+                }
+            })
+        }
+
+        $("input[name='id']").keyup(function() {
+            var value = $(this).val();
+            commonAjax("id", value);
+        })
+        $("input[name='name']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('name', value);
+        })
+        $("input[name='brand']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('brand', value);
+        })
+        $("input[name='category']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('category', value);
+        })
+        $("input[name='composition']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('composition', value);
+        })
+        $("input[name='color']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('color', value);
+        })
+        $("input[name='price']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('price', value);
+        })
+        $("input[name='status']").keyup(function() {
+            var value = $(this).val();
+            commonAjax('status', value);
+        })
+
+
+        $(".filterdata").on('click', function() {
+            let id = $("#id").val();
+            let compisition = $("#compisition").val();
+            let color = $("#color").val();
+            let status = $("#status").val();
+            let price = $("#price").val();
+            let name = $("#name").val();
+
+
+            var url = '/products/listing/magento-push-status?id=' + id + "&name=" + name +"&composition=" + composition + "&color=" + color + "&status=" + status + "&price=" + price;
+            $.ajax({
+                url: url,
+                type: 'GET',
+                success: function(response) {
+                    $(".appendDataPushMagento").html(response);
+                }
+            })
+        });
+    </script>
+@endpush
