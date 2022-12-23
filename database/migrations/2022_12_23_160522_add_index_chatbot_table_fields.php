3c54dc57da6e2efa0aa64866052c6fa2a4de21bf
ganeshaskan <ganeshaskan@users.noreply.github.com>
2022-12-23 11:33:04 +0000

    Apply laravel-pint fixes

--- database/migrations/2022_12_23_160522_add_index_chatbot_table_fields.php
+++ database/migrations/2022_12_23_160522_add_index_chatbot_table_fields.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class AddIndexChatbotTableFields extends Migration
 {
@@ -13,25 +12,25 @@ class AddIndexChatbotTableFields extends Migration
      */
     public function up()
     {
-         DB::statement('ALTER TABLE `chatbot_keyword_values` ADD INDEX(`chatbot_keyword_id`);');
-		 DB::statement('ALTER TABLE `chatbot_keyword_value_types` ADD INDEX(`chatbot_keyword_value_id`);');
-		 DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`store_website_id`);');
-		 DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`chatbot_question_id`);');
-		 DB::statement('ALTER TABLE `chatbot_question_examples` ADD INDEX(`chatbot_question_id`);');
-		 DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`phrase_id`);');
-		 DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`keyword_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`hubstuff_activity_user_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`chatbot_dialog_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`store_website_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`reply_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`chatbot_dialog_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`store_website_id`); ');
-		 DB::statement('ALTER TABLE `chatbot_replies` ADD INDEX(`is_read`);');
-		 DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`word_id`);');
-		 DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`chat_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`test_suites_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`bug_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` CHANGE `bug_id` `bug_id` INT(11) UNSIGNED NULL DEFAULT NULL;');
+        DB::statement('ALTER TABLE `chatbot_keyword_values` ADD INDEX(`chatbot_keyword_id`);');
+        DB::statement('ALTER TABLE `chatbot_keyword_value_types` ADD INDEX(`chatbot_keyword_value_id`);');
+        DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`store_website_id`);');
+        DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`chatbot_question_id`);');
+        DB::statement('ALTER TABLE `chatbot_question_examples` ADD INDEX(`chatbot_question_id`);');
+        DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`phrase_id`);');
+        DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`keyword_id`);');
+        DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`hubstuff_activity_user_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`chatbot_dialog_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`store_website_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`reply_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`chatbot_dialog_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`store_website_id`); ');
+        DB::statement('ALTER TABLE `chatbot_replies` ADD INDEX(`is_read`);');
+        DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`word_id`);');
+        DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`chat_id`);');
+        DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`test_suites_id`);');
+        DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`bug_id`);');
+        DB::statement('ALTER TABLE `chat_messages` CHANGE `bug_id` `bug_id` INT(11) UNSIGNED NULL DEFAULT NULL;');
     }
 
     /**
@@ -41,24 +40,24 @@ class AddIndexChatbotTableFields extends Migration
      */
     public function down()
     {
-         DB::statement('ALTER TABLE `chatbot_keyword_values` ADD INDEX(`chatbot_keyword_id`);');
-		 DB::statement('ALTER TABLE `chatbot_keyword_value_types` ADD INDEX(`chatbot_keyword_value_id`);');
-		 DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`store_website_id`);');
-		 DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`chatbot_question_id`);');
-		 DB::statement('ALTER TABLE `chatbot_question_examples` ADD INDEX(`chatbot_question_id`);');
-		 DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`phrase_id`);');
-		 DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`keyword_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`hubstuff_activity_user_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`chatbot_dialog_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`store_website_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`reply_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`chatbot_dialog_id`);');
-		 DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`store_website_id`); ');
-		 DB::statement('ALTER TABLE `chatbot_replies` ADD INDEX(`is_read`);');
-		 DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`word_id`);');
-		 DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`chat_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`test_suites_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`bug_id`);');
-		 DB::statement('ALTER TABLE `chat_messages` CHANGE `bug_id` `bug_id` INT(11) UNSIGNED NULL DEFAULT NULL;');
+        DB::statement('ALTER TABLE `chatbot_keyword_values` ADD INDEX(`chatbot_keyword_id`);');
+        DB::statement('ALTER TABLE `chatbot_keyword_value_types` ADD INDEX(`chatbot_keyword_value_id`);');
+        DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`store_website_id`);');
+        DB::statement('ALTER TABLE `chatbot_questions_reply` ADD INDEX(`chatbot_question_id`);');
+        DB::statement('ALTER TABLE `chatbot_question_examples` ADD INDEX(`chatbot_question_id`);');
+        DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`phrase_id`);');
+        DB::statement('ALTER TABLE `chat_bot_phrase_groups` ADD INDEX(`keyword_id`);');
+        DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`hubstuff_activity_user_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`chatbot_dialog_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`store_website_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_error_logs` ADD INDEX(`reply_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`chatbot_dialog_id`);');
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD INDEX(`store_website_id`); ');
+        DB::statement('ALTER TABLE `chatbot_replies` ADD INDEX(`is_read`);');
+        DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`word_id`);');
+        DB::statement('ALTER TABLE `chat_message_phrases` ADD INDEX(`chat_id`);');
+        DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`test_suites_id`);');
+        DB::statement('ALTER TABLE `chat_messages` ADD INDEX(`bug_id`);');
+        DB::statement('ALTER TABLE `chat_messages` CHANGE `bug_id` `bug_id` INT(11) UNSIGNED NULL DEFAULT NULL;');
     }
 }
--- database/migrations/2022_12_23_161348_add_foreign_key_chatbot_table_fields.php
+++ database/migrations/2022_12_23_161348_add_foreign_key_chatbot_table_fields.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class AddForeignKeyChatbotTableFields extends Migration
 {
@@ -13,12 +12,11 @@ class AddForeignKeyChatbotTableFields extends Migration
      */
     public function up()
     {
-		DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`store_website_id`) REFERENCES `store_websites`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`chatbot_dialog_id`) REFERENCES `chatbot_dialogs`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		DB::statement('ALTER TABLE `chatbot_keyword_values` ADD  FOREIGN KEY (`chatbot_keyword_id`) REFERENCES `chatbot_keywords`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');		
-		DB::statement('ALTER TABLE `chatbot_question_examples` ADD FOREIGN KEY (`chatbot_question_id`) REFERENCES `chatbot_questions`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		DB::statement('ALTER TABLE `chat_messages` ADD FOREIGN KEY (`bug_id`) REFERENCES `bug_trackers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`store_website_id`) REFERENCES `store_websites`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`chatbot_dialog_id`) REFERENCES `chatbot_dialogs`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chatbot_keyword_values` ADD  FOREIGN KEY (`chatbot_keyword_id`) REFERENCES `chatbot_keywords`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chatbot_question_examples` ADD FOREIGN KEY (`chatbot_question_id`) REFERENCES `chatbot_questions`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chat_messages` ADD FOREIGN KEY (`bug_id`) REFERENCES `bug_trackers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
     }
 
     /**
@@ -29,10 +27,9 @@ class AddForeignKeyChatbotTableFields extends Migration
     public function down()
     {
         DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`store_website_id`) REFERENCES `store_websites`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`chatbot_dialog_id`) REFERENCES `chatbot_dialogs`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		DB::statement('ALTER TABLE `chatbot_keyword_values` ADD  FOREIGN KEY (`chatbot_keyword_id`) REFERENCES `chatbot_keywords`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');		
-		DB::statement('ALTER TABLE `chatbot_question_examples` ADD FOREIGN KEY (`chatbot_question_id`) REFERENCES `chatbot_questions`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		DB::statement('ALTER TABLE `chat_messages` ADD FOREIGN KEY (`bug_id`) REFERENCES `bug_trackers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
-		
+        DB::statement('ALTER TABLE `chatbot_dialog_responses` ADD FOREIGN KEY (`chatbot_dialog_id`) REFERENCES `chatbot_dialogs`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chatbot_keyword_values` ADD  FOREIGN KEY (`chatbot_keyword_id`) REFERENCES `chatbot_keywords`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chatbot_question_examples` ADD FOREIGN KEY (`chatbot_question_id`) REFERENCES `chatbot_questions`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::statement('ALTER TABLE `chat_messages` ADD FOREIGN KEY (`bug_id`) REFERENCES `bug_trackers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
     }
 }
