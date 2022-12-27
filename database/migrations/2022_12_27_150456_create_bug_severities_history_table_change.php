f5ef125409c38aaecbe4bd0faca5da55be7d77f4
ganeshaskan <ganesh.askan@gmail.com>
2022-12-27 16:24:47 +0530

    Update and rename 2022_12_27_150456_create_bug_severities_history_table_new.php to 2022_12_27_150456_create_bug_severities_history_table_change.php

--- database/migrations/2022_12_27_150456_create_bug_severities_history_table_change.php
+++ database/migrations/2022_12_27_150456_create_bug_severities_history_table_change.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBugSeveritiesHistoryTableNew extends Migration
+class CreateBugSeveritiesHistoryTableChange extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateBugSeveritiesHistoryTableNew extends Migration
     public function up()
     {
         Schema::create('bug_severities_histories', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->integer('bug_id');
             $table->integer('old_severity_id')->nullable();
             $table->integer('severity_id');
