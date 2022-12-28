95e1ee666bd88ee339dedd646a7761d804bef1bd
ajeesh-john <98280030+ajeesh-john@users.noreply.github.com>
2022-12-28 20:48:51 +0530

    set time limit as 0 for duplicate store website

--- Modules/StoreWebsite/Http/Controllers/StoreWebsiteController.php
+++ Modules/StoreWebsite/Http/Controllers/StoreWebsiteController.php
@@ -254,6 +254,7 @@ class StoreWebsiteController extends Controller
      */
     public function saveDuplicateStore(Request $request)
     {
+        set_time_limit(0);
         $numberOfDuplicates = $request->get('number') - 1;
         if ($numberOfDuplicates <= 0) {
             return response()->json(['code' => 500, 'error' => 'Number of duplicates must be 1 or more!']);
