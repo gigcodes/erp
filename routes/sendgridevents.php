97e5cba115b04adb0676f1463e9ecc95a99f5b9a
sahil kataira <sahilkataria.1989@gmail.com>
2023-01-05 13:29:18 +0000

    Update send event

--- routes/sendgridevents.php
+++ routes/sendgridevents.php
@@ -5,7 +5,7 @@ use App\Http\Middleware\SendgridEventMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(SendgridEventMiddleware::class)->group(function () {
-    Route::any(config('sendgridevents.webhook_url'), [App\Http\Controllers\WebhookController::class, 'post'])->name('sendgrid.webhook');
+    Route::any(config('sendgridevents.webhook_url'), [\App\Http\Controllers\WebhookController::class, 'post'])->name('sendgrid.webhook');
 
     // Route::post(
     //     config('sendgridevents.webhook_url'),
