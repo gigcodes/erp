652b6b4aea6f6261b9b97205124fafcb8e1172e9
Manoj11dev <Manoj11dev@users.noreply.github.com>
2023-01-10 10:33:31 +0000

    Apply laravel-pint fixes

--- app/Exports/CsvTranslatorExport.php
+++ app/Exports/CsvTranslatorExport.php
@@ -2,30 +2,26 @@
 
 namespace App\Exports;
 
-use App\AccessoryRequest;
+use DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\FromArray;
-use DB;
 
-class CsvTranslatorExport implements  FromCollection, WithHeadings
+class CsvTranslatorExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-      return  DB::table('csv_translators')->select('csv_translators.id as id','csv_translators.key as key','csv_translators.en as en','csv_translators.es as es',
-                                    'csv_translators.ru as ru','csv_translators.ko as ko','csv_translators.ja  as ja','csv_translators.it as it',
-                                    'csv_translators.de as de','csv_translators.fr as fr','csv_translators.nl as nl','csv_translators.zh as zh',
-                                    'csv_translators.ar as ar','csv_translators.ur as ur','users.name as updator_name','csv_translators.status as status',
-                                    'csv_translators.updated_at as updated_date',)->leftjoin('users','users.id','csv_translators.updated_by_user_id')->get();
-      
+        return  DB::table('csv_translators')->select('csv_translators.id as id', 'csv_translators.key as key', 'csv_translators.en as en', 'csv_translators.es as es',
+            'csv_translators.ru as ru', 'csv_translators.ko as ko', 'csv_translators.ja  as ja', 'csv_translators.it as it',
+            'csv_translators.de as de', 'csv_translators.fr as fr', 'csv_translators.nl as nl', 'csv_translators.zh as zh',
+            'csv_translators.ar as ar', 'csv_translators.ur as ur', 'users.name as updator_name', 'csv_translators.status as status',
+            'csv_translators.updated_at as updated_date', )->leftjoin('users', 'users.id', 'csv_translators.updated_by_user_id')->get();
     }
 
     public function headings(): array
     {
-        return ["Id", "Key", "En", "Es", "Ru", "Ko", "Ja", "It", "De", "Fr", "Nl", "Zh", "Ar", "Ur", "Updated By", "Approved By", "Status", "Updated At"];
+        return ['Id', 'Key', 'En', 'Es', 'Ru', 'Ko', 'Ja', 'It', 'De', 'Fr', 'Nl', 'Zh', 'Ar', 'Ur', 'Updated By', 'Approved By', 'Status', 'Updated At'];
     }
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,6 @@ use App\Http\Controllers\CourierController;
 use App\Http\Controllers\Cron;
 use App\Http\Controllers\CronController;
 use App\Http\Controllers\CroppedImageReferenceController;
-use App\Http\Controllers\CsvTranslatorController;
 use App\Http\Controllers\CustomerCategoryController;
 use App\Http\Controllers\CustomerCharityController;
 use App\Http\Controllers\CustomerController;
