4bec6b24bc743b575f037ac64b33098ae8dcecb1
Manoj11dev <Manoj11dev@users.noreply.github.com>
2023-01-11 05:50:28 +0000

    Apply laravel-pint fixes

--- routes/web.php
+++ routes/web.php
@@ -72,10 +72,10 @@ use App\Http\Controllers\CourierController;
 use App\Http\Controllers\Cron;
 use App\Http\Controllers\CronController;
 use App\Http\Controllers\CroppedImageReferenceController;
+use App\Http\Controllers\CsvTranslatorController;
 use App\Http\Controllers\CustomerCategoryController;
 use App\Http\Controllers\CustomerCharityController;
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\CsvTranslatorController;
 use App\Http\Controllers\DailyActivityController;
 use App\Http\Controllers\DailyCashFlowController;
 use App\Http\Controllers\DailyPlannerController;
@@ -449,7 +449,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/csv-translator/export', [CsvTranslatorController::class, 'export'])->name('csvTranslator.export');
     Route::post('/csv-translator/update', [CsvTranslatorController::class, 'update'])->name('csvTranslator.update');
     Route::post('/csv-translator/history', [CsvTranslatorController::class, 'history'])->name('csvTranslator.history');
-   
 });
 
 /** Magento Settings */
