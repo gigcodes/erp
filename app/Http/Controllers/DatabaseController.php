e5a26936bffb44bb7c6518244b8650f87ffba30a
ajeesh-john <ajeesh-john@users.noreply.github.com>
2023-01-13 03:43:44 +0000

    Apply laravel-pint fixes

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -18,21 +18,21 @@ class DatabaseController extends Controller
 
         $customRange = $request->get('customrange');
 
-        if (!empty($customRange)) {
+        if (! empty($customRange)) {
             $range = explode(' - ', $customRange);
-            if (!empty($range[0])) {
+            if (! empty($range[0])) {
                 $startDate = $range[0];
             }
-            if (!empty($range[1])) {
+            if (! empty($range[1])) {
                 $endDate = $range[1];
             }
         }
 
-        if (!empty($startDate)) {
+        if (! empty($startDate)) {
             $databaseHis = $databaseHis->whereDate('created_at', '>=', $startDate);
         }
 
-        if (!empty($endDate)) {
+        if (! empty($endDate)) {
             $databaseHis = $databaseHis->whereDate('created_at', '<=', $endDate);
         }
 
@@ -41,7 +41,7 @@ class DatabaseController extends Controller
         $page = $databaseHis->currentPage();
 
         if ($request->ajax()) {
-            $tml = (string)view('database.partial.list', compact('databaseHis', 'page'));
+            $tml = (string) view('database.partial.list', compact('databaseHis', 'page'));
 
             return response()->json(['code' => 200, 'tpl' => $tml, 'page' => $page]);
         }
@@ -69,17 +69,18 @@ class DatabaseController extends Controller
     public function export(Request $request)
     {
         $dbName = $request->input('db_name');
-        $dumpName = str_replace(' ', '_', $dbName) . '_schema.sql';
-        $cmd = 'mysqldump -h erpdb -u erplive -p  --no-data ' . $dbName . ' > ' . $dumpName;
+        $dumpName = str_replace(' ', '_', $dbName).'_schema.sql';
+        $cmd = 'mysqldump -h erpdb -u erplive -p  --no-data '.$dbName.' > '.$dumpName;
         $allOutput = [];
         $allOutput[] = $cmd;
         $result = exec($cmd, $allOutput);
-        chmod("erp_live_schema.sql", 0755);
+        chmod('erp_live_schema.sql', 0755);
 
         header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=erp_live_schema.sql");
-        $dumpUrl = env('APP_URL') . '/' . $dumpName;
+        header('Content-Transfer-Encoding: Binary');
+        header('Content-disposition: attachment; filename=erp_live_schema.sql');
+        $dumpUrl = env('APP_URL').'/'.$dumpName;
+
         return response()->json(['code' => 200, 'data' => $dumpUrl, 'message' => 'Database exported successfully']);
     }
 }
