960c9d695fa9870ef3edec4bbea3eefae1abfec5
ganeshaskan <ganeshaskan@users.noreply.github.com>
2023-01-13 10:13:38 +0000

    Apply laravel-pint fixes

--- app/Http/Controllers/BugTrackingController.php
+++ app/Http/Controllers/BugTrackingController.php
@@ -1221,7 +1221,6 @@ class BugTrackingController extends Controller
             }
         }
 
-      
         $bug_ids = [];
         $website_ids = [];
         $bugs_html = '<table cellpadding="2" cellspacing="2" border="1" style="width:100%;font-size: 12px;"><tr><td style="text-align:center"><b>Action</b></td><td  style="text-align:center"><b>Bug Id</b></td  style="text-align:center"><td  style="text-align:center;"><b>Summary</b></td><td  style="text-align:center;"><b>Steps to Rep.</b></td><td  style="text-align:center;"><b>Screen / Video</b></td><td  style="text-align:center;"><b>Assign To</b></td><td  style="text-align:center;"><b>Module</b></td></tr>';
@@ -1232,8 +1231,8 @@ class BugTrackingController extends Controller
                 $bug_id = $bug_list[$i]['id'];
                 $assign_to = $bug_list[$i]['assign_to'];
                 $module_id = Str::limit($bug_list[$i]['module_id'], 20, '..');
-                $module_id_txt =  str_replace("'","",$bug_list[$i]['module_id']);
-                $module_id_txt =  htmlentities($module_id_txt);
+                $module_id_txt = str_replace("'", '', $bug_list[$i]['module_id']);
+                $module_id_txt = htmlentities($module_id_txt);
                 $step_to_reproduce = Str::limit($bug_list[$i]['step_to_reproduce'], 20, '..');
                 $step_to_reproduce_txt = htmlentities($bug_list[$i]['step_to_reproduce']);
                 $url = Str::limit($bug_list[$i]['url'], 15, '..');
@@ -1242,7 +1241,7 @@ class BugTrackingController extends Controller
                 if (count($userData) > 0 && isset($userData[0]['name'])) {
                     $name = $userData[0]['name'];
                 }
-                $bugs_html .= '<tr><td  style="text-align:center"><input style="height:13px;" type="checkbox" class="cls-checkbox-bugsids" name="chkBugId[]" value="'.$bug_id.'" id="name="chkBugId'.$bug_id.'"  /></td><td  style="text-align:center">'.$bug_id.'</td><td>&nbsp;'.$bug_list[$i]['summary'].'</td><td title="'.$step_to_reproduce_txt.'" data-toggle="tooltip">&nbsp;'.$step_to_reproduce .'</td><td>&nbsp;'.$url.' <button type="button" class="btn btn-copy-url btn-sm" data-id="'.$bug_list[$i]['url'].'">
+                $bugs_html .= '<tr><td  style="text-align:center"><input style="height:13px;" type="checkbox" class="cls-checkbox-bugsids" name="chkBugId[]" value="'.$bug_id.'" id="name="chkBugId'.$bug_id.'"  /></td><td  style="text-align:center">'.$bug_id.'</td><td>&nbsp;'.$bug_list[$i]['summary'].'</td><td title="'.$step_to_reproduce_txt.'" data-toggle="tooltip">&nbsp;'.$step_to_reproduce.'</td><td>&nbsp;'.$url.' <button type="button" class="btn btn-copy-url btn-sm" data-id="'.$bug_list[$i]['url'].'">
                 <i class="fa fa-clone" aria-hidden="true"></i></button></td><td>&nbsp;'.$name.'</td><td  title="'.$module_id_txt.'" data-toggle="tooltip">&nbsp;'.$module_id.'</td></tr>';
             }
         }
