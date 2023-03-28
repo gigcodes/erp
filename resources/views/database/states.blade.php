3f02e214cae2c817e48d290919697b9a438f4a74
shah-shreya <shreya.s@tridhyatech.com>
2023-03-28 15:45:03 +0530

    fix db schema export issue

--- resources/views/database/states.blade.php
+++ resources/views/database/states.blade.php
@@ -21,8 +21,11 @@
             </div>
             <div class="col-md-4 pr-5">
                 <form class="form-inline pull-right">
-                    <div class="form-group mr-2">
+                    <!-- <div class="form-group mr-2">
                         <input type="text" class="form-control" id="databaseName" placeholder="Enter database name">
+                    </div> -->
+                    <div id="progress" style="display: none;">
+                    <img src="/images/loading_new.gif" style="cursor: pointer; width: 30px;">
                     </div>
                     <button type="button" class="btn btn-secondary" onclick="dbExport()">Export Database</button>
                 </form>
@@ -84,13 +87,14 @@
   });
 
   function dbExport() {
-      let dbName = $('#databaseName').val();
-      if(dbName === '') {
-          toastr['error']('Please enter a valid database name!', 'error');
-          return false;
-      }
+      // let dbName = $('#databaseName').val();
+      // if(dbName === '') {
+      //     toastr['error']('Please enter a valid database name!', 'error');
+      //     return false;
+      // }
 
       if (confirm("Are you sure you want to run this command?")) {
+          $('#progress').show();
           $.ajax({
               url: "{{ route('database.export') }}",
               type: "post",
@@ -98,9 +102,10 @@
                   'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
               },
               data: {
-                  db_name: dbName
+                 // db_name: dbName
               }
           }).done(function (response) {
+              $('#progress').hide();
               if (response.code == '200') {
                   window.open(response.data, "_blank");
                   toastr['success'](response.message, 'success');
