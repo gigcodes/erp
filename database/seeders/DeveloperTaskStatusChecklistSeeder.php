72f886f32e1926970518fc22b07b198d671f00aa
Binal Gajjar <binal.gajjar7@gmail.com>
2023-04-07 18:58:51 +0530

    Add a checklist on update staus of task - User Complete

---
+++ database/seeders/DeveloperTaskStatusChecklistSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DeveloperTaskStatusChecklist;
+use Illuminate\Database\Seeder;
+
+class DeveloperTaskStatusChecklistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        $checklist = [
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Validate the migrations',
+            ],
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Code optimized',
+            ],
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Added menu item',
+            ],
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Recorded the working video',
+            ],
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Added a user guide',
+            ],
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Added a code guide',
+            ],
+            [
+                'task_status' => 'User Complete',
+                'name' => 'Check the coding standards',
+            ],
+        ];
+        foreach($checklist as $list){
+            DeveloperTaskStatusChecklist::create($list);
+        }
+    }
+}
