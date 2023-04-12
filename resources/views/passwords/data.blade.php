fdacb263fb2cde6a8d6058a2519291908c1f8fe2
Punja Solanki <punjasolanki197@gmail.com>
2023-04-12 13:17:23 +0530

    js issue resolve

--- resources/views/passwords/data.blade.php
+++ resources/views/passwords/data.blade.php
@@ -29,20 +29,22 @@
       <td>
           <div style="width: 100%;">
           <div class="d-flex">
-            <input type="text" name="remark_pop" class="form-control remark_pop" placeholder="Please enter remark" style="margin-bottom:5px;width:100%;display:inline;">
-            <button type="button" class="btn btn-sm btn-image sub_remark pointer" title="Send message" data-password_id="{{$password->id}}">
+            <input type="text" name="remark_pop" class="form-control remark_pop{{$password->id}}" placeholder="Please enter remark" style="margin-bottom:5px;width:100%;display:inline;">
+            <button type="button" class="btn btn-sm btn-image add_remark pointer" title="Send message" data-password_id="{{$password->id}}">
                 <img src="{{asset('images/filled-sent.png')}}">
             </button>
-          <button data-password_id="{{ $password->id }}" data-password_type="Quick-dev-task" class="btn btn-xs btn-image set-remark" title="Remark"><img src="{{asset('images/chat.png')}}" alt=""></button>
+          <button data-password_id="{{ $password->id }}" data-password_type="Quick-dev-task" class="btn btn-xs btn-image show-remark" title="Remark"><img src="{{asset('images/chat.png')}}" alt=""></button>
           </div>
           @if (isset($password_remark))
               <div style="width: 100%;">
                   <div class="expand-row-msg" data-id="{{$password->id}}">
                       <div class="d-flex justify-content-between expand-row-msg" data-id="{{$password->id}}">
-                            <span class="td-password-remark" style="margin:0px;">
+                            <span class="td-password-remark{{$password->id}}" style="margin:0px;">
                                 @php($i = 1)
                                 @foreach($password_remark as $pwd_remark)
+                                    @if($pwd_remark->password_id === $password->id)
                                     {{$i}}.{{$pwd_remark->remark}}
+                                    @endif
                                     @php($i++)
                                 @endforeach
                             </span>
@@ -62,3 +64,98 @@
     </tr>
   @endforeach
 @endif
+<script>
+    $(document).on("click",".add_remark",function(e) {
+        e.preventDefault();
+        var thiss = $(this);
+        var password_id = $(this).data('password_id');
+        var remark = $(`.remark_pop`+password_id).val();
+        console.log(remark, password_id);
+        $.ajax({
+            type: "POST",
+            url: passGetRemark,
+            headers: {
+                'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+            },
+            data: {
+                password_id : password_id,
+                remark : remark,
+                type : "Quick-dev-task",
+            },
+            beforeSend: function () {
+                $("#loading-image").show();
+            }
+        }).done(function (response) {
+            console.log(response, "response");
+            if(response.code == 200) {
+                $("#loading-image").hide();
+                if (remark == ''){
+                    $("#preview-task-create-get-modal").modal("show");
+                }
+                $(".task-create-get-list-view").html(response.data);
+                $(`.td-password-remark`+password_id).html(response.remark_data);
+                $(`.remark_pop`+password_id).val("");
+                toastr['success'](response.message);
+            }else{
+                $("#loading-image").hide();
+                if (remark == '') {
+                    $("#preview-task-create-get-modal").modal("show");
+                }
+                $(".task-create-get-list-view").html("");
+                toastr['error'](response.message);
+            }
+
+        }).fail(function (response) {
+            $("#loading-image").hide();
+            $("#preview-task-create-get-modal").modal("show");
+            $(".task-create-get-list-view").html("");
+            toastr['error'](response.message);
+        });
+    });
+    $(document).on("click",".show-remark",function(e) {
+        e.preventDefault();
+        var password_id = $(this).data('password_id');
+        var remark = $(`.remark_pop`+password_id).val();
+        $.ajax({
+            type: "POST",
+            url: passGetRemark,
+            headers: {
+                'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+            },
+            data: {
+                password_id : password_id,
+                remark : remark,
+                type : "Quick-dev-task",
+            },
+            beforeSend: function () {
+                $("#loading-image").show();
+            }
+        }).done(function (response) {
+            console.log(response, "response");
+            if(response.code == 200) {
+                $("#loading-image").hide();
+                if (remark == ''){
+                    $("#preview-task-create-get-modal").modal("show");
+                }
+                $(".task-create-get-list-view").html(response.data);
+                $(`.td-password-remark`+password_id).html(response.remark_data);
+                $(`.remark_pop`+password_id).val("");
+                toastr['success'](response.message);
+            }else{
+                $("#loading-image").hide();
+                if (remark == '') {
+                    $("#preview-task-create-get-modal").modal("show");
+                }
+                $(".task-create-get-list-view").html("");
+                toastr['error'](response.message);
+            }
+
+        }).fail(function (response) {
+            $("#loading-image").hide();
+            $("#preview-task-create-get-modal").modal("show");
+            $(".task-create-get-list-view").html("");
+            toastr['error'](response.message);
+        });
+    });
+
+</script>
