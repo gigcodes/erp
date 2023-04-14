5da958bef5a30fc499bb02b6901ea9897588998e
Virendra Jadeja <virendra.jadeja@bytestechnolab.com>
2023-04-14 19:03:01 +0530

    Added Add status color button

--- resources/views/uicheck/responsive.blade.php
+++ resources/views/uicheck/responsive.blade.php
@@ -93,7 +93,7 @@
 <br />
 <div class="col-lg-12 margin-tb">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-10">
 			<form>
 				<div class="row">
 					<div class="col-md-2">
@@ -153,6 +153,14 @@
 					</div>
 				</div>
 			</form>
+		</div>
+		<div class="col-md-2">
+			@if (Auth::user()->isAdmin())
+				<button class="btn btn-xs btn-secondary my-3"  data-toggle="modal" data-target="#uiResponsive"> UI Responsive</button>&nbsp;
+			@endif
+			@if (Auth::user()->isAdmin())
+				<button class="btn btn-xs btn-secondary my-3" data-toggle="modal" data-target="#newStatusColor"> Status Color</button>&nbsp;
+			@endif
 		</div>
 	</div>
 </div>
@@ -285,7 +293,43 @@
 		</div>
 	</div>
 </div>
+<div id="newStatusColor" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Task Status Color</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <form action="{{ route('livechat.statuscolor') }}" method="POST">
+                <?php echo csrf_field(); ?>
+                {{--                <div class="modal-content">--}}
+                <div class="form-group col-md-12">
+                    <table cellpadding="0" cellspacing="0" border="1" class="table table-bordered">
+                        <tr>
+                            <td class="text-center"><b>Status Name</b></td>
+                            <td class="text-center"><b>Color Code</b></td>
+                            <td class="text-center"><b>Color</b></td>
+                        </tr>
+                        <?php
+                        foreach ($taskstatus as $bugstatus) { ?>
+                        <tr>
+                            <td>&nbsp;&nbsp;&nbsp;<?php echo $bugstatus->name; ?></td>
+                            <td class="text-center"><?php echo $bugstatus->ticket_color; ?></td>
+                            <td class="text-center"><input type="color" name="color_name[<?php echo $bugstatus->id; ?>]" class="form-control" data-id="<?php echo $bugstatus->id; ?>" id="color_name_<?php echo $bugstatus->id; ?>" value="<?php echo $bugstatus->ticket_color; ?>" style="height:30px;padding:0px;"></td>
+                        </tr>
+                        <?php } ?>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary submit-status-color">Save changes</button>
+                </div>
+            </form>
+        </div>
 
+    </div>
+</div>
 
 @if (Auth::user()->hasRole('Admin'))
 <input type="hidden" id="user-type" value="Admin">
