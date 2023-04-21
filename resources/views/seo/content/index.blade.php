ddb91fcd7e9aed290fa2202dfa2ffe3d4b4fcccd
Yash Chauhan <yash.c@7span.com>
2023-04-21 20:40:53 +0530

    SEO Content With popup modal

--- resources/views/seo/content/index.blade.php
+++ resources/views/seo/content/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
 @php
-    $auth = auth()->user();
+$auth = auth()->user();
 @endphp
 <h2 class="page-heading">{{ $moduleName }}</h2>
 
@@ -11,21 +11,78 @@
     <div class="d-flex justify-content-end">
         <div class="">
             @if($auth->hasRole(['Admin', 'User']))
-                <a href="{{route('seo.content.create')}}" class="btn btn-secondary">Add new</a>
+            <a href="javascript:;" class="btn btn-secondary addNewBtn">Add new</a>
             @endif
         </div>
     </div>
     <div class="mt-3">
-        <div class="card-body">
+        <div class="row">
+            <div class="col-md-2">
+                <label for="">Select website</label>
+                <select name="website_id" class="form-control websiteFilter">
+                    <option value="">-- SELECT --</option>
+                    @foreach ($websites as $item)
+                        <option value="{{ $item->id }}">{{ $item->website }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-2">
+                <label for="">Select price status</label>
+                <select name="website_id" class="form-control priceStatusFilter">
+                    <option value="">-- SELECT --</option>
+                    <option value="1">Approved</option>
+                    <option value="2">Unapproved</option>
+                </select>
+            </div>
+            @if($auth->hasRole(['Admin']))
+                <div class="col-md-2">
+                    <label for="">Select user</label>
+                    <select name="website_id" class="form-control userFilter">
+                        <option value="">-- SELECT --</option>
+                        @foreach ($users as $item)
+                            @if(!$item->hasRole(['user']))
+                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+            @endif
+
+            <div class="col-md-2">
+                <label for="">Status</label>
+                <select name="website_id" class="form-control statusFilter">
+                    <option value="">-- SELECT --</option>
+                    <option value="planned">Planned</option>
+                    <option value="admin_approve">Admin Approved</option>
+                </select>
+            </div>
+
+
+            <div class="col-md-2 mt-5">
+                <label for="">&nbsp;</label>
+                <button class="btn btn-image search ui-autocomplete-input searchBtn">
+                    <img src="{{ url('images/search.png')}}" alt="Search" style="cursor: default;">
+                </button>
+            </div>
+        </div>
+        <div class="card-body table-responsive">
             <table class="table table-bordered" id="seoProcessTbl">
                 <thead>
                     <tr>
                         <th>#</th>
                         <th>Website</th>
+                        <th>Keywords</th>
+                        <th>Word count</th>
+                        <th>Suggestion</th>
+                        <th>SEO Checklist</th>
+                        <th>Publish Checklist</th>
+                        <th>Document Link</th>
+                        <th>Live Status Link</th>
+                        <th>SEO Status</th>
                         <th>User</th>
                         <th>Price</th>
                         <th>Publish Date</th>
-                        <th>Seo Status</th>
+                        <th>Status</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
@@ -34,74 +91,150 @@
     </div>
 </div>
 
+
+<!-- Add Form Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary submitBtn">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
 <script>
-
-    let $loader = $(document).find('.loaderSection');
-    async function getSeoProcess({ page }) {
-        return $.ajax({
-            type: "GET",
-            url: "",
-            async:true,
-            beforeSend: function(msg){
-                $loader.show();
-            },
-            data: {
-                page:page
-            },
-            dataType: "json",
-            success: function (response) {
-                $loader.hide();
-                return response;
-            }
-        });
-    }
-
-    function getHtmlContent(data) {
-        let html = ``;
-        data.forEach(ele => {
-            let editRoute = `{{ route('seo.content.edit', ":id") }}`;
-            editRoute = editRoute.replace(':id', ele.id)
-            html += `<tr>`;
-                html += `<td>${ele?.website.website}</td>`
-                html += `<td>${ele?.user.name}</td>`
-                html += `<td>${ele.price}</td>`
-                html += `<td>${ele.published_at}</td>`
-                html += `<td>${ele.status}</td>`
-                html += `<td><a href='${editRoute}' class='btn btn-warning btn-sm'>Edit</a></td>`
-            html += `</tr>`;
-        });
-
-        return html;
-    }
-
-    $(document).ready(function () {
-        
+    $(document).ready(function() {
         // Datatable 
         const $datatable = $('#seoProcessTbl').DataTable({
-            serverSide:true,
-            processing:true,
-            ajax:{
-                url:'',
-            },
-            columns:[
-                { data: 'DT_RowIndex', 'orderable': false, 'searchable': false },
-                {data:'website.website', name:'website.website'},
-                {data:'user.name', name:'user.name'},
-                {data:'price', name:'price'},
-                {data:'published_at', name:'published_at'},
-                {data:'status', name:'status'},
-                {data:'actions', name:'actions'},
-            ]
+            serverSide: true,
+            lengthMenu: [ [50, 100, 150, -1], [50, 100, 150, "All"] ],
+            searching:false
+            , ajax: {
+                url: '', 
+                data:{
+                    filter:{
+                        website_id: () => $(document).find('.websiteFilter').val(),
+                        price_status:() => $(document).find('.priceStatusFilter').val(),
+                        user_id:() => $(document).find('.userFilter').val(),
+                        status:() => $(document).find('.statusFilter').val(),
+                    }
+                }
+            , }
+            , columns: [{
+                    data: 'DT_RowIndex'
+                    , 'orderable': false
+                    , 'searchable': false
+                }
+                , {
+                    data: 'website_id'
+                    , name: 'website_id'
+                }
+                , {
+                    data: 'keywords'
+                    , name: 'keywords'
+                },
+                {
+                    data: 'word_count'
+                    , name: 'word_count'
+                },
+                {
+                    data: 'suggestion'
+                    , name: 'suggestion'
+                },
+                {
+                    data:'seoChecklist',
+                    name:'seoChecklist'
+                },
+                {
+                    data:'publishChecklist',
+                    name:'publishChecklist'
+                },
+                {
+                    data:'documentLink',
+                    name:'documentLink'
+                },
+                {
+                    data:'liveStatusLink',
+                    name:'liveStatusLink'
+                },
+                {
+                    data:'seoStatus',
+                    name:'seoStatus'
+                }
+                , {
+                    data: 'user_id'
+                    , name: 'user_id'
+                }
+                , {
+                    data: 'price'
+                    , name: 'price'
+                }
+                , {
+                    data: 'published_at'
+                    , name: 'published_at'
+                }
+                , {
+                    data: 'status'
+                    , name: 'status'
+                }
+                , {
+                    data: 'actions'
+                    , name: 'actions'
+                }
+            , ]
         });
 
-        async function fetchSeoProcess({page}) {
-            let resp = await getSeoProcess({
-                page:page
+        $(function() {
+            $(document).on('click', '.addNewBtn', function() {
+                let $formModal = $(document).find('#formModal');
+                $($formModal).modal('show');
+                $.ajax({
+                    type: "GET",
+                    url: "{{ route('seo.content.create') }}",
+                    data: {
+                        formType:"CREATE_FORM"
+                    },
+                    dataType: "json",
+                    success: function (response) {
+                        $($formModal).find('.modal-body').html(response.data)
+                    }
+                });
             });
-            let html = getHtmlContent(resp.data?.data);
-            $(document).find('#seoProcessTbl').append(html)
-        }
+
+            $(document).on('click', '.editBtn', function() {
+                let url = $(this).attr('data-url');
+                let $formModal = $(document).find('#formModal');
+                $($formModal).modal('show');
+                $.ajax({
+                    type: "GET",
+                    url: url,
+                    data: {
+                    },
+                    dataType: "json",
+                    success: function (response) {
+                        $($formModal).find('.modal-body').html(response.data)
+                    }
+                });
+            });
+
+            $(document).on('click', '.searchBtn', function() {
+                $datatable.clear().draw();
+            })
+        })
     });
+
 </script>
+
+@include('seo.content.script')
 @endsection
