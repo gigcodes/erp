23de37b1adfee5490f9dfc6bd12ef23e30825a14
Ankur Bhadania <abbhadania@gmail.com>
2023-06-22 16:54:42 +0530

    DEVTASK-23637
    DEVTASK-23637

--- Modules/ChatBot/Http/Controllers/MessageController.php
+++ Modules/ChatBot/Http/Controllers/MessageController.php
@@ -173,6 +173,78 @@ class MessageController extends Controller
         return view('chatbot::message.index', compact('pendingApprovalMsg', 'page', 'allCategoryList', 'reply_categories', 'allEntityType', 'variables', 'parentIntents'));
     }
 
+    public function todayMessagesCheck(Request $request)
+    {
+        $last_message_id=$request->cmid;
+        $tml='';
+        if($last_message_id){
+            
+        }
+        return response()->json(['code' => 200, 'tpl' => $tml]);
+    }
+    public function todayMessages(Request $request)
+    {
+        $pendingApprovalMsg = ChatMessage::with('taskUser', 'chatBotReplychat', 'chatBotReplychatlatest')
+            ->leftjoin('customers as c', 'c.id', 'chat_messages.customer_id')
+            ->leftJoin('vendors as v', 'v.id', 'chat_messages.vendor_id')
+            ->leftJoin('suppliers as s', 's.id', 'chat_messages.supplier_id')
+            ->leftJoin('store_websites as sw', 'sw.id', 'c.store_website_id')
+            ->leftJoin('bug_trackers  as bt', 'bt.id', 'chat_messages.bug_id')
+            ->leftJoin('chatbot_replies as cr', 'cr.replied_chat_id', 'chat_messages.id')
+            ->leftJoin('chat_messages as cm1', 'cm1.id', 'cr.chat_id')
+            ->leftJoin('emails as e', 'e.id', 'chat_messages.email_id')
+            ->leftJoin('tmp_replies as tmp', 'tmp.chat_message_id', 'chat_messages.id')
+            ->groupBy(['chat_messages.customer_id', 'chat_messages.vendor_id', 'chat_messages.user_id', 'chat_messages.task_id', 'chat_messages.developer_task_id', 'chat_messages.bug_id', 'chat_messages.email_id']); //Purpose : Add task_id - DEVTASK-4203
+
+        $pendingApprovalMsg = $pendingApprovalMsg->whereRaw('chat_messages.id in (select max(chat_messages.id) as latest_message from chat_messages LEFT JOIN chatbot_replies as cr on cr.replied_chat_id = `chat_messages`.`id` where ((customer_id > 0 or vendor_id > 0 or task_id > 0 or developer_task_id > 0 or user_id > 0 or supplier_id > 0 or bug_id > 0 or email_id > 0) OR (customer_id IS NULL
+        AND vendor_id IS NULL
+        AND supplier_id IS NULL
+        AND bug_id IS NULL
+        AND task_id IS NULL
+        AND developer_task_id IS NULL
+        AND email_id IS NULL
+        AND user_id IS NULL)) GROUP BY customer_id,user_id,vendor_id,supplier_id,task_id,developer_task_id, bug_id,email_id)');
+        
+        $pendingApprovalMsg = $pendingApprovalMsg->where(function ($q) {
+            $q->where('chat_messages.message', '!=', '');
+        })->select(['cr.id as chat_bot_id', 'cr.is_read as chat_read_id', 'chat_messages.*', 'cm1.id as chat_id', 'cr.question',
+            'cm1.message as answer','cm1.is_audio as answer_is_audio', 'c.name as customer_name', 'v.name as vendors_name', 's.supplier as supplier_name', 'cr.reply_from', 'sw.title as website_title', 'c.do_not_disturb as customer_do_not_disturb', 'e.name as from_name',
+            'tmp.id as tmp_replies_id', 'tmp.suggested_replay', 'tmp.is_approved', 'tmp.is_reject', 'c.is_auto_simulator as customer_auto_simulator',
+            'v.is_auto_simulator as vendor_auto_simulator', 's.is_auto_simulator as supplier_auto_simulator'])
+            ->orderByRaw("cr.id DESC, chat_messages.id DESC")
+            ->paginate(10);
+        // dd($pendingApprovalMsg);
+
+        $allCategory = ChatbotCategory::all();
+        $allCategoryList = [];
+        if (!$allCategory->isEmpty()) {
+            foreach ($allCategory as $all) {
+                $allCategoryList[] = ['id' => $all->id, 'text' => $all->name];
+            }
+        }
+        $page = $pendingApprovalMsg->currentPage();
+        $reply_categories = \App\ReplyCategory::with('approval_leads')->orderby('name')->get();
+
+        $allEntityType = DialogflowEntityType::all()->pluck('name', 'id')->toArray();
+        $variables = DialogFlowService::VARIABLES;
+        $parentIntents = ChatbotQuestion::where(['keyword_or_question' => 'intent'])->where('google_account_id' , '>', 0)
+            ->pluck('value', 'id')->toArray();
+        $totalpage=$pendingApprovalMsg->lastPage();
+        if ($request->ajax()) {
+            $tml='';
+            foreach ($pendingApprovalMsg as $index =>$pam) {
+                $tml .= (string)view('chatbot::message.partial.today-list', compact('pam', 'page', 'allCategoryList', 'reply_categories', 'allEntityType', 'variables', 'parentIntents'));
+            }
+            
+            return response()->json(['code' => 200, 'tpl' => $tml, 'page' => $page,'totalpage'=>$totalpage]);
+        }
+        
+       
+
+        //dd($pendingApprovalMsg);
+        return view('chatbot::message.today', compact('pendingApprovalMsg', 'page', 'allCategoryList', 'reply_categories', 'allEntityType', 'variables', 'parentIntents','totalpage'));
+    }
+
     public function approve()
     {
         $id = request('id');
