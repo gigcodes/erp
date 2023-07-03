6b9b0956ccc019f4bfe6a9f73e1f17f8fac70197
Ankur Bhadania <abbhadania@gmail.com>
2023-07-03 19:17:07 +0530

    #DEVTASK-23671-MAGENTO DB FROM ERP
    #DEVTASK-23671-MAGENTO DB FROM ERP

--- Modules/UserManagement/Resources/views/templates/list-template.php
+++ Modules/UserManagement/Resources/views/templates/list-template.php
@@ -469,11 +469,12 @@
 				<div class="col-lg-12">
 					<form>
 						<?php echo csrf_field(); ?>
-						<input type="hidden" name="database_user_id" id="database-user-id" value="{{:data.user_id}}">
+						<input type="hidden" name="database_user_id" id="database_user_id" value="{{:data.user_id}}">
 					  	<div class="row">
 					  		<div class="col">
 					      		<select class="form-control choose-db" name="connection">
-					      			<?php foreach (\App\StoreWebsite::DB_CONNECTION as $k => $connection) { ?>
+								  <option {{if data.connection == "mysql" }} selected='selected' {{/if}} value="mysql">Erp</option>
+					      			<?php foreach (\App\StoreWebsite::pluck('title','id')->toArray() as $k => $connection) { ?>
 					      				<option {{if data.connection == "<?php echo $k; ?>" }} selected='selected' {{/if}} value="<?php echo $k; ?>"><?php echo $connection; ?></option>
 					      			<?php } ?>		
 					      		</select>
