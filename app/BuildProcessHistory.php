9cfa98efc81058fc786004828876c515564498cf
Nadesh <nadesh@arkinfotec.com>
2023-07-13 19:50:02 +0530

    DEVTASK-23707: jobs column in build process logs page - Completed

--- app/BuildProcessHistory.php
+++ app/BuildProcessHistory.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use App\Github\GithubRepositoryJob;
 use Illuminate\Database\Eloquent\Model;
+use App\Helpers\GithubTrait;
 
 class BuildProcessHistory extends Model
 {
+    use GithubTrait;
+
     protected $table = 'build_process_histories';
 
     protected $fillable = ['id', 'store_website_id', 'status', 'text', 'build_name', 'build_number', 'created_by', 'github_organization_id', 'github_repository_id', 'github_branch_state_name'];
@@ -18,5 +22,32 @@ class BuildProcessHistory extends Model
     public function repository()
     {
         return $this->belongsTo(\App\Github\GithubRepository::class, 'github_repository_id', 'id');
+    }
+
+    public function getJobStatusAttribute()
+    {
+        $githubRepositoryId = $this->github_repository_id;
+        $githubBranchStateName = $this->github_branch_state_name;
+        // $githubRepositoryId = 231925646;
+        // $githubBranchStateName = "master";
+
+        $githubActionRuns = $this->getGithubActionRuns($githubRepositoryId, 1, null, null, $githubBranchStateName);
+        
+        // Get Repo Jobs from DB & Prepare the status. 
+        $githubRepositoryJobs = GithubRepositoryJob::where('github_repository_id',  $githubRepositoryId)->pluck('job_name')->toArray();
+
+        if($githubActionRuns->total_count > 0 && isset($githubActionRuns->workflow_runs)) {
+            $job_status = [];
+            $githubActionRunJobs = $this->getGithubActionRunJobs($githubRepositoryId, $githubActionRuns->workflow_runs[0]->id);
+            // Prepareing job status for every actions
+            foreach ($githubActionRunJobs->jobs as $job) {
+                if(in_array($job->name, $githubRepositoryJobs)) {
+                    $job_status[$job->name] = $job->status;
+                }
+            }
+            return $job_status;
+        } else {
+            return [];
+        }
     }
 }
