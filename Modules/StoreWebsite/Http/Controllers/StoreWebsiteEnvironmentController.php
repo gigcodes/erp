98652d33b974d3f007c28c7ed87516e38920e2c6
Nadesh <nadesh@arkinfotec.com>
2023-07-13 14:17:50 +0530

    DEVTASK-23712: Magento E N V Changes - Completed

--- Modules/StoreWebsite/Http/Controllers/StoreWebsiteEnvironmentController.php
+++ Modules/StoreWebsite/Http/Controllers/StoreWebsiteEnvironmentController.php
@@ -149,17 +149,153 @@ class StoreWebsiteEnvironmentController extends Controller
      *
      * @param  Request  $request [description]
      */
+    // Old Concept - Not need
+    // public function updateValue(Request $request)
+    // {
+    //     $post = $request->all();
+    //     $id = $request->get('id', 0);
+
+    //     $params = [
+    //         'value'           => 'required',
+    //         'command'           => 'required',
+    //     ];
+    //     $validator = Validator::make($post, $params);
+
+    //     if ($validator->fails()) {
+    //         $outputString = '';
+    //         $messages = $validator->errors()->getMessages();
+    //         foreach ($messages as $k => $errr) {
+    //             foreach ($errr as $er) {
+    //                 $outputString .= "$k : " . $er . '<br>';
+    //             }
+    //         }
+
+    //         return response()->json(['code' => 500, 'error' => $outputString]);
+    //     }
+
+    //     $records = StoreWebsiteEnvironment::find($id);
+
+    //     if (! $records) {
+    //         return response()->json(['code' => 500, 'error' => 'Store Website Environment data not found!']);
+    //     }
+    //     $environment_id=$records->id;
+    //     $old_value=$records->value;
+    //     $new_value=$request->value;
+    //     $updated_by=$request->user_id;
+    //     $cmd=$request->command;
+    //     $store_website_id=$records->store_website_id;
+    //     $path=$records->path;
+    //     //$records->save();
+
+    //     \Log::info("Start Environment Pushed");
+    //     $storeWebsite=StoreWebsite::where('id', $store_website_id)->first();
+    //     $cwd='';
+    //     $assetsmanager = new AssetsManager;
+    //     if($storeWebsite){
+    //         $cwd=$storeWebsite->working_directory;
+    //         $assetsmanager = AssetsManager::where('id', $storeWebsite->assets_manager_id)->first();
+    //     }
+        
+    //     if($assetsmanager && $assetsmanager->client_id!='')
+    //     {
+            
+            
+    //         $client_id=$assetsmanager->client_id;
+    //         $url="https://s10.theluxuryunlimited.com:5000/api/v1/clients/".$client_id."/commands";
+    //         $key=base64_encode("admin:86286706-032e-44cb-981c-588224f80a7d");
+            
+    //         $startTime = date('Y-m-d H:i:s', LARAVEL_START);
+            
+    //         $ch = curl_init();
+    //         curl_setopt($ch, CURLOPT_URL,$url);
+    //         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //         curl_setopt($ch, CURLOPT_POST, 1);
+    //         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    //         $parameters = [
+    //             'command' => $cmd, 
+    //             'cwd' => $cwd,
+    //             'is_sudo' => true, 
+    //             'timeout_sec' => 300, 
+    //         ];
+    //         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($parameters));
+
+    //         $headers = [];
+    //         $headers[] = 'Authorization: Basic '.$key;
+    //         $headers[] = 'Content-Type: application/json';
+    //         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //         $result = curl_exec($ch);
+            
+    //         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            
+    //         \App\LogRequest::log($startTime, $url, 'POST', json_encode($parameters), json_decode($result), 
+    //         $httpcode,\Modules\StoreWebsite\Http\Controllers\StoreWebsiteEnvironmentController::class, 'update');
+            
+    //         \Log::info("API result: ".$result);
+    //         \Log::info("API Error Number: ".curl_errno($ch));
+    //         if (curl_errno($ch)) {
+    //             \Log::info("API Error: ".curl_error($ch));
+                
+    //             StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' => curl_error($ch)]);
+                
+    //             return response()->json(['code' => 500, 'error' =>curl_error($ch)]);
+    //         }
+            
+    //         $response = json_decode($result);
+
+    //         curl_close($ch);
+                
+    //         if(isset($response->errors)){ 
+    //             $message='';
+    //             foreach($response->errors as $error){
+    //                 $message.=" ".$error->code.":".$error->title.":".$error->detail;
+    //             }
+                
+    //             StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' => $message]);
+    //             \Log::info($message);
+    //             return response()->json(['code' => 500, 'error' => $message]);
+
+    //         }else{
+    //             if(isset($response->data) && isset($response->data->jid) ){
+    //                 $job_id=$response->data->jid;
+    //                 $status="Success";
+                    
+    //                 StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Success", 'response' => 'Success', 'job_id' => $job_id]);
+    //                 $records->value=$new_value;
+    //                 $records->save();
+    //                 \Log::info("Job Id:".$job_id);
+    //                 return response()->json(['code' => 200, 'data' => $records,'message'=>'Request pushed on website successfully']);
+    //             }else{
+    //                 StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' =>"Job Id not found in response"]);
+                    
+    //                 \Log::info("Job Id not found in response!");
+    //                 return response()->json(['code' => 500, 'error' => 'Job Id not found in response!']);
+    //             }
+    //         }
+    //     }else{
+            
+    //         StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' => "Assets Manager & Client id not found the Store Website!"]);
+            
+    //         \Log::info("Assets Manager & Client id not found the Store Website!");
+            
+    //         return response()->json(['code' => 500, 'error' => 'Assets Manager & Client id not found the Store Website!']);
+    //     }
+
+    //     \Log::info("End Environment Pushed");
+
+    //     return response()->json(['code' => 200, 'data' => $records,'message'=>'update']);
+    // }
+
+    // New Concept 
     public function updateValue(Request $request)
     {
         $post = $request->all();
         $id = $request->get('id', 0);
 
         $params = [
-            'value'           => 'required',
-            'command'           => 'required',
+            'value' => 'required',
         ];
-        $validator = Validator::make($post, $params);
 
+        $validator = Validator::make($post, $params);
         if ($validator->fails()) {
             $outputString = '';
             $messages = $validator->errors()->getMessages();
@@ -173,112 +309,68 @@ class StoreWebsiteEnvironmentController extends Controller
         }
 
         $records = StoreWebsiteEnvironment::find($id);
-
         if (! $records) {
             return response()->json(['code' => 500, 'error' => 'Store Website Environment data not found!']);
         }
+
+        $store_website_id = $records->store_website_id;
+        $storeWebsite=StoreWebsite::where('id', $store_website_id)->first();
+        if (! $storeWebsite) {
+            return response()->json(['code' => 500, 'error' => 'Store Website not found!']);
+        }
+
+        \Log::info("Start Environment Pushed");
         $environment_id=$records->id;
         $old_value=$records->value;
         $new_value=$request->value;
         $updated_by=$request->user_id;
-        $cmd=$request->command;
-        $store_website_id=$records->store_website_id;
         $path=$records->path;
-        //$records->save();
-
-        \Log::info("Start Environment Pushed");
-        $storeWebsite=StoreWebsite::where('id', $store_website_id)->first();
-        $cwd='';
-        $assetsmanager = new AssetsManager;
-        if($storeWebsite){
-            $cwd=$storeWebsite->working_directory;
-            $assetsmanager = AssetsManager::where('id', $storeWebsite->assets_manager_id)->first();
-        }
+        $encodeValue = base64_encode($new_value);
+        $cwd = $storeWebsite->working_directory;
+        $title = $storeWebsite->title;
+        $serverIP = $storeWebsite->server_ip;
+        $scriptsPath = getenv('DEPLOYMENT_SCRIPTS_PATH');
         
-        if($assetsmanager && $assetsmanager->client_id!='')
-        {
-            
-            
-            $client_id=$assetsmanager->client_id;
-            $url="https://s10.theluxuryunlimited.com:5000/api/v1/clients/".$client_id."/commands";
-            $key=base64_encode("admin:86286706-032e-44cb-981c-588224f80a7d");
-            
-            $startTime = date('Y-m-d H:i:s', LARAVEL_START);
-            
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,$url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            $parameters = [
-                'command' => $cmd, 
-                'cwd' => $cwd,
-                'is_sudo' => true, 
-                'timeout_sec' => 300, 
-            ];
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($parameters));
+        $cmd = "bash $scriptsPath" . "magento-env-update.sh -w \"$title\" -s \"$serverIP\" -d \"$cwd\" -p \"$path\" -v \"$encodeValue\"";
+        $output = [];
+        exec($cmd, $output, $statusCode);
+        
+        if ($statusCode == 0) {
+            // Assuming the output is stored in $output variable as an array
+            $result = json_decode($output[0], true); // Decode the JSON string into an associative array
 
-            $headers = [];
-            $headers[] = 'Authorization: Basic '.$key;
-            $headers[] = 'Content-Type: application/json';
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            $result = curl_exec($ch);
-            
-            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            
-            \App\LogRequest::log($startTime, $url, 'POST', json_encode($parameters), json_decode($result), 
-            $httpcode,\Modules\StoreWebsite\Http\Controllers\StoreWebsiteEnvironmentController::class, 'update');
-            
-            \Log::info("API result: ".$result);
-            \Log::info("API Error Number: ".curl_errno($ch));
-            if (curl_errno($ch)) {
-                \Log::info("API Error: ".curl_error($ch));
-                
-                StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' => curl_error($ch)]);
-                
-                return response()->json(['code' => 500, 'error' =>curl_error($ch)]);
-            }
-            
-            $response = json_decode($result);
-
-            curl_close($ch);
-                
-            if(isset($response->errors)){ 
-                $message='';
-                foreach($response->errors as $error){
-                    $message.=" ".$error->code.":".$error->title.":".$error->detail;
+            // Check if the JSON decoding was successful and if it contains the necessary keys
+            if (is_array($result) && isset($result['status'])) {
+                $status = $result['status'];
+                // Handle the values accordingly
+                if ($status === 'true') {
+                    $statusText = "Success";
+                    $response = $output[0]; // This is json string
+                } else {
+                    $statusText = "Error";
+                    $response = $output[0]; // This is json string
                 }
-                
-                StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' => $message]);
-                \Log::info($message);
-                return response()->json(['code' => 500, 'error' => $message]);
-
-            }else{
-                if(isset($response->data) && isset($response->data->jid) ){
-                    $job_id=$response->data->jid;
-                    $status="Success";
-                    
-                    StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Success", 'response' => 'Success', 'job_id' => $job_id]);
-                    $records->value=$new_value;
-                    $records->save();
-                    \Log::info("Job Id:".$job_id);
-                    return response()->json(['code' => 200, 'data' => $records,'message'=>'Request pushed on website successfully']);
-                }else{
-                    StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' =>"Job Id not found in response"]);
-                    
-                    \Log::info("Job Id not found in response!");
-                    return response()->json(['code' => 500, 'error' => 'Job Id not found in response!']);
-                }
+            }  else {
+                // Invalid output format
+                $statusText = "Error";
+                $response = "Invalid output format";
             }
-        }else{
-            
-            StoreWebsiteEnvironmentHistory::create(['environment_id' => $environment_id,'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'key' => $path, 'old_value' => $old_value, 'new_value' => $new_value, 'command' => $cmd, 'status' => "Error", 'response' => "Assets Manager & Client id not found the Store Website!"]);
-            
-            \Log::info("Assets Manager & Client id not found the Store Website!");
-            
-            return response()->json(['code' => 500, 'error' => 'Assets Manager & Client id not found the Store Website!']);
+        } else {
+            $statusText = "Error";
+            $response = "Command Executed Failed";
         }
 
+        StoreWebsiteEnvironmentHistory::create([
+            'environment_id' => $environment_id,
+            'store_website_id' => $store_website_id, 
+            'updated_by' => $updated_by, 
+            'key' => $path, 
+            'old_value' => $old_value, 
+            'new_value' => $new_value, 
+            'command' => $cmd, 
+            'status' => $statusText, 
+            'response' => $response
+        ]);
         \Log::info("End Environment Pushed");
 
         return response()->json(['code' => 200, 'data' => $records,'message'=>'update']);
