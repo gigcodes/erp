be2e6da3baa18f494278aa65a4f98da6b9f114ad
kokilavanisumanas <kokilavanisumanas@users.noreply.github.com>
2023-08-28 15:05:51 +0000

    Apply Laravel Pint Changes

--- database/migrations/2023_08_28_174641_alter_action_to_github_actions_table.php
+++ database/migrations/2023_08_28_174641_alter_action_to_github_actions_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AlterActionToGithubActionsTable extends Migration
 {
@@ -14,20 +14,20 @@ class AlterActionToGithubActionsTable extends Migration
     public function up()
     {
         Schema::table('github_actions', function (Blueprint $table) {
-            $table->string('github_actor')->nullable()->change();;
-            $table->string('github_api_url')->nullable()->change();;
-            $table->string('github_base_ref')->nullable()->change();;
-            $table->string('github_event_name')->nullable()->change();;
-            $table->string('github_job')->nullable()->change();;
-            $table->string('github_ref')->nullable()->change();;
-            $table->string('github_ref_name')->nullable()->change();;
-            $table->string('github_ref_type')->nullable()->change();;
-            $table->string('github_repository')->nullable()->change();;
-            $table->integer('github_repository_id')->nullable()->change();;
-            $table->integer('github_run_attempt')->nullable()->change();;
-            $table->integer('github_run_id')->nullable()->change();;
-            $table->string('github_workflow')->nullable()->change();;
-            $table->string('runner_name')->nullable()->change();;
+            $table->string('github_actor')->nullable()->change();
+            $table->string('github_api_url')->nullable()->change();
+            $table->string('github_base_ref')->nullable()->change();
+            $table->string('github_event_name')->nullable()->change();
+            $table->string('github_job')->nullable()->change();
+            $table->string('github_ref')->nullable()->change();
+            $table->string('github_ref_name')->nullable()->change();
+            $table->string('github_ref_type')->nullable()->change();
+            $table->string('github_repository')->nullable()->change();
+            $table->integer('github_repository_id')->nullable()->change();
+            $table->integer('github_run_attempt')->nullable()->change();
+            $table->integer('github_run_id')->nullable()->change();
+            $table->string('github_workflow')->nullable()->change();
+            $table->string('runner_name')->nullable()->change();
         });
     }
 
