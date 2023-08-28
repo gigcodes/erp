5284c19c9b99c62f3be216e1e23b29818bc2a7bd
kokilavani.p <kokilavani.p@arkinfoc.com>
2023-08-28 10:17:42 +0530

    #DEVTASK-23817:Git Hub Action parameters changes

--- app/Http/Controllers/GitHubActionController.php
+++ app/Http/Controllers/GitHubActionController.php
@@ -11,20 +11,20 @@ class GitHubActionController extends Controller
     {
         $githubAction = new GitHubAction();
 
-        $githubAction->github_actor = $request->input('github_actor');
-        $githubAction->github_api_url = $request->input('github_api_url');
-        $githubAction->github_base_ref = $request->input('github_base_ref');
-        $githubAction->github_event_name = $request->input('github_event_name');
-        $githubAction->github_job = $request->input('github_job');
-        $githubAction->github_ref = $request->input('github_ref');
-        $githubAction->github_ref_name = $request->input('github_ref_name');
-        $githubAction->github_ref_type = $request->input('github_ref_type');
-        $githubAction->github_repository = $request->input('github_repository');
-        $githubAction->github_repository_id = $request->input('github_repository_id');
-        $githubAction->github_run_attempt = $request->input('github_run_attempt');
-        $githubAction->github_run_id = $request->input('github_run_id');
-        $githubAction->github_workflow = $request->input('github_workflow');
-        $githubAction->runner_name = $request->input('runner_name');
+        $githubAction->github_actor = $request->input('GITHUB_ACTOR');
+        $githubAction->github_api_url = $request->input('GITHUB_API_URL');
+        $githubAction->github_base_ref = $request->input('GITHUB_BASE_REF');
+        $githubAction->github_event_name = $request->input('GITHUB_EVENT_NAME');
+        $githubAction->github_job = $request->input('GITHUB_JOB');
+        $githubAction->github_ref = $request->input('GITHUB_REF');
+        $githubAction->github_ref_name = $request->input('GITHUB_REF_NAME');
+        $githubAction->github_ref_type = $request->input('GITHUB_REF_TYPE');
+        $githubAction->github_repository = $request->input('GITHUB_REPOSITORY');
+        $githubAction->github_repository_id = $request->input('GITHUB_REPOSITORY_ID');
+        $githubAction->github_run_attempt = $request->input('GITHUB_RUN_ATTEMPT');
+        $githubAction->github_run_id = $request->input('GITHUB_RUN_ID');
+        $githubAction->github_workflow = $request->input('GITHUB_WORKFLOW');
+        $githubAction->runner_name = $request->input('RUNNER_NAME');
         $githubAction->save();
 
         return response()->json(['message' => 'GitHub Action Stored Successfully'], 200);
