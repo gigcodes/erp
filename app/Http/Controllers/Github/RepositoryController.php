f92921b6fc8fb4e325bd48182e07e5646540a71c
kokilavani.p <kokilavani.p@arkinfoc.com>
2023-09-19 17:13:18 +0530

    #DEVTASK-23851-Git Hub Webhooks pagination added

--- app/Http/Controllers/Github/RepositoryController.php
+++ app/Http/Controllers/Github/RepositoryController.php
@@ -1563,87 +1563,98 @@ class RepositoryController extends Controller
     public function listAllNewPullRequests(Request $request)
     {
         $projects = Project::get();
-
+       
+        $perPage = 20; // Number of items per page
+    
         $repo_names = GithubPullRequest::distinct()->pluck('repo_name');
         $users = GithubPullRequest::distinct()->pluck('created_by');
 
         if ($request->ajax()) {
             ini_set('max_execution_time', -1);
-
-            if($request->repoId !== null) {
-                $repositories = GithubRepository::where('id', $request->repoId)->get();
-            } else {
-                $repositories = GithubRepository::get();
-            }
             $allPullRequests = [];
+    
+        if ($request->repoId !== null) {
+            $pullRequests = GithubPullRequest::where('github_repository_id', $request->repoId)->get();
+        } else {
+            $pullRequests = GithubPullRequest::get();
+        }
+    
+        foreach ($pullRequests as $key => $pullRequest) {
+            $getRepo = GithubRepository::find($request->repoId); 
+            $organizationId = $getRepo->github_organization_id ?? '';
 
-            foreach ($repositories as $repository) {
-                $organization = $repository->organization;
-                if($request->repoId !== null) {
-                    $pullRequests = GithubPullRequest::where('github_repository_id', $repository->id)->get();
-                } else {
-                    $pullRequests = GithubPullRequest::get();
-                }
-                foreach ($pullRequests as $key => $pullRequest) {
-                    //Need to execute the detail API as we require the mergeable_state which is only return in the PR detail API.
-                    $pullRequests[$key]['mergeable_state'] = $pullRequest->mergeable_state;
-                    $pullRequests[$key]['conflict_exist'] = $pullRequest->mergeable_state == 'dirty' ? true : false;
-                    // Get Latest Activity for this PR
-                    $pullRequests[$key]['latest_activity'] = [];
-                    $latestGithubPrActivity = GithubPrActivity::latest('activity_id')
-                        ->where('github_organization_id', $organization->id)
-                        ->where('github_repository_id', $repository->id)
-                        ->where('pull_number', $pullRequest->pull_number)
-                        ->first();
-                    if ($latestGithubPrActivity) {
-                        $pullRequests[$key]['latest_activity'] = [
-                            'activity_id' => $latestGithubPrActivity->activity_id,
-                            'user' => $latestGithubPrActivity->user,
-                            'event' => $latestGithubPrActivity->event,
-                            'label_name' => $latestGithubPrActivity->label_name,
-                            'label_color' => $latestGithubPrActivity->label_color,
-                        ];
-                    }
-
-                    // check build process logs
-                    $totalBuildProcessHistoryCount = BuildProcessHistory::where('github_organization_id', $organization->id)
-                        ->where('github_repository_id', $repository->id)
-                        ->where('github_branch_state_name', $pullRequest->source)
-                        ->count();
-
-                    $totalBuildProcessSuccessHistoryCount = BuildProcessHistory::where('github_organization_id', $organization->id)
-                        ->where('github_repository_id', $repository->id)
-                        ->where('github_branch_state_name', $pullRequest->source)
-                        ->where('status', 'SUCCESS')
-                        ->count();
-
-                    $pullRequests[$key]['build_process_history_status'] = '';
-                    if ($totalBuildProcessHistoryCount > 0) {
-                        if ($totalBuildProcessHistoryCount == $totalBuildProcessSuccessHistoryCount) {
-                            $pullRequests[$key]['build_process_history_status'] = 'Success';
-                        } else {
-                            $pullRequests[$key]['build_process_history_status'] = 'Danger';
-                        }
-                    }
-                }
-
-                $pullRequests = $pullRequests->toArray();
-
-                $pullRequests = array_map(
-                    function ($pullRequest) use ($repository) {
-                        $pullRequest['repository'] = $repository;
-
-                        return $pullRequest;
-                    },
-                    $pullRequests
-                );
-
-                $allPullRequests = array_merge($allPullRequests, $pullRequests);
+            $pullRequests[$key]['mergeable_state'] = $pullRequest->mergeable_state;
+            $pullRequests[$key]['conflict_exist'] = $pullRequest->mergeable_state == 'dirty' ? true : false;
+            // Get Latest Activity for this PR
+            $pullRequests[$key]['latest_activity'] = [];
+            $latestGithubPrActivity = GithubPrActivity::latest('activity_id')
+                ->where('github_organization_id', $organizationId)
+                ->where('github_repository_id', $request->repoId) 
+                ->where('pull_number', $pullRequest->pull_number)
+                ->first();
+            if ($latestGithubPrActivity) {
+                $pullRequests[$key]['latest_activity'] = [
+                    'activity_id' => $latestGithubPrActivity->activity_id,
+                    'user' => $latestGithubPrActivity->user,
+                    'event' => $latestGithubPrActivity->event,
+                    'label_name' => $latestGithubPrActivity->label_name,
+                    'label_color' => $latestGithubPrActivity->label_color,
+                ];
             }
+
+            // Check build process logs
+            $totalBuildProcessHistoryCount = BuildProcessHistory::where('github_organization_id', $organizationId)
+                ->where('github_repository_id', $request->repoId)
+                ->where('github_branch_state_name', $pullRequest->source)
+                ->count();
+
+            $totalBuildProcessSuccessHistoryCount = BuildProcessHistory::where('github_organization_id', $organizationId)
+                ->where('github_repository_id', $request->repoId)
+                ->where('github_branch_state_name', $pullRequest->source)
+                ->where('status', 'SUCCESS')
+                ->count();
+
+            $pullRequests[$key]['build_process_history_status'] = '';
+            if ($totalBuildProcessHistoryCount > 0) {
+                if ($totalBuildProcessHistoryCount == $totalBuildProcessSuccessHistoryCount) {
+                    $pullRequests[$key]['build_process_history_status'] = 'Success';
+                } else {
+                    $pullRequests[$key]['build_process_history_status'] = 'Danger';
+                }
+            }
+            
+            // Add the pull request key to the array
+            $pullRequests[$key]['pull_request_key'] = 'Your Pull Request Key Data Here';
+            
+            // Add build process data to the array
+            $pullRequests[$key]['build_process_data'] = 'Your Build Process Data Here';
+        }
+    
+            $pullRequests = $pullRequests->toArray();
+
+            $currentPage = request()->query('page', 1);
+            $start = ($currentPage - 1) * $perPage;
+            $slicedData = array_slice($pullRequests, $start, $perPage);
+    
+            $githubPullPaginated = new LengthAwarePaginator(
+                $slicedData,
+                count($pullRequests),
+                $perPage,
+                $currentPage,
+                ['path' => Paginator::resolveCurrentPath()]
+            );
+            // Preserve existing query parameters
+            $pullRequests = $slicedData;
+    
+            $allPullRequests = array_merge($allPullRequests, $pullRequests);
+            $githubPullPaginated->appends(request()->query());
+            $githubPullPaginated->withPath(Paginator::resolveCurrentPath());
+            $links = $githubPullPaginated->links();
 
             return response()->json([
-                'tbody' => view('github.include.new-pull-request-list', compact(['pullRequests', 'projects']))->render(),              
+                'tbody' => view('github.include.new-pull-request-list', compact(['pullRequests', 'projects', 'githubPullPaginated', 'links']))->render(),
                 'count' => count($pullRequests),
+                'links' => $links,
             ], 200);
         }
 
