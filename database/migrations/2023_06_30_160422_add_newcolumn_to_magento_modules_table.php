00bac8a147c3c2ac919b7755f528feb64d4f4dc1
Jatin Bhatt <jatin.b.php@gmail.com>
2023-09-22 11:39:38 +0530

    reverted migration file name agagain to it's original

--- database/migrations/2023_06_30_160422_add_newcolumn_to_magento_modules_table.php
+++ database/migrations/2023_06_30_160422_add_newcolumn_to_magento_modules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNewcolumnToMagentoModulesTable extends Migration
+class AddNewcolumnReviewToMagentoModulesTable extends Migration
 {
     /**
      * Run the migrations.
