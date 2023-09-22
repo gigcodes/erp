5bfa2db85872833c10cfc8637117d0a0281f337c
Nadesh <nadesh@arkinfotec.com>
2023-09-22 18:15:29 +0530

    DEVTASK-23870: Log bug fixing

--- app/Http/Controllers/MagentoModuleController.php
+++ app/Http/Controllers/MagentoModuleController.php
@@ -1094,8 +1094,8 @@ class MagentoModuleController extends Controller
                 \Log::info('magentoModuleUpdateStatus output:' . print_r($output, true));
                 \Log::info('magentoModuleUpdateStatus return_var:' . $return_var);
 
-                // [4] => {"status":"success"}
-                if (! isset($output[4])) {
+                // [0] => {"status":"success"}
+                if (! isset($output[0])) {
                     MagentoModuleLogs::create(['magento_module_id' => $magento_module_id, 'store_website_id' => $store_website_id, 'updated_by' => $updated_by, 'command' => $cmd, 'status' => 'Error', 'response' => json_encode($output)]);
 
                     $return_data[] = ['code' => 500, 'message' => 'The response is not found!', 'store_website_id' => $store_website_id, 'magento_module_id' => $magento_module_id];
@@ -1103,7 +1103,7 @@ class MagentoModuleController extends Controller
                     continue;
                 }
 
-                $response = json_decode($output[4]);
+                $response = json_decode($output[0]);
                 if (isset($response->status) && ($response->status == 'success' || $response->status)) {
                     $message = 'Magento module status change successfully';
                     if (isset($response->message) && $response->message != '') {
