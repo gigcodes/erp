6c257ac60dbff03173c9707f7fdb9ed7f8650fdb
bohdan <b.ryb@intobi.com>
2023-10-26 23:33:33 +0300

    removed files

--- routes/web.php
+++ routes/web.php
@@ -536,8 +536,6 @@ Route::middleware('auth')->group(function () {
 Route::post('auto-build-process', [ProjectController::class, 'pullRequestsBuildProcess'])->name('project.pullRequests.buildProcess');
 
 Route::middleware('auth')->group(function () {
-    Route::get('magento_modules/listing-cms-page/{storeId?}', [\App\Http\Controllers\PageBuilder\MagentoPageBuilderController::class, 'index'])->name('magento_module_pagebuilder_listing');
-
     Route::post('magento_modules/verified-status-update', [MagentoModuleController::class, 'verifiedStatusUpdate'])->name('magento_module.verified-status-update');
     Route::get('magento_modules/listing', [MagentoModuleController::class, 'magentoModuleList'])->name('magento_module_listing');
     Route::get('magento_modules/listing-careers', [\App\Http\Controllers\MagentoCareersController::class, 'index'])->name('magento_module_listing_careers');
