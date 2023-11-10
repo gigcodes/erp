c2a2500a0a9c56a32c28c9cd7b95f6fbd80a7e05
bohdan <b.ryb@intobi.com>
2023-11-10 17:28:56 +0200

    fix status

--- app/Observers/ChatMessageIndexObserver.php
+++ app/Observers/ChatMessageIndexObserver.php
@@ -3,21 +3,44 @@
 namespace App\Observers;
 
 use App\ChatMessage;
+use App\Elasticsearch\Elasticsearch;
+use App\Elasticsearch\Reindex\Interfaces\Reindex;
 use App\Elasticsearch\Reindex\Messages;
+use App\Models\IndexerState;
 
 class ChatMessageIndexObserver
 {
     public function created(ChatMessage $chatMessage): void
     {
-        $message = new Messages;
-
-        $message->indexOne($chatMessage);
+        try {
+            $elastic = new Elasticsearch();
+            $elastic->connect();
+            $elastic->getConn()->ping();
+            $message = new Messages;
+            $message->indexOne($chatMessage);
+        }
+        catch (\Exception $e) {
+            $indexer = IndexerState::where('index', 'chatbot_messages')->first();
+            if ($indexer !== null) {
+                $indexer->setStatus(Reindex::INVALIDATE);
+            }
+        }
     }
 
     public function updated(ChatMessage $chatMessage): void
     {
-        $message = new Messages;
-
-        $message->indexOne($chatMessage);
+        try {
+            $elastic = new Elasticsearch();
+            $elastic->connect();
+            $elastic->getConn()->ping();
+            $message = new Messages;
+            $message->indexOne($chatMessage);
+        }
+        catch (\Exception $e) {
+            $indexer = IndexerState::find('index', 'chatbot_messages');
+            if ($indexer !== null) {
+                $indexer->setStatus(Reindex::INVALIDATE);
+            }
+        }
     }
 }
