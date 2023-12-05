56f59357a99b285e0d9b39951986b6e1bcde0496
Jatin Bhatt <jatin.b.php@gmail.com>
2023-12-05 18:49:36 +0530

    remove space

--- resources/views/emails/content-view.blade.php
+++ resources/views/emails/content-view.blade.php
@@ -1,19 +1,15 @@
 @if($emailAddresses->signature_title)
 Title: {!! $emailAddresses->signature_title !!}
 @endif
-
 @if($emailAddresses->signature_name)
 Name: {!! $emailAddresses->signature_name !!}
 @endif
-
 @if($emailAddresses->signature_email)
 Signature Email: {!! $emailAddresses->signature_email !!}
 @endif
-
 @if($emailAddresses->signature_phone)
 Signature Phone: {!! $emailAddresses->signature_phone !!}
 @endif
-
 @if($emailAddresses->signature_website)
 Signature Websiter: {!! $emailAddresses->signature_website !!}
 @endif
