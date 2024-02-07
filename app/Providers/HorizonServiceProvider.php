b1c999ca271680cb1c2a2d3597a8a69841c3a583
Karthick <karthickk1996@hotmail.com>
2024-02-07 17:41:40 +0530

    laravel horizon authentication enable in production mode

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -16,12 +16,6 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     public function boot()
     {
         parent::boot();
-
-        // Horizon::routeMailNotificationsTo('yogeshmordani@icloud.com');
-
-        // Horizon::routeSmsNotificationsTo('15556667777');
-        // Horizon::routeMailNotificationsTo('example@example.com');
-        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
     }
 
     /**
