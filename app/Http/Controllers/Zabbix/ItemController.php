ec8bcd8ade720df69c1e95459491ed085eb236dd
Vignesh M <m.vignes1998@gmail.com>
2024-02-12 23:00:47 +0530

    Removed commented codes from "app/Controllers/Zabbix" folder php files

--- app/Http/Controllers/Zabbix/ItemController.php
+++ app/Http/Controllers/Zabbix/ItemController.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Zabbix;
 
-use App\Models\Zabbix\Trigger;
-use App\Zabbix\ZabbixApi;
-use App\Zabbix\ZabbixException;
 use Exception;
-use Illuminate\Routing\Controller;
-use Illuminate\Http\Request;
+use App\Zabbix\ZabbixApi;
 use App\Models\Zabbix\Item;
+use Illuminate\Http\Request;
+use App\Models\Zabbix\Trigger;
+use App\Zabbix\ZabbixException;
+use Illuminate\Routing\Controller;
 
 class ItemController extends Controller
 {
-
     public function __construct(
         private Trigger $trigger,
         private ZabbixApi $zabbix
@@ -22,7 +21,6 @@ class ItemController extends Controller
     }
 
     /**
-     * @param Request $request
      * @return array|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index(Request $request, $hostId = null)
@@ -39,12 +37,11 @@ class ItemController extends Controller
 
         return view('zabbix.item.index', [
             'items' => $items,
-            'templates' => $templates
+            'templates' => $templates,
         ]);
     }
 
     /**
-     * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function save(Request $request)
@@ -53,44 +50,41 @@ class ItemController extends Controller
 
         try {
             $item = new Item();
-            $itemId = (int)$data['id'] ?? null;
-            if (!empty($data['id'])) {
+            $itemId = (int) $data['id'] ?? null;
+            if (! empty($data['id'])) {
                 $item = $item->getById($itemId);
             }
 
             $item->setKey($data['key'] ?? '');
             $item->setName($data['name'] ?? '');
             $item->setType($data['type'] ?? '');
-            $item->setValueType((int)$data['value_type'] ?? 1);
+            $item->setValueType((int) $data['value_type'] ?? 1);
             $item->setDelay($data['delay'] ?? 1);
-            $item->setHostId((int)$data['host_id']);
+            $item->setHostId((int) $data['host_id']);
             $item->setUnits($data['units'] ?? '');
-            $item->setInterfaceid((int)$data['interfaceid'] ?? 0);
+            $item->setInterfaceid((int) $data['interfaceid'] ?? 0);
 
-            if (!empty($data['template_id'])) {
-                $item->setTemplateId((int)$data['template_id']);
+            if (! empty($data['template_id'])) {
+                $item->setTemplateId((int) $data['template_id']);
             }
 
             $item->save();
-        }
-        catch (ZabbixException $zabbixException)
-        {
+        } catch (ZabbixException $zabbixException) {
             return response()->json([
                 'message' => $zabbixException->getMessage(),
-                'code' => 500
+                'code' => 500,
             ]);
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             return response()->json([
                 'message' => 'Something went wrong.',
-                'code' => 500
+                'code' => 500,
             ]);
         }
 
         return response()->json([
             'message' => sprintf('Item with name: %s was edited. Reload page.', $item->getName()),
             'item' => $item,
-            'code' => 200
+            'code' => 200,
         ]);
     }
 
@@ -100,32 +94,29 @@ class ItemController extends Controller
 
         try {
             $user = new Item();
-            $userId = (int)$data['id'] ?? null;
-            if (!empty($data['id'])) {
+            $userId = (int) $data['id'] ?? null;
+            if (! empty($data['id'])) {
                 $user = $user->getById($userId);
             } else {
                 throw new ZabbixException(sprintf('Item with id: %s not found.', $userId));
             }
 
             $user->delete();
-        }
-        catch (ZabbixException $zabbixException)
-        {
+        } catch (ZabbixException $zabbixException) {
             return response()->json([
                 'message' => $zabbixException->getMessage(),
-                'code' => 500
+                'code' => 500,
             ]);
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             return response()->json([
                 'message' => 'Something went wrong.',
-                'code' => 500
+                'code' => 500,
             ]);
         }
 
         return response()->json([
             'message' => sprintf('Item with id: %s was deleted. Reload page.', $userId),
-            'code' => 200
+            'code' => 200,
         ]);
     }
 }
