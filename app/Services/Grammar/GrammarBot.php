b18110d264049a05a62d5a8fa6a38b68c15c3946
Vignesh M <m.vignes1998@gmail.com>
2024-02-13 17:34:23 +0530

    Removed commented codes and refactored from "App\Services\Grammar" folder php files

--- app/Services/Grammar/GrammarBot.php
+++ app/Services/Grammar/GrammarBot.php
@@ -21,7 +21,6 @@ class GrammarBot
         try {
             $response = $this->request->request('POST', 'https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck', [
                 'form_params' => [
-                    //                    'mode' => 'Spell',
                     'text' => $text,
                 ],
                 'headers' => [
@@ -37,18 +36,13 @@ class GrammarBot
 
         $data = json_decode($response->getBody()->getContents(), true);
 
-//        dump($data);
-
         if ($data['flaggedTokens'] === []) {
-//            dump($text);
             return $text;
         }
         //if there are tokens to be corrected, loop through them and correct it.
         foreach ($data['flaggedTokens'] as $suggestion) {
             $text = substr_replace($text, $suggestion['suggestions'][0]['suggestion'], $suggestion['offset'], strlen($suggestion['token']));
         }
-
-//        dump($text);
 
         return $text;
     }
