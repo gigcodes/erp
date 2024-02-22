dbb22fda43a495d767a82367c5dfbecdfb12a157
Karthick <karthickk1996@hotmail.com>
2024-02-22 10:48:58 +0530

    migration fix

--- database/migrations/2024_02_21_085042_add_can_reply_column_to_social_contacts_table.php
+++ database/migrations/2024_02_21_085042_add_can_reply_column_to_social_contacts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCanReplyColumnToSocialMessagesTable extends Migration
+class CanReplyColumnSocialMessagesTable extends Migration
 {
     /**
      * Run the migrations.
