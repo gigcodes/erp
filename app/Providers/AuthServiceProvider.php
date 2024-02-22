5708a034fbe3266e8cea2697d71800ac7ce90289
Binita Desai <desai.binita92@gmail.com>
2024-02-22 17:47:47 +0530

    view websockets

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return true;
+        });
     }
 }
